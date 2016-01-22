140df33141d382321bae28390198f0d62c87a0e1
Steven Fountain <StevenFountain@live.co.uk>
2016-01-22 11:16:07 +0100

    Logout on settings page
    Logout functionality moved to settings page, moved to controller as
    syntax.

--- client/src/partials/settings/settings.js
+++ client/src/partials/settings/settings.js
@@ -1,59 +1,62 @@
 angular.module('bhima.controllers')
-.controller('settings', [
-  '$scope',
-  '$routeParams',
-  '$translate',
-  '$location',
-  'appcache',
-  'messenger',
-  'tmhDynamicLocale',
-  'store',
-  'amMoment',
-  function($scope, $routeParams, $translate, $location, Appcache, messenger, tmhDynamicLocale, Store, amMoment) {
+.controller('settings', Settings);
+
+Settings.$inject = ['$http','$routeParams','$translate','$location','appcache','messenger','tmhDynamicLocale','store','amMoment', 'SessionService'];
+
+function Settings($http, $routeParams, $translate, $location, Appcache, messenger, tmhDynamicLocale, Store, amMoment, SessionService) {
+  var vm = this;
+
+  // TODO issue discussing DB modelling of languages, quick suggestion (id, label, translateKey, localeKey)
+  var languageStore = new Store({identifier : 'translateKey'});
+  var languages = [
+    {
+      translateKey : 'en',
+      localeKey : 'en-us',
+      label : 'English'
+    },
+    {
+      translateKey : 'fr',
+      localeKey : 'fr-be',
+      label : 'French'
+    },
+    {
+      translateKey : 'ln',
+      localeKey : 'fr-cd',
+      label : 'Lingala'
+    }
+  ];
+  languageStore.setData(languages);
+  
+  vm.url = $routeParams.url || '';
+  var cache = new Appcache('preferences');
+
+  cache.fetch('language')
+  .then(function (res) {
+    if (res) {
+      vm.settings = { language: res.translateKey };
+    }
+  });
+
+  vm.updateLanguage = function updateLanuage(key) {
+    var language = languageStore.get(key);
     
-    // TODO issue discussing DB modelling of languages, quick suggestion (id, label, translateKey, localeKey)
-    var languageStore = new Store({identifier : 'translateKey'});
-    var languages = [
-      {
-        translateKey : 'en',
-        localeKey : 'en-us',
-        label : 'English'
-      },
-      {
-        translateKey : 'fr',
-        localeKey : 'fr-be',
-        label : 'French'
-      },
-      {
-        translateKey : 'ln',
-        localeKey : 'fr-cd',
-        label : 'Lingala'
-      }
-    ];
-    languageStore.setData(languages);
-    
-    $scope.url = $routeParams.url || '';
-    var cache = new Appcache('preferences');
+    $translate.use(language.translateKey);
+    tmhDynamicLocale.set(language.localeKey);
+    amMoment.changeLocale(language.translateKey);
 
-    cache.fetch('language')
-    .then(function (res) {
-      if (res) {
-        $scope.settings = { language: res.translateKey };
-      }
-    });
+    cache.put('language', language);
+  };
 
-    $scope.updateLanguage = function updateLanuage(key) {
-      var language = languageStore.get(key);
-      
-      $translate.use(language.translateKey);
-      tmhDynamicLocale.set(language.localeKey);
-      amMoment.changeLocale(language.translateKey);
-
-      cache.put('language', language);
-    };
-
-    $scope.back = function () {
-      $location.url($scope.url);
-    };
+  vm.back = function () {
+    $location.url($scope.url);
+  };
+  
+  /** @todo Wrap logout call in a service */
+  vm.logout = function () { 
+    $http.get('/logout')
+      .then(function () {
+        SessionService.destroy();
+        $location.url('/login');
+      });
   }
-]);
+}
