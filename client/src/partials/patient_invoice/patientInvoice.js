49d8c2271b5f978987b8d4a6fd56b87e5b0d9ef0
Steven Fountain <StevenFountain@live.co.uk>
2016-01-25 16:18:25 +0100

    Added initial structural files for patient invoice
    Controller and template files for patient invoice, hooked up routing in
    app.js. Experimenting with more complicated (compact) invoice layout.
    Prototyping a UI-grid look for sale items (providing overflow etc.)

--- client/src/partials/patient_invoice/patientInvoice.js
+++ client/src/partials/patient_invoice/patientInvoice.js
@@ -1,52 +1,22 @@
 angular.module('bhima.controllers')
 .controller('PatientInvoiceController', PatientInvoiceController);
 
-PatientInvoiceController.$inject = ['$http', 'uuid'];
+PatientInvoiceController.$inject = [];
 
-function PatientInvoiceController($http, uuid) { 
+function PatientInvoiceController() { 
   var vm = this;
-
-  // Set default invoice date to today 
-  vm.invoiceDate = new Date();
-  vm.invoiceId = uuid();
-
-  // FIXME 
-  vm.distributable = "true";
-  vm.itemIncrement = 1;
-
-  // TODO 02/08 - Replace with Dedrick's service API
-  $http.get('services')
-    .then(function (services) { 
-      vm.services = services.data;
-
-      // Select default service
-      vm.service = vm.services[0];
-    });
-
-  var saleData = [];
-
+  
   var mockOptions = { 
     enableSorting : false,
-    enableColumnMenus : false,
+    gridMenuShowHideColumns : false,
     columnDefs : [
-      { field : 'code' },
-      { field : 'description' },
-      { field : 'quantity' },
-      { field : 'unit_price' },
-      { field : 'amount' }
-    ],
-    data : saleData
+      { field : 'Quanity' },
+      { field : 'Code' },
+      { field : 'Description' },
+      { field : 'Unit Price' },
+      { field : 'Amount' }
+    ]
   };
-  
-  vm.mockOptions = mockOptions;
-  
-  // TODO Move to items service
-  vm.addInventoryItem = function addInventoryItem(totalItems) { 
-    
-    // TODO Validate total items is a reasonable value
 
-    for (var i = 0; i < totalItems; i++) { 
-      saleData.push({});
-    }
-  }
+  vm.mockOptions = mockOptions;
 }
