06168b5548333e3a6319d6b7e57c34c33b5302fb
DedrickEnc <kitamuka@gmail.com>
2016-01-27 10:46:46 +0000

    Amending the PR

--- server/test/api/debtors.js
+++ server/test/api/debtors.js
@@ -12,7 +12,7 @@ describe('The /debtors API', function () {
   var agent = chai.request.agent(helpers.baseUrl);
 
   var inspectDebtorGroup;
-
+  var debtorUuid = '3be232f9-a4b9-4af6-984c-5d3f87d5c107';
   // Logs in before each test
   beforeEach(helpers.login(agent));
 
@@ -54,18 +54,8 @@ describe('The /debtors API', function () {
       .catch(helpers.handler);
   });
 
- it.skip('METHOD : GET, PATH : /debtors/:uuid/invoices?balanced=3 returns an error bad value sent', function () {
-    return agent.get('/debtors/3be232f9-a4b9-4af6-984c-5d3f87d5c107/invoices?balanced=3')
-      .then(function (result) {
-        expect(result).to.have.status(400);
-        expect(result.body).to.not.be.empty;
-        expect(result.body).to.have.all.keys('code', 'httpStatus', 'reason');
-      })
-      .catch(helpers.handler);
-  });
-  
   it('METHOD : GET, PATH : /debtors/:uuid/invoices?balanced=0 returns a list of unbalance invoices of a given debtor', function () {
-    return agent.get('/debtors/3be232f9-a4b9-4af6-984c-5d3f87d5c107/invoices?balanced=0')
+    return agent.get('/debtors/:uuid/invoices?balanced=0'.replace(':uuid', debtorUuid))
       .then(function (result) {
         expect(result).to.have.status(200);
         expect(result.body).to.not.be.empty;
@@ -75,7 +65,7 @@ describe('The /debtors API', function () {
   });
 
 it('METHOD : GET, PATH : /debtors/:uuid/invoices?balanced=1 returns a list of balanced invoices of a given debtor', function () {
-    return agent.get('/debtors/3be232f9-a4b9-4af6-984c-5d3f87d5c107/invoices?balanced=1')
+    return agent.get('/debtors/:uuid/invoices?balanced=1'.replace(':uuid', debtorUuid))
       .then(function (result) {
         expect(result).to.have.status(200);
         expect(result.body).to.have.length(0);
@@ -85,7 +75,7 @@ it('METHOD : GET, PATH : /debtors/:uuid/invoices?balanced=1 returns a list of ba
 
 
  it('METHOD : GET, PATH : /debtors/:uuid/invoices returns a list of all invoices of a given debtor', function () {
-    return agent.get('/debtors/3be232f9-a4b9-4af6-984c-5d3f87d5c107/invoices')
+    return agent.get('/debtors/:uuid/invoices'.replace(':uuid', debtorUuid))
       .then(function (result) {
         expect(result).to.have.status(200);
         expect(result.body).to.not.be.empty;
