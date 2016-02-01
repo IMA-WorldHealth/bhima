6ff2fe6914de7208087000d1d5232d1a4865fc5b
Steven Fountain <StevenFountain@live.co.uk>
2016-02-01 15:10:23 +0100

    Base invoice controller state
    Hooking up basic UI elements to controller, commit for UI-router
    research.

--- client/src/partials/patient_invoice/patientInvoice.js
+++ client/src/partials/patient_invoice/patientInvoice.js
@@ -1,26 +1,52 @@
 angular.module('bhima.controllers')
 .controller('PatientInvoiceController', PatientInvoiceController);
 
-PatientInvoiceController.$inject = ['uuid'];
+PatientInvoiceController.$inject = ['$http', 'uuid'];
 
-function PatientInvoiceController(uuid) { 
+function PatientInvoiceController($http, uuid) { 
   var vm = this;
-  
+
   // Set default invoice date to today 
   vm.invoiceDate = new Date();
   vm.invoiceId = uuid();
 
+  // FIXME 
+  vm.distributable = "true";
+  vm.itemIncrement = 1;
+
+  // TODO 02/08 - Replace with Dedrick's service API
+  $http.get('services')
+    .then(function (services) { 
+      vm.services = services.data;
+
+      // Select default service
+      vm.service = vm.services[0];
+    });
+
+  var saleData = [];
+
   var mockOptions = { 
     enableSorting : false,
-    gridMenuShowHideColumns : false,
+    enableColumnMenus : false,
     columnDefs : [
-      { field : 'Quanity' },
-      { field : 'Code' },
-      { field : 'Description' },
-      { field : 'Unit Price' },
-      { field : 'Amount' }
-    ]
+      { field : 'code' },
+      { field : 'description' },
+      { field : 'quantity' },
+      { field : 'unit_price' },
+      { field : 'amount' }
+    ],
+    data : saleData
   };
-
+  
   vm.mockOptions = mockOptions;
+  
+  // TODO Move to items service
+  vm.addInventoryItem = function addInventoryItem(totalItems) { 
+    
+    // TODO Validate total items is a reasonable value
+
+    for (var i = 0; i < totalItems; i++) { 
+      saleData.push({});
+    }
+  }
 }
