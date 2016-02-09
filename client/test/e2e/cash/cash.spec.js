4ca03322dbc05afa921ed04d90f18d7e2a16c036
Jonathan Niles <jonathanwniles@gmail.com>
2016-02-09 12:42:54 +0100

    TEST(cash): implement end to end test for caution
    This commit implements an end to end test to demonstrate that cautions
    work.  The test should be refactored as more components get added to
    use.

--- client/test/e2e/cash/cash.spec.js
+++ client/test/e2e/cash/cash.spec.js
@@ -95,7 +95,7 @@ describe.only('Cash Payments Module', function () {
       expect(getCurrentPath()).to.eventually.equal(target);
     });
 
-    it.only('should allow a user to select and deselect a cashbox', function () {
+    it('should allow a user to select and deselect a cashbox', function () {
 
       var targetInitial = path.concat('/' + cashboxA.id);
       var targetFinal = path.concat('/' + cashboxB.id);
@@ -106,7 +106,7 @@ describe.only('Cash Payments Module', function () {
 
       // make sure we are in the correct cashbox
       var hasCashboxAText = EC.textToBePresentInElement($('[data-cashbox-text]'), cashboxA.text);
-      browser.wait(hasCashboxAText, 1000);
+      browser.wait(hasCashboxAText, 10000);
 
       // use the button to navigate back to the cashbox select module
       var backBtn = element(by.css('[data-change-cashbox]'));
@@ -121,24 +121,116 @@ describe.only('Cash Payments Module', function () {
 
       // verify that we get to the cashboxB page
       expect(getCurrentPath()).to.eventually.equal(targetFinal);
-
-      // confirm that I can find cashbox B's text
-      var hasCashboxBText = EC.textToBePresentInElement($('[data-cashbox-text]'), cashboxB.text);
-      browser.wait(hasCashboxBText, 1000);
     });
   });
 
   /** tests for the cash payments form page */
-  describe('Cash Payments Form Page', function () {
+  describe.only('Cash Payments Form Page', function () {
 
     /** navigate to the page before each function */
     beforeEach(function () {
       browser.get(path);
     });
 
-    var mockInvoicePayment = { };
+    // this code assumes that the find-patient directive is well tested.
+    // we should be able to use a patient ID without thinking about the potential
+    // failures
 
-    var mockCautionPayment = { };
+    // This caution payment should succeed
+    var mockCautionPayment = {
+      patientId: 'TPA1',
+      amount : 15
+    };
 
+    // This payment against patient invoices should succeed
+    var mockInvoicePayment = {
+      patientId: 'TPA2',
+      date : new Date('2016-06-01'),
+      amount : 5.12
+    };
+
+    /**
+     * wraps the find-patient directive to search by id easily
+     * @todo - move this into a common shared utility for modules to use in the
+     * future
+     */
+    function findPatientById(id) {
+
+      // locate the find-patient directive's dropdown toggle and open
+      var dropdown = element(by.css('[data-find-patient-dropdown-toggle]'));
+      dropdown.click();
+
+      // select the search by id option
+      var searchByIdOption = element(by.css('[data-find-patient-option="FIND.PATIENT_ID"]'));
+      searchByIdOption.click();
+
+      // get the input and enter the id provided
+      var input = element(by.model('session.idInput'));
+      input.sendKeys(id);
+
+      // submit the id to the server
+      var submit = element(by.css('[data-find-patient-submit]'));
+      submit.click();
+    }
+
+    /**
+     * wraps the edit date component to easily change dates
+     * @todo - move this into a common shared utility for modules to use in the
+     * future
+     */
+    function editDate(date, form) {
+
+      // click the edit date button to toggle into readonly mode
+      var btn = element(by.css('[data-edit-date-btn]'));
+      btn.click();
+
+      // pre-format date for insertion to the date input
+      var val = date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
+
+      // currently, protractor does not support sending keys to all known HTML5
+      // date input controls (see https://github.com/angular/protractor/issues/562).
+      // so, we must the document methods.
+      var src = 'var ipt = document.querySelector("[data-edit-date-btn]"); ' +
+        'ipt.value = "' + val + '"; ' +
+        'angular.element(ipt).scope().$apply(' +
+          'function (s) { s.' + form + '[ipt.name].$setViewValue("' + val + '"); });';
+
+      // run the script
+      browser.executeScript(src);
+
+      // lock the date input back in readonly mode
+      btn.click();
+    }
+
+    it('should make a caution payment', function () {
+
+      // select the proper patient
+      findPatientById(mockCautionPayment.patientId);
+
+      // we will leave the date input as default
+
+      // select the proper is caution type
+      var cautionOption = element(by.css('[data-caution-option="1"]'));
+      cautionOption.click();
+
+      // select the FC currency from the currency
+      var currencyOption = element(by.css('[data-currency-option="1"]'));
+      currencyOption.click();
+
+      // enter the amount to pay for a caution
+      var currencyInput = element(by.css('[data-currency-input]'));
+      currencyInput.sendKeys(mockCautionPayment.amount);
+
+      // click the submit button
+      var submit = element(by.css('[data-action="submit"]'));
+      submit.click();
+
+      // expect the receipt modal to appear
+      expect(element(by.css('[data-cash-receipt-modal]')).isPresent()).to.eventually.equal(true);
+    });
+
+    it('should make a payment against previous invoices');
+
+    it('should not allow a payment without a date');
   });
 });
