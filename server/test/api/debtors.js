011dce85256fd322a7ee273fd9dbbfcaf72e83c2
Bruce Mbayo <mbayopanda@gmail.com>
2016-02-10 15:18:52 +0100

    Fixing debtors.js test by removing all debtor group's tests

--- server/test/api/debtors.js
+++ server/test/api/debtors.js
@@ -16,44 +16,6 @@ describe('(/debtors) The /debtors API', function () {
   // Logs in before each test
   beforeEach(helpers.login(agent));
 
-  it('GET /debtors/groups returns a list of debtor groups', function () {
-    var INITIAL_TEST_DEBTORS = 2;
-
-    return agent.get('/debtors/groups')
-      .then(function (result) {
-        expect(result).to.have.status(200);
-        expect(result.body).to.not.be.empty;
-        expect(result.body).to.have.length(INITIAL_TEST_DEBTORS);
-
-        inspectDebtorGroup = result.body[0].uuid;
-      })
-      .catch(helpers.handler);
-  });
-
-  it('GET /debtors/groups/:id returns all details for a valid debtor group', function () {
-    return agent.get('/debtors/groups/' + inspectDebtorGroup)
-      .then(function (result) {
-        var debtorGroup;
-        var expectedKeySubset = ['uuid', 'account_id', 'name', 'location_id'];
-
-        expect(result).to.have.status(200);
-
-        debtorGroup = result.body;
-        expect(debtorGroup).to.contain.all.keys(expectedKeySubset);
-      })
-      .catch(helpers.handler);
-  });
-
-  it('GET /debtors/groups/:id returns not found for invalid id', function () {
-    return agent.get('/debtors/groups/invalid')
-      .then(function (result) {
-
-        expect(result).to.have.status(404);
-        expect(result.body).to.not.be.empty;
-      })
-      .catch(helpers.handler);
-  });
-
  it('GET /debtors/:uuid/invoices returns a list of all invoices of a given debtor', function () {
     return agent.get('/debtors/:uuid/invoices'.replace(':uuid', debtorUuid))
       .then(function (result) {
