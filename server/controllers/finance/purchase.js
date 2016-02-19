cadbcc811d2162a120b3523a9d76dcdd74ab68b3
lomamech <lomamech@gmail.com>
2016-02-19 13:00:17 +0100

    Complete Purchase Order API

--- server/controllers/finance/purchase.js
+++ server/controllers/finance/purchase.js
@@ -4,6 +4,27 @@ var uuid = require('./../../lib/guid');
 var journal = require('./journal');
 
 
+
+/** 
+ * Utility method to ensure purchase items lines reference purchase.
+ * @param {Object} purchaseItems - An Array of all purchase items to be written 
+ * @param {string} purchaseUuid - UUID of referenced purchase order
+ * @returns {Object} An Array of all purchases items with guaranteed UUIDs and Purchase orders references
+ */
+function linkPurchaseItems(purchaseItems, purchaseUuid) { 
+  return purchaseItems.map(function (purchaseItem) { 
+     
+    purchaseItem.uuid = purchaseItem.uuid || uuid();
+    purchaseItem.purchase_uuid = purchaseUuid;
+   
+    // Collapse sale item into Array to be inserted into database
+    return Object.keys(purchaseItem).map(function (key) { 
+      return purchaseItem[key];
+    });
+  });
+}
+
+
 /**
 * Create a Purchase Order in the database
 *
@@ -13,18 +34,35 @@ function create (req, res, next) {
   'use strict';
 
   var purchase = req.body;
-
-  purchase.uuid = purchase.uuid || uuid();
-
-  var sql = 'INSERT INTO purchase SET ?';
+  var transaction;  
+  var purchaseOrder = purchase.purchase_order;
 
 
-  db.exec(sql, [purchase])
-  .then(function (result) {
-    res.status(201).json({ uuid : purchase.uuid });
-  })
-  .catch(next)
-  .done();
+  var sqlPurchase = 'INSERT INTO purchase SET ?';
+
+  var sqlPurchaseItem = 'INSERT INTO purchase_item (uuid, inventory_uuid, quantity, unit_price, ' +
+    'total, purchase_uuid) VALUES ?'; 
+
+  var dataPurchaseItem = linkPurchaseItems(purchase.purchase_item, purchaseOrder.uuid);
+
+  transaction = db.transaction();
+
+  transaction
+    .addQuery(sqlPurchase, [purchase.purchase_order])
+    .addQuery(sqlPurchaseItem,[linkPurchaseItems(purchase.purchase_item, purchaseOrder.uuid)]);
+
+  transaction.execute()
+    .then(function (results) {
+      var confirmation =  { 
+        uuid : purchaseOrder.uuid,
+        results : results
+      };
+
+      res.status(201).json(confirmation);
+      return;
+    })
+    .catch(next)
+    .done();
 }
 
 
@@ -57,7 +95,7 @@ function list (req, res, next) {
 *
 * Returns the details of a single purchase order
 */
-function detail (req, res, next) {
+/*function detail (req, res, next) {
   'use strict';
 
   var purchaseUuid = req.params.uuid;
@@ -72,11 +110,52 @@ function detail (req, res, next) {
 
   db.exec(sql, [purchaseUuid])
   .then(function (rows) {
+
     res.status(200).json(rows);
   })
   .catch(next)
   .done();
 }
+*/
+function detail (req, res, next) {
+  'use strict';
+
+  var purchaseUuid = req.params.uuid,
+    purchaseRow, purchaseItems;
+
+  var sqlPurchase =
+    'SELECT purchase.uuid, purchase.reference, purchase.cost, purchase.discount, purchase.purchase_date, purchase.paid, ' +
+    'creditor.text, employee.name, employee.prenom, user.first, user.last ' +
+    'FROM purchase ' +
+    'JOIN creditor ON creditor.uuid = purchase.creditor_uuid ' +
+    'JOIN employee ON employee.id = purchase.purchaser_id ' +
+    'JOIN user ON user.id = purchase.emitter_id ' +
+    'WHERE purchase.uuid = ?';
+
+  var sqlPurchaseItem =
+    'SELECT purchase_item.purchase_uuid, purchase_item.uuid, purchase_item.quantity, purchase_item.unit_price, ' +
+    'purchase_item.total, inventory.text ' +
+    'FROM purchase_item ' +
+    'JOIN inventory ON inventory.uuid = purchase_item.inventory_uuid ' +
+    'WHERE purchase_item.purchase_uuid = ? ';  
+
+  db.exec(sqlPurchase, [purchaseUuid])
+  .then(function (row) {
+    purchaseRow = row;
+    return db.exec(sqlPurchaseItem, [purchaseUuid]);
+  })
+  .then(function (rows) { 
+    purchaseItems = rows;
+    res.status(200).json({
+      purchase : purchaseRow, 
+      purchaseItems : purchaseItems
+    });
+
+  })  
+  .catch(next)
+  .done();
+}
+
 
 
 // create a new purchase order
--- server/test/api/purchase.js
+++ server/test/api/purchase.js
@@ -2,6 +2,7 @@
 
 var chai = require('chai');
 var expect = chai.expect;
+var date = new Date();
 
 var helpers = require('./helpers');
 helpers.configure(chai);
@@ -18,8 +19,8 @@ describe('The /Purchase Order API endpoint', function () {
   // purchase we will add during this test suite.
   var purchase_order = {
       uuid          : '858f7a85-16d9-4ae5-b5c0-1fe007a8d2e',
-      cost          : 1000,
-      purchase_date : new Date('2015-10-10'),
+      cost          : 546.7520,
+      purchase_date : new Date('2016-02-19'),
       currency_id   : 1,
       creditor_uuid : '7ac4e83c-65f2-45a1-8357-8b025003d794',
       receiver_id: 1,
@@ -29,23 +30,48 @@ describe('The /Purchase Order API endpoint', function () {
       is_direct : 1
     };
 
+   var purchase_item = [
+    { 
+      uuid : '017dbe1e-c37c-11e5-a86e-843a4b0cdadc', 
+      inventory_uuid : '289cc0a1-b90f-11e5-8c73-159fdc73ab02',
+      quantity : 200,
+      unit_price : 0.0538,
+      total : 10.7520 
+    },
+    { 
+      uuid : 'ab8528de-1b0a-41df-84b4-88eff430f2f7',       
+      inventory_uuid : 'c48a3c4b-c07d-4899-95af-411f7708e296',
+      quantity : 16000,
+      unit_price : 0.0335,
+      total : 536.0000 
+    }    
+   ]; 
+
+  var purchase = {
+    purchase_order : purchase_order,
+    purchase_item : purchase_item
+  };
+
+
   // login before each request  
   beforeEach(helpers.login(agent));
 
   it('POST /purchase should create a new purchase order', function () {
 
     return agent.post('/purchase')
-      .send(purchase_order)
+      .send(purchase)
       .then(function (res) {
         expect(res).to.have.status(201);
         expect(res).to.be.json;
         var purchaseUuid = res.body.uuid;
-        return agent.get('/purchase/' + res.body.uuid);
+        return agent.get('/purchase/' + purchaseUuid);
       })
       .then(function (res) {
+        var purchase = res.body.purchase[0];
+
         expect(res).to.have.status(200);
         expect(res).to.be.json;
-        expect(res.body[0].cost).to.equal(purchase_order.cost);
+        expect(purchase.cost).to.equal(purchase_order.cost);
       })
       .catch(helpers.handler);
   });
@@ -53,140 +79,13 @@ describe('The /Purchase Order API endpoint', function () {
   it('GET /purchase/:uuid should return a single JSON purchase order', function () {
     return agent.get('/purchase/' + purchase_order.uuid)
       .then(function (res) {
+        var purchase = res.body.purchase[0];
+
         expect(res).to.have.status(200);
         expect(res.body).to.not.be.empty;
-        expect(res.body[0].uuid).to.exist;
+        expect(purchase.uuid).to.exist;
       })
       .catch(helpers.handler);
   });
 
-
-/*
-  it('GET /projects returns a list of projects', function () {
-    return agent.get('/projects')
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res.body).to.not.be.empty;
-        expect(res.body[0]).to.have.keys('id', 'name');
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/:id should not be found for unknown id', function () {
-    return agent.get('/projects/unknownproject')
-      .then(function (res) {
-        expect(res).to.have.status(404);
-        expect(res.body).to.not.be.empty;
-        expect(res.body.code).to.equal('ERR_NOT_FOUND');
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/:id should return a single JSON project', function () {
-    return agent.get('/projects/1')
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res.body).to.not.be.empty;
-        expect(res.body.id).to.exist;
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/ ? COMPLETE = 1 returns a complete List of project  ', function () { 
-    return agent.get('/projects?complete=1')
-      .then(function (result) { 
-        expect(result).to.have.status(200);
-        expect(result).to.be.json;
-        expect(result.body).to.have.length(INITIAL_PROJECTS);
-        expect(result.body[0]).to.contain.all.keys(PROJECT_KEY); 
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/ ? LOCKED = 0 returns a complete List of unlocked projects  ', function () { 
-    return agent.get('/projects?locked=0')
-      .then(function (result) { 
-        expect(result).to.have.status(200);
-        expect(result).to.be.json;
-        expect(result.body[0].locked).to.equal(UNLOCKED);
-        expect(result.body[0]).to.contain.all.keys(PROJECT_KEY);
-        expect(result.body).to.have.length(INITIAL_PROJECTS);        
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/ ? LOCKED = 1 returns a complete List of locked projects  ', function () { 
-    return agent.get('/projects?locked=1')
-      .then(function (result) { 
-        expect(result).to.have.status(200);
-        expect(result.body).to.be.empty;        
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/ ? INCOMPLETE LOCKED = 0 returns a simple List of unlocked projects  ', function () { 
-    return agent.get('/projects?incomplete_locked=0')
-      .then(function (result) { 
-        expect(result).to.have.status(200);
-        expect(result).to.be.json;
-        expect(result.body[0]).to.have.keys('id', 'name');
-        expect(result.body).to.have.length(INITIAL_PROJECTS);
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('GET /projects/ ? INCOMPLETE LOCKED = 1 returns a simple List of locked projects  ', function () { 
-    return agent.get('/projects?incomplete_locked=1')
-      .then(function (result) { 
-        expect(result).to.have.status(200);
-        expect(result.body).to.be.empty;
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('POST /projects should create a new project', function () {
-
-    return agent.post('/projects')
-      .send(project)
-      .then(function (res) {
-        expect(res).to.have.status(201);
-        expect(res).to.be.json;
-        return agent.get('/projects/' + res.body.id);
-      })
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res).to.be.json;
-        expect(res.body.name).to.equal(project.name);
-        project.id = res.body.id;
-      })
-      .catch(helpers.handler);
-  });
-
-  it('PUT /projects should update an existing project', function () {
-    return agent.put('/projects/' + project.id)
-      .send({ name : 'Temp Project' })
-      .then(function (res) {
-        expect(res).to.have.status(200);
-        expect(res.body).to.have.keys(PROJECT_KEY);
-        expect(res.body.name).to.not.equal(project.name);
-      })
-      .catch(helpers.handler);
-  });
-
-  it('DELETE /projects/:id will send back a 404 if the prjects does not exist', function () {
-    return agent.delete('/projects/unknownproject')
-      .then(function (res) {
-        expect(res).to.have.status(404);
-        expect(res.body).to.be.empty;
-      })
-      .catch(helpers.handler);
-  });
-*/
-/*  it('DELETE /projects/:id should delete an existing and unused project', function () {
-    return agent.delete('/projects/' + project.id)
-      .then(function (res) {
-        expect(res).to.have.status(204);
-      })
-      .catch(helpers.handler);
-  });*/
 });
