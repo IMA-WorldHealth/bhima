b22ac127ea14dc63b2b97383507dedda6d90040c
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-02-29 14:50:50 +0100

    Moved UI grid templates into individual files
    Hardcoded template strings removed and replaced with template files,
    controller cleaned up to reflect change. Templates require refining
    (hardcoded styles etc.)

--- client/src/partials/patient_invoice/patientInvoice.js
+++ client/src/partials/patient_invoice/patientInvoice.js
@@ -10,9 +10,6 @@ PatientInvoiceController.$inject = ['$http', '$q', 'uuid', 'uiGridConstants', 'P
 function PatientInvoiceController($http, $q, uuid, uiGridConstants, Patients, PriceLists, Invoice) { 
   var vm = this;
 
-  // 1. Allow generic configuration of page - all financial details wait on the patient to be invoiced to be set 
-  // 2. Patient being selected will trigger all other actions
-  // 3. Billing services downloaded for patient 
   // 4. Price lists downloaded for patient 
   // 5. Invoice.items service initialised with price list and billing services prices in as optional parameters 
   //    (These will need to be included in the price of quanities as well as the total calculation)
@@ -51,50 +48,20 @@ function PatientInvoiceController($http, $q, uuid, uiGridConstants, Patients, Pr
   vm.Invoice = Invoice;
   vm.Invoice.items = vm.Invoice.items;
 
-  var saleData = vm.Invoice.items.current.data;
-  
-  var mockOptions = { 
+  var gridOptions = { 
     appScopeProvider : vm,
     enableSorting : false,
     enableColumnMenus : false,
-    // showGridFooter : true,
-    // gridFooterTemplate : footer,
-    // onRegisterApi : validate,
-    // footerTemplate : 'partials/patient_invoice/templates/footer.tmpl.html',
-    // showColumnFooter : true,
     columnDefs : [
-      { field : 'status',
-        displayName : '',
-        width : '25',
-        cellTemplate : 
-          '<div ng-if="!row.entity.confirmed" class="bg-danger" style="padding : 5px;"><span class="text-danger glyphicon glyphicon-remove-sign"></span></div>' + 
-          '<div ng-if="row.entity.confirmed" class="bg-success" style="padding : 5px;"><span class="text-success glyphicon glyphicon-ok-sign"></span></div>'
-      },
-      { 
-        field : 'code',
-        cellTemplate : '<div style="padding : 5px;">' + 
-          '<div ng-if="!row.entity.confirmed">' + 
-          '<input class="form-control" typeahead-append-to-body="true" uib-typeahead="item.uuid as (item.label + \' [\' + item.code + \']\') for item in grid.appScope.Invoice.items.available()" ng-model="row.entity.inventoryUuid" typeahead-on-select="grid.appScope.Invoice.items.confirmItem(row.entity)"></input>'+ 
-          '</div>' + 
-          '<div nf-if="row.entity.confirmed">' + 
-          '<p>{{row.entity.code}}</p>' +
-          '</div>'
-        },
-      { field : 'description' },
-      { field : 'quantity', 
-        cellTemplate : '<div style="padding : 5px;"><input min="0" type="number" ng-disabled="!row.entity.confirmed" class="form-control" style="padding-left : 5px !important" ng-model="row.entity.quantity"></input></div>'
-        // cellFooterTemplate : '<div>eh</div>',
-        // aggregationType : uiGridConstants.aggregationTypes.sum
-      },
-      { field : 'unit_price', 
-        cellTemplate : '<div style="padding : 5px;"><input min="0" type="number" ng-disabled="!row.entity.confirmed" class="form-control" style="padding-left : 5px !important" ng-model="row.entity.unit_price"></input></div>'},
-      { field : 'amount',
-        cellTemplate : '<div style="padding : 5px;">{{row.entity.quantity * row.entity.unit_price | currency}}</div>'},
-      { field : 'actions',
-        width : 25,
-        cellTemplate : '<div style="padding : 5px;"><a href="" ng-click="grid.appScope.Invoice.items.removeItem(row.entity)"><span class="glyphicon glyphicon-trash"></span></a></div>'}
+      {field : 'status', width : 25, displayName : '', cellTemplate : 'partials/patient_invoice/templates/grid/status.tmpl.html'},
+      {field : 'code', cellTemplate :  'partials/patient_invoice/templates/grid/code.tmpl.html'},
+      {field : 'description'},
+      {field : 'quantity', cellTemplate : 'partials/patient_invoice/templates/grid/quantity.tmpl.html'},
+      {field : 'unit_price', cellTemplate : 'partials/patient_invoice/templates/grid/unit.tmpl.html'},
+      {field : 'amount', cellTemplate : 'partials/patient_invoice/templates/grid/amount.tmpl.html'},
+      {field : 'actions', width : 25, cellTemplate : 'partials/patient_invoice/templates/grid/actions.tmpl.html'}
     ],
-    data : saleData
+    data : vm.Invoice.items.current.data
   };
   
   // TODO Move this code - initial inventory item 
@@ -104,8 +71,7 @@ function PatientInvoiceController($http, $q, uuid, uiGridConstants, Patients, Pr
   // Quantity should be required to be whole integer 
   // Unit price should be number
 
-  vm.mockOptions = mockOptions;
-  vm.saleData = saleData;
+  vm.gridOptions = gridOptions;
 
   vm.setPatient = function setPatient(patient) { 
 
@@ -153,22 +119,5 @@ function PatientInvoiceController($http, $q, uuid, uiGridConstants, Patients, Pr
 
         vm.invoicePatient = patient;
       });
-      // .then(function (result) { 
-        // console.log('got patient subsidies');
-        // console.log(result);
-      // });
-    
-    // console.log('current patient is', patient);
-    // Information is required to know if we should be applying subsidies, 
-    // billing services and discounts
-    
-            // .then(function (result) { 
-          // console.log('got price list');
-          // console.log(result);
-        // });
-    // }
   }
-  
-  
-  window.gridOptions = mockOptions;
 }
