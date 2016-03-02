ceea37392579878cbd710a61db845f7a54d4e573
lomamech <lomamech@gmail.com>
2016-03-02 10:46:17 +0100

    Improve label and comment

--- server/test/api/functions.js
+++ server/test/api/functions.js
@@ -7,9 +7,9 @@ helpers.configure(chai);
 /**
 * The /function  API endpoint
 *
-* This test suite implements full CRUD on the /function  HTTP API endpoint.
+* This test suite implements full CRUD on the /functions  HTTP API endpoint.
 */
-describe('The /function  API endpoint', function () {
+describe('The /functions  API endpoint', function () {
   var agent = chai.request.agent(helpers.baseUrl);
 
       // Function we will add during this test suite.
--- server/test/api/grades.js
+++ server/test/api/grades.js
@@ -8,9 +8,9 @@ helpers.configure(chai);
 /**
 * The /grade   API endpoint
 *
-* This test suite implements full CRUD on the /grade   HTTP API endpoint.
+* This test suite implements full CRUD on the /grades   HTTP API endpoint.
 */
-describe('The /grade  API endpoint', function () {
+describe('The /grades  API endpoint', function () {
   var agent = chai.request.agent(helpers.baseUrl);
 
   // Grade we will add during this test suite.
