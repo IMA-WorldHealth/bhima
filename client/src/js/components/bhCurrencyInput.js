fcc8b8f64e6b5c0e7bfe5d9c59abc29bc9fac58e
DedrickEnc <kitamuka@gmail.com>
2016-03-10 00:01:35 +0100

    custom validation rule for part of currency

--- client/src/js/components/bhCurrencyInput.js
+++ client/src/js/components/bhCurrencyInput.js
@@ -5,7 +5,7 @@
  * validation based on the currency being validated.
  */
 
-function CurrencyInputController() {
+function CurrencyInputController(precision) {
   var ctrl = this;
 
   /** atach empty function if no callback is provided**/
@@ -18,9 +18,35 @@ function CurrencyInputController() {
     /**the currency input component can not update the currencyId, it is not his responsability**/
     ctrl.onCurrencyChange({currency : currency});    
   }
+
+  /** this is a custom form validation just for checking step**/
+  function validate (){
+    delete ctrl.form.currencyInputValue.$error.step;
+    ctrl.form.currencyInputValue.$invalid = false;
+    ctrl.form.currencyInputValue.$valid = true;
+    ctrl.form.$valid = true;
+    ctrl.form.$invalid = false;
+  }
+
+  /** this is a custom form validation just for checking step**/
+  function invalidate (){
+    ctrl.form.currencyInputValue.$error.step = true;
+    ctrl.form.currencyInputValue.$invalid = true;
+    ctrl.form.currencyInputValue.$valid = false;
+    ctrl.form.$valid = false;
+    ctrl.form.$invalid = true;
+  }
+
+  function checkModel() {
+    (precision.scale(ctrl.model) % precision.scale(ctrl.currency.min_monentary_unit) !== 0) ?
+      invalidate() : validate(); 
+  }
   
   ctrl.handleCurrencyChange = handleCurrencyChange;
+  ctrl.checkModel = checkModel;
 }
+
+CurrencyInputController.$inject = [ 'precision' ];
 
 angular.module('bhima.components')
 .component('bhCurrencyInput', {
@@ -31,7 +57,7 @@ angular.module('bhima.components')
     currencyId : '<',       // one-way binding
     model : '=',            // two way binding
     maxValue : '<',         // one way binding
-    form : '<',             // one-way binding
+    form : '=',             // one-way binding
     validationTrigger : '<' // one-way binding
   }
 });
