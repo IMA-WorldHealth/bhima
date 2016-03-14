a75284dd7d51f0fdf6f36825355bdca1c88e0ae5
Jonathan Niles <jonathanwniles@gmail.com>
2016-03-14 14:49:32 +0100

    feat(simple vouchers): client-side simple vouchers
    This commit adds a simplified journal voucher interface to the client
    side.  The simplified journal vouchers exist to facilitate cash
    transactions between two accounts and return a simple receipt.
    
    Unforunately, the server-side API has not been properly developed, so
    future commits will address this setback before being able to write
    end-to-end tests for this particular condition.

--- client/src/js/services/VoucherService.js
+++ client/src/js/services/VoucherService.js
@@ -9,6 +9,7 @@ function VoucherService ($http, util) {
 
   service.create = create;
   service.read = read;
+  service.createSimple = createSimple;
 
   /** send an http request to create a voucher**/
   function create(voucher) {
@@ -21,6 +22,39 @@ function VoucherService ($http, util) {
      var url = baseUrl.concat(id || '');
      return $http.get(url)
      .then(util.unwrapHttpResponse);
+  }
+
+  /**
+   * Creates a simple journal voucher, transforming the object into double-entry
+   * accounting
+   * @method createSimple
+   * @param {object} voucher - the raw journal voucher
+   */
+  function createSimple(voucher) {
+
+    // create a local working copy for manipulation
+    var clean = angular.copy(voucher);
+
+    // in a simple journal voucher, there are two items
+    var items = [{
+      debit : clean.amount,
+      credit: 0,
+      account_id : clean.fromAccount.id
+    }, {
+      debit : 0,
+      credit : clean.amount,
+      account_id : clean.toAccount.id
+    }];
+
+    // clean up the voucher by removing view properties
+    delete clean.toAccount;
+    delete clean.fromAccount;
+
+    // bind the voucher items
+    clean.items = items;
+
+    return $http.post(baseUrl, clean)
+    .then(util.unwrapHttpResponse);
   }
 
   return service;
