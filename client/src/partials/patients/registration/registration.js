3f397db1f21f69616efd150120586c1c6af0dba7
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-03-26 11:37:40 +0100

    (REFACTOR) Handle exceptions w/patient reg. module
    This commit implements improved exception handling into the patient
    registration controller.
    
    If no debtor groups are downloaded this is reported at the debtor group
    select (with a uib-alert); this must be explained as this is a required
    field for the page to submit a new patient request.
    
    Added a uib-alert at the top of the page that displays information if an
    exception object is found on the view model. Updated relevent services
    to pass the latest standard of translatable keys to ensure error
    propegation is handled correctly at each stage.
    
    $anchorScroll service is used to scroll to the error alert if the screen
    is not currenlty focussed on this element; initial call is not included
    in $apply block so for now the call is wrapped in a $timeout request.
    All of this functionality should be wrapped in a service to reduce
    number of requires for the controller.

--- client/src/partials/patients/registration/registration.js
+++ client/src/partials/patients/registration/registration.js
@@ -3,10 +3,10 @@ angular.module('bhima.controllers')
 .controller('PatientRegistrationController', PatientRegistrationController);
 
 PatientRegistrationController.$inject = [
-  '$location', 'Patients', 'Debtors', 'util', 'SessionService'
+  '$location', '$anchorScroll', '$timeout', 'Patients', 'Debtors', 'util', 'SessionService'
 ];
 
-function PatientRegistrationController($location, patients, debtors, util, Session) {
+function PatientRegistrationController($location, $anchorScroll, $timeout, Patients, Debtors, util, Session) {
   var viewModel = this;
 
   // Models for collecting patient data in logical groups
@@ -19,11 +19,12 @@ function PatientRegistrationController($location, patients, debtors, util, Sessi
   viewModel.medical.current_location_id = Session.enterprise.location_id;
 
   // Set up page elements data (debtor select data)
-  debtors.groups()
+  Debtors.groups()
     .then(function (results) {
       viewModel.options.debtorGroups = results;
-    });
-
+    })
+    .catch(handleServerError);
+  
   // Define limits for DOB
   viewModel.minDOB = util.minDOB;
   viewModel.maxDOB = util.maxDOB;
@@ -39,18 +40,19 @@ function PatientRegistrationController($location, patients, debtors, util, Sessi
       return;
     }
    
-    patients.create(viewModel.medical, viewModel.finance)
+    Patients.create(viewModel.medical, viewModel.finance)
       .then(function (result) {
 
         // create patient success - mark as visiting
-        return patients.logVisit(result.uuid);
+        return Patients.logVisit(result.uuid);
       })
       .then(function (confirmation) {
         var patientCardPath = '/invoice/patient/';
 
         //TODO Hospital card should recieve a value that notifies the user of register success
         $location.path(patientCardPath.concat(confirmation.uuid));
-      });
+      })
+      .catch(handleServerError);
   };
 
   /**
@@ -63,4 +65,38 @@ function PatientRegistrationController($location, patients, debtors, util, Sessi
   viewModel.calculateYOB = function calculateYOB(value) {
     viewModel.medical.dob = value && value.length === 4 ? new Date(value + '-' + util.defaultBirthMonth) : undefined;
   };
+  
+  /** 
+   * This method is responsible for handling exceptions thrown by the server 
+   * that the client has not anticipated. 
+   * 
+   * @todo  Discuss if this should be a library to account for standard client side errors,
+   *        -1 for offline etc. This should not have to be done everywhere. 
+   * @params  {object}  error   An Error object that has been sent from the server. 
+   */
+  function handleServerError(error) {
+    var NO_INTERNET_CONNECTION = -1;
+  
+    // case - there is no internet connection 
+    if (error.status === NO_INTERNET_CONNECTION) { 
+      viewModel.exception = { 
+        status : -1,
+        data : { 
+          code : 'ERRORS.NO_CONNECTION',
+          description : 'No internet connection found - please contact your system administrator'
+        }
+      };
+    } else { 
+      
+      // No cases have been matched - provide error details to the UI 
+      viewModel.exception = error; 
+    }
+
+    // scroll to the error message ensuring any screen size responds to the problem
+    // - scroll doesn't get digested initially without being wrapped in an $apply block
+    $timeout(function () { 
+      $location.hash('exceptionAlert');
+      $anchorScroll();
+    });
+  }
 }
