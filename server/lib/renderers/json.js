340c1ed5d98103a6207e2b78d3f9c5976944e789
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-04-04 08:14:56 +0100

    (Feature) Infrastructure for specifying renderer
    This commit introduces the proposed render API. Any renderer can be
    written to accept `data` and `options` parameters - the renderer must
    return a promise that will resolve with the compiled report.
    
    This example provides a JSON render intergace that simply returns the
    data object given this standard interface.
    
    Render target are attached to a `supportedRender` object in the report
    controller and any key provided not attached to this object is sent back
    to the client as a `400 Bad Request`.
    
    Two additional integration tests added to account for additional end
    point functionality:
    * A valid render target should return a correctly formatted report
    * An invalid render target should return `400 Bad Request`

---
+++ server/lib/renderers/json.js
@@ -0,0 +1,35 @@
+/**
+ * BHIMA JSON Report Renderer (Wrapper) 
+ *
+ * This server library is responsible for rendering reports in JSON format. 
+ * It adheres to the standard BHIMA Report Renderer API, accepting data
+ * and options and returning a single compiled object. 
+ *
+ * This renderer demonstrates the API however is mostly a wrapper to be used 
+ * uniformly with the other renderers (PDF/ HTML/ CSV).
+ *
+ * @todo    JSON Wrapper is currently just a wrapper to provide a uniform API, it 
+ *          could also provide functionality like JSON verification (JSON parse/
+ *          JSON stringify).
+ * 
+ * @module  lib/renderers/json
+ */
+var q = require('q');
+
+module.exports = renderJSON;
+
+/**
+ * JSON Render Method
+ *
+ * @params {Object} data      Contains data in any format that will be used to 
+ *                            drive the final report - this will be exposed to 
+ *                            the view by the renderer. 
+ * @params {Object} options   (Optional) parameters that can be passed to switch 
+ *                            render features on/off.
+ *
+ * @returns {Object}          JSON Object representing a report that can be sent
+ *                            to the client.
+ */
+function renderJSON(data, options) { 
+  return q.resolve(data);
+}
