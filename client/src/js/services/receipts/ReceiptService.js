5d01b5bf1b39b996716b6434f6d9f6fb32297d35
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-04-04 16:29:00 +0100

    (Feature) Added core receipt client side services
    This commit introduces the architecture for many of the client side
    services and controllers required to include the receipt modal in an
    angular controller.
    
    * ReceiptModal.js - Modal service, this is injected into modules and
                        invoked
    * ReceiptService.js - Interacting with the server end point (data)
    * receiptModal.tmpl.html - Modal template
    * receiptModalController.js - Modal wrapper controller
    * invoiceReceipt.tmpl.html - invoice template for rendering JSON
      response

---
+++ client/src/js/services/receipts/ReceiptService.js
@@ -0,0 +1,34 @@
+angular.module('bhima.services')
+.service('ReceiptService', ReceiptService);
+
+ReceiptService.$inject = ['$http', 'util'];
+
+/**
+ * Receipts Service 
+ * 
+ * This service is responsible for interfacing with any receipts routes on the
+ * server. 
+ *
+ * @todo  currently this server 1:1 maps with the ReceiptModal service/controller. 
+ *        This relationship improved or justified with unit tests.  
+ * @module services/receipts/ReciptService
+ */ 
+function ReceiptService($http, util) { 
+  var service = this;
+
+  service.invoice = invoice;
+  
+  /** 
+   * Fetch invoice report data from /reports/invoices/:uuid
+   *
+   * @params {String} uuid      Target invoice UUID to report on
+   * @params {Object} options   Configuration options for the server generated 
+   *                            report, this includes things like render target.
+   * @return {Promise}          Eventually returns report object from server
+   */
+  function invoice(uuid, options) { 
+    var route = '/reports/invoices/'.concat(uuid);
+    return $http.get(route, { params : options })
+      .then(util.unwrapHttpResponse);
+  }
+}
