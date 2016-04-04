c7338e14ec9605c620d48af9d893c6592eb2a06f
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-04-04 08:14:56 +0100

    (Feature) Infrastructure for specifying renderer
    This commit introduces the proposed render API. Any renderer can be
    written to accept `data` and `options` parameters - the renderer must
    return a promise that will resolve with the compiled report.
    
    This example provides a JSON render intergace that simply returns the
    data object given this standard interface.
    
    Render target are attached to a `supportedRender` object in the report
    controller and any key provided not attached to this object is sent back
    to the client as a `400 Bad Request`.
    
    Two additional integration tests added to account for additional end
    point functionality:
    * A valid render target should return a correctly formatted report
    * An invalid render target should return `400 Bad Request`

--- server/controllers/finance/reports/invoiceReceipt.js
+++ server/controllers/finance/reports/invoiceReceipt.js
@@ -23,6 +23,13 @@ var Invoices    = require('../patientInvoice');
 var Patients    = require('../../medical/patient');
 var Enterprises = require('../../admin/enterprises');
 
+var supportedRender = {};
+
+// currently supports only JSON rendering 
+supportedRender.json = require('../../../lib/renderers/json');
+
+const defaultRender = 'json';
+
 const FLAG_TRUE = 1;
 const SUCCESS_STATUS = 200;
 
@@ -55,23 +62,36 @@ function build(req, res, next) {
   var queryString = req.query;
   var invoiceUuid = req.params.uuid;
   var enterpriseId = req.session.enterprise.id;
-
+  
   var invoiceResponse = {};
+  
+  var renderTarget = queryString.render || defaultRender;
+  var render = supportedRender[renderTarget];
+
+  /** @todo delegate to additional method */
+  if (_.isUndefined(render)) { 
+    throw new BadRequest('Render target provided is invalid or not supported by this report '.concat(renderTarget));
+  }
 
   /** @todo Implement minimal flag */
-  if (queryString.minimal === FLAG_TRUE) {};   
-  
+  if (queryString.minimal === FLAG_TRUE) {}
+
   reportData(invoiceUuid)
-    .then(function (result) { 
-      var recipientUuid = result.patient_uuid;
-      _.extend(invoiceResponse, result);
+    .then(function (reportResult) { 
+      var recipientUuid = reportResult.patient_uuid;
+      _.extend(invoiceResponse, reportResult);
       
       return headerData(recipientUuid, enterpriseId);
     })
-    .then(function (result) { 
-      _.extend(invoiceResponse, result);
-
-      res.status(SUCCESS_STATUS).send(invoiceResponse);
+    .then(function (headerResult) { 
+      _.extend(invoiceResponse, headerResult);
+    
+      return render(invoiceResponse);
+    })
+    .then(function (renderedResult) { 
+  
+      // send the final (rendered) object to the client
+      res.status(SUCCESS_STATUS).send(renderedResult);
     })
     .catch(next)
     .done();
