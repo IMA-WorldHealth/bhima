d570cb6980cf35389c80290522307f21ef5f0b72
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-04-05 18:24:51 +0100

    Seperated modal and receipt infrastructure/ tmpl.
    Moved all modal logic out of template value - template value is now
    passed into modal controller (if required) on settup.
    Modal support for 'pdf' and 'json aadded to modal infrastructure
    `pdf`   - <object> tag populated with receipt data (loaded via ng-src)
    `json`  - relevent receipt template passed to modal controller, this is
    loaded via ng-include

--- client/src/js/services/receipts/ReceiptModal.js
+++ client/src/js/services/receipts/ReceiptModal.js
@@ -20,6 +20,7 @@ function ReceiptModal(Modal, Receipts) {
   var service = this;
   
   var receiptController = 'ReceiptModalController as ReceiptCtrl';
+  var receiptTemplate   = '/js/services/receipts/modal/receiptModal.tmpl.html';
   var receiptSize       = 'md';
   var receiptBackdrop   = 'static';
   var animateReceipt    = false;
@@ -30,18 +31,20 @@ function ReceiptModal(Modal, Receipts) {
 
     /** @todo Discuss if these should be overridable from the controller or if the config should be set here */
     var renderTarget      = 'json';
-    var invoiceTemplate   = '/partials/patient_invoice/receipt/invoice.receipt.tmpl.html';
+    var invoiceTemplate   = 'partials/patient_invoice/receipt/invoice.receipt.tmpl.html';
     var invoiceRequest    = Receipts.invoice(uuid, { render : renderTarget });
   
     /** @todo The template passed in should be the /services/modal template which will transclude the invoice template */
     var instance = Modal.open({
-      templateUrl : invoiceTemplate,
+      templateUrl : receiptTemplate,
       controller  : receiptController,
       size        : receiptSize,
       backdrop    : receiptBackdrop,
       animation   : animateReceipt,
       resolve     : { 
-        receipt : function receiptProvider() { return { promise : invoiceRequest }; }
+        receipt       : function receiptProvider() { return { promise : invoiceRequest }; },
+        template      : function templateProvider() { return invoiceTemplate; },
+        render        : function renderProvider() { return renderTarget; }
       }
     });
 
