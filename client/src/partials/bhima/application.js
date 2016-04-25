b916a57973733a59ac1532bbc17cd71aa7ac2cfb
Jonathan Niles <jonathanwniles@gmail.com>
2016-04-25 11:16:57 +0100

    chore: remove appstate
    This commit removes appstate.  It also removes some of the older
    directives/filters that used it and are no longer used or relevant in
    bhima-2.X.
    
    Closes #304.

--- client/src/partials/bhima/application.js
+++ client/src/partials/bhima/application.js
@@ -2,18 +2,17 @@ angular.module('bhima.controllers')
 .controller('ApplicationController', ApplicationController);
 
 ApplicationController.$inject = [
-  '$timeout', 'AppCache', 'appstate', 'connect', 'util',
-  'SessionService', 'LanguageService', '$state'
+  'AppCache', 'SessionService', 'LanguageService', '$state'
 ];
 
-function ApplicationController($timeout, AppCache, appstate, connect, util, Session, Languages, $state) {
+function ApplicationController(AppCache, Session, Languages, $state) {
   var vm = this;
 
   // load in the application cache
   var cache = AppCache('preferences');
 
   // Default sidebar state
-  /** @todo Load sidebar state before angular is bootstraped to remove 'flicker' */
+  /** @todo Load sidebar state before angular is bootstrapped to remove 'flicker' */
   vm.sidebarExpanded = false;
 
   // set up the languages for the application, including default languages
@@ -21,67 +20,13 @@ function ApplicationController($timeout, AppCache, appstate, connect, util, Sess
   Languages.read(true);
 
   vm.isLoggedIn = function isLoggedIn() {
-    return Session.user;
+    return !!Session.user;
   };
 
+  // toggle session settings
   if (vm.isLoggedIn()) {
     vm.sidebarExpanded = cache.sidebar && cache.sidebar.expanded;
-  }
-
-  // on refresh, if we have a session load the rest of the state
-  if (vm.isLoggedIn()) { loadState(); }
-
-  // on login, load the state
-  appstate.register('login', function (bool) {
-    if (bool) { loadState(); }
-  });
-
-  // loads dependencies used by the application during runtime
-  //   FiscalYear
-  //   ExchangeRate
-  //   Currencies
-  // Also contains a hack to make sure the appstate has the correct
-  // enterprise, user, and project from SessionService
-  function loadState() {
-    var currencies, exchangeRate, fiscalYear;
-
-    fiscalYear = {
-      'tables' : {
-        'period' : { 'columns' : ['id', 'start_date', 'end_date', 'fiscal_year_id'] },
-        'fiscal_year' : { 'columns': ['label', 'start_date', 'previous_fiscal_year_id', 'enterprise_id'] }
-      },
-      join : ['period.fiscal_year_id=fiscal_year.id'],
-      where : ['period.start_date<=' + util.sqlDate(), 'AND', 'period.end_date>=' + util.sqlDate()]
-    };
-
-    // set appstate variables
-    setEnvironmentVariable('fiscalYears', fiscalYear);
-
-    // FIXME
-    // set DEPRECATED appstate variables until we can change them
-    // throughout the application.
-    appstate.register('fiscalYears', function (data) {
-      var currentFiscal = data[0];
-      if (currentFiscal) {
-        currentFiscal.period_id = currentFiscal.id;
-        currentFiscal.id = currentFiscal.fiscal_year_id;
-        appstate.set('fiscal', currentFiscal);
-      }
-    });
-
-    // Optionally expand sidebar
-    // vm.sidebarExpanded = true;
     vm.projectName = Session.project.name;
-  }
-
-  // utility function to set appstate() variables
-  function setEnvironmentVariable(key, data) {
-    connect.fetch(data)
-    .then(function (values) {
-      $timeout(function () {
-        appstate.set(key, values);
-      });
-    });
   }
 
   /**
