4edac07c86513116600e32b17ec7bdee57cf41a4
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-06-08 16:35:13 +0100

    Display recent check ins - client module
    This commit adds business logic that determines if the patient has
    recently bene checked in and ensures the view displays the correct
    warnings.

--- client/src/partials/patients/record/units/checkin.js
+++ client/src/partials/patients/record/units/checkin.js
@@ -1,21 +1,42 @@
 angular.module('bhima.controllers')
 .controller('CheckInController', CheckInController);
 
-CheckInController.$inject = ['$stateParams', 'PatientService'];
+CheckInController.$inject = ['$stateParams', 'moment', 'PatientService'];
 
-function CheckInController($stateParams, Patients) { 
+function CheckInController($stateParams, moment, Patients) { 
   var vm = this;
   var id = $stateParams.patientID;
 
   // total number of visits to display in the 'Recent Visits' component
-  var limitVisitsDisplay = 2;
-
+  var limitVisits = 3;
+  
+  vm.limitVisitDisplay = 3;
+  
+  // warn a user after x number of days 
+  /** @todo this number and logic should be defined as a global constant that drives all visit logic */
+  var visitDurationWarnLimit = 5;
+   
   vm.loaded = false;
   
-  Patients.Visits.read(id, {limit : limitVisitsDisplay})
+  Patients.Visits.read(id, {limit : limitVisits})
     .then(function (visits) {
       vm.loaded = true;
       vm.visits = visits; 
+    
+      // exit early if there are no patient visits
+      if (!visits.length) {
+        vm.outdatedVisit = true;
+        return;
+      }
+      
+      // check how long ago the most recent patient visit was
+      vm.latestCheckIn = moment().diff(visits[0].start_date, 'days');
+
+      // check to see if the latest visit was before the duration warn limit
+      if (visitDurationWarnLimit - vm.latestCheckIn <= 0) {
+        vm.limitVisitDisplay = 2;
+        vm.outdatedVisit = true;
+      }
     });
   
 }
