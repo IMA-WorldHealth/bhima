aab37ab62cd71d39bba617b93d61ebc2c2297bc8
Jonathan Niles <jonathanwniles@gmail.com>
2016-06-21 10:10:02 +0100

    docs(bhFiltersApplied): improve directive docs
    This commit adds doc comments to the bhFiltersApplied directive for
    better readability and developer productivity in the future.

--- client/src/js/components/bhFiltersApplied.js
+++ client/src/js/components/bhFiltersApplied.js
@@ -10,6 +10,35 @@ angular.module('bhima.components')
 
 bhFiltersAppliedController.$inject = [ '$scope', '$filter' ];
 
+/**
+ * @class bhFiltersApplied
+ *
+ * @description
+ * The bhFiltersApplied directive watches the component boundary for changes in
+ * the filters passed in. Filters appear as a list of JSON objects, with a
+ * similar API to ui grid.  At minimum, they must contain:
+ *  1. displayName (this will be passed through $translate as the column name)
+ *  2. value
+ * They can optionally contain:
+ *  1. comparitor (usually ">", "<", etc)
+ *  2. filter (an angular or custom filter to apply to the value)
+ *
+ * When the filters change, the values are re-digested.  Each filter also has an
+ * "X" button, which simply calls `onRemoveFilter()`.  It is up to the parent
+ * controller to determine what should happen in this case.
+ *
+ * @example
+ * var filters = [
+ *  { displayName : 'SOME.COLUMN', value: 5 },
+ *  { displayName : 'SOME.DATE.COLUMN', value : '06/12/1993', filter: 'moment' }
+ * ];
+ *
+ * // in the HTML, it will look something like:
+ * // <bh-filters-applied
+ * //  filters="SomeCtrl.filters"
+ * //  on-remove-filter="SomeCtrl.removeFilter">
+ * // </bh-filters-applied>
+ */
 function bhFiltersAppliedController($scope, $filter) {
 
   $scope.$watch('$ctrl.filters', formatViewValues);
