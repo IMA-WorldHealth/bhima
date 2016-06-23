ddaa6c25e8a8508cdc1728151068be104a79a61c
Steven Fountain <stevenfountain@imaworldhealth.org>
2016-06-23 11:49:22 +0100

    Re: AccountGrid service; guarantee account order
    This commit re-factors the account grid service to remove the dependency
    on accounts being sorted correctly. By always formatting/ using a single
    store for accounts it does not matter if data is passed in from a cache
    or the server. Simply sorting the account list also simplifies a lot of
    the display logic.
    
    * Re-factor AccountGrid Service
    * Clean up Account Management controller, removing unneeded dependencies
    * Moved grid templates into individual files

--- client/src/partials/accounts/AccountGrid.js
+++ client/src/partials/accounts/AccountGrid.js
@@ -3,34 +3,26 @@ angular.module('bhima.services')
 
 AccountGridService.$inject = ['AccountStoreService', 'AccountService', 'Store'];
 
-function AccountGridService(AccountStore, Accounts, Store, Notify) { 
-  /** @todo import from constants */
-  var ROOT_ACCOUNT = 0;
-
+function AccountGridService(AccountStore, Accounts, Store, Notify) {
   /**
    * @constructor
-   * 
+   *
    * @description
    * @todo sets up the account grid
    */
   function AccountGrid() {
-    // track service state - these variables can be exposed to drive the UI if required
-    this._initialised = false;
-    
     // store that will be used to hold accounts
     this._cachedAccountList = null;
-    
-    // exposed variables and methods 
+
+    // exposed variables and methods
     this._store = null;
-    this.data = null; 
-    
-    console.log(this);
+    this.data = null;
   }
 
   /**
-   * @method settup 
-   * 
-   * @description 
+   * @method settup
+   *
+   * @description
    * Requests the latest account list from the AccountStore service and updates loading variables
    */
   AccountGrid.prototype.settup = function settup() {
@@ -40,99 +32,67 @@ function AccountGridService(AccountStore, Accounts, Store, Notify) {
       .then(function (result) {
         this._cachedAccountList = angular.copy(result.data);
         this._store = result;
-        this._initialised = true;
 
         // order and expose data made available through the store
-        this.data = Accounts.order(this._store.data);
+        // this.data = Accounts.order(this._store.data);
+        this.formatStore();
+
+        // update exposed store driven data
+        this.data = angular.copy(this._store.data);
       }.bind(this));
   };
 
-  /**
-   * @method updateViewInsert 
-   * 
-   * @description
-   * This method updates the models that drive a UI grid based on an account object passed in by the Account 
-   * creation process.
-   */
+  AccountGrid.prototype.formatStore = function formatStore() {
+    // sort underlying data to ensure it is ordered by number - this is how it
+    // is sent from the server by default
+    this._store.data.sort(orderByNumber);
+
+    // ensure data respects groups
+    this._store.data = Accounts.order(this._store.data);
+
+    this._store.recalculateIndex();
+  };
+
+  function orderByNumber(a, b) {
+    return a.number - b.number;
+  }
+
+  AccountGrid.prototype.insertDifference = function insertDifference(account, index) {
+    this.data.splice(index, 0, account);
+  }
+
   AccountGrid.prototype.updateViewInsert = function updateViewInsert(event, account) {
-    var insertAccount;
-    
-    // ensure received account is formatted as accounts recieved from the database 
+    var insertedIndex;
+
     account.number = Number(account.number);
     account.hrlabel = Accounts.label(account);
 
     this._store.post(account);
-    updateCachedAccountList(this._cachedAccountList, account);
-    recalculateAccountChildren(this._store, account);
-  
-    // getting an account details relative to the latest ordering and inserting it allows us to respect the current 
-    // expand/ collapse tree view (as apposed to simply setting new data)
-    insertAccount = getNestedAccountDetails(this._cachedAccountList, account);
-    this.data.splice(insertAccount.index, 0, insertAccount.details);
+
+    this.formatStore();
+    insertedIndex = this._store.index[account.id];
+    this.insertDifference(account, insertedIndex);
   };
-  
+
   AccountGrid.prototype.updateViewEdit = function updateViewEdit(event, account) {
-    
-  };
+    var storeRecord = this._store.get(account.id);
+    var parentHasChanged = account.parent !== storeRecord.parent;
 
-  /**
-   * @method updateCachedAccountList
-   *  
-   * @description 
-   * Insert a provided account into the currently stored cached list of accounts 
-   * 
-   * @param list    {Array}
-   * @param account {Object}
-   */
-  function updateCachedAccountList(list, account) {
-    // naive brute force search - this should only be run once per major page update, if this results in performance 
-    // issues either a faster search or a data structure more suited to inserts can be used
-    var insertIndex = list.length;
-    list.some(function (item, index) {
-      if (Number(item.number) > account.number) { 
-        insertIndex = index;
-        return true;
-      }
-    });
-    
-    list.splice(insertIndex, 0, account);
-  }
+    account.hrlabel = Accounts.label(account);
+    angular.extend(storeRecord, account);
 
-  /**
-   * @method recalculateAccountChildren
-   *
-   * @param accounts  {Object}  Store object
-   * @param account   {Object}  Account details
-   */
-  function recalculateAccountChildren(accounts, account) { 
-    if (account.parent !== ROOT_ACCOUNT) { 
-      var parent = accounts.get(account.parent);
-      
-      parent.children = parent.children || [];
-      parent.children.push(account);
+    if (parentHasChanged) {
+      this.formatStore();
+
+      // this will invalidate the any references to the services data up to this point
+      this.data = angular.copy(this._store.data);
+      return true;
     }
-  }
-  
-  /**
-   * @method getNestedAccountDetails 
-   * 
-   * @param list {Array} Flat list of account as returned from the server
-   * 
-   * @returns {Object} Object with an index and account details
-   */
-  function getNestedAccountDetails(list, account) {
-    var nestedAccount = {};
-    var orderedList = Accounts.order(angular.copy(list));
 
-    orderedList.some(function (item, index) {
-      if (item.id === account.id) { 
-        nestedAccount.index = index;
-        nestedAccount.details = item;
-        return true;
-      }
-    })
-    return nestedAccount;
-  }
+    // only semantic information about the account has changed - override the current account
+    this.data.splice(this._store.index[account.id], 1, storeRecord);
+    return false;
+  };
 
   return AccountGrid;
 }
--- client/src/partials/accounts/accounts.js
+++ client/src/partials/accounts/accounts.js
@@ -1,20 +1,11 @@
-/** @todo Cache accounts on download and expose store to add new and updated accounts */
-/** @todo Accounts should be sorted whilst strictly in the tree structure by account ID */
-/** @todo Investigate if there is a better way of passing data back to the parent other than $scope */
-/** @todo if a record cannot be found it still shows the form */
 /** @todo there are performance issues on this page - this should be because of  row/ cell templates, informally investigate */
-/** @todo known limitation - as only the current account is updated, a reordering of numbers is not performed on update */
 /** @todo you should not be able to change the type of a title account with children */
 /** @todo you cannot update the number or type of an account */
 /** @todo business logic for deleting an account */
 
-/** @todo move underlying account data into AccountList service that can be tested */
-
-/** @todo accounts expand even if the underlying data is not reset */
-
 /**
  * Business rules implemented
- * 1. you cannot update an account number 
+ * 1. you cannot update an account number
  * 2. you cannot update an account type
  * 3. you can only delete an ccount if it is unused
  */
@@ -22,127 +13,68 @@ angular.module('bhima.controllers')
 .controller('AccountsController', AccountsController);
 
 AccountsController.$inject = [
-  '$rootScope', '$state', 'AccountGridService', 'AccountStoreService', 'AccountService', 'NotifyService'
+  '$rootScope', 'AccountGridService', 'NotifyService'
 ];
 
-/**
- * Note : all flattening and depth display depends on account order, if a reorder or filter is performed this 
- * may need to be recalculated
- */
-function AccountsController($rootScope, $state, AccountGrid, AccountStore, Accounts, Notify) { 
-  
+function AccountsController($rootScope, AccountGrid, Notify) {
   var vm = this;
-  var initialRequestCache; 
-  
-  vm.targetId = $state.params.id; 
-  
-  vm.Accounts = new AccountGrid();
-  
-  vm.Accounts.settup()
-    .then(bindGridData)
-    .catch(Notify.handleError);
-  
-  function bindGridData() { 
-    vm.gridOptions.data = vm.Accounts.data;
-  }
-  
+
   /** @todo get this from constant definition */
   vm.TITLE_ACCOUNT = 4;
   vm.ROOT_ACCOUNT = 0;
-  
-  var leafRowTemplate = `
-      <div
-        ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid"
-        ui-grid-one-bind-id-grid="rowRenderIndex + '-' + col.uid + '-cell'"
-        class="ui-grid-cell"
-        ng-class="{ 'ui-grid-row-header-cell': col.isRowHeader, 'text-clear' : row.entity.type_id !== grid.appScope.TITLE_ACCOUNT }"
-        data-vals="{{col.isRowHeader}}"
-        role="{{col.isRowHeader ? 'rowheader' : 'gridcell'}}"
-        ui-grid-cell>
-       </div>
-  `;
-  
-  var indentCellTemplate = '<div class="ui-grid-cell-contents"><span ng-click="grid.api.treeBase.toggleRowTreeState(row)" ng-class="{\'text-action\' : row.treeNode.children.length > 0}"> <span style="padding-left : {{row.treeLevel * 20}}px;"></span><i ng-if="row.entity.locked" class="fa fa-lock"></i> {{grid.getCellValue(row, col)}}</span> <a ng-if="row.entity.type_id === grid.appScope.TITLE_ACCOUNT" ui-sref="accounts.create({ parentId : row.entity.id })"> <i class="fa fa-plus-square-o"></i> Add child account</a></div>';
-  var actionsCellTemplate = '<div class="ui-grid-cell-contents"><a ui-sref="accounts.edit({id:row.entity.id})"><i class="fa fa-edit"></i> Edit {{row.entity.number}}</a></div>'; 
-  
-  var initialDataSet = true;
-  
+
+  vm.indentTitleSpace = 20; // indent value in pixels
+  vm.initialDataSet = true;
+
+  vm.Accounts = new AccountGrid();
+
+  vm.Accounts.settup()
+    .then(bindGridData)
+    .catch(Notify.handleError);
+
+  var columns = [
+    { field : 'number', displayName : '', cellClass : 'text-right', width : 70},
+    { field : 'label', displayName : 'FORM.LABELS.ACCOUNT', cellTemplate : '/partials/accounts/templates/grid.indentCell.tmpl.html', headerCellFilter : 'translate' },
+    { name : 'actions', displayName : '', cellTemplate : '/partials/accounts/templates/grid.actionsCell.tmpl.html', headerCellFilter : 'translate', width : 140 }
+  ];
+
   vm.gridOptions = {
     appScopeProvider : vm,
     enableSorting : false,
-    // enableGroupHeaderSelection : true
     showTreeExpandNoChildren : false,
     enableColumnMenus : false,
-    // showHeader : true,
-    rowTemplate : leafRowTemplate,
-    onRegisterApi : function (api) {
-      
-      api.grid.registerDataChangeCallback(function (grid) {
-        if (initialDataSet && grid.options.data.length) {
-          api.treeBase.expandAllRows();
-          initialDataSet = false;
-        }
-      });
-    }
+    rowTemplate : '/partials/accounts/templates/grid.leafRow.tmpl.html',
+    onRegisterApi : registerAccountEvents,
+    columnDefs : columns
   };
-  
-  var columns = [
-    { field : 'number', displayName : '', cellClass : 'text-right', width : 70},
-    { field : 'label', displayName : 'FORM.LABELS.ACCOUNT', cellTemplate : indentCellTemplate, headerCellFilter : 'translate' },
-    { name : 'actions', displayName : '', cellTemplate : actionsCellTemplate, headerCellFilter : 'translate', width : 140 }
-  ];
-  
-  vm.gridOptions.columnDefs = columns;
-  
-  // insert flat 
-  // build order tree 
-  // insert live - this maintains the expand/ collapse state of the current tree
-  
-  // 1. insert account into correct order in the data - accounts are ordered by number implicitly when returned from the server
-  // 2. build an account tree of new data set with the new account - this will ensure the hierachy is respected
-  // 3. set the grids data to the newly generated flat list
 
-  // assume the only entities that can be updated here are account text (name - label) and parent
-  function updateViewEdit(event, account) { 
-    AccountStore.store().then(function (result) { 
-      var t = result.get(account.id);
-    
-      // update grouping - only if this has changed, support this? potentially just set data and refresh the view
-      // forces grid set data refresh
-      if (t.parent !== account.parent) { 
-        
-  
-        initialDataSet = true;
-        /** @fixme change this to using a store */
-        initialRequestCache.some(function (item, index) {
-          if (item.id === account.id) { 
-            angular.extend(item, account);
-            delete item.$$treeLevel;
-            delete item.children;
-            return true;
-          } 
-          return false;
-        });
-        vm.gridOptions.data = Accounts.order(angular.copy(initialRequestCache));
-
-        console.log('parent changed - refreshing view');
-      } else {
-        console.log('parent not changed, simply update attributes');
-      }
-
-      // update stored attributes 
-      angular.extend(t, account);
-    });
-  }
-  
-  // because the modal is instantiated on onEnter in the ui-router configuration the 
-  // $parent $scope for the modal is $rootScope, it is impossible to inject the $scope of the 
+  // because the modal is instantiated on onEnter in the ui-router configuration the
+  // $parent $scope for the modal is $rootScope, it is impossible to inject the $scope of the
   // parent state into the onEnter callback. for this reason $rootScope is used for now
-  
-  // this is implemented because it this is a very heavy page to force an entire reload
-  
-  // AccountGrid.insertAccount
-  // AccountGrid.updateAccount
   $rootScope.$on('ACCOUNT_CREATED', vm.Accounts.updateViewInsert.bind(vm.Accounts));
-  $rootScope.$on('ACCOUNT_UPDATED', updateViewEdit);
+  $rootScope.$on('ACCOUNT_UPDATED', handleUpdatedAccount);
+
+  function handleUpdatedAccount(event, account) {
+    var forceRefresh = vm.Accounts.updateViewEdit(event, account);
+
+    if (forceRefresh) {
+      vm.initialDataSet = true;
+      bindGridData();
+    }
+  }
+
+  function registerAccountEvents(api) {
+    api.grid.registerDataChangeCallback(expandOnSetData);
+  }
+
+  function expandOnSetData(grid) {
+    if (vm.initialDataSet && grid.options.data.length) {
+      grid.api.treeBase.expandAllRows();
+      vm.initialDataSet = false;
+    }
+  }
+
+  function bindGridData() {
+    vm.gridOptions.data = vm.Accounts.data;
+  }
 }
