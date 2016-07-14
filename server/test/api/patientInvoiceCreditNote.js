242f0636a524c1da0a9df72a9058a3b4bdcda50b
Chris Lomame <lomamech@gmail.com>
2016-07-14 05:24:33 +0200

    Sanitaze test integration for Patient Credit Note

--- server/test/api/patientInvoiceCreditNote.js
+++ server/test/api/patientInvoiceCreditNote.js
@@ -16,11 +16,9 @@ describe('(/Journal) Credit notes to reverse invoice transactions', function () 
     return agent.post('/journal/' + fetchableInvoiceUuid + '/reverse')
       .send({ description : 'Credit Note', type_id: 9 })
       .then(function (res) {
-        expect(res);
         return agent.get('/invoices/'.concat(fetchableInvoiceUuid));
       })
       .then(function (res){
-
         expect(res).to.have.status(200);
         expect(res.body.type_id).to.equal(9);
       })
