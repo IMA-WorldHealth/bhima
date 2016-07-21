874d76d028389436a04fa971fed4c647bec06561
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2016-07-21 11:38:32 +0100

    fix(test): fix failing e2e patient uploads
    This commit fixes the failing patient upload tests by making sure path
    resolution works locally on Linux.  It should work properly on windows
    systems as well.
    
    It also implements FU.hasText() to check if an element contains given
    text.  This helps clean up the tests nicely.
    
    Finally, it catches some invalid assertions (`element.isPresent()`
    without the expect) and ensures they are valid and pass.

--- client/test/e2e/patient/picture.spec.js
+++ client/test/e2e/patient/picture.spec.js
@@ -12,24 +12,22 @@ describe('Patient Record', function () {
   const root = '#/patients/';
   const id = '274c51ae-efcc-4238-98c6-f402bfb39866';
 
-
   const path = root.concat(id);
   before(() => helpers.navigate(path));
 
-  it('Upload a patient Picture', function () {
-    var fileToUpload = '../../../../server/test/api/data/patient.png',
-      absolutePath = paths.resolve(__dirname, fileToUpload);
-    
+  it('uploads a patient picture', function () {
+    const fileToUpload = '../../../../server/test/api/data/patient.png';
+    const absolutePath = paths.resolve(__dirname, fileToUpload);
+
     element.all(by.css('input[type=file]')).get(0).sendKeys(absolutePath);
     components.notification.hasSuccess();
   });
 
-  it('Unable to Upload a file who aren\'t a picture', function () {
-    var fileToUpload = '../../../../server/test/api/data/sample.pdf',
-      absolutePath = paths.resolve(__dirname, fileToUpload);
-    
+  it('unable to upload a file that is not a picture', function () {
+    const fileToUpload = '../../../../server/test/api/data/sample.pdf';
+    const absolutePath = paths.resolve(__dirname, fileToUpload);
+
     element.all(by.css('input[type=file]')).get(0).sendKeys(absolutePath);
     components.notification.hasDanger();
   });
-
 });
