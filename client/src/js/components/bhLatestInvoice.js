73d1f321faa5725b0f9af67ea89418153fc0f938
Chris Lomame <lomamech@gmail.com>
2016-08-18 09:12:47 +0200

    Sanitaze code Adding moment in $inject Remove unused Variable

--- client/src/js/components/bhLatestInvoice.js
+++ client/src/js/components/bhLatestInvoice.js
@@ -4,34 +4,24 @@ angular.module('bhima.components')
   controllerAs : '$ctrl',
   templateUrl  : 'partials/templates/bhLatestInvoice.tmpl.html',
   bindings: {
-    enablePatientDetails : '<',  // bind boolean (true|false) : Enable patient details option
-    enableOptionBar      : '<',  // bind boolean (true|false) : Enable option for add, display list or display thumbnail in a bar
-    enableSearch         : '<',  // bind boolean (true|false) : Enable search bar option
-    display              : '@',  // bind (list|thumbnail)  : Display either in list or thumbnail mode
-    height               : '@',  // bind the height of list of contents
     debtorUuid          : '<'  // Required patient uuid
   }
 });
 
 LatestInvoice.$inject = [
-  'PatientService', 'NotifyService'
+  'PatientService', 'moment'
 ];
 
 /**
  * Find Document Component
  * This component is responsible for displaying the Latest Invoice 
  */
-function LatestInvoice(Patient, Notify) {
+function LatestInvoice(Patient, moment) {
   var vm = this;
 
   /** global variables */
   vm.session = {
     debtorUuid     : this.debtorUuid,
-    enableOptionBar : Boolean(this.enableOptionBar),
-    enableSearch    : Boolean(this.enableSearch),
-    display         : this.display,
-    height          : this.height,
-    showAction      : false
   };
 
   // startup the component
@@ -45,8 +35,7 @@ function LatestInvoice(Patient, Notify) {
     .then(function (patient) {
       vm.session.patientInvoice = patient;          
       vm.session.patientInvoice.durationDays = moment().diff(vm.session.patientInvoice.date, 'days');
-    })
-    .catch(Notify.handleError);
+    });
   }
 
 }
