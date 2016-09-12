3656eff75716928d95bb3a59784347f888a21a24
DedrickEnc <kitamuka@gmail.com>
2016-09-12 13:33:24 +0100

    ipdating the end to end test

--- test/end-to-end/journal/trial_balance/trialBalance.config.js
+++ test/end-to-end/journal/trial_balance/trialBalance.config.js
@@ -8,7 +8,6 @@ helpers.configure(chai);
 
 const JournalCorePage = require('../journal.page.js');
 const TrialBalancePage = require('./trialBalance.page.js');
-const ErrorViewPage = require('./errorView.page.js');
 const GeneralLedgerPage = require('../../general_ledger/generalLedger.page.js');
 
 function TrialBalanceTest(){
@@ -17,7 +16,6 @@ function TrialBalanceTest(){
   const journal = new JournalCorePage();
   const trialBalance = new TrialBalancePage();
   const generalLedger = new GeneralLedgerPage();
-  const errorModal = new ErrorViewPage();
   const path = '#/journal';
 
   // This will be run before every single test ('it') - navigating the browser to the correct page.
@@ -33,27 +31,38 @@ function TrialBalanceTest(){
   it('it should consider that the related transaction is selected even if you selected just one row', function () {
     journal.checkRow(1);
     journal.openTrialBalanceModal();
-    expect(trialBalance.getLineCount()).to.eventually.equal(3);
+    expect(trialBalance.getLineCount()).to.eventually.equal(2);
     trialBalance.closeTrialBalance();
   });
 
   it('it should switch the view successfully', function () {
     journal.checkRow(0);
     journal.openTrialBalanceModal();
-    expect(trialBalance.getLineCount()).to.eventually.equal(3);
-    trialBalance.switchView(); //from group by transaction to account
     expect(trialBalance.getLineCount()).to.eventually.equal(2);
     trialBalance.switchView(); //from group by account to transaction
     expect(trialBalance.getLineCount()).to.eventually.equal(3);
+    trialBalance.switchView(); //from group by transaction to account
+    expect(trialBalance.getLineCount()).to.eventually.equal(2);
+    trialBalance.viewErrorList(); //will print error grid
+    expect(trialBalance.getLineCount()).to.eventually.equal(2);
+    trialBalance.resetView(); //back to the group by account view
+    expect(trialBalance.getLineCount()).to.eventually.equal(2);
+    trialBalance.showAccountDetailInTransaction(1); //will print list of transaction relative to selected account line(index 0 in occurrence)
+    expect(trialBalance.getLineCount()).to.eventually.equal(3);
+    trialBalance.resetView();
     trialBalance.closeTrialBalance();
   });
 
-  it('it should display the error modal successfully', function () {
-    journal.checkRow(1);
+  it('it should remember the last main view visited', function () {
+    journal.checkRow(0);
     journal.openTrialBalanceModal();
-    trialBalance.openErrorModal();
-    expect(errorModal.getLineCount()).to.eventually.equal(2);
-    errorModal.closeErrorModal();
+    expect(trialBalance.getLineCount()).to.eventually.equal(2);
+    trialBalance.switchView(); //list data by transaction
+    expect(trialBalance.getLineCount()).to.eventually.equal(3);
+    trialBalance.viewErrorList();
+    trialBalance.resetView();
+    expect(trialBalance.getLineCount()).to.eventually.equal(3); //make sure we go back to the transaction view
+    trialBalance.closeTrialBalance();
   });
 }
 
