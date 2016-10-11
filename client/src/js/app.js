7229375a33221f3066690c22ae610f7153ed3d16
Dedrick Kitamuka <kitamuka@gmail.com>
2016-10-11 11:59:52 +0100

    completing translation and isolate debtor group routes (#797)
    * creatng a separate file for debtor and debtor group routes
    
    * update journal.routes file
    
    * completing translation
    
    * changing create/new route to /create

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -107,18 +107,6 @@ function bhimaConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvi
     controller : 'CountryController as CountryCtrl',
     templateUrl: 'partials/locations/country/country.html'
   })
-
-
-  /* journal routes */
-
-  .state('journalPrint', {
-    controller : 'journal.print',
-    templateUrl : 'partials/journal/print.html'
-  })
-  .state('journalVoucher', {
-    controller: 'JournalVoucherController as JournalVoucherCtrl',
-    templateUrl: 'partials/journal/voucher/voucher.html'
-  })
   .state('simpleVouchers', {
     url : '/vouchers/simple',
     controller: 'SimpleJournalVoucherController as SimpleVoucherCtrl',
@@ -142,38 +130,7 @@ function bhimaConfig($stateProvider, $urlRouterProvider, $urlMatcherFactoryProvi
     templateUrl : 'partials/general_ledger/general_ledger.html'
   })
 
-  /* debtors routes */
-  .state('debtorGroups', {
-    url : '/debtors/groups/:uuid',
-    abstract : true,
-    params : {
-      uuid : { squash : true, value : null }
-    },
-    controller : 'DebtorGroupController as GroupCtrl',
-    templateUrl: 'partials/debtors/groups.html'
-  })
-
-    .state('debtorGroups.list', {
-      url : '',
-      templateUrl : 'partials/debtors/groups.list.html'
-    })
-
-    .state('debtorGroups.create', {
-
-      // setting the URL as simply create mathces as a :uuid - there should be a way to set orders
-      // this should ideally route to /create
-      url : '/create/new',
-      templateUrl : 'partials/debtors/groups.edit.html',
-      controller : 'DebtorGroupCreateController as GroupEditCtrl'
-    })
-    .state('debtorGroups.update', {
-      url : '/update',
-      templateUrl : 'partials/debtors/groups.edit.html',
-      controller : 'DebtorGroupUpdateController as GroupEditCtrl',
-      data : { label : null }
-    })
-
-
+  
   /* references routes */
 
   .state('references', {
