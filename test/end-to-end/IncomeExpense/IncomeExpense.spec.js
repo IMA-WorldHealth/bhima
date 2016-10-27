1a85b4ba7923b9a4a14838689aa376fd0cab210a
Chris Lomame <lomamech@gmail.com>
2016-10-27 10:01:24 +0200

    Adding test E2E for report of Incomes ans Expenses

---
+++ test/end-to-end/IncomeExpense/IncomeExpense.spec.js
@@ -0,0 +1,32 @@
+/* global browser, element, by */
+
+const chai = require('chai');
+const expect = chai.expect;
+
+const components = require('../shared/components');
+const FU = require('../shared/FormUtils');
+const helpers = require('../shared/helpers');
+helpers.configure(chai);
+
+describe('Income Expense report generator', () => {
+  'use strict';
+
+  before(() => helpers.navigate('#/finance/reports/incomeExpense'));
+
+  it('GET /finance/incomeExpense return incomeExpense report at the client', () => {
+
+    // set report configurations
+    // date interval component admit date in this format : dd/MM/yyyy
+    components.dateInterval.range('01/01/2016', '31/12/2016');
+    FU.select('IncomeExpenseConfigCtrl.cashbox', 'Cashbox A $');
+    FU.select('IncomeExpenseConfigCtrl.report', 'Recettes et dépenses');
+
+    // focus on the button zone
+    let area = element(by.css('[data-submit-area]'));
+    area.element(by.css('[data-method="submit"]')).click();
+
+    // Income Expense report
+    FU.exists(by.id('income'), true);
+    FU.exists(by.id('expense'), false);
+  });
+});
