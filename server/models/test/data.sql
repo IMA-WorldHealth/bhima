2954f043bdfd3f4ecb120d4a1a87d5e63f14c383
Chris Lomame <lomamech@gmail.com>
2016-10-28 10:02:08 +0200

    Resolve conflict of duplicate key

--- server/models/test/data.sql
+++ server/models/test/data.sql
@@ -52,7 +52,7 @@ INSERT INTO unit VALUES
   (148, 'Chart of Accounts', 'REPORT.CHART_OF_ACCOUNTS', 'The COA Report', 144, '/partials/finance/chart_of_accounts', '/reports/accounts_chart'),
   (146, 'Creditor Groups Management', 'TREE.CREDITOR_GROUP', 'Creditor Groups Management module', 1, '/partials/admin/creditor_groups/', '/admin/creditor_groups'),
   (147, 'Cash Payment Registry', 'TREE.CASH_PAYMENT_REGISTRY', 'Cash Payment Registry', 5, '/partials/finance/reports/cash_payemnt', '/finance/reports/cash_payment'),
-  (148, 'Income Expenses', 'TREE.INCOME_EXPENSE', 'The Report of income and expenses', 144, '/partials/finance/incomeExpense', '/finance/reports/incomeExpense');
+  (149, 'Income Expenses', 'TREE.INCOME_EXPENSE', 'The Report of income and expenses', 144, '/partials/finance/incomeExpense', '/finance/reports/incomeExpense');
 
 -- Reserved system account type
 INSERT INTO `account_type` VALUES
@@ -258,7 +258,7 @@ INSERT INTO permission (unit_id, user_id) VALUES
 (147,1),
 
 -- Income Expense Report
-(148,1);
+(149,1);
 
 -- Fiscal Year 2015
 SET @fiscalYear2015 = 0;
