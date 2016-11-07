45b0b9f0765e06f85dbb98e5a76dd9d1c23527ac
Chris Lomame <lomamech@gmail.com>
2016-11-07 15:09:09 +0100

    Implementation reportAccounts

--- server/controllers/finance/reports/reportAccounts/index.js
+++ server/controllers/finance/reports/reportAccounts/index.js
@@ -1,9 +1,8 @@
 'use strict';
 
-const Accounts = require('../../accounts');
 const ReportManager = require('../../../../lib/ReportManager');
-
-const TEMPLATE = './server/controllers/finance/reports/accounts/chart.handlebars';
+const db            = require('../../../../lib/db');
+const TEMPLATE      = './server/controllers/finance/reports/reportAccounts/chart.handlebars';
 
 /**
  * @method document
@@ -18,7 +17,41 @@ function document(req, res, next) {
   let params = req.query;
   params.user = req.session.user;
 
-  // build the main part of the SQL query
+  try {
+    report = new ReportManager(TEMPLATE, req.session, params);
+  } catch(e) {
+    return next(e);
+  }
+
+  return queryReportAccount(params.account_id)
+    .then(accounts => {
+
+      let sum = {
+        credit : 0,
+        debit : 0,
+        balance: 0
+      };
+
+      accounts.forEach(function (account) {
+        sum.debit += account.debit;
+        sum.credit += account.credit; 
+        sum.balance = sum.debit - sum.credit;
+      });
+      return report.render({ accounts, sum });
+
+    })
+    .then(result => {
+      res.set(result.headers).send(result.report);
+    })
+    .catch(next)
+    .done();
+}
+
+
+/**
+ * @function queryReportAccount
+*/ 
+function queryReportAccount(accountId){
   let sql = `
     SELECT transaction.trans_id, transaction.entity_uuid, transaction.description, transaction.trans_date, sum(transaction.credit_equiv) as credit, sum(transaction.debit_equiv) as debit,
     transaction.reference, transaction.abbr
@@ -28,41 +61,20 @@ function document(req, res, next) {
       FROM posting_journal
       LEFT JOIN invoice ON invoice.uuid = posting_journal.record_uuid
       LEFT JOIN project ON invoice.project_id = project.id
-      WHERE posting_journal.entity_uuid = ? AND posting_journal.entity_type = 'D'
+      WHERE posting_journal.account_id = ?
       UNION 
       SELECT general_ledger.trans_id, BUID(general_ledger.entity_uuid) AS entity_uuid, general_ledger.description,
       general_ledger.trans_date, general_ledger.debit_equiv, general_ledger.credit_equiv, invoice.reference, project.abbr
       FROM general_ledger
       LEFT JOIN invoice ON invoice.uuid = general_ledger.record_uuid
       LEFT JOIN project ON invoice.project_id = project.id 
-      WHERE general_ledger.entity_uuid = ? AND general_ledger.entity_type = 'D'
+      WHERE general_ledger.account_id = ?
     ) as transaction
     GROUP BY transaction.trans_id 
     ORDER BY transaction.trans_date ASC;`;
 
-
-  console.log('KKKKKKKKKKKKKKKKKKK');
-  console.log(params);
-  console.log(sql);
-  console.log('HHHHHHHH');
-
-/*
-  try {
-    report = new ReportManager(TEMPLATE, req.session, params);
-  } catch(e) {
-    return next(e);
-  }
-
-  Accounts.lookupAccount()
-    .then(Accounts.processAccountDepth)
-    .then(accounts => {
-      return report.render({ accounts });
-    })
-    .then(result => {
-      res.set(result.headers).send(result.report);
-    })
-    .catch(next)
-    .done();*/
+  return db.exec(sql, [accountId, accountId]);
 }
+
 
 exports.document = document;
