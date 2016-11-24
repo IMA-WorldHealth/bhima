0896421ddfacc19c2b6f2f843faf56faa0f5726e
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2016-11-24 13:13:53 +0100

    fix(cash): hook up cashbox link
    This commit hooks up the "add a cashbox" to the correct state.
    
    Closes #923.

--- client/src/partials/cash/cashboxes/cashboxes.routes.js
+++ client/src/partials/cash/cashboxes/cashboxes.routes.js
@@ -1,45 +1,45 @@
 angular.module('bhima.routes')
   .config(['$stateProvider', function ($stateProvider) {
-    
+
     $stateProvider
-      .state('cashboxes', { 
-        abstract : true, 
-        url : '/cashboxes', 
-        controller : 'CashboxController as CashCtrl', 
+      .state('cashboxes', {
+        abstract : true,
+        url : '/cashboxes',
+        controller : 'CashboxController as CashCtrl',
         templateUrl : 'partials/cash/cashboxes/cashboxes.html'
       })
 
-      .state('cashboxes.list', { 
-        url : '', 
+      .state('cashboxes.list', {
+        url : '',
         templateUrl : 'partials/cash/cashboxes/cashboxes.list.html'
       })
 
-      .state('cashboxes.create', { 
+      .state('cashboxes.create', {
         url : '/create',
-        templateUrl : 'partials/cash/cashboxes/update/add.html', 
+        templateUrl : 'partials/cash/cashboxes/update/add.html',
         controller : 'CashboxUpdateController as UpdateCtrl'
       })
 
       .state('cashboxes.edit', {
-        url : '/:uuid/edit', 
-        params : { 
+        url : '/:uuid/edit',
+        params : {
           id : { squash : true, value : null }
-        }, 
+        },
         templateUrl : 'partials/cash/cashboxes/update/edit.html',
         controller : 'CashboxUpdateController as UpdateCtrl'
       });
 
   }]);
 
-function addModal($modal) { 
-  $modal.open({ 
+function addModal($modal) {
+  $modal.open({
     keyboard : false,
-    backdrop : 'static', 
-    templateUrl : 'partials/cash/cashboxes/update/add.modal.html', 
+    backdrop : 'static',
+    templateUrl : 'partials/cash/cashboxes/update/add.modal.html',
     controller : 'CashboxUpdateController as UpdateCtrl'
   });
 }
 
-function closeModal($uibModalStack) { 
+function closeModal($uibModalStack) {
   $uibModalStack.dismissAll();
 }
