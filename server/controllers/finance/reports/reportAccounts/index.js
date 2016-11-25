aaada58c964749418648ceee600442d62984cc2e
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2016-11-25 13:22:16 +0100

    fix(report): account report uses ent currency
    This commit fixes account report to use the enterprise currency in
    rendering totals.  It also migrates the account selection to a ui-select
    for good measure.  Finally, the loading indicator on the form is now
      hooked up.
    
    Partially fixes #924.

--- server/controllers/finance/reports/reportAccounts/index.js
+++ server/controllers/finance/reports/reportAccounts/index.js
@@ -5,7 +5,6 @@ const db            = require('../../../../lib/db');
 const TEMPLATE      = './server/controllers/finance/reports/reportAccounts/report.handlebars';
 const BadRequest    = require('../../../../lib/errors/BadRequest');
 
-
 /**
  * @method document
  *
@@ -13,7 +12,6 @@ const BadRequest    = require('../../../../lib/errors/BadRequest');
  * generate Report of accounts as a document
  */
 function document(req, res, next) {
-
   let report;
 
   let params = req.query;
@@ -22,7 +20,6 @@ function document(req, res, next) {
   if (!params.account_id) {
     throw new BadRequest('Account ID missing', 'ERRORS.BAD_REQUEST');
   }
-
 
   try {
     report = new ReportManager(TEMPLATE, req.session, params);
@@ -47,11 +44,9 @@ function document(req, res, next) {
 
       accounts.forEach(function (account) {
         sum.debit += account.debit;
-        sum.credit += account.credit; 
+        sum.credit += account.credit;
         sum.balance = sum.debit - sum.credit;
       });
-
-
 
       return report.render({ accounts, title, sum });
 
@@ -67,54 +62,49 @@ function document(req, res, next) {
 /**
  * @function queryReportAccount
  * This feature select all transactions for a specific account
-*/ 
-function queryReportAccount(accountId, source){
+*/
+function queryReportAccount(accountId, source) {
   source = parseInt(source);
+  let sql;
 
-  if(source === 1){
-
-    let sql = `
+  if (source === 1) {
+    sql = `
       SELECT general_ledger.trans_id, BUID(general_ledger.entity_uuid) AS entity_uuid, general_ledger.description,
       general_ledger.trans_date, general_ledger.debit_equiv as debit, general_ledger.credit_equiv as credit
       FROM general_ledger
       WHERE general_ledger.account_id = ?
       GROUP BY general_ledger.trans_id
-      ORDER BY general_ledger.trans_date ASC;`;
-
-    return db.exec(sql, [accountId]);
-      
-  } else if(source === 2) {
-
-    let sql = `
+      ORDER BY general_ledger.trans_date ASC;
+    `;
+  } else if (source === 2) {
+    sql = `
       SELECT posting_journal.trans_id, BUID(posting_journal.entity_uuid) AS entity_uuid, posting_journal.description,
       posting_journal.trans_date, posting_journal.debit_equiv as debit, posting_journal.credit_equiv as credit
       FROM posting_journal
       WHERE posting_journal.account_id = ?
       GROUP BY posting_journal.trans_id
-      ORDER BY posting_journal.trans_date ASC;`;
-
-    return db.exec(sql, [accountId]);
-
-  } else if(source === 3){
-    let sql = `
+      ORDER BY posting_journal.trans_date ASC;
+    `;
+  } else if (source === 3) {
+    sql = `
       SELECT transaction.trans_id, transaction.entity_uuid, transaction.description, transaction.trans_date, sum(transaction.credit_equiv) as credit, sum(transaction.debit_equiv) as debit
       FROM(
         SELECT posting_journal.trans_id, BUID(posting_journal.entity_uuid) AS entity_uuid, posting_journal.description,
         posting_journal.trans_date, posting_journal.debit_equiv, posting_journal.credit_equiv
         FROM posting_journal
         WHERE posting_journal.account_id = ?
-        UNION 
+        UNION
         SELECT general_ledger.trans_id, BUID(general_ledger.entity_uuid) AS entity_uuid, general_ledger.description,
         general_ledger.trans_date, general_ledger.debit_equiv, general_ledger.credit_equiv
         FROM general_ledger
         WHERE general_ledger.account_id = ?
       ) as transaction
-      GROUP BY transaction.trans_id 
-      ORDER BY transaction.trans_date ASC;`;
-
-    return db.exec(sql, [accountId, accountId]);
+      GROUP BY transaction.trans_id
+      ORDER BY transaction.trans_date ASC;
+    `;
   }
 
+  return db.exec(sql, [accountId, accountId]);
 }
 
 exports.document = document;
