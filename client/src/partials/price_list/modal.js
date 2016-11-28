1be07023ab9733e4178750be5a1c221931114906
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2016-11-28 13:52:03 +0100

    fix(prices): update client to 2.x standards
    This commit fixes the price list client, giving it Notify for error
    handling and loading buttons.  It also implements the client-side
    validation logic for price lists.

--- client/src/partials/price_list/modal.js
+++ client/src/partials/price_list/modal.js
@@ -2,23 +2,24 @@ angular.module('bhima.controllers')
 .controller('PriceListModalController', PriceListModalController);
 
 PriceListModalController.$inject = [
-  '$uibModalInstance', 'InventoryService', 'util'
+  '$uibModalInstance', 'InventoryService', 'util', 'NotifyService'
 ];
 
-function PriceListModalController( $uibModalInstance, Inventory, util) {
+function PriceListModalController( $uibModalInstance, Inventory, util, Notify) {
   var vm = this;
 
   // bind variables
-
   vm.submit = submit;
   vm.cancel = cancel;
   vm.action = null;
+  vm.isValidItem = isValidItem;
 
-  // load Inventory 
-  Inventory.getInventoryItems()
-  .then(function (data) {
-    vm.inventories = data;
-  }).catch(handler);
+  // load Inventory
+  Inventory.read()
+    .then(function (inventory) {
+      vm.inventories = inventory;
+    })
+    .catch(Notify.handleError);
 
   // bind methods
   vm.submit = submit;
@@ -27,17 +28,29 @@ function PriceListModalController( $uibModalInstance, Inventory, util) {
 
   vm.data = { is_percentage : 0 };
 
-  function handler(error) {
-    throw error;
+  function isValidItem() {
+    var hasNegativePrice = (!vm.data.is_percentage && vm.data.value < 0);
+
+    if (hasNegativePrice) {
+      vm.error = 'PRICE_LIST.ERRORS.HAS_NEGATIVE_PRICE';
+      Notify.danger(vm.error);
+    }
+
+    return !hasNegativePrice;
   }
 
   function submit(invalid) {
     if (invalid) { return; }
+
+    // make sure that there are no negative prices
+    if (!isValidItem()) {
+      return;
+    }
+
     return $uibModalInstance.close(vm.data);
   }
 
   function cancel() {
     $uibModalInstance.dismiss();
   }
-
 }
