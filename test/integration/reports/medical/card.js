2aacd03647e0ce1a314d38a072da57caa2524a3e
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-01-08 20:04:32 +0100

    test(patient): restore patient card test
    This commit unskips the patient card test.

--- test/integration/reports/medical/card.js
+++ test/integration/reports/medical/card.js
@@ -2,4 +2,4 @@ const RenderingTests = require('../rendering');
 
 const patientUuid = '274c51ae-efcc-4238-98c6-f402bfb39866';
 const target = '/reports/medical/patients/';
-describe.skip(`(${target}:uuid) Patient Card`, RenderingTests(target + patientUuid));
+describe(`(${target}:uuid) Patient Card`, RenderingTests(target + patientUuid));
