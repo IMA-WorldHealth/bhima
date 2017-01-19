4cb7be154c4107b3970c32e9f7e44e39a0deb76e
DedrickEnc <kitamuka@gmail.com>
2017-01-19 12:00:16 +0100

    listing, adding and updating fee center done

--- client/src/partials/fee_center/fee_center.routes.js
+++ client/src/partials/fee_center/fee_center.routes.js
@@ -9,10 +9,39 @@ angular.module('bhima.routes')
         controller : 'feeCenterController as feeCenterCtrl',
         templateUrl : 'partials/fee_center/fee_center.html'
       })
+      .state('feeCenter.create', {
+        url : '/create',
+        params : {
+          creating : {value : true}
+        },
+        onEnter : ['$uibModal', feeCenterModal],
+        onExit : ['$uibModalStack', closeModal]
+      })
       .state('feeCenter.list', {
         url : '/:id',
         params : {
             id : {squash : true, value : null}
         }
+      })
+      .state('feeCenter.edit', {
+        url : '/:id/edit',
+        params : {
+          id : null
+        },
+        onEnter :['$uibModal', feeCenterModal],
+        onExit : ['$uibModalStack', closeModal]
       });
     }]);
+
+function closeModal(ModalStack) {
+  ModalStack.dismissAll();
+}
+
+function feeCenterModal($modal) {
+  $modal.open({
+    keyboard : false,
+    backdrop : 'static',
+    templateUrl: 'partials/fee_center/feeCenter.modal.html',
+    controller: 'FeeCenterModalController as FeeCenterModalCtrl'
+  });
+}
