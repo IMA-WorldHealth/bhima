221d8a6557c2eb91f32a3ebe1d3c88c523daadc7
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-02-06 17:33:48 +0100

    fix(bhRendererDropdown): send filters server
    This commit fixes the `bhRendererDropdown` to ensure it sends the report
    filter parameters to the server for rendering.
    
    Closes https://github.com/Vanga-Hospital/bhima-2.X/issues/221.

--- client/src/js/components/bhRendererDropdown.js
+++ client/src/js/components/bhRendererDropdown.js
@@ -8,15 +8,16 @@ angular.module('bhima.components')
     controller : bhRendererController
   });
 
-bhRendererController.$inject = [ 'appcache' ];
+bhRendererController.$inject = [ 'appcache', '$httpParamSerializer' ];
 
-function bhRendererController(AppCache) {
+function bhRendererController(AppCache, $httpParamSerializer) {
   var $ctrl = this;
 
   var cache = new AppCache('bhRendererComponent');
 
   // delay between GET request completion and loading indication, this is used
   // to compensate for the delay in browsers opening the print dialog
+  // @todo - make this work!
   var loadingIndicatorDelay = 1000;
 
   $ctrl.$onInit = function () {
@@ -27,20 +28,42 @@ function bhRendererController(AppCache) {
 
     $ctrl.selection = cache.selection || $ctrl.options[0];
 
-    $ctrl.reportOptions = $ctrl.reportOptions || {};
-
     $ctrl.$loading = false;
+
+    combineAndSerializeParameters();
   };
 
   $ctrl.select = function (option) {
     $ctrl.selection = cache.selection = option;
   };
 
-  /*
+  // watch for changes on the component's border and behave appropriately
   $ctrl.$onChanges = function (changes) {
-    console.log('changes:', changes);
+    var hasParameterChanges = (changes && changes.reportOptions);
+
+    // if there are changes to the report options, serialize them
+    if (hasParameterChanges) {
+      combineAndSerializeParameters();
+    }
   };
-  */
+
+  /**
+   * @method combineAndSerializeParameters
+   *
+   * @description
+   * This method combines the renderers parameter and the reportOptions into
+   * a url to be passed to ngHref.
+   */
+  function combineAndSerializeParameters() {
+    var rendererParams = $ctrl.selection && $ctrl.selection.parameters || {};
+    var reportParams = $ctrl.reportOptions || {};
+
+    // combine the parameters into one
+    var combined = angular.merge(rendererParams, reportParams);
+
+    // serialize the parameters with the $http parameter serializer
+    $ctrl.params = $httpParamSerializer(combined);
+  }
 
   /**
    * @method toggleLoading
