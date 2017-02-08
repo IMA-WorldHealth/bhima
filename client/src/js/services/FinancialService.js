59714638a24f9b5a78ce72124810815c6e4588e8
DedrickEnc <kitamuka@gmail.com>
2017-02-08 16:46:14 +0100

    integrating suggestion

--- client/src/js/services/FinancialService.js
+++ client/src/js/services/FinancialService.js
@@ -5,28 +5,34 @@ FinancialService.$inject = [ '$http', 'util' ];
 
 function FinancialService ($http, util) {
   var service = this;
+  var baseUrl = '/fee_centers/';
 
+  service.readFeeCenter = readFeeCenter;
   service.readCostCenter = readCostCenter;
   service.readProfitCenter = readProfitCenter;
   service.getFeeValue = getFeeValue;
 
+  function readFeeCenter(id, params) {
+    var url = baseUrl.concat(id || '');
+    return $http.get(baseUrl, { params : params })
+      .then(util.unwrapHttpResponse);
+  }
+
   // this will read all cost centers
   function readCostCenter(id, params) {
-     var url = '/fee_centers?is_cost=1';
-     return $http.get(url, { params : params })
-     .then(util.unwrapHttpResponse);
+    var costParameters = angular.extend(params, { is_cost : 1 });
+    return readFeeCenter(id, costParameters);
   }
 
   // this will read all Profit Centers
   function readProfitCenter(id, params) {
-     var url = '/fee_centers?is_cost=0';
-     return $http.get(url, { params : params })
-     .then(util.unwrapHttpResponse);
+    var profitParameters = angular.extend(params, { is_cost : 0 });
+    return readFeeCenter(id, profitParameters);
   }
 
   //Gives the value of the charges of a cost center
   function getFeeValue(feeId) {
-     var url = '/fee_centers/' + feeId + '/value';
+     var url = baseUrl + feeId + '/value';
      return $http.get(url)
      .then(util.unwrapHttpResponse);
   }
