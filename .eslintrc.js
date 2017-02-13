decf909e2e1853f12c16b11106c32b1e994f09eb
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-02-13 11:47:05 +0100

    refactor(eslint): make eslint failures noncritical
    This commit temporarily turns off eslint failures while the code base
    can be migrated.  ESLint failures will be reported in Travis, but will
    not break the build.

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,34 @@ module.exports = {
     "sourceType": "module"
   },
   "rules" : {
-    "padded-blocks": ["error", "never"],
-    "indent": ["error", 2]
+    "padded-blocks": "warn",
+    "array-bracket-spacing" : "warn",
+    "indent": ["error", 2],
+    "comma-dangle" : ["error", "always-multiline"],
+    "key-spacing": ["warn", {
+      "singleLine": {
+        "beforeColon": false,
+        "afterColon": true
+      },
+      "multiLine": {
+        "beforeColon": true,
+        "afterColon": true,
+        "align" : "colon"
+      }
+    }],
+    "no-use-before-define": ["error", {
+      "functions": false
+    }],
+    "no-var" : "off",
+    "max-len": ["warn", 120],
+    "func-names" : ["warn", "as-needed"],
+    "no-underscore-dangle": ["error", {
+      "allowAfterThis": true
+    }]
   },
   "globals" : {
     "Chart" : true,
-    "Angular" : true,
+    "angular" : true,
+    "$" : true,
   }
 };
