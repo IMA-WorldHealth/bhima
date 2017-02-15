c8ca2cfc23c32ebcddfc1df19d51771b2b1769ae
Chris Lomame <lomamech@gmail.com>
2017-02-15 12:22:44 +0100

    Improve inventory groups Update Inventory group service for get option include_members Sanitaze configuration groups.js removing unused function

--- server/controllers/inventory/inventory/groups.js
+++ server/controllers/inventory/inventory/groups.js
@@ -17,8 +17,12 @@ exports.remove  = remove;
 exports.countInventory = countInventory;
 
 /** list inventory group */
-function list () {
-  return getGroups();
+function list (include_members) {
+  if(include_members){
+    return getGroupsMembers();    
+  } else {
+    return getGroups();
+  }  
 }
 
 /** details of inventory group */
@@ -67,6 +71,21 @@ function getGroups(uid) {
   uid = (uid) ? db.bid(uid) : undefined;
   sql += (uid) ? ' WHERE uuid = ?;' : ';';
   return db.exec(sql, [uid]);
+}
+
+
+/**
+ * Get the inventory groups and the number of inventors that make up this group
+ */
+function getGroupsMembers() {
+  let sql = `
+    SELECT BUID(ig.uuid) AS uuid, ig.code, ig.name, ig.sales_account, ig.cogs_account, ig.stock_account, COUNT(i.uuid) AS inventory_counted
+    FROM inventory_group AS ig
+    JOIN inventory AS i ON i.group_uuid = ig.uuid
+    GROUP BY ig.uuid;
+    `;
+
+  return db.exec(sql);
 }
 
 /**
