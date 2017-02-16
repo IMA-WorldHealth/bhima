b55d0dfdf764ab05a97403e40553eed46afbf732
Bruce Mbayo <mbayopanda@gmail.com>
2017-02-16 15:57:39 -0800

    fix account report

--- server/controllers/finance/reports/reportAccounts/index.js
+++ server/controllers/finance/reports/reportAccounts/index.js
@@ -1,8 +1,8 @@
-
+const db = require('../../../../lib/db');
 const ReportManager = require('../../../../lib/ReportManager');
-const db            = require('../../../../lib/db');
-const TEMPLATE      = './server/controllers/finance/reports/reportAccounts/report.handlebars';
-const BadRequest    = require('../../../../lib/errors/BadRequest');
+
+const TEMPLATE = './server/controllers/finance/reports/reportAccounts/report.handlebars';
+const BadRequest = require('../../../../lib/errors/BadRequest');
 
 /**
  * @method document
@@ -13,7 +13,7 @@ const BadRequest    = require('../../../../lib/errors/BadRequest');
 function document(req, res, next) {
   let report;
 
-  let params = req.query;
+  const params = req.query;
   params.user = req.session.user;
 
   if (!params.account_id) {
@@ -22,35 +22,33 @@ function document(req, res, next) {
 
   try {
     report = new ReportManager(TEMPLATE, req.session, params);
-  } catch(e) {
+  } catch (e) {
     return next(e);
   }
 
   return queryReportAccount(params.account_id, params.sourceId)
-    .then(accounts => {
-
-      let sum = {
-        credit : 0,
-        debit : 0,
-        balance: 0
+    .then((accounts) => {
+      const sum = {
+        credit  : 0,
+        debit   : 0,
+        balance : 0,
       };
 
-      let title = {
+      const title = {
         accountNumber : params.account_number,
-        accountLabel : params.account_label,
-        source       : params.sourceLabel
+        accountLabel  : params.account_label,
+        source        : params.sourceLabel,
       };
 
-      accounts.forEach(function (account) {
+      accounts.forEach((account) => {
         sum.debit += account.debit;
         sum.credit += account.credit;
         sum.balance = sum.debit - sum.credit;
       });
 
       return report.render({ accounts, title, sum });
-
     })
-    .then(result => {
+    .then((result) => {
       res.set(result.headers).send(result.report);
     })
     .catch(next)
@@ -86,7 +84,8 @@ function queryReportAccount(accountId, source) {
     `;
   } else if (source === 3) {
     sql = `
-      SELECT transaction.trans_id, transaction.entity_uuid, transaction.description, transaction.trans_date, sum(transaction.credit_equiv) as credit, sum(transaction.debit_equiv) as debit
+      SELECT transaction.trans_id, transaction.entity_uuid, transaction.description, 
+        transaction.trans_date, sum(transaction.credit_equiv) as credit, sum(transaction.debit_equiv) as debit
       FROM(
         SELECT posting_journal.trans_id, BUID(posting_journal.entity_uuid) AS entity_uuid, posting_journal.description,
         posting_journal.trans_date, posting_journal.debit_equiv, posting_journal.credit_equiv
