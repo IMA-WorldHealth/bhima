cc86400a092de16056fa2746c2f38887dcd7652c
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-02-16 14:04:40 +0100

    fix(purchases): use proper checkbox html
    This commit fixes the purchase registry to use bootstrap-compliant HTML in
    its search modal.

--- client/src/partials/purchases/modals/search.modal.js
+++ client/src/partials/purchases/modals/search.modal.js
@@ -1,15 +1,15 @@
 angular.module('bhima.controllers')
-.controller('SearchPurchaseOrderModalController', SearchPurchaseOrderModalController);
+  .controller('SearchPurchaseOrderModalController', SearchPurchaseOrderModalController);
 
 // dependencies injections
 SearchPurchaseOrderModalController.$inject = [
-  'UserService', 'SupplierService', 'NotifyService', '$uibModalInstance', 'SearchFilterFormatService'
+  'UserService', 'SupplierService', 'NotifyService', '$uibModalInstance', 'SearchFilterFormatService',
 ];
 
 function SearchPurchaseOrderModalController(Users, Suppliers, Notify, Instance, SearchFilterFormat) {
   var vm = this;
 
-  // gloabal variables 
+  // global variables
   var noMissingDatePart;
 
   // global methods
@@ -17,31 +17,29 @@ function SearchPurchaseOrderModalController(Users, Suppliers, Notify, Instance, 
   vm.cancel = Instance.close;
   vm.submit = submit;
 
-  // init 
+  // init
   init();
 
-  // load users 
+  // load users
   Users.read()
-  .then(function (users) {
+    .then(function (users) {
       vm.users = users;
-  })
-  .catch(Notify.handleError);
+    })
+    .catch(Notify.handleError);
 
-  // load suppliers 
+  // load suppliers
   Suppliers.read()
-  .then(function (suppliers) {
+    .then(function (suppliers) {
       vm.suppliers = suppliers;
-  })
-  .catch(Notify.handleError);
+    })
+    .catch(Notify.handleError);
 
   function init() {
-    vm.bundle = { 
-      dateFrom: new Date(), 
-      dateTo: new Date(),
-      is_confirmed: 0,
-      is_received: 0,
-      is_cancelled: 0
+    vm.bundle = {
+      dateFrom : new Date(),
+      dateTo   : new Date(),
     };
+
     validate();
   }
 
@@ -54,5 +52,4 @@ function SearchPurchaseOrderModalController(Users, Suppliers, Notify, Instance, 
     noMissingDatePart = (vm.bundle.dateFrom && vm.bundle.dateTo) || (!vm.bundle.dateFrom && !vm.bundle.dateTo);
     vm.validDateRange = noMissingDatePart ? true : false;
   }
-
 }
