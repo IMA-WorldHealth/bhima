a882be1ea5244cb5d5abc3c79b4873c9661a78b2
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-02-20 10:16:17 +0100

    feat(build): build with revision control
    This commit builds the gulp file with revision control such that our
    caching scheme can be implemented at infinite and reduce network load.
    
    Closes #1219.

--- sh/test-ends.sh
+++ sh/test-ends.sh
@@ -16,15 +16,26 @@ DB_NAME="bhima_test"
 # set the build timeout
 TIMEOUT=${BUILD_TIMEOUT:-8}
 
-mysql -u $DB_USER -p$DB_PASS -e "DROP DATABASE bhima_test;"
-mysql -u $DB_USER -p$DB_PASS -e "CREATE DATABASE bhima_test;"
+# build the test database
+mysql -u $DB_USER -p$DB_PASS -e "DROP DATABASE IF EXISTS $DB_NAME ;"
+mysql -u $DB_USER -p$DB_PASS -e "CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+
+echo "Building schema"
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/schema.sql
+echo "Building triggers"
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/triggers.sql
+echo "Building functions and procedures"
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/functions.sql
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/procedures.sql
+mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/admin.sql
+echo "Building test database"
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/test/icd10.sql
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/bhima.sql
 mysql -u $DB_USER -p$DB_PASS $DB_NAME < server/models/test/data.sql
+
+mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "Call zRecomputeEntityMap();"
+mysql -u $DB_USER -p$DB_PASS $DB_NAME -e "Call zRecomputeDocumentMap();"
+
 
 echo "Building server ...."
 
