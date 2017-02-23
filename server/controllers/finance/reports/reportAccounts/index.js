0db0bb9cef918dd0fd1a256091d14507256834a3
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-02-23 09:36:17 +0100

    fix(accounts): fix rendering bugs
    This commit fixes two bugs in the account report where cumulative
    summation and the description were not rendering due to incorrect SQL.
    It also ensures that the JS file passes lint.

--- server/controllers/finance/reports/reportAccounts/index.js
+++ server/controllers/finance/reports/reportAccounts/index.js
@@ -32,7 +32,7 @@ function document(req, res, next) {
     accountLabel  : params.account_label,
     source        : params.sourceLabel,
     dateFrom      : params.dateFrom,
-    dateTo        : params.dateTo    
+    dateTo        : params.dateTo,
   };
 
   params.user = req.session.user;
@@ -73,41 +73,41 @@ function getAccountTransactions(accountId, source, dateFrom, dateTo) {
   const params = [accountId];
   let dateCondition = '';
 
-  if(dateFrom && dateTo){
-    dateCondition = `AND DATE(trans_date) BETWEEN ? AND ?`
+  if (dateFrom && dateTo) {
+    dateCondition = 'AND DATE(trans_date) BETWEEN DATE(?) AND DATE(?)';
     params.push(dateFrom, dateTo);
   }
 
-  const csum = `SET @csum := 0`;
+  const csum = 'SET @csum := 0;';
 
   const sql = `
-    SELECT a.trans_id, a.debit, a.credit, a.balance, a.trans_date, (@csum := @csum + a.balance) AS cumulBalance
-    FROM(
-      SELECT trans_id, BUID(entity_uuid) AS entity_uuid, description, trans_date, 
+    SELECT a.trans_id, a.debit, a.credit, a.balance, a.trans_date, (@csum := @csum + a.balance) AS cumulBalance,
+      a.description
+    FROM (
+      SELECT trans_id, BUID(entity_uuid) AS entity_uuid, description, trans_date,
         SUM(debit_equiv) as debit, SUM(credit_equiv) as credit, (SUM(debit_equiv) - SUM(credit_equiv)) AS balance
       FROM ${tableName}
       WHERE account_id = ? ${dateCondition}
-      GROUP BY trans_id 
-      ORDER BY trans_date ASC    
+      GROUP BY trans_id
+      ORDER BY trans_date ASC
     ) AS a`;
 
 
-  const sqlAggrega = ` SELECT SUM(t.debit) AS debit, SUM(t.credit) AS credit, SUM(t.debit - t.credit) AS balance 
+  const sqlAggrega = `
+    SELECT SUM(t.debit) AS debit, SUM(t.credit) AS credit, SUM(t.debit - t.credit) AS balance
     FROM (
-      SELECT trans_id, BUID(entity_uuid) AS entity_uuid, description, trans_date, 
+      SELECT trans_id, BUID(entity_uuid) AS entity_uuid, description, trans_date,
         SUM(debit_equiv) as debit, SUM(credit_equiv) as credit
       FROM ${tableName}
       WHERE account_id = ? ${dateCondition}
-      GROUP BY trans_id 
+      GROUP BY trans_id
       ORDER BY trans_date ASC
-    ) AS t 
+    ) AS t
     `;
 
   const bundle = {};
   return db.exec(csum)
-  .then((err) => {
-      return db.exec(sql, params);    
-    })
+    .then(() => db.exec(sql, params))
     .then((transactions) => {
       _.extend(bundle, { transactions });
       return db.one(sqlAggrega, params);
