2d18db9166aadece3b181875f435807786491e25
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-03-01 14:20:16 +0100

    test(vouchers): implement search end-to-end tests
    This commit implements a few end-to-end tests for the voucher search
    registry.  These tests are relatively few and should be expanded with
    further functionality added in the future.

---
+++ test/end-to-end/vouchers/vouchers.spec.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* global browser, element, by */
+const helpers = require('../shared/helpers');
+const GU = require('../shared/GridUtils');
+const VoucherRegistrySearch = require('./vouchers.search');
+
+describe('Voucher Registry', function () {
+  const numVouchers = 9;
+  const gridId = 'voucher-grid';
+
+  before(() => helpers.navigate('vouchers'));
+
+  it(`displays ${numVouchers} vouchers on the page`, () => {
+    GU.expectRowCount(gridId, numVouchers);
+  });
+
+  describe('Search', VoucherRegistrySearch);
+});
