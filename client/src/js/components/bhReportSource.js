cfe29ef384edea10b63dd4195bd99745c3b9b714
Steven Fountain <stevenfountain@imaworldhealth.org>
2017-03-08 16:00:28 +0100

    feat(reports) report source selection component
    This commit implements a simple component to select report sources. It
    also refactors the account report client controller defining standards
    for caching report configuration options on submit.

---
+++ client/src/js/components/bhReportSource.js
@@ -0,0 +1,48 @@
+angular.module('bhima.components')
+.component('bhReportSource', {
+  bindings : {
+    onSelectCallback : '&?',
+    label : '@?',
+    name : '@?',
+
+    // allows the controller to set a default value
+    value : '<'
+  },
+  template :
+    '<div class="form-group">' +
+      '<label class="control-label" translate>{{$ctrl.label}}</span></label>' +
+      '<select ng-change="$ctrl.onSelect($ctrl.value)" class="form-control" ng-model="$ctrl.value" ng-options="source.id as (source.label | translate) for source in $ctrl.sources" required>' +
+        '<option value="" disabled translate>FORM.SELECT.SOURCE</option>' +
+      '</select>' +
+      '{{$ctrl.invalue}}' +
+    '</div>',
+  controller : bhReportSourceController
+});
+
+bhReportSourceController.$inject = [];
+
+function bhReportSourceController() {
+  var $ctrl = this;
+  var DEFAULT_SOURCE = 1;
+
+  $ctrl.label = $ctrl.label || 'FORM.SELECT.SOURCE';
+  $ctrl.name = $ctrl.name || 'source';
+
+  /* @const */
+  $ctrl.sources = [
+    {id: 1, label : 'FORM.LABELS.GENERAL_LEDGER'},
+    {id: 2, label : 'FORM.LABELS.POSTING_JOURNAL'},
+    {id: 3, label : 'FORM.LABELS.ALL'}
+  ];
+
+  $ctrl.$onInit = function() {
+    // select default value if nothing is selected
+    if (angular.isUndefined($ctrl.value)) {
+      $ctrl.onSelect(DEFAULT_SOURCE);
+    }
+  }
+
+  $ctrl.onSelect = function ($item) {
+    $ctrl.onSelectCallback({ source : $item });
+  }
+}
--- client/src/partials/reports/modals/report_accounts.config.js
+++ client/src/partials/reports/modals/report_accounts.config.js
@@ -2,78 +2,59 @@ angular.module('bhima.controllers')
   .controller('report_accountsController', ReportAccountsConfigController);
 
 ReportAccountsConfigController.$inject = [
-  '$sce', '$state', 'NotifyService', 'LanguageService', 'BaseReportService', 'bhConstants', 'reportData'
+  '$sce', 'NotifyService', 'BaseReportService', 'AppCache', 'reportData'
 ];
 
-/**
- * ReportAccounts Config Controller
- *
- * @description
- * This controller is responsible for the configuration of the ReportAccounts report modal. All report
- * settings are sent to the server to generate a report document.
- */
-function ReportAccountsConfigController($sce, $state, Notify, Languages, SavedReports, bhConstants, reportData) {
+function ReportAccountsConfigController($sce, Notify, SavedReports, AppCache, reportData) {
   var vm = this;
-  // var report = reportDetails;
-
-  // expose to the view
-  vm.generate = generate;
-  // vm.cancel = ModalInstance.dismiss;
-  // vm.report = report;
-  vm.bhConstants = bhConstants;
-  vm.onAccountSelect = onAccountSelect;
+  var cache = new AppCache('configure_reports_account');
+  var reportUrl = 'reports/finance/account';
 
   vm.previewGenerated = false;
+  vm.reportDetails = {};
 
-  vm.reportSource = [
-    {id: 1, label : 'FORM.LABELS.GENERAL_LEDGER'},
-    {id: 2, label : 'FORM.LABELS.POSTING_JOURNAL'},
-    {id: 3, label : 'FORM.LABELS.ALL'}
-  ];
+  checkCachedConfiguration();
 
-  vm.source = vm.reportSource[0];
-
-  function onAccountSelect(account) {
-    vm.accountId = account.id;
-    vm.accountLabel = account.label;
-    vm.accountNumber = account.number;
+  vm.selectAccount = function selectAccount(account) {
+    vm.reportDetails.account = account.id;
   }
 
-  function generate(form) {
+  vm.selectSource = function selectSource(source) {
+    vm.reportDetails.source = source;
+  }
+
+  vm.clearPreview = function clearPreview() {
+    vm.previewGenerated = false;
+    vm.previewResult = null;
+  }
+
+  vm.preview = function preview(form) {
     if (form.$invalid) { return; }
 
-    var url = 'reports/finance/account';
+    parseDateInterval(vm.reportDetails);
 
-    var reportId = reportData.id;
+    // update cached configuration
+    cache.reportDetails = angular.copy(vm.reportDetails);
 
-    if(!vm.dateInterval){
-      vm.dateTo = null;
-      vm.dateFrom = null;
-    }
-
-    var options = {
-      account_id      : vm.accountId,
-      account_label   : vm.accountLabel,
-      account_number  : vm.accountNumber,
-      sourceId        : vm.source.id,
-      sourceLabel     : vm.source.label,
-      label           : vm.label,
-      lang            : Languages.key,
-      dateTo          : vm.dateTo,
-      dateFrom        : vm.dateFrom,
-      reportType      : vm.type
-    };
-
-    return SavedReports.requestPreview(url, reportId, options)
+    return SavedReports.requestPreview(reportUrl, reportData.id, angular.copy(vm.reportDetails))
       .then(function (result) {
 
         vm.previewGenerated = true;
         vm.previewResult = $sce.trustAsHtml(result);
-      });
+      })
+      .catch(Notify.handleError);
   }
 
-  vm.removePreview = function removePreview() {
-    vm.previewGenerated = false;
-    vm.previewResult = null;
+  // @TODO validation on dates - this should be done through a 'period select' component
+  function parseDateInterval(reportDetails) {
+    if (!vm.dateInterval) {
+      reportDetails.dateTo = reportDetails.dateFrom = null;
+    }
+  }
+
+  function checkCachedConfiguration() {
+    if (cache.reportDetails) {
+      vm.reportDetails = angular.copy(cache.reportDetails);
+    }
   }
 }
