f99102dde01dc67b1e5e76b9260fd660f78e944d
Chris Lomame <lomamech@gmail.com>
2017-03-15 14:01:58 +0100

    Adding directive loading

---
+++ client/src/js/directives/loading.js
@@ -0,0 +1,7 @@
+angular.module('bhima.directives')
+.directive('loadingIndicator', function () {
+  return {
+    restrict : 'E',
+    template: '<h3><p class="text-info"><span class="fa fa-circle-o-notch fa-spin"></span> <span translate>FORM.INFO.LOADING</span></p></h3>',
+  };
+});
