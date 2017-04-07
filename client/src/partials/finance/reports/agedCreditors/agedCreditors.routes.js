91e093cfd442f47874f87228498c813235678a17
Chris Lomame <lomamech@gmail.com>
2017-04-07 11:07:14 +0200

    - Fix translation key in aged.handlebars - Remove Unnecessary folder reports/agedCreditors

--- client/src/partials/finance/reports/agedCreditors/agedCreditors.routes.js
+++
@@ -1,9 +0,0 @@
-angular.module('bhima.routes')
-  .config(['$stateProvider', function ($stateProvider) {
-    $stateProvider
-      .state('agedCreditors', {
-        url : '/finance/reports/creditors/aged',
-        controller : 'AgedCreditorsController as AgedCreditorsCtrl',
-        templateUrl : 'partials/finance/reports/agedCreditors/agedCreditors.html'
-      });
-  }]);
