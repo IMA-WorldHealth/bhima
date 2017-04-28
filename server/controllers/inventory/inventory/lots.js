f4b6650adcbbd3f360a4867f645b25c182cbf702
Jonathan Niles <jonathanwniles@imaworldhealth.org>
2017-04-28 12:10:30 +0100

    refactor(inventory): fix eslint issues on server
    This commit refactors the syntax on all server-side inventory modules.
    Many of them are probably no longer relevant or used.  However, we
    cannot enforce eslint as a testing tool until the existing code passes,
    so this commit tries to ensure that.
    
    Partially addresses #1429.

--- server/controllers/inventory/inventory/lots.js
+++ server/controllers/inventory/inventory/lots.js
@@ -1,5 +1,4 @@
-
-var db = require('../../../lib/db');
+const db = require('../../../lib/db');
 
 exports.getInventoryLotsById = getInventoryLotsById;
 
@@ -11,14 +10,12 @@ exports.getInventoryLotsById = getInventoryLotsById;
 * @returns {Promise} The database query
 */
 function getInventoryLotsById(uuid) {
-
-  var sql;
-
-  sql =
-    `SELECT s.lot_number, s.quantity - IF(c.canceled, 0, IFNULL(c.quantity, 0)) AS quantity
+  const sql = `
+    SELECT s.lot_number, s.quantity - IF(c.canceled, 0, IFNULL(c.quantity, 0)) AS quantity
     FROM stock AS s LEFT JOIN consumption AS c ON
       s.tracking_number = c.tracking_number
-    WHERE s.inventory_uuid = ?`;
+    WHERE s.inventory_uuid = ?;
+  `;
 
   return db.exec(sql, [uuid]);
 }
--- server/controllers/inventory/inventory/status.js
+++ server/controllers/inventory/inventory/status.js
@@ -1,9 +1,7 @@
-var db          = require('../../../lib/db'),
-    core        = require('./core'),
-    consumption = require('./consumption'),
-    leadtimes   = require('./leadtimes'),
-    stock       = require('./stock'),
-    q           = require('q');
+const core = require('./core');
+const consumption = require('./consumption');
+const leadtimes = require('./leadtimes');
+const stock = require('./stock');
 
 exports.getInventoryStatusById = getInventoryStatusById;
 
@@ -16,72 +14,70 @@ exports.getInventoryStatusById = getInventoryStatusById;
 * @returns {Promise} The database query promise
 */
 function getInventoryStatusById(uuid) {
-
-  var data;
+  let data;
 
   // get the min stock values
   return core.getItemsMetadataById(uuid)
-  .then(function (rows) {
-    if (!rows.length) {
-      throw core.errors.NO_INVENTORY_ITEMS;
-    }
+    .then((rows) => {
+      if (!rows.length) {
+        throw core.errors.NO_INVENTORY_ITEMS;
+      }
 
-    data = rows[0];
+      data = rows[0];
 
-    return stock.getStockLevelsById(uuid);
-  })
-  .then(function (rows) {
-    data.quantity = rows.length > 0 ? rows[0].quantity : 0;
+      return stock.getStockLevelsById(uuid);
+    })
+    .then((rows) => {
+      data.quantity = rows.length > 0 ? rows[0].quantity : 0;
 
-    return leadtimes.getInventoryLeadTimesById(uuid);
-  })
-  .then(function (rows) {
-    data.leadtime = rows.length > 0 ? rows[0].days : 0;
+      return leadtimes.getInventoryLeadTimesById(uuid);
+    })
+    .then((rows) => {
+      data.leadtime = rows.length > 0 ? rows[0].days : 0;
 
-    return consumption.getAverageItemConsumption(uuid, {});
-  })
-  .then(function (rows) {
-    var daysOfStockRemaining,
-        avg = rows.length > 0 ? rows[0].average : 0;
+      return consumption.getAverageItemConsumption(uuid, {});
+    })
+    .then((rows) => {
+      const avg = rows.length > 0 ? rows[0].average : 0;
 
-    data.consumption = avg;
+      data.consumption = avg;
 
-    // calculate the days of stock remaining in the inventory (in days)
-    daysOfStockRemaining =
-      (data.quantity / data.consumption) - data.leadtime;
+      // calculate the days of stock remaining in the inventory (in days)
+      const daysOfStockRemaining =
+        (data.quantity / data.consumption) - data.leadtime;
 
-    data.remaining = daysOfStockRemaining;
+      data.remaining = daysOfStockRemaining;
 
-    // if there are fewer days of stock remaining than the preset minimum value,
-    // we are facing a shortage and should reorder stock immediately
-    data.shortage = data.remaining < data.stock_min;
+      // if there are fewer days of stock remaining than the preset minimum value,
+      // we are facing a shortage and should reorder stock immediately
+      data.shortage = data.remaining < data.stock_min;
 
-    // if there are more days of stock remaining than the preset maximum value,
-    // we are facing an overstock and should sell off some of the excess stock
-    data.overstock = data.remaining > data.stock_max;
+      // if there are more days of stock remaining than the preset maximum value,
+      // we are facing an overstock and should sell off some of the excess stock
+      data.overstock = data.remaining > data.stock_max;
 
-    // if the quantity of stock is zero, we have a stockout and must purchase stock
-    // immediately.
-    data.stockout = data.quantity === 0;
+      // if the quantity of stock is zero, we have a stockout and must purchase stock
+      // immediately.
+      data.stockout = data.quantity === 0;
 
-    // TODO
-    // We should probably have a fixed quantity of stock that a pharmacist can
-    // set for the minimum and maximum values of stock they are comfortable
-    // allowing in their pharmacy.
-    //
-    // For example, the pharamacy cannot handle (no storage space) 1,000
-    // mosquito nets, so the maximum is 800.  Similarly, the pharmacist is
-    // comfortable (from experience) with only 200 vials of penicillin.  They
-    // can set that as the minimum value.
-    //
-    // This would make the calculation for shortage
-    //  data.shortage = data.remaining < MAX(data.min_days, data.min_quantity)
-    // and similarly for overstock
-    //  data.overstock = data.remaining > MIN(data.max_days, data.max_quantity)
-    //
-    // You can then toggle a warning or error state based on which condition was
-    // violated - the quantity or the number of days.
+      // TODO
+      // We should probably have a fixed quantity of stock that a pharmacist can
+      // set for the minimum and maximum values of stock they are comfortable
+      // allowing in their pharmacy.
+      //
+      // For example, the pharmacy cannot handle (no storage space) 1,000
+      // mosquito nets, so the maximum is 800.  Similarly, the pharmacist is
+      // comfortable (from experience) with only 200 vials of penicillin.  They
+      // can set that as the minimum value.
+      //
+      // This would make the calculation for shortage
+      //  data.shortage = data.remaining < MAX(data.min_days, data.min_quantity)
+      // and similarly for overstock
+      //  data.overstock = data.remaining > MIN(data.max_days, data.max_quantity)
+      //
+      // You can then toggle a warning or error state based on which condition was
+      // violated - the quantity or the number of days.
 
-    return data;
-  });
+      return data;
+    });
 }
