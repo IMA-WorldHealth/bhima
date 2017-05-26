d63028cbf0b795ee4cc3225b220edf87b0996670
Jonathan Niles <jonathanwniles@gmail.com>
2017-05-26 13:11:34 +0100

    test(VoucherForm): add unit tests for VoucherForm
    This commit adds client unit tests with karma for the
    VoucherFormService.  This should ensure that any API changes that occur
    to the Voucher Form Service are properly tracked and should give us more
    confidence in its ability to properly validate and calculate totals.

--- client/src/modules/transaction-type/transaction-type-store.service.js
+++ client/src/modules/transaction-type/transaction-type-store.service.js
@@ -12,25 +12,24 @@ function TransactionTypeStoreService($q, TransactionType, Store) {
   var data = new Store();
   var initialLoad = true;
 
-  var request = TransactionType.read()
-    .then(function (result) {
-      data.setData(result);
-      initialLoad = false;
-      return data;
-    });
-
   service.load = transactionTypeStore;
   service.refresh = refresh;
 
   function refresh() {
     TransactionType.read()
-    .then(function (result) {
-      data.setData(result);
-    });
+      .then(function (result) {
+        data.setData(result);
+      });
   }
 
   function transactionTypeStore() {
+    var request = TransactionType.read()
+      .then(function (result) {
+        data.setData(result);
+        initialLoad = false;
+        return data;
+      });
+
     return initialLoad ? request : $q.resolve(data);
   }
-
 }
