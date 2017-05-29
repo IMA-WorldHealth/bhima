36e72c971aa020f2e4750ed28e169e01ae420779
Chris LOMAME <lomamech@gmail.com>
2017-05-29 12:08:31 +0200

    chore(i18n): rename "tools" to "menu" (#1695)
    * Using Menus instead all Util to Menu
    
    * Using only one key for Menu in Form.json and update Menus to Menu`

--- client/src/modules/vouchers/complex-voucher.ctrl.js
+++ client/src/modules/vouchers/complex-voucher.ctrl.js
@@ -55,7 +55,7 @@ function ComplexJournalVoucherController(Vouchers, $translate, Currencies, Sessi
 
   // breadcrumb dropdown
   vm.dropdown = [{
-    label  : 'VOUCHERS.GLOBAL.TOOLS',
+    label  : 'FORM.LABELS.MENU',
     color  : 'btn-default',
     icon   : 'fa-cogs',
     option : Toolkit.options,
