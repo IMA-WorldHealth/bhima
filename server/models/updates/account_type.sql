23028ef8e54d040955f9551019f678541b7c7371
Bruce Mbayo <mbayopanda@gmail.com>
2017-07-10 13:43:47 -0700

    fix and enhancements

--- server/models/updates/account_type.sql
+++ server/models/updates/account_type.sql
@@ -9,8 +9,12 @@ UPDATE account SET type_id = 4 WHERE type_id = 1;
 -- expense
 UPDATE account SET type_id = 5 WHERE type_id = 2;
 
+-- equity
+UPDATE account SET type_id = 3 WHERE LEFT(number, 1) IN (1) AND type_id = 3;
+
 -- assets
 UPDATE account SET type_id = 1 WHERE LEFT(number, 1) IN (2, 3, 4, 5) AND type_id = 3;
 
--- equity
-UPDATE account SET type_id = 3 WHERE LEFT(number, 1) IN (1) AND type_id = 3;
+-- update exploitation account
+update account set type_id = 5 where LEFT(number, 1) = 6 AND type_id <> 6;
+update account set type_id = 4 where LEFT(number, 1) = 7 AND type_id <> 6;
