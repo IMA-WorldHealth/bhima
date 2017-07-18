5e7056b2e665d032a9e171f9df803ff30e2f4c16
Jonathan Niles <jonathanwniles@gmail.com>
2017-07-18 11:50:10 -0400

    fix(reports): hack to refresh reportData
    This commit implements a $rootScope hack to refresh the $state's
    reportData.
    
    Closes #1821.

--- client/src/modules/reports/reports.routes.js
+++ client/src/modules/reports/reports.routes.js
@@ -3,7 +3,7 @@ angular.module('bhima.routes')
     // a list of all supported reported and their respective keys, this allows
     // the ui-view to be populated with the correct report configuration form
     /* @const */
-    var SUPPORTED_REPORTS = [      
+    var SUPPORTED_REPORTS = [
       'cash_report',
       'account_report',
       'balance_sheet_report',
@@ -40,7 +40,7 @@ angular.module('bhima.routes')
         url : '/'.concat(key),
         controller : key.concat('Controller as ReportConfigCtrl'),
         templateUrl : '/modules/reports/generate/'.concat(key, '/', key, '.html'),
-        params : { key : key },
+        params : { key : key }
       });
     });
   }]);
