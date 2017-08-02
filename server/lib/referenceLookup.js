290439e8eb5bc8706fc57e67db1212877bf9939e
Steven Fountain <stevenfountain@imaworldhealth.org>
2017-08-02 13:31:49 +0100

    fix(libs) handle Bad Request and 404 cases for references
    This commit introduces checks to ensure that the server does not
    get stuck in a redirect loop if invalid information is passed
    to the reference lookup method.

--- server/lib/referenceLookup.js
+++ server/lib/referenceLookup.js
@@ -3,20 +3,14 @@ const _ = require('lodash');
 
 // module dependencies
 const db = require('../lib/db');
-// const NotFound = require('../lib/errors/NotFound');
-// const BadRequest = require('../lib/errors/BadRequest');
 const identifiers = require('../config/identifiers');
+const BadRequest = require('../lib/errors/BadRequest');
 
 exports.getEntity = getEntity;
 
 // loading identifiers keys, used for defining the table name
 const identifiersIndex = {};
-function indexIdentifiers() {
-  _.forEach(identifiers, (entity) => {
-    identifiersIndex[entity.key] = entity;
-  });
-}
-
+indexIdentifiers();
 
 // This function render a report in the browser
 // It search a saved entity
@@ -24,29 +18,39 @@ function indexIdentifiers() {
 // The reference code is a combination of table_key.project_abbr.reference
 // The table name is variable, it can be :invoice, cash or voucher
 function getEntity(req, res, next) {
-  // loading identifiers keyies, used for defining the table name
-  indexIdentifiers();
-
   const codeRef = req.params.codeRef.split('.');
   const language = req.params.language;
 
   const code = codeRef[0];
   const projectName = codeRef[1];
-  const refence = codeRef[2];
+  const reference = codeRef[2];
   const documentDefinition = identifiersIndex[code];
 
+  if (!documentDefinition) {
+    throw new BadRequest(`Invalid document type provided - '${code}'`);
+  }
+
   const query = `
-    SELECT BUID(uuid) as uuid FROM ${documentDefinition.table}  tb, project p
-    WHERE tb.project_id=p.id AND p.abbr=? AND tb.reference=?
+    SELECT BUID(uuid) as uuid 
+    FROM ${documentDefinition.table} as documentTable JOIN project ON documentTable.project_id = project.id
+    WHERE project.abbr = ? AND documentTable.reference = ?
   `;
 
   // search for full UUID
-  db.one(query, [projectName, refence])
-    .then(result => {
-      const uuid = result.uuid;
+  db.one(query, [projectName, reference])
+    .then((entity) => {
+      const uuid = entity.uuid;
       const url = `${documentDefinition.documentPath}${uuid}?lang=${language}&renderer=pdf`;
+
       res.redirect(url);
     })
     .catch(next)
     .done();
 }
+
+function indexIdentifiers() {
+  _.forEach(identifiers, (entity) => {
+    identifiersIndex[entity.key] = entity;
+  });
+} 
+
