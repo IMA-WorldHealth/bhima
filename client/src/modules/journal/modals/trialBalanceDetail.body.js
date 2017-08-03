39b3ede5494c002e54383139fb70705c21662204
Jonathan Niles <jonathanwniles@gmail.com>
2017-08-03 11:09:59 +0100

    refactor(trial balance): use standard colors
    This commit migrates the Trial Balance to using standardized colors for
    the ui-grid on success/error states. These colors are based off the
    bootstrap colors already ubiquitous throughout the application.
    
    Closes #1717.

--- client/src/modules/journal/modals/trialBalanceDetail.body.js
+++ client/src/modules/journal/modals/trialBalanceDetail.body.js
@@ -2,7 +2,7 @@ angular.module('bhima.controllers')
   .controller('TrialBalanceDetailBodyController', TrialBalanceDetailBodyController);
 
 TrialBalanceDetailBodyController.$inject = [
-  'SessionService', 'TrialBalanceService', 'GridGroupingService', '$state', '$stateParams'
+  'SessionService', 'TrialBalanceService', 'GridGroupingService', '$state', '$stateParams',
 ];
 
 /**
@@ -13,28 +13,35 @@ TrialBalanceDetailBodyController.$inject = [
  */
 function TrialBalanceDetailBodyController(Session, trialBalanceService, Grouping, $state, $stateParams) {
   var vm = this;
-  var cssClass = trialBalanceService.getCSSClass($stateParams.feedBack);
 
-  var columns = [
-    { field : 'trans_id', displayName : 'TABLE.COLUMNS.TRANSACTION', headerCellFilter: 'translate', headerCellClass : cssClass},
-    { field : 'account_number', displayName : 'TABLE.COLUMNS.ACCOUNT', headerCellFilter: 'translate', headerCellClass : cssClass},
-    {
-      field            : 'debit_equiv',
-      displayName      : 'TABLE.COLUMNS.DEBIT',
-      headerCellFilter : 'translate',
-      headerCellClass  : cssClass,
-      cellFilter       : 'currency:' + Session.enterprise.currency_id,
-      cellClass        : 'text-right',
-    },
-    {
-      field            : 'credit_equiv',
-      displayName      : 'TABLE.COLUMNS.CREDIT',
-      headerCellFilter : 'translate',
-      headerCellClass  : cssClass,
-      cellFilter       : 'currency:' + Session.enterprise.currency_id,
-      cellClass        : 'text-right',
-    }
-  ];
+  var hasErrorState = $state.params.feedBack.hasError;
+  var columnCssClass = hasErrorState ? 'bg-danger' : 'bg-primary';
+
+  var columns = [{
+    field : 'trans_id',
+    displayName : 'TABLE.COLUMNS.TRANSACTION',
+    headerCellFilter : 'translate',
+    headerCellClass : columnCssClass,
+  }, {
+    field : 'account_number',
+    displayName : 'TABLE.COLUMNS.ACCOUNT',
+    headerCellFilter : 'translate',
+    headerCellClass : columnCssClass,
+  }, {
+    field            : 'debit_equiv',
+    displayName      : 'TABLE.COLUMNS.DEBIT',
+    headerCellFilter : 'translate',
+    headerCellClass  : columnCssClass,
+    cellFilter       : 'currency:'.concat(Session.enterprise.currency_id),
+    cellClass        : 'text-right',
+  }, {
+    field            : 'credit_equiv',
+    displayName      : 'TABLE.COLUMNS.CREDIT',
+    headerCellFilter : 'translate',
+    headerCellClass  : columnCssClass,
+    cellFilter       : 'currency:'.concat(Session.enterprise.currency_id),
+    cellClass        : 'text-right',
+  }];
 
   vm.stateParams = $stateParams;
 
@@ -51,15 +58,18 @@ function TrialBalanceDetailBodyController(Session, trialBalanceService, Grouping
   vm.enterprise = Session.enterprise;
   vm.gridOptions.data = $stateParams.lines;
 
-  /**
-   * @function viewErrorList
-   * @description
-   * This function is responsible of switching the view from trialBalanceDetail to trialBalanceError
-   **/
+  /*
+  * @function viewErrorList
+  * @description
+  * This function is responsible of switching the view from trialBalanceDetail to trialBalanceError
+  */
   function viewErrorList() {
     // FIX ME : what is the good way? storing original data in appcache?
     var lines = trialBalanceService.parseErrorRecord($stateParams.errors);
-    $state.go('trialBalanceErrors', { lines: lines, feedBack: $stateParams.feedBack, records: $stateParams.records }, { reload : false });
+    $state.go('trialBalanceErrors',
+      { lines: lines, feedBack: $stateParams.feedBack, records: $stateParams.records },
+      { reload : false }
+    );
   }
 
   vm.viewErrorList = viewErrorList;
