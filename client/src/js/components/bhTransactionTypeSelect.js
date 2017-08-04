28c8769a1943bbffa7785ac764f889eff20bc99d
DedrickEnc <kitamuka@gmail.com>
2017-08-04 15:30:55 +0100

    feat(component): Add a transaction type selection component

---
+++ client/src/js/components/bhTransactionTypeSelect.js
@@ -0,0 +1,53 @@
+angular.module('bhima.components')
+  .component('bhTransactionTypeSelect', {
+    templateUrl : 'modules/templates/bhTransactionTypeSelect.tmpl.html',
+    controller  : transactionTypeSelectController,
+    bindings    : { 
+      label            : '@?',
+      onSelectCallback : '&',
+      onRemoveCallback : '&',
+      formName         : '@?',
+    },
+  });
+
+transactionTypeSelectController.$inject = [
+  'TransactionTypeService', 'NotifyService', '$translate'
+];
+
+/**
+ * transaction type Selection Component
+ *
+ */
+function transactionTypeSelectController(TransactionTypes, Notify, $translate) {
+  var $ctrl = this;
+
+  $ctrl.$onInit = function onInit() {
+    //label to display
+    $ctrl.label = $ctrl.label || 'FORM.LABELS.TRANSACTION_TYPE';
+
+    // fired when a transaction type has been selected
+    $ctrl.onSelectCallback = $ctrl.onSelectCallback || angular.noop;
+
+    // default for form name
+    $ctrl.formName = $ctrl.formName || 'TransactionTypeForm';
+
+    // load all Transaction types
+    TransactionTypes.read()
+      .then(function (tts) {
+        tts.forEach(function(item){
+          item.plainText = $translate.instant(item.text);
+        });
+        $ctrl.transactionTypes = tts;
+      })
+      .catch(Notify.handleError);
+  };
+
+  // fires the onSelectCallback bound to the component
+  $ctrl.onSelect = function (models) {
+    $ctrl.onSelectCallback({ transactionTypes : models });
+  };
+
+  $ctrl.onRemove = function (models) {
+    $ctrl.onRemoveCallback({ transactionTypes : models });
+  };
+}
