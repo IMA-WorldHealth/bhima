ff1242997c9e0ba6f857729b881c469e35ec92ed
Steven Fountain <stevenfountain@imaworldhealth.org>
2017-08-09 18:10:03 +0100

    feat(journal) editing ui grid transaction rows
    This commit introduces row editing for shared and individual row
    values. UI Grid rows directly edit the underlying data and shared
    values are applied to every grid row.
    
    Objects are set up for tracking added, removed and edited rows.

--- client/src/modules/journal/modals/editTransaction.modal.js
+++ client/src/modules/journal/modals/editTransaction.modal.js
@@ -1,12 +1,13 @@
 angular.module('bhima.controllers')
   .controller('JournalEditTransactionController', JournalEditTransactionController);
 
-JournalEditTransactionController.$inject = ['JournalService', '$uibModalInstance', 'transactionUuid'];
+JournalEditTransactionController.$inject = ['JournalService', 'Store', 'TransactionTypeService', '$uibModalInstance', 'transactionUuid', 'uiGridConstants', 'uuid'];
 
-function JournalEditTransactionController(Journal, Modal, transactionUuid) { 
-  try { 
+function JournalEditTransactionController(Journal, Store, TransactionType, Modal, transactionUuid, uiGridConstants, uuid) { 
+  // try { 
+  var gridApi = {};
   var vm = this;
-  
+  console.log('got here'); 
   // @TODO(sfount) column definitions currently duplicated across journal and here
   var editColumns = [ 
     { field              : 'description',
@@ -25,18 +26,20 @@ function JournalEditTransactionController(Journal, Modal, transactionUuid) {
       displayName                      : 'TABLE.COLUMNS.DEBIT',
       headerCellFilter                 : 'translate',
       enableFiltering : true,
-      footerCellFilter : 'currency:grid.appScope.enterprise.currency_id'
+      aggregationHideLabel : true,
+      aggregationType : uiGridConstants.aggregationTypes.sum
     },
     { field                            : 'credit_equiv',
       displayName                      : 'TABLE.COLUMNS.CREDIT',
       headerCellFilter                 : 'translate',
       enableFiltering : true,
-      footerCellFilter : 'currency:grid.appScope.enterprise.currency_id'
+      aggregationHideLabel : true,
+      aggregationType : uiGridConstants.aggregationTypes.sum
     },
     { field                : 'hrEntity',
       displayName          : 'TABLE.COLUMNS.RECIPIENT',
       headerCellFilter     : 'translate',
-      editableCellTemplate : '/modules/journal/templates/entity.edit.html',
+      // editableCellTemplate : '/modules/journal/templates/entity.edit.html',
       visible              : true },
     { field            : 'hrReference',
       displayName      : 'TABLE.COLUMNS.REFERENCE',
@@ -47,7 +50,15 @@ function JournalEditTransactionController(Journal, Modal, transactionUuid) {
   ];
 
   vm.gridOptions = { 
-    columnDefs : editColumns
+    columnDefs : editColumns, 
+    showColumnFooter : true,
+    // enableCellEditOnFocus : true
+    showGridFooter : true,
+    gridFooterTemplate : '<div class="ui-grid-cell-contents"><span translate>POSTING_JOURNAL.ROWS</span> <span>{{grid.rows.length}}</span></div>', 
+    onRegisterApi : function (api) { 
+      gridApi = api; 
+      gridApi.edit.on.afterCellEdit(null, handleCellEdit);
+    }
   };
 
   vm.close = Modal.close;
@@ -55,20 +66,31 @@ function JournalEditTransactionController(Journal, Modal, transactionUuid) {
   // @TODO(sfount) move to component
   vm.dateEditorOpen = false;
   vm.openDateEditor = function () { vm.dateEditorOpen = !vm.dateEditorOpen; }
+ 
+  console.log('here too');
+  // module dependencies 
+  TransactionType.read()
+    .then(function (typeResults) { 
+      vm.transactionTypes = typeResults;
+    });
 
+  TransactionType.read
   Journal.grid(transactionUuid)
     .then(function (transaction) { 
       vm.transactionDetails = transaction.aggregate[0];
-      vm.rows = transaction.journal;
+      
+      vm.rows = new Store({ identifier : 'uuid' });
+      vm.rows.setData(transaction.journal);
 
       // @FIXME(sfount) date ng-model hack 
-      vm.rows.forEach(function (row) { row.trans_date = new Date(row.trans_date); });
+      vm.rows.data.forEach(function (row) { row.trans_date = new Date(row.trans_date); });
 
-      vm.shared = sharedDetails(vm.rows[0]);
+      vm.shared = sharedDetails(vm.rows.data[0]);
 
-      vm.gridOptions.data = vm.rows;
+      vm.gridOptions.data = vm.rows.data;
       console.log('got info on transaction', transaction);
-    });
+    })
+    .catch(console.error);
   
   // takes a transaction row and returns all parameters that are shared among the transaction
   // @TODO(sfount) rewrite method given current transaction service code
@@ -82,7 +104,77 @@ function JournalEditTransactionController(Journal, Modal, transactionUuid) {
     return shared;
   }
   
-  } catch(e) { 
-    console.error(e);
+  
+  // Variables for tracking edits
+  var removedRows = [];
+  var addedRows = [];
+  var changes = {};
+
+  // at submission time, added rows are seperated and sent on their own
+  // removed rows are sent from those that haven't been added 
+  // edits are submitted with removed rows removed
+
+  // Editing global transaction attributes 
+  vm.handleTransactionTypeChange = function handleTransactionTypeChange(currentValue) { 
+    applyAttributeToRows('origin_id', currentValue);
+    
+    console.log(vm.rows.data);
+  };
+
+  vm.handleTransactionDateChange = function handleTransactionDateChange(currentValue) { 
+    applyAttributeToRows('trans_date', currentValue);
+
+    console.log(vm.rows.data);
+  };
+
+  // Edit rows functions
+  vm.addRow = function addRow() { 
+    var row = { uuid : uuid(), debit_equiv : 0, credit_equiv : 0 };
+    angular.extend(row, angular.copy(vm.shared));
+
+    addedRows.push(row.uuid);
+    vm.rows.post(row);
+  };
+
+  vm.removeRows = function removeRows() { 
+    var selectedRows = gridApi.selection.getSelectedRows();
+
+    selectedRows.forEach(function (row) { 
+      removedRows.push(row.uuid);
+      vm.rows.remove(row.uuid) 
+    }); 
   }
+
+  function handleCellEdit(rowEntity, colDef, newValue, oldValue) { 
+    if (oldValue !== newValue) { 
+      var isOriginalRow = !contains(addedRows, rowEntity.uuid);
+
+      if (isOriginalRow) { 
+        changes[rowEntity.uuid] = changes[rowEntity.uuid] || {};
+        console.log(changes);
+        console.log(colDef);
+        changes[rowEntity.uuid][colDef.field] = newValue;
+      }
+    }
+
+    console.log(addedRows);
+    console.log(removedRows);
+    console.log(changes);
+  }
+
+  // Edit utilities 
+  function applyAttributeToRows(key, value) { 
+    vm.rows.data.forEach(function (row) { 
+      row[key] = value;
+    });
+  }
+
+  function contains(array, value) {
+    return array.indexOf(value) !== -1;
+  }
+
+  // } catch(e) { 
+    // console.error(e);
+  // }
+
 }
