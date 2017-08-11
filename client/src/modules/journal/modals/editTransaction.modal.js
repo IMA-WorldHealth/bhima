b9fb9623420307357764e356cc1afafba5fcafd3
Steven Fountain <stevenfountain@imaworldhealth.org>
2017-08-11 18:35:11 +0100

    refactor(journal) remove legacy in-line editing code
    This commit refactors the journal and core grid services to rely
    on the new feature modal editing and removes legacy structur required
    to edit in-line.
    
    It also updates the optimistic updates on the Journal module with a
    visual warning for not being able to not having enough information to
    update the client (with a prompt to refresh the data set as it may be
    out of date).

--- client/src/modules/journal/modals/editTransaction.modal.js
+++ client/src/modules/journal/modals/editTransaction.modal.js
@@ -16,20 +16,19 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
       displayName        : 'TABLE.COLUMNS.DESCRIPTION',
       headerCellFilter   : 'translate',
     },
+
     { field                : 'account_number',
       displayName          : 'TABLE.COLUMNS.ACCOUNT',
       editableCellTemplate : '<div><div ui-grid-edit-account></div></div>',
-      enableCellEdit       : true,
       cellTemplate         : '/modules/journal/templates/account.cell.html',
-      headerCellFilter     : 'translate',
-    }, {
-      field                            : 'debit_equiv',
+      headerCellFilter     : 'translate' }, 
+    
+    { field                            : 'debit_equiv',
       displayName                      : 'TABLE.COLUMNS.DEBIT',
       headerCellFilter                 : 'translate',
-      enableFiltering : true,
       aggregationHideLabel : true,
-      aggregationType : uiGridConstants.aggregationTypes.sum
-    },
+      aggregationType : uiGridConstants.aggregationTypes.sum },
+
     { field                            : 'credit_equiv',
       displayName                      : 'TABLE.COLUMNS.CREDIT',
       headerCellFilter                 : 'translate',
@@ -40,20 +39,18 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
     { field                : 'hrEntity',
       displayName          : 'TABLE.COLUMNS.RECIPIENT',
       headerCellFilter     : 'translate',
-      // editableCellTemplate : '/modules/journal/templates/entity.edit.html',
       visible              : true },
+
     { field            : 'hrReference',
       displayName      : 'TABLE.COLUMNS.REFERENCE',
       cellTemplate     : '/modules/journal/templates/references.link.html',
       headerCellFilter : 'translate',
-      visible          : true
-    }
+      visible          : true }
   ];
 
   vm.gridOptions = { 
     columnDefs : editColumns, 
     showColumnFooter : true,
-    // enableCellEditOnFocus : true
     showGridFooter : true,
     gridFooterTemplate : '<div class="ui-grid-cell-contents"><span translate>POSTING_JOURNAL.ROWS</span> <span>{{grid.rows.length}}</span></div>', 
     onRegisterApi : function (api) { 
@@ -62,11 +59,9 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
     }
   };
 
-  vm.validation = { 
-    errored : false
-  };
+  vm.validation = { errored : false };
 
-  vm.close = Modal.close;
+  vm.close = Modal.dismiss;
 
   // @TODO(sfount) apply read only logic to save buttons and grid editing logic
   vm.readOnly = readOnly || false;
@@ -75,7 +70,6 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
   vm.dateEditorOpen = false;
   vm.openDateEditor = function () { vm.dateEditorOpen = !vm.dateEditorOpen; }
  
-  console.log('here too');
   // module dependencies 
   TransactionType.read()
     .then(function (typeResults) { 
@@ -93,52 +87,28 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
 
       // @FIXME(sfount) date ng-model hack 
       vm.rows.data.forEach(function (row) { row.trans_date = new Date(row.trans_date); });
-
       vm.shared = sharedDetails(vm.rows.data[0]);
-
       vm.gridOptions.data = vm.rows.data;
-      console.log('got info on transaction', transaction);
     })
     .catch(function (error) { 
       vm.hasError = true;  
     })
     .finally(function () { 
       vm.loadingTransaction = false; 
-    });
-  
-  // takes a transaction row and returns all parameters that are shared among the transaction
-  // @TODO(sfount) rewrite method given current transaction service code
-  function sharedDetails(row) { 
-    var columns = ['hrRecord', 'record_uuid', 'project_name', 'trans_id', 'origin_id', 'display_name', 'trans_date', 'project_id', 'fiscal_year_id', 'currency_id', 'user_id', 'period_id'];
-    var shared = {};
-  
-    columns.forEach(function (column) { 
-      shared[column] = row[column];
-    });
-    return shared;
-  }
-  
+    });  
   
   // Variables for tracking edits
   var removedRows = [];
   var addedRows = [];
   var changes = {};
 
-  // at submission time, added rows are seperated and sent on their own
-  // removed rows are sent from those that haven't been added 
-  // edits are submitted with removed rows removed
-
   // Editing global transaction attributes 
   vm.handleTransactionTypeChange = function handleTransactionTypeChange(currentValue) { 
     applyAttributeToRows('origin_id', currentValue);
-    
-    console.log(vm.rows.data);
   };
 
   vm.handleTransactionDateChange = function handleTransactionDateChange(currentValue) { 
     applyAttributeToRows('trans_date', currentValue);
-
-    console.log(vm.rows.data);
   };
 
   // Edit rows functions
@@ -151,12 +121,6 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
   };
 
   vm.saveTransaction = function saveTransaction() { 
-    console.log('saving');
-    console.log('shared info', vm.shared);
-    console.log('added', addedRows);
-    console.log('removed', removedRows);
-    console.log('edits', changes);
-
     var noChanges = addedRows.length === 0 && removedRows.length === 0 && Object.keys(changes).length === 0;
 
     if (noChanges) { 
@@ -168,13 +132,9 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
     vm.validation.errored = false;
     vm.validation.message = null;
     vm.saving = true;
-
-    // @TODO(sfount) run local validation to sanity check basic rules - this should 
-    // be applied in a ValidationService that can be shared between modules dealing
-    // with transaciton rows
   
     // building object to conform to legacy API 
-    // @TODO(sfount) update journal service API for human readable interface
+    // @FIXME(sfount) update journal service API for human readable interface
     var transactionRequest = { 
       uuid : vm.shared.record_uuid, 
       newRows : { data : filterRowsByUuid(vm.rows.data, addedRows) },
@@ -183,14 +143,20 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
 
     Journal.saveChanges(transactionRequest, changes)
       .then(function (resultUpdatedTransaction) { 
-        console.log('got save result', resultUpdatedTransaction);
+        var transaction = new Store({ identifier : 'uuid' });
+        transaction.setData(resultUpdatedTransaction);
 
-        Modal.close(resultUpdatedTransaction);
+        // collapse information for the module that might expect to apply optimistic updates
+        var editSessionResult = { 
+          edited : Object.keys(changes),
+          added : addedRows, 
+          removed : removedRows,
+          updatedTransaction : transaction
+        };
+
+        Modal.close(editSessionResult);
       })
       .catch(function (error) { 
-        // initial errors are handled internally by the modal
-        console.log('server error', error);
-
         vm.validation.errored = true;
         vm.validation.message = error.data.code;
       })
@@ -205,7 +171,6 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
     var result = rows.filter(function (row) { 
       return contains(uuids, row.uuid);
     });
-    console.log('filter rows result', result);
     return result;
   }
 
@@ -234,15 +199,9 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
 
       if (isOriginalRow) { 
         changes[rowEntity.uuid] = changes[rowEntity.uuid] || {};
-        console.log(changes);
-        console.log(colDef);
         changes[rowEntity.uuid][colDef.field] = newValue;
       }
     }
-
-    console.log(addedRows);
-    console.log(removedRows);
-    console.log(changes);
   }
 
   // Edit utilities 
@@ -261,5 +220,17 @@ function JournalEditTransactionController(Journal, Store, TransactionType, Modal
   function invalidate(message) { 
     vm.validation.errored = true;
     vm.validation.message = message;
+  }
+
+  // takes a transaction row and returns all parameters that are shared among the transaction
+  // @TODO(sfount) rewrite method given current transaction service code
+  function sharedDetails(row) { 
+    var columns = ['hrRecord', 'record_uuid', 'project_name', 'trans_id', 'origin_id', 'display_name', 'trans_date', 'project_id', 'fiscal_year_id', 'currency_id', 'user_id', 'period_id'];
+    var shared = {};
+  
+    columns.forEach(function (column) { 
+      shared[column] = row[column];
+    });
+    return shared;
   }
 }
