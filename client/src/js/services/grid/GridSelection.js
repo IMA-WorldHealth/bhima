7e8574238094a9ceebb2061dbe95795ada96860c
Jonathan Niles <jonathanwniles@gmail.com>
2017-08-15 09:47:13 +0100

    chore(journal): merge and update latest journal
    This commit update the Trial Balance's journal branch with @sfount's
    changes, combining the journal.js file and removing duplicated effort
    where possible.

--- client/src/js/services/grid/GridSelection.js
+++ client/src/js/services/grid/GridSelection.js
@@ -3,40 +3,40 @@ angular.module('bhima.services')
 
 GridSelectionService.$inject = ['util'];
 
-function GridSelectionService(util) { 
+function GridSelectionService(util) {
 
-  function GridSelection(gridOptions) { 
+  function GridSelection(gridOptions) {
     // key used to determine if multiple selected rows are in the same group
     this._uniqueKey = 'trans_id';
-    this.selected = { 
-      groups : []
+    this.selected = {
+      groups : [],
     };
 
     this.gridOptions = gridOptions;
 
-    util.after(gridOptions, 'onRegisterApi', function onRegisterApi(api) { 
+    util.after(gridOptions, 'onRegisterApi', function onRegisterApi(api) {
       this._gridApi = api;
-    
+
       this._gridApi.selection.on.rowSelectionChanged(null, selectedHook.bind(this));
       this._gridApi.selection.on.rowSelectionChangedBatch(null, selectedHook.bind(this));
     }.bind(this));
   }
 
-  function selectedHook() { 
+  function selectedHook() {
     var currentSelection = this._gridApi.selection.getSelectedRows();
     var currentGroups = collapseRowsToGroups.bind(this)(currentSelection);
     this.selected.groups = currentGroups;
   }
 
-  function collapseRowsToGroups(rows) { 
+  function collapseRowsToGroups(rows) {
     var groups = {};
-    
-    rows.forEach(function (row) { 
+
+    rows.forEach(function (row) {
       groups[row[this._uniqueKey]] = groups[row[this._uniqueKey]] || [];
       groups[row[this._uniqueKey]].push(row);
     }.bind(this));
     return Object.keys(groups);
   }
-  
+
   return GridSelection;
 }
