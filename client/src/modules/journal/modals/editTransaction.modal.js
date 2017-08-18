dae52c563258938f2477befa73916f682f864335
Steven Fountain <stevenfountain@imaworldhealth.org>
2017-08-18 15:54:39 +0100

    refactor(journal) client side validation; editing posted records
    This commit verifies on the client if the transaction to be edited is
    marked as posted. This allows the application to stop the user before trying
    to edit a posted record, the server will also respond with an invalid message if
    this is circumvented.

--- client/src/modules/journal/modals/editTransaction.modal.js
+++ client/src/modules/journal/modals/editTransaction.modal.js
@@ -3,7 +3,7 @@ angular.module('bhima.controllers')
 
 JournalEditTransactionController.$inject = ['JournalService', 'LanguageService', 'Store', 'TransactionTypeService', '$uibModalInstance', 'transactionUuid', 'readOnly', 'uiGridConstants', 'uuid'];
 
-function JournalEditTransactionController(Journal, Languages, Store, TransactionType, Modal, transactionUuid, readOnly, uiGridConstants, uuid) { 
+function JournalEditTransactionController(Journal, Languages, Store, TransactionType, Modal, transactionUuid, readOnly, uiGridConstants, uuid) {
   var gridApi = {};
   var vm = this;
 
@@ -11,12 +11,17 @@ function JournalEditTransactionController(Journal, Languages, Store, Transaction
   vm.languages = Languages;
   vm.loadingTransaction = false;
   vm.settupComplete = false;
-  
+
   // @TODO(sfount) apply read only logic to save buttons and grid editing logic
   vm.readOnly = readOnly || false;
-  
+
+  vm.validation = {
+    errored : false,
+    blockedPostedTransactionEdit : false
+  };
+
   // @TODO(sfount) column definitions currently duplicated across journal and here
-  var editColumns = [ 
+  var editColumns = [
     { field              : 'description',
       displayName        : 'TABLE.COLUMNS.DESCRIPTION',
       headerCellFilter   : 'translate',
@@ -30,8 +35,8 @@ function JournalEditTransactionController(Journal, Languages, Store, Transaction
       cellTemplate         : '/modules/journal/templates/account.cell.html',
       enableCellEdit : !vm.readOnly,
       allowCellFocus : !vm.readOnly,
-      headerCellFilter     : 'translate' }, 
-    
+      headerCellFilter     : 'translate' },
+
     { field                            : 'debit_equiv',
       displayName                      : 'TABLE.COLUMNS.DEBIT',
       headerCellFilter                 : 'translate',
@@ -65,69 +70,85 @@ function JournalEditTransactionController(Journal, Languages, Store, Transaction
       visible          : true }
   ];
 
-  vm.gridOptions = { 
-    columnDefs : editColumns, 
+  vm.gridOptions = {
+    columnDefs : editColumns,
     showColumnFooter : true,
     showGridFooter : true,
     appScopeProvider : vm,
-    gridFooterTemplate : '<div class="ui-grid-cell-contents"><span translate>POSTING_JOURNAL.ROWS</span> <span>{{grid.rows.length}}</span></div>', 
-    onRegisterApi : function (api) { 
-      gridApi = api; 
+    gridFooterTemplate : '<div class="ui-grid-cell-contents"><span translate>POSTING_JOURNAL.ROWS</span> <span>{{grid.rows.length}}</span></div>',
+    onRegisterApi : function (api) {
+      gridApi = api;
       gridApi.edit.on.afterCellEdit(null, handleCellEdit);
     }
   };
 
-  vm.validation = { errored : false };
-
   vm.close = Modal.dismiss;
-    
+
   // @TODO(sfount) move to component vm.dateEditorOpen = false;
   vm.openDateEditor = function () { vm.dateEditorOpen = !vm.dateEditorOpen; }
- 
-  // module dependencies 
+
+  // module dependencies
   TransactionType.read()
-    .then(function (typeResults) { 
+    .then(function (typeResults) {
       vm.transactionTypes = new Store({ identifier : 'id' });
       vm.transactionTypes.setData(typeResults);
     });
 
   vm.loadingTransaction = true;
   Journal.grid(transactionUuid)
-    .then(function (transaction) { 
+    .then(function (transaction) {
       vm.settupComplete = true;
+
+      verifyEditableTransaction(transaction);
 
       vm.rows = new Store({ identifier : 'uuid' });
       vm.rows.setData(transaction);
 
-      // @FIXME(sfount) date ng-model hack 
+      // @FIXME(sfount) date ng-model hack
       vm.rows.data.forEach(function (row) { row.trans_date = new Date(row.trans_date); });
       vm.shared = sharedDetails(vm.rows.data[0]);
       vm.gridOptions.data = vm.rows.data;
     })
-    .catch(function (error) { 
+    .catch(function (error) {
       console.error(error);
-      vm.hasError = true;  
+      vm.hasError = true;
     })
-    .finally(function () { 
-      vm.loadingTransaction = false; 
-    });  
-  
+    .finally(function () {
+      vm.loadingTransaction = false;
+    });
+
+  function verifyEditableTransaction(transaction) {
+    var posted = transaction[0].posted;
+
+    if (posted) {
+      vm.validation.blockedPostedTransactionEdit = true;
+      vm.readOnly = true;
+
+      // notify the grid of options change - the grid should no longer be editable
+      vm.gridOptions.columnDefs.forEach(function (column) {
+        column.allowCellFocus = !vm.readOnly;
+        column.enableCellEdit = !vm.readOnly;
+      });
+      gridApi.core.notifyDataChange(uiGridConstants.dataChange.ALL);
+    }
+  }
+
   // Variables for tracking edits
   var removedRows = [];
   var addedRows = [];
   var changes = {};
 
-  // Editing global transaction attributes 
-  vm.handleTransactionTypeChange = function handleTransactionTypeChange(currentValue) { 
+  // Editing global transaction attributes
+  vm.handleTransactionTypeChange = function handleTransactionTypeChange(currentValue) {
     applyAttributeToRows('origin_id', currentValue);
   };
 
-  vm.handleTransactionDateChange = function handleTransactionDateChange(currentValue) { 
+  vm.handleTransactionDateChange = function handleTransactionDateChange(currentValue) {
     applyAttributeToRows('trans_date', currentValue);
   };
 
   // Edit rows functions
-  vm.addRow = function addRow() { 
+  vm.addRow = function addRow() {
     var row = { uuid : uuid(), debit_equiv : 0, credit_equiv : 0 };
     angular.extend(row, angular.copy(vm.shared));
 
@@ -135,67 +156,67 @@ function JournalEditTransactionController(Journal, Languages, Store, Transaction
     vm.rows.post(row);
   };
 
-  vm.saveTransaction = function saveTransaction() { 
+  vm.saveTransaction = function saveTransaction() {
     var noChanges = addedRows.length === 0 && removedRows.length === 0 && Object.keys(changes).length === 0;
 
-    if (noChanges) { 
+    if (noChanges) {
       Modal.close();
       return;
     }
 
-    // reset error validation 
+    // reset error validation
     vm.validation.errored = false;
     vm.validation.message = null;
     vm.saving = true;
-  
-    // building object to conform to legacy API 
+
+    // building object to conform to legacy API
     // @FIXME(sfount) update journal service API for human readable interface
-    var transactionRequest = { 
-      uuid : vm.shared.record_uuid, 
+    var transactionRequest = {
+      uuid : vm.shared.record_uuid,
       newRows : { data : filterRowsByUuid(vm.rows.data, addedRows) },
       removedRows : removedRows.map(function (uuid) { return { uuid : uuid }; })
     };
 
     Journal.saveChanges(transactionRequest, changes)
-      .then(function (resultUpdatedTransaction) { 
+      .then(function (resultUpdatedTransaction) {
         var transaction = new Store({ identifier : 'uuid' });
         transaction.setData(resultUpdatedTransaction);
 
         // collapse information for the module that might expect to apply optimistic updates
-        var editSessionResult = { 
+        var editSessionResult = {
           edited : Object.keys(changes),
-          added : addedRows, 
+          added : addedRows,
           removed : removedRows,
           updatedTransaction : transaction
         };
 
         Modal.close(editSessionResult);
       })
-      .catch(function (error) { 
+      .catch(function (error) {
         vm.validation.errored = true;
         vm.validation.message = error.data.code;
       })
-      .finally(function () { 
-        vm.saving = false; 
+      .finally(function () {
+        vm.saving = false;
       });
   };
-  
-  // rows - array of rows 
+
+  // rows - array of rows
   // uuids - array of uuids
-  function filterRowsByUuid(rows, uuids) { 
-    var result = rows.filter(function (row) { 
+  function filterRowsByUuid(rows, uuids) {
+    var result = rows.filter(function (row) {
       return contains(uuids, row.uuid);
     });
     return result;
   }
 
-  vm.removeRows = function removeRows() { 
+  vm.removeRows = function removeRows() {
     var selectedRows = gridApi.selection.getSelectedRows();
 
-    selectedRows.forEach(function (row) { 
+    selectedRows.forEach(function (row) {
       var isOriginalRow = !contains(addedRows, row.uuid);
 
-      if (isOriginalRow) { 
+      if (isOriginalRow) {
         // remove any changes tracked against this record
         delete changes[row.uuid];
         removedRows.push(row.uuid);
@@ -204,24 +225,24 @@ function JournalEditTransactionController(Journal, Languages, Store, Transaction
         addedRows.splice(addedRows.indexOf(row.uuid), 1);
       }
 
-      vm.rows.remove(row.uuid) 
-    }); 
+      vm.rows.remove(row.uuid)
+    });
   }
 
-  function handleCellEdit(rowEntity, colDef, newValue, oldValue) { 
-    if (oldValue !== newValue) { 
+  function handleCellEdit(rowEntity, colDef, newValue, oldValue) {
+    if (oldValue !== newValue) {
       var isOriginalRow = !contains(addedRows, rowEntity.uuid);
 
-      if (isOriginalRow) { 
+      if (isOriginalRow) {
         changes[rowEntity.uuid] = changes[rowEntity.uuid] || {};
         changes[rowEntity.uuid][colDef.field] = newValue;
       }
     }
   }
 
-  // Edit utilities 
-  function applyAttributeToRows(key, value) { 
-    vm.rows.data.forEach(function (row) { 
+  // Edit utilities
+  function applyAttributeToRows(key, value) {
+    vm.rows.data.forEach(function (row) {
       handleCellEdit({ uuid : row.uuid }, { field : key}, value, row[key]);
       row[key] = value;
     });
@@ -232,18 +253,18 @@ function JournalEditTransactionController(Journal, Languages, Store, Transaction
   }
 
   // @TODO(sfount)
-  function invalidate(message) { 
+  function invalidate(message) {
     vm.validation.errored = true;
     vm.validation.message = message;
   }
 
   // takes a transaction row and returns all parameters that are shared among the transaction
   // @TODO(sfount) rewrite method given current transaction service code
-  function sharedDetails(row) { 
+  function sharedDetails(row) {
     var columns = ['hrRecord', 'record_uuid', 'project_name', 'trans_id', 'origin_id', 'display_name', 'trans_date', 'project_id', 'fiscal_year_id', 'currency_id', 'user_id', 'posted', 'period_id'];
     var shared = {};
-  
-    columns.forEach(function (column) { 
+
+    columns.forEach(function (column) {
       shared[column] = row[column];
     });
     return shared;
