4e2393ae257d43a1d9f05b81756b79a71dd19354
Chris Lomame <lomamech@gmail.com>
2017-08-21 14:17:44 +0200

    test(Aged Credit) - Update dataSet and Expected element for the OpenDeptors Regression test

--- test/temporary-financial-reports/openDebtors.js
+++ test/temporary-financial-reports/openDebtors.js
@@ -7,33 +7,27 @@ describe('Open Debtors Report', () => {
   const reportURL = '/reports/finance/debtors/open';
 
   const dataSet = { 
-    source: 'patient-name-asc',
+    order: 'patient-name-asc',
     lang: 'fr',
     renderer: 'json',
     reportId: '8',
     saveReport: '0'
   };
 
-  var debtPaHsp9 = 0.72;
+  var debtPaHsp5 = 0.72;
   var aggregates = {
-    numDebtors: 13,
-    debit: 146.6802,
-    credit: 89.3665,
-    balance: 57.3137 
+    numDebtors: 17,
+    balance: 166.4337, 
   };
 
   it('Returns expected Open Debtor Report ', () => {
     return agent.get(reportURL)
       .query(dataSet)
       .then((result) => {
-        expect(Number(result.body.debtors[12].debt)).to.equal(debtPaHsp9);
-
+        expect(Number(result.body.debtors[5].balance)).to.equal(debtPaHsp5);
         expect(result.body.aggregates.numDebtors).to.equal(aggregates.numDebtors);
-        expect(result.body.aggregates.debit).to.equal(aggregates.debit);
-        expect(result.body.aggregates.credit).to.equal(aggregates.credit);
         expect(result.body.aggregates.balance).to.equal(aggregates.balance);
-        expect(result.body.aggregates.balance).to.not.equal(debtPaHsp9);
-      
+        expect(result.body.aggregates.balance).to.not.equal(debtPaHsp5);      
       });
   });
 
