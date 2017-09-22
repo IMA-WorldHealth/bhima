8dbca7ea65c5cafc7bafd38549327cb040e26521
Chris Lomame <lomamech@gmail.com>
2017-09-22 14:21:59 +0200

    refactor(Account Management) - Upgrate Account management module us Dropdown Menus - Remove bhRenderDropDown - Improve chart handlebars - Add function download in AccountGridService

--- server/controllers/finance/reports/accounts/index.js
+++ server/controllers/finance/reports/accounts/index.js
@@ -24,6 +24,7 @@ function chart(req, res, next) {
     csvKey : 'accounts',
     filename : 'REPORT.CHART_OF_ACCOUNTS',
     orientation : 'landscape',
+    footerRight : '[page] / [toPage]',
   });
 
   try {
