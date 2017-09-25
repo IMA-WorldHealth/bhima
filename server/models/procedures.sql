f26c095ea6508a15720f8a336ed6413ba8ee25b7
James Brooking <jambroo@gmail.com>
2017-09-25 16:06:02 +0100

    added meaningful comments in procedures.sql.

--- server/models/procedures.sql
+++ server/models/procedures.sql
@@ -5,18 +5,49 @@
 
 DELIMITER $$
 
+/*
+  Invoicing procedures include: StageInvoice, StageInvoiceItem,
+  StageBillingService, VerifySubsidyStageTable, PostInvoice, PostingSetupUtil,
+  PostingJournalErrorHandler, CopyInvoiceToPostingJournal and PostToGeneralLedger
+*/
 SOURCE procedures/invoicing.sql
 
+/*
+  Cash procedures include: HandleCashRounding, PostCash, StageCash,
+  StageCashItem, VerifyCashTemporaryTables, CalculateCashInvoiceBalances,
+  WriteCashItems and WriteCash
+*/
 SOURCE procedures/cash.sql
 
-SOURCE procedures/periods.sql
+/*
+  Time period procedures include: CreateFiscalYear, GetPeriodRange and
+  CreatePeriods
+*/
+SOURCE procedures/time_period.sql
 
+/*
+  Voucher procedures include: PostVoucher and ReverseTransaction
+*/
 SOURCE procedures/voucher.sql
 
+/*
+  Location procedures include: MergeLocations
+*/
 SOURCE procedures/location.sql
 
+/*
+  Trial balance procedures include: StageTrialBalanceTransaction and
+  TrialBalance
+*/
 SOURCE procedures/trial_balance.sql
 
+/*
+  Stock procedures include: ComputeStockConsumptionByPeriod,
+  ComputeStockConsumptionByDate and ComputeMovementReference
+*/
 SOURCE procedures/stock.sql
 
+/*
+  Posting procedures include: PostPurchase and PostIntegration
+*/
 SOURCE procedures/posting.sql
