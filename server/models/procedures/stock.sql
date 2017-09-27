77575095ced9bb22a9f2a5132dc50659f8bbec16
Bruce Mbayo <mbayopanda@gmail.com>
2017-09-27 20:48:15 +0100

    mysql procedure for writing into the journal via vouchers

--- server/models/procedures/stock.sql
+++ server/models/procedures/stock.sql
@@ -42,52 +42,8 @@ BEGIN
   ON DUPLICATE KEY UPDATE uuid = uuid;
 END $$
 
-CREATE PROCEDURE CreateStageStockMovement (
-  IN documentUuid BINARY(16),
-  IN isExit TINYINT(1),
-  IN projectId SMALLINT(5),
-  IN currencyId SMALLINT(5)
-)
-BEGIN
-  DECLARE `no_stock_movement_stage` TINYINT(1) DEFAULT 0;
-  DECLARE CONTINUE HANDLER FOR SQLSTATE '42S02' SET `no_stock_movement_stage` = 1;
-  SELECT NULL FROM stage_stock_movement LIMIT 0;
-
-  IF (`no_stock_movement_stage` = 1) THEN
-    CREATE TEMPORARY TABLE stage_stock_movement (
-      SELECT 
-        projectId as project_id, currencyId as currency_id,
-        m.uuid, m.description, m.date, m.flux_id, m.is_exit, m.document_uuid, m.quantity, m.unit_cost, m.user_id,
-        ig.cogs_account, ig.stock_account 
-      FROM stock_movement m 
-      JOIN lot l ON l.uuid = m.lot_uuid
-      JOIN inventory i ON i.uuid = l.inventory_uuid
-      JOIN inventory_group ig 
-        ON ig.uuid = i.group_uuid AND (ig.stock_account IS NOT NULL AND ig.cogs_account IS NOT NULL)
-      WHERE m.document_uuid = documentUuid AND m.is_exit = isExit
-    );
-  ELSE
-    INSERT INTO stage_stock_movement (
-      SELECT 
-        projectId as project_id, currencyId as currency_id,
-        m.uuid, m.description, m.date, m.flux_id, m.is_exit, m.document_uuid, m.quantity, m.unit_cost, m.user_id,
-        ig.cogs_account, ig.stock_account 
-      FROM stock_movement m 
-      JOIN lot l ON l.uuid = m.lot_uuid
-      JOIN inventory i ON i.uuid = l.inventory_uuid
-      JOIN inventory_group ig 
-        ON ig.uuid = i.group_uuid AND (ig.stock_account IS NOT NULL AND ig.cogs_account IS NOT NULL)
-      WHERE m.document_uuid = documentUuid AND m.is_exit = isExit
-    );
-  END IF;
-END $$
-
-CREATE PROCEDURE DropStageStockMovement ()
-BEGIN 
-  DROP TEMPORARY TABLE stage_stock_movement;
-END $$
-
--- post stock movement into journal
+-- post stock movement into vouchers
+DROP PROCEDURE IF EXISTS PostStockMovement;
 CREATE PROCEDURE PostStockMovement (
   IN documentUuid BINARY(16),
   IN isExit TINYINT(1),
@@ -112,12 +68,24 @@ BEGIN
   DECLARE voucher_item_account_credit DECIMAL(19, 4);
   DECLARE voucher_item_voucher_uuid BINARY(16);
   DECLARE voucher_item_document_uuid BINARY(16);
+  
+  -- variables
+	DECLARE v_stock_account INT(10);
+	DECLARE v_cogs_account INT(10);
+	DECLARE v_unit_cost DECIMAL(19, 4);
+	DECLARE v_quantity INT(11);
+	DECLARE v_document_uuid BINARY(16);
+	DECLARE v_is_exit TINYINT(1);
 
 
   -- the cursor 
   DECLARE v_finished INTEGER DEFAULT 0;
+  
+  DECLARE stage_stock_movement_cursor CURSOR FOR 
+  	SELECT temp.stock_account, temp.cogs_account, temp.unit_cost, temp.quantity, temp.document_uuid, temp.is_exit 
+	FROM stage_stock_movement as temp;
+  
   DECLARE CONTINUE HANDLER FOR NOT FOUND SET v_finished = 1;
-  DECLARE stage_stock_movement_cursor CURSOR FOR SELECT stock_account, cogs_account, unit_cost, quantity, document_uuid, is_exit FROM stage_stock_movement;
 
   -- temporarise the stock movement
   CREATE TEMPORARY TABLE stage_stock_movement (
@@ -144,7 +112,7 @@ BEGIN
   SET voucher_amount = (SELECT SUM(unit_cost * quantity) FROM stage_stock_movement);
 
   -- insert into voucher
-  INSERT INTO voucher VALUES (
+  INSERT INTO voucher (uuid, date, project_id, currency_id, user_id, type_id, description, amount) VALUES (
     voucher_uuid, voucher_date, voucher_project_id, voucher_currency_id, voucher_user_id,
     voucher_type_id, voucher_description, voucher_amount
   );
@@ -155,27 +123,27 @@ BEGIN
   -- loop in the cursor
   insert_voucher_item : LOOP
 
-    FETCH stage_stock_movement_cursor INTO stock_account, cogs_account, unit_cost, quantity, document_uuid, is_exit;
+    FETCH stage_stock_movement_cursor INTO v_stock_account, v_cogs_account, v_unit_cost, v_quantity, v_document_uuid, v_is_exit;
 
     IF v_finished = 1 THEN 
       LEAVE insert_voucher_item;
     END IF;
 
-    if (is_exit = 1) THEN
-      SET voucher_item_account_debit = cogs_account;
-      SET voucher_item_account_credit = stock_account;
+    if (v_is_exit = 1) THEN
+      SET voucher_item_account_debit = v_cogs_account;
+      SET voucher_item_account_credit = v_stock_account;
     ELSE
-      SET voucher_item_account_debit = stock_account;
-      SET voucher_item_account_credit = cogs_account;
-    END IF
+      SET voucher_item_account_debit = v_stock_account;
+      SET voucher_item_account_credit = v_cogs_account;
+    END IF;
 
     -- insert debit
     INSERT INTO voucher_item (uuid, account_id, debit, credit, voucher_uuid, document_uuid)
-      VALUES (HUID(UUID()), voucher_item_account_debit, (unitCost * quantity), 0, voucher_uuid, document_uuid);
+      VALUES (HUID(UUID()), voucher_item_account_debit, (v_unit_cost * v_quantity), 0, voucher_uuid, v_document_uuid);
 
     -- insert credit
     INSERT INTO voucher_item (uuid, account_id, debit, credit, voucher_uuid, document_uuid)
-      VALUES (HUID(UUID()), voucher_item_account_credit, 0, (unitCost * quantity), voucher_uuid, document_uuid);
+      VALUES (HUID(UUID()), voucher_item_account_credit, 0, (v_unit_cost * v_quantity), voucher_uuid, v_document_uuid);
 
   END LOOP insert_voucher_item;
 
@@ -183,7 +151,7 @@ BEGIN
   CLOSE stage_stock_movement_cursor;
 
   -- drop the stage tabel
-  CALL DropStageStockMovement();
+  DROP TEMPORARY TABLE stage_stock_movement;
 
   -- post voucher into journal
   CALL PostVoucher(voucher_uuid);
