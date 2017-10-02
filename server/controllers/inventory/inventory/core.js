d9cb36a4155f32cfd8bafbcb36f2d2ce94e0d914
DedrickEnc <kitamuka@gmail.com>
2017-10-02 12:17:52 +0100

    feat(inventory): Adding locking functionnality

--- server/controllers/inventory/inventory/core.js
+++ server/controllers/inventory/inventory/core.js
@@ -115,7 +115,7 @@ function getItemsMetadata(params) {
 
   const sql =
     `SELECT BUID(inventory.uuid) as uuid, inventory.code, inventory.text AS label, inventory.price, iu.text AS unit,
-      it.text AS type, ig.name AS groupName, BUID(ig.uuid) AS group_uuid, inventory.consumable, inventory.stock_min,
+      it.text AS type, ig.name AS groupName, BUID(ig.uuid) AS group_uuid, inventory.consumable,inventory.locked, inventory.stock_min,
       inventory.stock_max, inventory.created_at AS timestamp, inventory.type_id, inventory.unit_id,
       inventory.unit_weight, inventory.unit_volume, ig.sales_account, ig.stock_account, ig.donation_account,
       ig.cogs_account, inventory.default_quantity
@@ -133,6 +133,7 @@ function getItemsMetadata(params) {
   filters.equals('code');
   filters.equals('price');
   filters.equals('consumable');
+  filters.equals('locked');
   filters.equals('label');
 
   filters.custom('inventory_uuids', 'inventory.uuid IN (?)', params.inventory_uuids);
@@ -155,7 +156,7 @@ function getItemsMetadata(params) {
 function getItemsMetadataById(uid) {
   const sql =
     `SELECT BUID(i.uuid) as uuid, i.code, i.text AS label, i.price, iu.text AS unit,
-      it.text AS type, ig.name AS groupName, BUID(ig.uuid) AS group_uuid, i.consumable, i.stock_min,
+      it.text AS type, ig.name AS groupName, BUID(ig.uuid) AS group_uuid, i.consumable, i.locked, i.stock_min,
       i.stock_max, i.created_at AS timestamp, i.type_id, i.unit_id, i.unit_weight, i.unit_volume,
       ig.sales_account, i.default_quantity, i.avg_consumption, i.delay, i.purchase_interval
     FROM inventory AS i JOIN inventory_type AS it
