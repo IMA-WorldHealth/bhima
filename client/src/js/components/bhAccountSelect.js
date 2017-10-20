b80219d8eed9a25b1e82509f87bf7171455b6816
Jonathan Niles <jonathanwniles@gmail.com>
2017-10-20 12:04:00 +0100

    feat: add bh-account-typeahead-inline component
    This commit adds the bhAccountTypeaheadInlineComponent for use in grids,
    such as the Complex Voucher grid.  It is prototyped and demonstrated in
    the Complex Vouchers module.
    
    Closes #1289.

--- client/src/js/components/bhAccountSelect.js
+++ client/src/js/components/bhAccountSelect.js
@@ -11,7 +11,7 @@ angular.module('bhima.components')
       label            : '@?',
       name             : '@?',
       excludeTitleAccounts : '@?',
-      validationTrigger:  '<?',
+      validationTrigger :  '<?',
     },
   });
 
