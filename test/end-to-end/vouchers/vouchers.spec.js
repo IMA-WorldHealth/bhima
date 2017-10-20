99386d7d097b99b6e1388b8bc629b19d0089f6e1
DedrickEnc <kitamuka@gmail.com>
2017-10-20 15:54:11 +0100

    test(e2e): Fix the end to end test for voucjer registry

--- test/end-to-end/vouchers/vouchers.spec.js
+++ test/end-to-end/vouchers/vouchers.spec.js
@@ -6,7 +6,7 @@ const GridRow = require('../shared/GridRow');
 const components = require('../shared/components');
 
 describe('Voucher Registry', function () {
-  const numVouchers = 20;
+  const NUM_VOUCHERS = 20;
   const gridId = 'voucher-grid';
 
   before(() => helpers.navigate('vouchers'));
