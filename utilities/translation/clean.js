d30b8988c13e9f03cde9f718d13721ac3177947e
DedrickEnc <kitamuka@gmail.com>
2017-11-01 02:16:34 +0100

    feat(script): i18n script pruning finished

--- utilities/translation/clean.js
+++ utilities/translation/clean.js
@@ -30,6 +30,8 @@ const selectedExtension = '.html';
 
 const initialRoute = path.resolve(__dirname, routeToken);
 
+const i18nRoute = '../../client/src/i18n/en/';
+
 fileNames = getTemplates(initialRoute, [], false);
 
 fileNames.forEach((item) => {
@@ -42,7 +44,6 @@ fileNames.forEach((item) => {
         if (lastElement) {
             keyString = lastElement.data;
             if (keyString) {
-                // console.log(keyString);
                 en[keyString.split('.')[0]] = removeLastKey(keyString, en);
             }
         }
@@ -50,14 +51,64 @@ fileNames.forEach((item) => {
     });
 });
 
-console.log(en);
+let remainKeys = Object.keys(en).sort();
 
-function getTemplates(route, res, end) {
+remainKeys.forEach((key) => {
+    key = key.toLowerCase().trim();
+    if (key.indexOf('{') === -1 &&
+        key.indexOf('}') === -1 &&
+        key.indexOf('$') === -1 &&
+        key !== '') {
+        try {
+            console.log('traitement', key, '.json');
+            var jsonRoute = path.resolve(__dirname, i18nRoute, key + '.json');
+            let obj = require(jsonRoute);
+            var list = handle(obj, '', []);
+            list.forEach((item) => {
+                if (isPresent(item)) {
+                    obj[item.split('.')[0]] = removeLastKey(item, obj);
+                }
+            });
+
+            let objAsString = JSON.stringify(obj);
+            var f = fs.openSync(jsonRoute, 'w');
+            fs.writeFileSync(f, objAsString, 'utf8');
+        }
+        catch (e) {
+
+        }
+    }
+
+
+});
+
+function handle(obj, path, res) {
+    res = res || [];
+
+    keys = Object.keys(obj).sort();
+
+    keys.forEach((key) => {
+        if (typeof obj[key] === 'object') {
+            if (path.length > 0) {
+                return res.concat(handle(obj[key], path + '.' + key, res));
+            } else {
+                return res.concat(handle(obj[key], key, res));
+            }
+        } else {
+            res.push(path.length > 0 ? path + '.' + key : key);
+        }
+
+    });
+
+    return res
+}
+
+function getTemplates(route, res) {
     res = res || [];
 
     if (fs.lstatSync(route).isDirectory()) {
         fs.readdirSync(route).forEach((file) => {
-            return res.concat(getTemplates(path.resolve(route, file), res, false));
+            return res.concat(getTemplates(path.resolve(route, file), res));
         });
     } else if (!fs.lstatSync(route).isDirectory() && path.extname(route) === selectedExtension) {
         res.push(route);
@@ -76,7 +127,6 @@ function removeLastKey(keyString, baseObejct) {
         } else {
             if (typeof current[next] === 'string') {
                 delete current[next];
-
                 return current;
             } else {
                 return current;
@@ -84,4 +134,39 @@ function removeLastKey(keyString, baseObejct) {
         }
 
     }, {});
+}
+
+function getCleanedPropertyList() {
+    var properties = Object.getOwnPropertyNames(en);
+
+    return properties.reduce((current, property) => {
+        lproperty = property.toLowerCase().trim();
+        if (
+            lproperty.indexOf('{') === -1 &&
+            lproperty.indexOf('}') === -1 &&
+            lproperty.indexOf('$') === -1 &&
+            lproperty !== '' &&
+            current.indexOf(lproperty) === -1) {
+            current.push(lproperty);
+            return current;
+        } else {
+            return current;
+        }
+    }, []);
+}
+
+function isPresent(keyString) {
+    let found = true;
+    let keys = keyString.split('.');
+    let currentObj = en;
+
+    for (var i = 0; i < keys.length; i++) {
+        currentObj = currentObj[keys[i]];
+        if (!currentObj) {
+            found = false;
+            break;
+        }
+    }
+
+    return found;
 }
