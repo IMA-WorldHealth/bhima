caa8e1cc51504a9146e77fee026ec8a78df24c27
Jonathan Niles <jonathanwniles@gmail.com>
2017-11-16 11:48:14 +0100

    test(components): add bhHiddenField unit tests
    This commit adds unit tests for the `bhHiddenField` component.  The
    component is relatively simple - is only shows and hides transcluded
    data.  These tests assert that the data is hidden by default and visible
    when toggled on.

--- client/src/js/components/bhHiddenField.js
+++ client/src/js/components/bhHiddenField.js
@@ -5,22 +5,9 @@
  * This component allows a programmer to optionally hide a field in a form at
  * startup that may toggled open by the user if they need it.
  */
-
-var template =
- '<p>' +
-   '<a ng-click="$ctrl.toggleVisibility()" bh-hidden-field-toggle href>' +
-      '<span ng-hide="$ctrl.visible"> + <span translate>{{ $ctrl.showText }}</span></span>' +
-      '<span ng-show="$ctrl.visible"> - <span translate>{{ $ctrl.hideText }}</span></span>' +
-   '</a>' +
- '</p>' +
- '<div ng-if="$ctrl.visible">' +
-  '<ng-transclude></ng-transclude>' +
- '</div>';
-
-
 angular.module('bhima.components')
   .component('bhHiddenField', {
-    template : template,
+    templateUrl : 'modules/templates/bhHiddenField.html',
     transclude : true,
     controller : bhHiddenFieldController,
     bindings : {
