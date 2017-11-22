a9ea41b140840b4bb83846e2aba0286a54804cb0
jeremielodi <jeremielodi@gmail.com>
2017-11-22 10:05:08 +0100

    removing array's functions

--- client/src/modules/invoices/registry/modalCreditNote.js
+++ client/src/modules/invoices/registry/modalCreditNote.js
@@ -23,34 +23,37 @@ function ModalCreditNoteController(Instance, Invoices, data, Vouchers, Notify, $
       vm.patientInvoiceItems = response.items;
       vm.currencySymbol = CurrencyService.symbol(response.currency_id);
 
-      response.billing.filter(bill => {
-        vm.billingAmount += bill.value;
-      });
-
-      response.subsidy.filter(sub => {
-        vm.subsidyAmount += sub.value;
-      });
+      vm.billingAmount = sum(response.billing, 'value');
+      vm.subsidyAmount = sum(response.subsidy, 'value');
+   
     })
     .catch(Notify.handleError);
 
+  function sum(array, key) {
+    var _sum = 0;
+    for (var i = 0; i < array.length; i++) {
+      _sum += array[i][key];
+    }
+    return _sum;
+  }
+
   function submit(form) {
-     // stop submission if the form is invalid
+    // stop submission if the form is invalid
     if (form.$invalid) { return; }
 
-    var note = angular.copy(vm.creditNote);
+      var note = angular.copy(vm.creditNote);
 
-    var creditNoteMessage = $translate.instant('FORM.INFO.CREDIT_NOTE_INVOICE', {
-      invoiceReference : vm.patientInvoice.reference,
-      description      : vm.creditNote.description,
-      debtorName       : data.invoice.patientName,
-      debtorIdentifier : data.invoice.patientReference,
-    });
+      var creditNoteMessage = $translate.instant('FORM.INFO.CREDIT_NOTE_INVOICE', {
+        invoiceReference : vm.patientInvoice.reference,
+        description      : vm.creditNote.description,
+        debtorName       : data.invoice.patientName,
+        debtorIdentifier : data.invoice.patientReference,
+      });
 
-    note.description = creditNoteMessage;
+      note.description = creditNoteMessage;
 
-    return Vouchers.reverse(note)
-      .then(function () {
-        return Instance.close(true);
+      return Vouchers.reverse(note).then(function () {
+          return Instance.close(true);
       });
   }
 }
