02711291c080d47dc2744da40daa84ba5617ce73
jeremielodi <jeremielodi@gmail.com>
2017-11-23 11:37:07 +0100

    using vm instead of $scope

--- client/src/js/services/Snapshot.js
+++ client/src/js/services/Snapshot.js
@@ -1,6 +1,6 @@
 
 angular.module('bhima.services')
-.service('SnapshotService', SnapshotService);
+  .service('SnapshotService', SnapshotService);
 
 SnapshotService.$inject = ['$uibModal', '$http'];
 
@@ -10,10 +10,11 @@ function SnapshotService($uibModal, $http) {
   service.dataUriToFile = dataUriToFile;
   service.openWebcamModal = openWebcamModal;
 
-  function openWebcamModal() { 
+  function openWebcamModal() {
     return $uibModal.open({
-      templateUrl: 'modules/templates/bhSnapShot.html',
-      controller: snapshotController,
+      templateUrl : 'modules/templates/bhSnapShot.html',
+      controller : snapshotController,
+      controllerAs : 'snapshotCtrl',
       backdrop     : 'static',
       animation    : false,
       size : 'lg',
@@ -23,11 +24,13 @@ function SnapshotService($uibModal, $http) {
   // convert the data_url to a file object
   function dataUriToFile(dataUri, fileName, mimeType) {
     return (
-      $http.get(dataUri, {responseType:'arraybuffer'})
-    .then(function(res){
-      return res.data;
-    })
-    .then(function(buf){return new File([buf], fileName, {type:mimeType});})
+      $http.get(dataUri, { responseType : 'arraybuffer' })
+        .then(function (res) {
+          return res.data;
+        })
+        .then(function(buf) {
+          return new File([buf], fileName, { type : mimeType });
+        })
     );
   }
 
@@ -40,82 +43,76 @@ function SnapshotService($uibModal, $http) {
 
 angular.module('bhima.controllers').controller('snapshotController', snapshotController);
 
-snapshotController.$inject = ['$scope', '$uibModalInstance', 'AppCache'];
+snapshotController.$inject = ['$uibModalInstance', 'AppCache'];
 
-function snapshotController($scope, $uibModalInstance, AppCache) {
-  'use strict';
-  var _video = null, patData = null;
+function snapshotController($uibModalInstance, AppCache) {
+  var vm = this;
+  var _video = null;
+  var patData = null;
 
-  $scope.showDemos = false;
-  $scope.mono = false;
-  $scope.invert = false;
-  $scope.hasDataUrl = false;
+  vm.showDemos = false;
+  vm.mono = false;
+  vm.invert = false;
+  vm.hasDataUrl = false;
   //
   let snapshotCache = AppCache('snapshot', true);
   // help to display the saving botton
 
-  $scope.patOpts = snapshotCache.coordonates || { x: 0, y: 0, w: 25, h: 25 };
+  vm.patOpts = snapshotCache.coordonates || { x : 0, y : 0, w : 25, h : 25 };
 
   // Setup a channel to receive a video property
   // with a reference to the video element
   // See the HTML binding in main.html
-  $scope.channel = {};
+  vm.channel = {};
 
-  $scope.webcamError = false;
-  $scope.onError = function (err) {
-    $scope.$apply(
-      function () {
-        $scope.webcamError = err;
-      }
-    );
+  vm.webcamError = false;
+  vm.onError = function (err) {
+    vm.webcamError = err;
   };
 
-  $scope.onSuccess = function () {  
-  // The video element contains the captured camera data
-    _video = $scope.channel.video;
-    $scope.$apply(function() {
-      $scope.patOpts.w = _video.width;
-      $scope.patOpts.h = _video.height;
-      $scope.showDemos = true;
-    });
+  vm.onSuccess = function () {
+    // The video element contains the captured camera data
+    _video = vm.channel.video;
+    vm.patOpts = snapshotCache.coordonates || { x : 0, y : 0, w : _video.width, h :  _video.height };
+    vm.showDemos = true;
   };
 
   /**
    * Make a snapshot of the camera data and show it in another canvas.
    */
-  $scope.makeSnapshot = function makeSnapshot() {  
+  vm.makeSnapshot = function makeSnapshot() {
     if (_video) {
       var patCanvas = document.querySelector('#snapshot');
       if (!patCanvas) return;
 
-        patCanvas.width = _video.width;
-        patCanvas.height = _video.height;
-        var ctxPat = patCanvas.getContext('2d');
-        var idata = getVideoData($scope.patOpts.x, $scope.patOpts.y, $scope.patOpts.w, $scope.patOpts.h);
-        ctxPat.putImageData(idata, 0, 0);
-        storeImageBase64(patCanvas.toDataURL());
-        patData = idata;
+      patCanvas.width = _video.width;
+      patCanvas.height = _video.height;
+      var ctxPat = patCanvas.getContext('2d');
+      var idata = getVideoData(vm.patOpts.x, vm.patOpts.y, vm.patOpts.w, vm.patOpts.h);
+      ctxPat.putImageData(idata, 0, 0);
+      storeImageBase64(patCanvas.toDataURL());
+      patData = idata;
 
-        // cache storing
-        snapshotCache.coordonates = $scope.patOpts;
+      // cache storing
+      snapshotCache.coordonates = vm.patOpts;
     };
-    }
-    /**
+  }
+  /**
      * Redirect the browser to the URL given.
      * Used to download the image by passing a dataURL string
      */
-    $scope.downloadSnapshot = function downloadSnapshot(dataURL) {
-      window.location.href = dataURL;
-    };
+  vm.downloadSnapshot = function downloadSnapshot(dataURL) {
+    window.location.href = dataURL;
+  };
 
-    var getVideoData = function getVideoData(x, y, w, h) {
-      var hiddenCanvas = document.createElement('canvas');
-      hiddenCanvas.width = _video.width;
-      hiddenCanvas.height = _video.height;
-      var ctx = hiddenCanvas.getContext('2d');
-      ctx.drawImage(_video, 0, 0, _video.width, _video.height);
-      return ctx.getImageData(x, y, w, h);
-    };
+  var getVideoData = function getVideoData(x, y, w, h) {
+    var hiddenCanvas = document.createElement('canvas');
+    hiddenCanvas.width = _video.width;
+    hiddenCanvas.height = _video.height;
+    var ctx = hiddenCanvas.getContext('2d');
+    ctx.drawImage(_video, 0, 0, _video.width, _video.height);
+    return ctx.getImageData(x, y, w, h);
+  };
 
     /**
      * This function could be used to send the image data
@@ -123,10 +120,10 @@ function snapshotController($scope, $uibModalInstance, AppCache) {
      *
      * In this example, we simply store it in the scope for display.
      */
-    var storeImageBase64 = function storeImageBase64(imgBase64) {
-      $scope.snapshotData = imgBase64;
-      $scope.hasDataUrl = true;
-    };
+  var storeImageBase64 = function storeImageBase64(imgBase64) {
+    vm.snapshotData = imgBase64;
+    vm.hasDataUrl = true;
+  };
 
     // var getPixelData = function getPixelData(data, width, col, row, offset) {
     //     return data[((row*(width*4)) + (col*4)) + offset];
@@ -136,13 +133,13 @@ function snapshotController($scope, $uibModalInstance, AppCache) {
     //     data[((row*(width*4)) + (col*4)) + offset] = value;
     // };
 
-    (function() {
-        var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+  (function () {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
                                     window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-        window.requestAnimationFrame = requestAnimationFrame;
-    })();
+    window.requestAnimationFrame = requestAnimationFrame;
+  })();
 
-    var start = Date.now();
+  var start = Date.now();
 
   /**
    * Apply a simple edge detection filter.
@@ -156,13 +153,13 @@ function snapshotController($scope, $uibModalInstance, AppCache) {
       var resCanvas = document.querySelector('#result');
 
       if (!resCanvas) return;
-    
+
       resCanvas.width = _video.width;
       resCanvas.height = _video.height;
       var ctxRes = resCanvas.getContext('2d');
       ctxRes.putImageData(videoData, 0, 0);
       // apply edge detection to video image
-      Pixastic.process(resCanvas, "edges", {mono:$scope.mono, invert : $scope.invert});
+      Pixastic.process(resCanvas, "edges", {mono : vm.mono, invert : vm.invert});
     }
 
     if (progress < 20000) {
@@ -170,11 +167,11 @@ function snapshotController($scope, $uibModalInstance, AppCache) {
     }
   }
 
-  $scope.getDataUrl = function () {
-    $uibModalInstance.close($scope.snapshotData);
+  vm.getDataUrl = function () {
+    $uibModalInstance.close(vm.snapshotData);
   }
 
-  $scope.closeModal = function () {
+  vm.closeModal = function () {
     $uibModalInstance.dismiss('cancel');
   };
   requestAnimationFrame(applyEffects);
