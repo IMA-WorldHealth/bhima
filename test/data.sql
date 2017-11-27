4189c4f780f80b9635f05beb900d4bc5ba5f79c5
Bruce Mbayo <mbayopanda@gmail.com>
2017-11-27 14:04:57 +0100

    fix integration test data related to location

--- test/data.sql
+++ test/data.sql
@@ -1,11 +1,5 @@
 -- bhima test database
 
--- locations (enterprise location only)
-INSERT INTO `country` VALUES (HUID('dbe330b6-5cde-4830-8c30-dc00eccd1a5f'),'Democratiq Republic of Congo'),(HUID('dbe330b6-5cde-4830-8c30-dc00eccd1a51'), 'Test Hook Country');
-INSERT INTO `province` VALUES (HUID('f6fc7469-7e58-45cb-b87c-f08af93edade'),'Bas Congo', HUID('dbe330b6-5cde-4830-8c30-dc00eccd1a5f')), (HUID('dbe330b6-5cdf-4830-8c30-dc00eccd1a21'), 'Test Hook Province', HUID('dbe330b6-5cde-4830-8c30-dc00eccd1a51'));
-INSERT INTO `sector` VALUES (HUID('0404e9ea-ebd6-4f20-b1f8-6dc9f9313450'),'Tshikapa',HUID('f6fc7469-7e58-45cb-b87c-f08af93edade')), (HUID('dbe330b6-5cdf-4830-8c30-dc00eccd1a22'), 'Test Hook Sector', HUID('dbe330b6-5cdf-4830-8c30-dc00eccd1a21'));
-INSERT INTO `village` VALUES (HUID('1f162a10-9f67-4788-9eff-c1fea42fcc9b'),'KELE2',HUID('0404e9ea-ebd6-4f20-b1f8-6dc9f9313450')), (HUID('dbe330b6-5cdf-4830-8c30-dc00eccd1a22'), 'Test Hook Village',HUID('dbe330b6-5cdf-4830-8c30-dc00eccd1a22'));
-
 -- Enterprise
 INSERT INTO `enterprise` VALUES (1,'Test Enterprise','TE','243 81 504 0540','enterprise@test.org',HUID('1f162a10-9f67-4788-9eff-c1fea42fcc9b'),NULL,2,103, NULL, NULL);
 
--- test/integration/reports/finance/cashflowByService.js
+++ test/integration/reports/finance/cashflowByService.js
@@ -1,4 +1,5 @@
 const RenderingTests = require('../rendering');
+
 const target = '/reports/finance/cashflow/services';
-const params = { dateFrom : new Date('2010-01-01'), dateTo: new Date() };
+const params = { dateFrom : new Date('2010-01-01'), dateTo : new Date() };
 describe(`(${target}) CashFlow By Service Report`, RenderingTests(target, null, params));
