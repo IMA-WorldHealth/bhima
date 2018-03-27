17bd5ed02572f652a269524f4b0b3d53a63992b6
Jonathan Niles <jonathanwniles@gmail.com>
2018-03-27 16:00:16 +0100

    feat(barcode): implement barcode component
    This commit implements a barcode component to make working with barcodes
    a bit easier.  It replaces the cash barcode modal's custom logic with
    generic logic to be tested.
    
    Key features:
     1. If the input loses focus, the user is notified and a button appears
     to re-establish focus on the hidden input.
     2. Simple API - only a single callback is provided for the success
     case.
     3. Record agnostic.
    
    Closes #1529.

---
+++ client/src/js/components/bhBarcodeScanner.js
@@ -0,0 +1,69 @@
+angular.module('bhima.components')
+  .component('bhBarcodeScanner', {
+    templateUrl : '/modules/templates/bhBarcodeScanner.html',
+    controller  : bhBarcodeScanner,
+    bindings    : {
+      onScanCallback : '&',
+    },
+  });
+
+bhBarcodeScanner.$inject = [
+  '$timeout', '$window', 'BarcodeService',
+];
+
+function bhBarcodeScanner($timeout, $window, Barcode) {
+  const $ctrl = this;
+
+  // steps in the search space
+  const steps = {
+    AWAIT_READ :  2,
+    AWAIT_HTTP : 4,
+    READ_SUCCESS : 8,
+    READ_ERROR : 16,
+    LOST_FOCUS : 32,
+  };
+
+  $ctrl.$onInit = () => {
+    angular.extend($ctrl, { steps });
+    $ctrl.currentStep = steps.AWAIT_READ;
+    $ctrl.setFocusOnHiddenInput();
+  };
+
+  const setFocusOnHiddenInput = () => {
+    // clear previous values
+    delete $ctrl.barcode;
+
+    // find and focus the input
+    const input = $window.document.getElementById('hidden-barcode-input');
+    input.focus();
+
+    // set view state correctly
+    $ctrl.isResetButtonVisible = false;
+    $ctrl.currentStep = steps.AWAIT_READ;
+  };
+
+  // wrap setFocusOnHiddenInput() in $timeout to trigger $digest
+  $ctrl.setFocusOnHiddenInput = () => {
+    $timeout(setFocusOnHiddenInput);
+  };
+
+  $ctrl.triggerBarcodeRead = () => {
+    $ctrl.currentStep = steps.AWAIT_HTTP;
+
+    Barcode.search($ctrl.barcode)
+      .then(record => {
+        $ctrl.currentStep = steps.READ_SUCCESS;
+        $ctrl.record = record;
+        $ctrl.onScanCallback({ record });
+      })
+      .catch(() => {
+        $ctrl.currentStep = steps.READ_ERROR;
+        $ctrl.isResetButtonVisible = true;
+      });
+  };
+
+  $ctrl.showResetButton = () => {
+    $ctrl.isResetButtonVisible = true;
+    $ctrl.currentStep = steps.LOST_FOCUS;
+  };
+}
