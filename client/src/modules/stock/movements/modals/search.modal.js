ebd72e7627b527c95ec1c8aeff50e33c68a3f145
mbayopanda <mbayopanda@gmail.com>
2018-04-05 10:18:20 +0100

    fix registries filter

--- client/src/modules/stock/movements/modals/search.modal.js
+++ client/src/modules/stock/movements/modals/search.modal.js
@@ -20,6 +20,10 @@ function SearchMovementsModalController(data, Notify, Instance, Flux, $translate
   vm.searchQueries = {};
   vm.defaultQueries = {};
 
+  // keep track of the initial search queries to make sure we properly restore
+  // default display values
+  const initialSearchQueries = angular.copy(vm.searchQueries);
+
   // load flux
   Flux.read()
     .then(handleFluxes)
@@ -106,13 +110,20 @@ function SearchMovementsModalController(data, Notify, Instance, Flux, $translate
   vm.submit = function submit() {
 
     // push all searchQuery values into the changes array to be applied
-    angular.forEach(vm.searchQueries, (_value, key) => {
-      let _displayValue;
+    angular.forEach(vm.searchQueries, (value, key) => {
+      if (angular.isDefined(value)) {
 
-      if (angular.isDefined(_value)) {
-        // default to the original value if no display value is defined
-        _displayValue = displayValues[key] || lastDisplayValues[key] || _value;
-        changes.post({ key, value : _value, displayValue : _displayValue });
+        // To avoid overwriting a real display value, we first determine if the value changed in the current view.
+        // If so, we do not use the previous display value.  If the values are identical, we can restore the
+        // previous display value without fear of data being out of date.
+        const usePreviousDisplayValue =
+        angular.equals(initialSearchQueries[key], value) &&
+        angular.isDefined(lastDisplayValues[key]);
+
+        // default to the raw value if no display value is defined
+        const displayValue = usePreviousDisplayValue ? lastDisplayValues[key] : displayValues[key] || value;
+
+        changes.post({ key, value, displayValue });
       }
     });
 
