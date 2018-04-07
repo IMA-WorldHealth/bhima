cd51a5cd5ddeadfd9fcc4da32c4763d0504c6cfd
Chris Lomame <lomamech@gmail.com>
2018-04-07 15:19:57 +0200

    feature(Payroll Status Select) - Implement component Payroll Status Select

---
+++ client/src/modules/templates/bhPayrollStatusSelect.js
@@ -0,0 +1,50 @@
+angular.module('bhima.components')
+  .component('bhPayrollStatusSelect', {
+    templateUrl : 'modules/templates/bhPayrollStatusSelect.tmpl.html',
+    controller  : PayrollStatusSelectController,
+    bindings    : {
+      onChange : '&',
+      statusId : '<?',
+      label : '@?',
+      required : '<?',
+      validationTrigger : '<?',
+    },
+  });
+
+PayrollStatusSelectController.$inject = [
+  'PayrollConfigurationService', 'NotifyService', '$translate',
+];
+
+/**
+ * purchase status Selection Component
+ *
+ */
+function PayrollStatusSelectController(Payroll, Notify, $translate) {
+  var $ctrl = this;
+
+  $ctrl.$onInit = function onInit() {
+    // label to display
+    $ctrl.label = $ctrl.label || 'FORM.LABELS.STATUS';
+
+    // fired when a purchase status has been selected or removed from the list
+    $ctrl.onChange = $ctrl.onChange || angular.noop;
+
+    // init the model
+    $ctrl.selectedPayrollStatus = $ctrl.statusId || [];
+
+    // load all Payroll status
+    Payroll.paiementStatus()
+      .then(function (status) {
+        status.forEach(function (item) {
+          item.plainText = $translate.instant(item.text);
+        });
+        $ctrl.paiementStatus = status;
+      })
+      .catch(Notify.handleError);
+  };
+
+  // fires the onSelectCallback bound to the component
+  $ctrl.handleChange = function (models) {
+    $ctrl.onChange({ paiementStatus : models });
+  };
+}
