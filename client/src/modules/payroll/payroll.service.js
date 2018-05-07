56a807680ad8237ae80a01ec33640c992e49a228
Chris Lomame <lomamech@gmail.com>
2018-05-07 12:05:35 +0200

    Sanitaze and Improve Code

--- client/src/modules/payroll/payroll.service.js
+++ client/src/modules/payroll/payroll.service.js
@@ -11,15 +11,14 @@ PayrollConfigurationService.$inject = ['PrototypeApiService'];
  * Encapsulates common requests to the /payroll_config/ URL.
  */
 function PayrollConfigurationService(Api) {
-  var service = new Api('/payroll_config/');
+  const service = new Api('/payroll_config/');
 
   service.paiementStatus = paiementStatus;
 
   function paiementStatus() {
-    var url = ''.concat('paiementStatus');
+    const url = ''.concat('paiementStatus');
     return Api.read.call(service, url);
   }
-
 
   return service;
 }
