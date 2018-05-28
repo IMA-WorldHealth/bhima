c19ab1e864a1eaea228259a6da63f3a8f639ac61
Chris Lomame <lomamech@gmail.com>
2018-05-28 14:52:43 +0200

    test(Employee Standing) - Implement integration test for Employee Standing Report

---
+++ test/integration/reports/finance/employee_standing.js
@@ -0,0 +1,5 @@
+const RenderingTests = require('../rendering');
+
+const target = '/reports/finance/employeeStanding';
+const params = { employee_uuid : '75e09694-65f2-45a1-a8a2-8b025003d793' };
+describe(`(${target}) Employee Standing Report`, RenderingTests(target, null, params));
