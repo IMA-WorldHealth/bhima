a4336a7b4562521bcab25264ed55f3d76e79790f
jeremielodi <jeremielodi@gmail.com>
2018-05-29 09:57:04 +0100

    feat(voucher registry) link from cash payment registry

--- client/src/js/components/bhJournalLink.js
+++ client/src/js/components/bhJournalLink.js
@@ -1,5 +1,5 @@
 var templateStr =
-  '<a ui-sref="journal({ filters : [{ key : \'record_uuid\', value : $ctrl.recordUuid, displayValue: $ctrl.display }] })" ui-sref-opts="{ reload : false }">' +
+  '<a ui-sref="journal({ filters : [{ key : \'record_uuid\', value : $ctrl.recordUuid, displayValue: $ctrl.display }, { key : "includeNonPosted", value : 1 }, { key : "period", value : "allTime" }] })" ui-sref-opts="{ reload : false }">' +
     '<span translate>{{ $ctrl.display }}</span>' +
   '</a>';
 
