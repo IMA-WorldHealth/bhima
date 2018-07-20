76651762d70816712b966405d505e1456b94cb86
mbayopanda <mbayopanda@gmail.com>
2018-07-20 16:29:29 +0100

    reference value calculation

--- server/controllers/finance/accounts/references.js
+++ server/controllers/finance/accounts/references.js
@@ -16,7 +16,7 @@
  *
  * @todo HANDLE ACCOUNT REFERENCE ITEMS
  */
-
+const Q = require('q');
 const db = require('../../../lib/db');
 
 /**
@@ -83,6 +83,8 @@ function create(req, res, next) {
   const sql = 'INSERT INTO account_reference SET ?;';
   const sqlItems = 'INSERT INTO account_reference_item SET ?;';
 
+  record.is_amo_dep = record.is_amo_dep ? 1 : 0;
+
   delete record.id;
   delete record.accounts;
   delete record.accountsException;
@@ -127,6 +129,8 @@ function update(req, res, next) {
 
   const sql = 'UPDATE account_reference SET ? WHERE id = ?';
   const sqlItems = 'INSERT INTO account_reference_item SET ?;';
+
+  record.is_amo_dep = record.is_amo_dep ? 1 : 0;
 
   delete record.id;
   delete record.accounts;
@@ -186,6 +190,17 @@ function remove(req, res, next) {
 }
 
 /**
+ * @method getAllValues
+ */
+function getAllValues(req, res, next) {
+  computeAccountReferenceValues(50)
+    .then(rows => {
+      res.status(200).json(rows);
+    })
+    .catch(next);
+}
+
+/**
  * @method lookupAccountReference
  *
  * @description
@@ -220,8 +235,160 @@ function lookupAccountReference(id) {
     });
 }
 
+/**
+ * @method computeAccountReferenceValues
+ *
+ * @description
+ * compute value of all account references
+ *
+ * @param {number} periodId - the period needed
+ */
+function computeAccountReferenceValues(periodId) {
+  const glb = {};
+
+  // get fiscal year information for the given period
+  const queryFiscalYear = `
+    SELECT fy.id, p.number AS period_number FROM fiscal_year fy
+    JOIN period p ON p.fiscal_year_id = fy.id
+    WHERE p.id = ?
+  `;
+
+  // get all references
+  const queryAccountReferences = `
+    SELECT id, abbr, description, is_amo_dep FROM account_reference;
+  `;
+
+  return db.one(queryFiscalYear, [periodId])
+    .then(fiscalYear => {
+      glb.fiscalYear = fiscalYear;
+
+      return db.exec(queryAccountReferences);
+    })
+    .then(accountReferences => {
+      const dbPromises = accountReferences.map(ar => {
+        return getValueForReference(ar.abbr, ar.is_amo_dep, glb.fiscalYear.period_number, glb.fiscalYear.id);
+      });
+      return Q.all(dbPromises);
+    })
+    .then(data => {
+      const accountReferenceValues = data.map(line => line[0]);
+      return accountReferenceValues;
+    });
+}
+
+/**
+ * @method lookupAccountReferenceValueByAbbr
+ *
+ * @description
+ * Returns the balance of the account reference given
+ *
+ * @param {string} abbr - the reference of accounts. ex. AA or AX
+ * @param {number} periodId - the period needed
+ * @param {boolean} isAmoDep - the concerned reference is for amortissement, depreciation or provision
+ */
+function lookupAccountReferenceValueByAbbr(abbr, isAmoDep = 0, periodId) {
+  // get fiscal year information for the given period
+  const queryFiscalYear = `
+    SELECT fy.id, p.number AS period_number FROM fiscal_year fy
+    JOIN period p ON p.fiscal_year_id = fy.id
+    WHERE p.id = ?
+  `;
+
+  return db.one(queryFiscalYear, [periodId])
+    .then(fiscalYear => {
+      return getValueForReference(abbr, isAmoDep, fiscalYear.period_number, fiscalYear.id);
+    });
+}
+
+/**
+ * @method getValueForReference
+ *
+ * @description
+ * Returns computed value of the reference in a given period and fiscal_year
+ *
+ * @param {number} fiscalYearId
+ * @param {number} periodNumber
+ * @param {string} abbr - the reference of accounts. ex. AA or AX
+ * @param {boolean} isAmoDep - the concerned reference is for amortissement, depreciation or provision
+ */
+function getValueForReference(abbr, isAmoDep = 0, periodNumber, fiscalYearId) {
+  const queryTotals = `
+  SELECT abbr, is_amo_dep, IFNULL(debit, 0) AS debit, IFNULL(credit, 0) AS credit, IFNULL(balance, 0) AS balance FROM (
+    SELECT ? AS abbr, ? AS is_amo_dep, 
+      SUM(IFNULL(pt.debit, 0)) AS debit, SUM(IFNULL(pt.credit, 0)) AS credit,
+      SUM(IFNULL(pt.debit - pt.credit, 0)) AS balance
+    FROM period_total pt 
+    JOIN period p ON p.id = pt.period_id
+    WHERE pt.fiscal_year_id = ? AND pt.locked = 0 AND p.number BETWEEN 0 AND ? AND pt.account_id IN (?)
+  )z
+  `;
+
+  return getAccountsForReference(abbr, isAmoDep)
+    .then(accounts => {
+      console.log('accounts : ', accounts);
+      const accountIds = accounts.map(a => a.account_id);
+      const parameters = [
+        abbr,
+        isAmoDep,
+        fiscalYearId,
+        periodNumber,
+        accountIds.length ? accountIds : null,
+      ];
+      return db.exec(queryTotals, parameters);
+    });
+}
+
+/**
+ * @method getAccountsForReference
+ *
+ * @description
+ * Returns all accounts concerned by the reference without exception accounts
+ *
+ * @param {string} abbr - the reference of accounts. ex. AA or AX
+ * @param {boolean} isAmoDep - the concerned reference is for amortissement, depreciation or provision
+ */
+function getAccountsForReference(abbr, isAmoDep = 0) {
+  /**
+   * Get the list of accounts of the reference without excepted accounts
+   * mysql implementation of minus operator
+   * @link http://www.mysqltutorial.org/mysql-minus/
+   */
+  const queryAccounts = `
+    SELECT includeTable.account_id, includeTable.account_number FROM (
+      SELECT DISTINCT 
+        account.id AS account_id, account.number AS account_number, t.is_exception FROM account
+        JOIN (
+          SELECT a.id, a.number, ar.is_amo_dep, ari.is_exception FROM account a 
+          JOIN account_reference_item ari ON ari.account_id = a.id
+          JOIN account_reference ar ON ar.id = ari.account_reference_id
+          WHERE ar.abbr LIKE ? AND ar.is_amo_dep = ? AND ari.is_exception = 0
+        ) AS t ON LEFT(account.number, CHAR_LENGTH(t.number)) LIKE t.number 
+    ) AS includeTable 
+    LEFT JOIN (
+      SELECT DISTINCT 
+        account.id AS account_id, account.number AS account_number, z.is_exception FROM account
+        JOIN (
+          SELECT a.id, a.number, ar.is_amo_dep, ari.is_exception FROM account a 
+          JOIN account_reference_item ari ON ari.account_id = a.id
+          JOIN account_reference ar ON ar.id = ari.account_reference_id
+          WHERE ar.abbr LIKE ? AND ar.is_amo_dep = ? AND ari.is_exception = 1
+        ) AS z ON LEFT(account.number, CHAR_LENGTH(z.number)) LIKE z.number
+    ) AS excludeTable ON excludeTable.account_id = includeTable.account_id 
+    WHERE excludeTable.account_id IS NULL
+    ORDER BY CONVERT(includeTable.account_number, char(10));
+  `;
+  return db.exec(queryAccounts, [abbr, isAmoDep, abbr, isAmoDep]);
+}
+
 exports.list = list;
 exports.create = create;
 exports.update = update;
 exports.remove = remove;
 exports.detail = detail;
+exports.getAllValues = getAllValues;
+
+// expose methods
+exports.getAccountsForReference = getAccountsForReference;
+exports.lookupAccountReferenceValueByAbbr = lookupAccountReferenceValueByAbbr;
+exports.getValueForReference = getValueForReference;
+exports.computeAccountReferenceValues = computeAccountReferenceValues;
