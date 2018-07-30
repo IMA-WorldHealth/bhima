bb2b02939b924fcccedd1c0e955787af1dbc218b
Chris Lomame <lomamech@gmail.com>
2018-07-30 13:14:43 +0200

    improvement(Payroll Modules) - Add new function commitmentFunction for Generate data by Employees to   make Commitment and Split the function Commitments

--- server/controllers/payroll/multiplePayroll/commitment.js
+++ server/controllers/payroll/multiplePayroll/commitment.js
@@ -15,7 +15,7 @@ const util = require('../../../lib/util');
 const db = require('../../../lib/db');
 const uuid = require('uuid/v4');
 const moment = require('moment');
-const Exchange = require('../../finance/exchange');
+const commitmentFunction = require('./commitmentFunction');
 
 const COMMITMENT_TYPE_ID = 15;
 const WITHHOLDING_TYPE_ID = 16;
@@ -26,99 +26,90 @@ function commitments(employees, rubrics, rubricsConfig, account, projectId, user
   const accountPayroll = account[0].account_id;
   const periodPayroll = moment(account[0].dateFrom).format('MM-YYYY');
   const labelPayroll = account[0].label;
-
-  let totalCommitments = 0;
-  let totalBasicSalaries = 0;
-
   const commitmentUuid = uuid();
   const voucherCommitmentUuid = db.bid(commitmentUuid);
-
   const withholdingUuid = uuid();
   const voucherWithholdingUuid = db.bid(withholdingUuid);
-  
   const chargeRemunerationUuid = uuid();
   const voucherChargeRemunerationUuid = db.bid(chargeRemunerationUuid);
-
-  const transactions = [];
-  const employeesBenefitsItem = [];
+  const identificationCommitment = {
+    voucherCommitmentUuid : voucherCommitmentUuid,
+    voucherWithholdingUuid : voucherWithholdingUuid,
+    voucherChargeRemunerationUuid : voucherChargeRemunerationUuid,    
+  };
+  const enterpriseChargeRemunerations = [];
 
   let rubricsBenefits = [];
-  let RubricsWithholdings = [];
-  let chargeRemunerations = [];
-
+  let rubricsWithholdings = [];
+  let chargesRemunerations = [];
+  let rubricsWithholdingsNotAssociat = [];
+  let voucherChargeRemuneration = {};
+  let voucherWithholding = {};
+  let totalCommitments = 0;
+  let totalBasicSalaries = 0;
+  let totalChargesRemuneration = 0;
+  let totalWithholdings = 0;
 
   rubricsConfig.forEach(rubricsConfig => {
     rubricsConfig.totals = 0;
-
     rubrics.forEach(rubric => {
       let exchangeRate = 1;
-
-      // {{ exchangeRates }} contains a matrix containing the current exchange rate of all currencies 
+      // {{ exchangeRates }} contains a matrix containing the current exchange rate of all currencies
       // against the currency of the Enterprise
       exchangeRates.forEach(exchange => {
         if (parseInt(exchange.currency_id) === parseInt(rubric.currency_id)) {
           exchangeRate = exchange.rate;
         }
-      })
-
-      console.log('RUBRAAAAAAAAAaa');
-      console.log(rubric);
+      });
+      rubric.value /= exchangeRate;
 
       if (rubricsConfig.id === rubric.id) {
         rubricsConfig.totals += rubric.value / exchangeRate;
       }
-    });    
+    });
   });
 
   // Get Rubrics benefits
   rubricsBenefits = rubricsConfig.filter(item => (item.is_discount !== 1 && item.totals > 0));
 
   // Get Expenses borne by the employees
-  RubricsWithholdings = rubricsConfig.filter(item => (item.is_discount && item.is_employee && item.totals > 0));
+  rubricsWithholdings = rubricsConfig.filter(item => (item.is_discount && item.is_employee && item.totals > 0));
+
+  // Get the list of payment Rubrics Not associated with the identifier
+  rubricsWithholdingsNotAssociat = rubricsConfig.filter(
+    item => (item.is_discount && item.is_employee && item.totals > 0 && item.is_associated_employee !== 1));
 
   // Get Enterprise charge on remuneration
-  chargeRemunerations = 
+  chargesRemunerations =
     rubricsConfig.filter(item => (item.is_employee !== 1 && item.is_discount === 1 && item.totals > 0));
 
-
-  employees.forEach(employee => {
-    // Exchange Rate if the employee.currency is equal enterprise currency
-    let exchangeRate = 1;
-
-    // {{ exchangeRates }} contains a matrix containing the current exchange rate of all currencies 
-    // against the currency of the Enterprise
-    exchangeRates.forEach(exchange => {
-      if (parseInt(exchange.currency_id) === parseInt(employee.currency_id)) {
-        exchangeRate = exchange.rate;
-      }
-    })
-
-    const conversionGrossSalary = employee.gross_salary / exchangeRate;
-
-    // Conversion in case the employee has been configured with a currency other than the Enterprise's currency
-    totalCommitments += employee.gross_salary / exchangeRate;
-    totalBasicSalaries += employee.basic_salary / exchangeRate; 
-
-    const rubricsPaiement = [];
-
-    employeesBenefitsItem.push([
-      db.bid(uuid()),
-      employee.account_id,
-      0,
-      conversionGrossSalary,
-      voucherCommitmentUuid,
-      db.bid(employee.creditor_uuid),
-    ]);
-
-    rubrics.forEach(rubric => {
-      if (employee.employee_uuid === rubric.employee_uuid) {
-        rubricsPaiement.push(rubric);
-      }
-    });
+  chargesRemunerations.forEach(charge => {
+    totalChargesRemuneration += charge.totals;
   });
 
-  totalCommitments = util.roundDecimal(totalCommitments, DECIMAL_PRECISION);
-  totalBasicSalaries = util.roundDecimal(totalBasicSalaries, DECIMAL_PRECISION);
+  rubricsWithholdings.forEach(charge => {
+    totalWithholdings += charge.totals;
+  });
+
+  const dataCommitment = commitmentFunction.dataCommitment(
+    employees, 
+    exchangeRates,
+    rubrics,
+    identificationCommitment
+  );
+
+  const transactions = dataCommitment.transactions;
+  const employeesBenefitsItem = dataCommitment.employeesBenefitsItem;
+  const employeesWithholdingItem = dataCommitment.employeesWithholdingItem;
+
+  // console.log('EMP_BENEFITSSSSSSS');
+  // console.log(employeesBenefitsItem);
+
+  // console.log('WITH_HOLDINGGGggggggg');
+  // console.log(employeesWithholdingItem);
+
+  totalCommitments = util.roundDecimal(dataCommitment.totalCommitments, DECIMAL_PRECISION);
+  totalBasicSalaries = util.roundDecimal(dataCommitment.totalBasicSalaries, DECIMAL_PRECISION);
 
   const voucherCommitment = {
     uuid : voucherCommitmentUuid,
@@ -145,9 +136,65 @@ function commitments(employees, rubrics, rubricsConfig, account, projectId, user
       employeesBenefitsItem.push([
         db.bid(uuid()),
         benefits.expense_account_id,
-        benefits.value,
+        benefits.totals,
         0,
         voucherCommitmentUuid,
+        null,
+      ]);
+    });
+  }
+
+  if (chargesRemunerations.length) {
+    // Social charge on remuneration
+    voucherChargeRemuneration = {
+      uuid : voucherChargeRemunerationUuid,
+      date : new Date(),
+      project_id : projectId,
+      currency_id : currencyId,
+      user_id : userId,
+      type_id : CHARGES_TYPE_ID,
+      description : `CHARGES SOCIALES SUR REMUNERATION [${periodPayroll}]/ ${labelPayroll}`,
+      amount : util.roundDecimal(totalChargesRemuneration, 2),
+    };
+
+    chargesRemunerations.forEach(chargeRemuneration => {
+      enterpriseChargeRemunerations.push([
+        db.bid(uuid()),
+        chargeRemuneration.debtor_account_id,
+        0,
+        chargeRemuneration.totals,
+        voucherChargeRemunerationUuid,
+        null,
+      ], [
+        db.bid(uuid()),
+        chargeRemuneration.expense_account_id,
+        chargeRemuneration.totals,
+        0,
+        voucherChargeRemunerationUuid,
+        null,
+      ]);
+    });
+  }
+
+  if (rubricsWithholdings.length) {
+    voucherWithholding = {
+      uuid : voucherWithholdingUuid,
+      date : new Date(),
+      project_id : projectId,
+      currency_id : currencyId,
+      user_id : userId,
+      type_id : WITHHOLDING_TYPE_ID,
+      description : `RETENUE DU PAIEMENT [${periodPayroll}]/ ${labelPayroll}`,
+      amount : util.roundDecimal(totalWithholdings, 2),
+    };
+
+    rubricsWithholdingsNotAssociat.forEach(withholding => {
+      employeesWithholdingItem.push([
+        db.bid(uuid()),
+        withholding.debtor_account_id,
+        0,
+        util.roundDecimal(withholding.totals, 2),
+        voucherWithholdingUuid,
         null,
       ]);
     });
@@ -160,213 +207,38 @@ function commitments(employees, rubrics, rubricsConfig, account, projectId, user
   }, {
     query : 'INSERT INTO voucher_item (uuid, account_id, debit, credit, voucher_uuid, entity_uuid) VALUES ?',
     params : [employeesBenefitsItem],
+  }, {
+    query : 'CALL PostVoucher(?);',
+    params : [voucherCommitment.uuid],
   });
 
+  if (chargesRemunerations.length) {
+    transactions.push({
+      query : 'INSERT INTO voucher SET ?',
+      params : [voucherChargeRemuneration],
+    }, {
+      query : `INSERT INTO voucher_item
+        (uuid, account_id, debit, credit, voucher_uuid, entity_uuid) VALUES ?`,
+      params : [enterpriseChargeRemunerations],
+    }, {
+      query : 'CALL PostVoucher(?);',
+      params : [voucherChargeRemuneration.uuid],
+    });
+  }
 
-
-
-  // employees.forEach(employee => {
-      // const paiementUuid = db.bid(employee.uuid);
-      // const commitmentUuid = uuid();
-      // const withholdingUuid = uuid();
-      // const chargeRemunerationUuid = uuid();
-
-  //   const rubricsPaiement = [];
-
-  //   rubrics.forEach(rubric => {
-  //     if (employee.employee_uuid === rubric.employee_uuid) {
-  //       rubricsPaiement.push(rubric);
-  //     }
-  //   });
-
-  //   let totalWithholding = 0;
-  //   let totalChargeRemuneration = 0;
-
-  //   let employeeBenefits = [];
-  //   let employeeWithholdings = [];
-  //   let chargeRemunerations = [];
-
-  //   let voucherWithholding = {};
-  //   let voucherChargeRemuneration = {};
-
-  //   if (rubricsPaiement.length) {
-  //     // Get Employee benefits
-  //     employeeBenefits = rubricsPaiement.filter(item => (item.is_discount !== 1));
-
-  //     // Get Expenses borne by the employee
-  //     employeeWithholdings = rubricsPaiement.filter(item => (item.is_discount && item.is_employee));
-
-  //     // Get Enterprise charge on remuneration
-  //     chargeRemunerations = rubricsPaiement.filter(item => (item.is_employee !== 1 && item.is_discount === 1));
-
-  //     employeeWithholdings.forEach(withholding => {
-  //       totalWithholding += util.roundDecimal(withholding.value, 2);
-  //     });
-
-  //     chargeRemunerations.forEach(chargeRemuneration => {
-  //       totalChargeRemuneration += util.roundDecimal(chargeRemuneration.value, 2);
-  //     });
-
-  //     const employeeBenefitsItem = [];
-  //     const employeeWithholdingItem = [];
-  //     const enterpriseChargeRemunerations = [];
-  //     const voucherCommitmentUuid = db.bid(commitmentUuid);
-
-  //     const voucherCommitment = {
-  //       uuid : voucherCommitmentUuid,
-  //       date : new Date(),
-  //       project_id : projectId,
-  //       currency_id : employee.currency_id,
-  //       user_id : userId,
-  //       type_id : COMMITMENT_TYPE_ID,
-  //       description : `ENGAGEMENT DE PAIE [${periodPayroll}]/ ${employee.display_name}`,
-  //       amount : employee.gross_salary,
-  //       reference_uuid : db.bid(paiementUuid),
-  //     };
-
-  //     // Benefits Item
-  //     employeeBenefitsItem.push([
-  //       db.bid(uuid()),
-  //       employee.account_id,
-  //       0,
-  //       employee.gross_salary,
-  //       voucherCommitmentUuid,
-  //       db.bid(employee.creditor_uuid),
-  //     ]);
-
-  //     employeeBenefitsItem.push([
-  //       db.bid(uuid()),
-  //       accountPayroll,
-  //       employee.basic_salary,
-  //       0,
-  //       voucherCommitmentUuid,
-  //       null,
-  //     ]);
-
-  //     if (employeeBenefits.length) {
-  //       employeeBenefits.forEach(benefits => {
-  //         employeeBenefitsItem.push([
-  //           db.bid(uuid()),
-  //           benefits.expense_account_id,
-  //           benefits.value,
-  //           0,
-  //           voucherCommitmentUuid,
-  //           null,
-  //         ]);
-  //       });
-  //     }
-
-  //     // WithholdingItem
-  //     const voucherWithholdingUuid = db.bid(withholdingUuid);
-
-  //     if (employeeWithholdings.length) {
-  //       voucherWithholding = {
-  //         uuid : voucherWithholdingUuid,
-  //         date : new Date(),
-  //         project_id : projectId,
-  //         currency_id : employee.currency_id,
-  //         user_id : userId,
-  //         type_id : WITHHOLDING_TYPE_ID,
-  //         description : `RETENUE DU PAIEMENT [${periodPayroll}]/ ${employee.display_name}`,
-  //         amount : util.roundDecimal(totalWithholding, 2),
-  //         reference_uuid : db.bid(paiementUuid),
-  //       };
-
-  //       employeeWithholdingItem.push([
-  //         db.bid(uuid()),
-  //         employee.account_id,
-  //         util.roundDecimal(totalWithholding, 2),
-  //         0,
-  //         voucherWithholdingUuid,
-  //         db.bid(employee.creditor_uuid),
-  //       ]);
-
-  //       employeeWithholdings.forEach(withholding => {
-  //         const employeeCreditorUuid = withholding.is_associated_employee === 1
-  //           ? db.bid(employee.creditor_uuid) : null;
-
-  //         employeeWithholdingItem.push([
-  //           db.bid(uuid()),
-  //           withholding.debtor_account_id,
-  //           0,
-  //           util.roundDecimal(withholding.value, 2),
-  //           voucherWithholdingUuid,
-  //           employeeCreditorUuid,
-  //         ]);
-  //       });
-  //     }
-
-  //     const voucherChargeRemunerationUuid = db.bid(chargeRemunerationUuid);
-  //     if (chargeRemunerations.length) {
-  //       // Social charge on remuneration
-  //       voucherChargeRemuneration = {
-  //         uuid : voucherChargeRemunerationUuid,
-  //         date : new Date(),
-  //         project_id : projectId,
-  //         currency_id : employee.currency_id,
-  //         user_id : userId,
-  //         type_id : CHARGES_TYPE_ID,
-  //         description : `CHARGES SOCIALES SUR REMUNERATION [${periodPayroll}]/ ${employee.display_name}`,
-  //         amount : util.roundDecimal(totalChargeRemuneration, 2),
-  //         reference_uuid : db.bid(paiementUuid),
-  //       };
-
-  //       chargeRemunerations.forEach(chargeRemuneration => {
-  //         enterpriseChargeRemunerations.push([
-  //           db.bid(uuid()),
-  //           chargeRemuneration.debtor_account_id,
-  //           0,
-  //           chargeRemuneration.value,
-  //           voucherChargeRemunerationUuid,
-  //           null,
-  //         ], [
-  //           db.bid(uuid()),
-  //           chargeRemuneration.expense_account_id,
-  //           chargeRemuneration.value,
-  //           0,
-  //           voucherChargeRemunerationUuid,
-  //           null,
-  //         ]);
-  //       });
-  //     }
-
-      // initialise the transaction handler
-      // transactions.push({
-      //   query : 'INSERT INTO voucher SET ?',
-      //   params : [voucherCommitment],
-      // }, {
-      //   query : 'INSERT INTO voucher_item (uuid, account_id, debit, credit, voucher_uuid, entity_uuid) VALUES ?',
-      //   params : [employeesBenefitsItem],
-      // });
-
-  //     if (employeeWithholdings.length) {
-  //       transactions.push({
-  //         query : 'INSERT INTO voucher SET ?',
-  //         params : [voucherWithholding],
-  //       }, {
-  //         query : `INSERT INTO voucher_item 
-  //           (uuid, account_id, debit, credit, voucher_uuid, entity_uuid) VALUES ?`,
-  //         params : [employeeWithholdingItem],
-  //       });
-  //     }
-
-  //     if (chargeRemunerations.length) {
-  //       transactions.push({
-  //         query : 'INSERT INTO voucher SET ?',
-  //         params : [voucherChargeRemuneration],
-  //       }, {
-  //         query : `INSERT INTO voucher_item 
-  //           (uuid, account_id, debit, credit, voucher_uuid, entity_uuid) VALUES ?`,
-  //         params : [enterpriseChargeRemunerations],
-  //       });
-  //     }
-
-  //     transactions.push({
-  //       query : 'UPDATE paiement set status_id = 3 WHERE uuid = ?',
-  //       params : [paiementUuid],
-  //     });
-  //   }
-  // });
+  if (rubricsWithholdings.length) {
+    transactions.push({
+      query : 'INSERT INTO voucher SET ?',
+      params : [voucherWithholding],
+    }, {
+      query : `INSERT INTO voucher_item 
+        (uuid, account_id, debit, credit, voucher_uuid, entity_uuid) VALUES ?`,
+      params : [employeesWithholdingItem],
+    }, {
+      query : 'CALL PostVoucher(?);',
+      params : [voucherWithholding.uuid],
+    });
+  }
 
   return transactions;
 }
---
+++ server/controllers/payroll/multiplePayroll/commitmentFunction.js
@@ -0,0 +1,120 @@
+/**
+ * @method dataCommitment
+ *
+ * This function is used to prepare the data necessary to pass the transactions of payment encumbrance, 
+ * this function retrieves in parameter the list of employees, and calculates the total base salaries, 
+ * profit totals per employee, the totals of the retentions of the Payments by Employees, and return the lists of 
+ * transactions to be executed, the list of profits, retained
+ *
+ * @requires util
+ * @requires db
+ * @requires uuid
+ */
+
+const util = require('../../../lib/util');
+const db = require('../../../lib/db');
+const uuid = require('uuid/v4');
+
+function dataCommitment(employees, exchangeRates, rubrics, identificationCommitment) {
+  const transactions = [];
+  let totalCommitments = 0;
+  let totalBasicSalaries = 0;
+
+  const voucherCommitmentUuid = identificationCommitment.voucherCommitmentUuid;
+  const voucherWithholdingUuid = identificationCommitment.voucherWithholdingUuid;
+  const voucherChargeRemunerationUuid = identificationCommitment.voucherChargeRemunerationUuid;
+
+  const employeesBenefitsItem = [];
+  const employeesWithholdingItem = [];
+
+  employees.forEach(employee => {
+    const paiementUuid = db.bid(employee.uuid);
+
+    transactions.push({
+      query : 'UPDATE paiement set status_id = 3 WHERE uuid = ?',
+      params : [paiementUuid],
+    });
+
+    // Exchange Rate if the employee.currency is equal enterprise currency
+    let exchangeRate = 1;
+
+    // {{ exchangeRates }} contains a matrix containing the current exchange rate of all currencies
+    // against the currency of the Enterprise
+    exchangeRates.forEach(exchange => {
+      if (parseInt(exchange.currency_id) === parseInt(employee.currency_id)) {
+        exchangeRate = exchange.rate;
+      }
+    });
+
+    const conversionGrossSalary = employee.gross_salary / exchangeRate;
+
+    // Conversion in case the employee has been configured with a currency other than the Enterprise's currency
+    totalCommitments += employee.gross_salary / exchangeRate;
+    totalBasicSalaries += employee.basic_salary / exchangeRate;
+
+    const rubricsPaiement = [];
+    let employeeWithholdings = [];
+
+    employeesBenefitsItem.push([
+      db.bid(uuid()),
+      employee.account_id,
+      0,
+      conversionGrossSalary,
+      voucherCommitmentUuid,
+      db.bid(employee.creditor_uuid),
+    ]);
+
+    rubrics.forEach(rubric => {
+      if (employee.employee_uuid === rubric.employee_uuid) {
+        rubricsPaiement.push(rubric);
+      }
+    });
+
+    let totalEmployeeWithholding = 0;
+
+    if (rubricsPaiement.length) {
+      // Get Expenses borne by the employee
+      employeeWithholdings = rubricsPaiement.filter(item => (item.is_discount && item.is_employee));
+
+      employeeWithholdings.forEach(withholding => {
+        totalEmployeeWithholding += util.roundDecimal(withholding.value, 2);
+      });
+
+      employeesWithholdingItem.push([
+        db.bid(uuid()),
+        employee.account_id,
+        util.roundDecimal(totalEmployeeWithholding, 2),
+        0,
+        voucherWithholdingUuid,
+        db.bid(employee.creditor_uuid),
+      ]);
+
+      if (employeeWithholdings.length) {
+        employeeWithholdings.forEach(withholding => {
+          if (withholding.is_associated_employee === 1) {
+            employeesWithholdingItem.push([
+              db.bid(uuid()),
+              withholding.debtor_account_id,
+              0,
+              util.roundDecimal(withholding.value, 2),
+              voucherWithholdingUuid,
+              employee.creditor_uuid,
+            ]);
+          }
+        });
+      }
+    }
+  });
+
+  const data = {
+    transactions,
+    employeesBenefitsItem,
+    employeesWithholdingItem,
+    totalCommitments,    
+    totalBasicSalaries,
+  };
+
+  return data;
+}
+
+exports.dataCommitment = dataCommitment;
