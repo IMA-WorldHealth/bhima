06be8058a14ef8ab62fae668cae8fe232a439e93
mbayopanda <mbayopanda@gmail.com>
2018-08-13 09:50:44 +0100

    add schema

--- server/models/Update/add_account_reference_module.sql
+++ server/models/Update/add_account_reference_module.sql
@@ -4,6 +4,30 @@
   NOTA : Please create `account_reference` and `account_reference_item` tables first
 */
 
+DROP TABLE IF EXISTS `account_reference`;
+CREATE TABLE `account_reference` (
+  `id` MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `abbr` VARCHAR(35) NOT NULL,
+  `description` VARCHAR(100) NOT NULL,
+  `parent` MEDIUMINT(8) UNSIGNED NULL,
+  `is_amo_dep` TINYINT(1) NULL DEFAULT 0 COMMENT 'Ammortissement or depreciation',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `account_reference_1` (`abbr`, `is_amo_dep`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `account_reference_item`;
+CREATE TABLE `account_reference_item` (
+  `id` MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `account_reference_id` MEDIUMINT(8) UNSIGNED NOT NULL,
+  `account_id` INT(10) UNSIGNED NOT NULL,
+  `is_exception` TINYINT(1) NULL DEFAULT 0 COMMENT 'Except this for reference calculation',
+  PRIMARY KEY (`id`),
+  KEY `account_reference_id` (`account_reference_id`),
+  KEY `account_id` (`account_id`),
+  FOREIGN KEY (`account_reference_id`) REFERENCES `account_reference` (`id`),
+  FOREIGN KEY (`account_id`) REFERENCES `account` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 INSERT INTO unit VALUES 
 (205, 'Account Reference Management','TREE.ACCOUNT_REFERENCE_MANAGEMENT','',1,'/modules/account_reference','/account_reference'),
 (206, 'Account Reference Report','TREE.ACCOUNT_REFERENCE_REPORT','',144,'/modules/reports/account_reference','/reports/account_reference');
