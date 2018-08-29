176ced571c5d188da44714ea037df3f50eaf62bb
Steven Fountain <stevenfountain@imaworldhealth.org>
2018-08-29 22:58:48 +0100

    test(journal): include tests for full transactions
    This commit introduces end to end tests to verify the
    showFullTransactions filter works on the Posting Journal.

--- test/end-to-end/journal/journal.config.js
+++ test/end-to-end/journal/journal.config.js
@@ -43,6 +43,27 @@ function JournalConfigurationModal() {
 
     page.expectColumnCount(defaultVisibleColumnCount);
   });
+
+  it('correctly fetches full transactions together with `showFullTransactions` flag', () => {
+    const PATIENT_REFERENCE = 'PA.TPA.2';
+    const NUMBER_OF_REFERENCED_LINES = 4;
+    const TOTAL_TRANSACTION_LINES_REFERENCED = 8;
+
+    // limit to patient reference rows
+    page.openGridSearchModal();
+    FU.input('ModalCtrl.searchQueries.hrEntity', PATIENT_REFERENCE);
+    FU.modal.submit();
+
+    // check there are exclusively the rows referenced
+    page.expectRowCount(NUMBER_OF_REFERENCED_LINES);
+
+    // select `showAllTransactionRows`
+    page.showFullTransactions(true);
+
+    // check there are now full transactions shown for these rows
+    page.expectRowCount(TOTAL_TRANSACTION_LINES_REFERENCED);
+    page.resetSearchFilters();
+  });
 }
 
 module.exports = JournalConfigurationModal;
