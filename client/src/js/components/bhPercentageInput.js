534fb0a3e351975b617c267d6c1d2d86707fecaa
Chris Lomame <lomamech@gmail.com>
2018-10-02 13:18:41 +0200

    feature(Fee Center) Implement component Percentage Input

---
+++ client/src/js/components/bhPercentageInput.js
@@ -0,0 +1,31 @@
+angular.module('bhima.components')
+  .component('bhPercentageInput', {
+    templateUrl : 'modules/templates/bhPercentageInput.tmpl.html',
+    controller : PercentageInputController,
+    bindings : {
+      currencyId : '<', // one-way binding
+      model : '=', // two way binding
+      validationTrigger : '<', // one-way binding
+      label : '@?',
+      symbol : '@?',
+      disabled : '<?',
+      min   : '@?',
+      max   : '@?',
+    },
+  });
+
+PercentageInputController.$inject = [];
+
+/**
+ * Percentage Input Component
+ *
+ * This is a Percentage input form based on <input type="number">, with specific
+ * validation based on the min and max value.
+ */
+function PercentageInputController() {
+  const $ctrl = this;
+  const isDefined = angular.isDefined;
+
+  // translated label for the form input
+  $ctrl.label = $ctrl.label || 'FORM.LABELS.VALUE';
+}
