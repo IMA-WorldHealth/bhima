171363919eb60c9d4880185404a209aeafe88eee
Chris Lomame <lomamech@gmail.com>
2018-10-25 16:19:12 +0200

    test(Distribution Key) - Implement distribution key integration test

--- test/integration/feesCentersKey.js
+++ test/integration/feesCentersKey.js
@@ -7,90 +7,38 @@ const helpers = require('./helpers');
  *
  * This test suite implements full CRUD on the /fee_center  HTTP API endpoint.
  */
-describe.skip('(/fee_center_distribution_key) The /fee_center  API endpoint', () => {
+describe.only('(/fee_center_distribution_key) The /fee_center  API endpoint', () => {
   // distribution_center/distribution_key  we will add during this test suite.
 
-  // it('POST /FEE_CENTER should create a new Auxiliary Fee Center 1', () => {
-  //   return agent.post('/fee_center')
-  //     .send(auxiliary1)
-  //     .then((res) => {
-  //       console.log(res.body);
+  const distributionKey1 = {
+    data : {
+      auxiliary_fee_center_id: 6,
+      values: { '1': 50, '2': 35, '3': 15 },
+    }
+  };
 
-  //       const response = res.body;
-  //       expect(response.length).to.equal(1);
-  //     })
-  //     .catch(helpers.handler);
-  // });
+  const distributionKey2 = {
+    data : {
+      auxiliary_fee_center_id: 4,
+      values: { '1': 95, '2': 3, '3': 2 },
+    }
+  };
 
-  // it('POST /FEE_CENTER should create a new Auxiliary Fee Center 2', () => {
-  //   return agent.post('/fee_center')
-  //     .send(auxiliary2)
-  //     .then((res) => {
-  //     })
-  //     .catch(helpers.handler);
-  // });
+  it('POST /distribution_fee_center/proceed a new Distribution Fee Center Key: 1', () => {
+    return agent.post('/distribution_fee_center/distributionKey')
+      .send(distributionKey1)
+      .then((res) => {
+        helpers.api.created(res);
+      })
+      .catch(helpers.handler);
+  });
 
-  // it('POST /FEE_CENTER should create a new Auxiliary Fee Center 3', () => {
-  //   return agent.post('/fee_center')
-  //     .send(auxiliary3)
-  //     .then((res) => {
-  //     })
-  //     .catch(helpers.handler);
-  // });  
-
-  // it('POST /FEE_CENTER should create a new Auxiliary Fee Center 4', () => {
-  //   return agent.post('/fee_center')
-  //     .send(auxiliary4)
-  //     .then((res) => {
-  //     })
-  //     .catch(helpers.handler);
-  // });
-
-
-
-  // it('GET /FEE_CENTER/:ID should not be found for unknown id', () => {
-  //   return agent.get('/fee_center/unknownRubric')
-  //     .then((res) => {
-  //       const response = res.body;
-
-  //       expect(response.feeCenter.length).to.equal(0);
-  //       expect(response.references.length).to.equal(0);
-  //     })
-  //     .catch(helpers.handler);
-  // });
-
-  // it('PUT /FEE_CENTER  should update the Label for an existing Fee Center ', () => {
-  //   return agent.put('/fee_center/'.concat(feeCenterId))
-  //     .send(feeCenterUpt1)
-  //     .then((res) => {
-  //       const response = res.body;
-  //       expect(res).to.have.status(200);
-  //       expect(response.feeCenter[0].label).to.equal('Update Test');
-  //     })
-  //     .catch(helpers.handler);
-  // });
-
-
-  // it('PUT /FEE_CENTER  should update Fee Center Type, Reference fee Center for an existing Fee Center ', () => {
-  //   return agent.put('/fee_center/'.concat(feeCenterId))
-  //     .send(feeCenterUpt2)
-  //     .then((res) => {
-  //       const response = res.body;
-  //       expect(res).to.have.status(200);
-  //       expect(response.feeCenter[0].is_principal).to.equal(feeCenterUpt2.is_principal);
-  //       expect(response.references[0].account_reference_id).to.equal(
-  //         feeCenterUpt2.reference_fee_center[0].account_reference_id
-  //       );
-  //       expect(response.references[0].is_cost).to.equal(feeCenterUpt2.reference_fee_center[0].is_cost);
-  //     })
-  //     .catch(helpers.handler);
-  // });
-
-  // it('DELETE /FEE_CENTER/:ID should delete a Fee Center', () => {
-  //   return agent.delete('/fee_center/'.concat(feeCenterId))
-  //     .then((res) => {
-  //       helpers.api.deleted(res);
-  //     })
-  //     .catch(helpers.handler);
-  // });
+  it('POST /distribution_fee_center/proceed a new Distribution Fee Center Key: 2', () => {
+    return agent.post('/distribution_fee_center/distributionKey')
+      .send(distributionKey2)
+      .then((res) => {
+        helpers.api.created(res);
+      })
+      .catch(helpers.handler);
+  });
 });
