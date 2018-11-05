607bd8c58174734279f4a67275f65dc0cc9baaea
mbayopanda <mbayopanda@gmail.com>
2018-11-05 16:10:53 +0100

    remove on unique key

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -1317,7 +1317,7 @@ CREATE TABLE `entity` (
   `updated_at`         TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`reference`),
   UNIQUE KEY `entity_uuid` (`uuid`),
-  UNIQUE KEY `display_name` (`display_name`),
+  KEY `display_name` (`display_name`),
   KEY `entity_type_id` (`entity_type_id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
 
