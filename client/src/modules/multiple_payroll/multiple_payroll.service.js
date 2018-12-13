c145c4fc7a25fecf888ab55fa38e0e2fc1e16af7
Chris Lomame <lomamech@gmail.com>
2018-12-13 12:45:50 +0100

    refactor(Conversion) - Update translate key

--- client/src/modules/multiple_payroll/multiple_payroll.service.js
+++ client/src/modules/multiple_payroll/multiple_payroll.service.js
@@ -72,7 +72,7 @@ function MultiplePayrollService(
     { key : 'display_name', label : 'FORM.LABELS.EMPLOYEE_NAME' },
     { key : 'code', label : 'FORM.LABELS.CODE' },
     { key : 'status_id', label : 'FORM.LABELS.STATUS' },
-    { key : 'conversion_rate', label : 'FORM.LABELS.CONVERTION_RATE' },
+    { key : 'conversion_rate', label : 'FORM.LABELS.CONVERSION_RATE' },
   ]);
 
 
