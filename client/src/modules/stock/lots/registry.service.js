62cb468b3ae905e23ac1bbe3b62dd8d05cf28cf4
mbayopanda <mbayopanda@gmail.com>
2018-12-13 08:40:56 +0100

    move some codes into service

---
+++ client/src/modules/stock/lots/registry.service.js
@@ -0,0 +1,139 @@
+angular.module('bhima.services')
+  .service('LotsRegistryService', LotsRegistryService);
+
+LotsRegistryService.$inject = [
+  'uiGridConstants', 'SessionService',
+];
+
+/**
+ * This service encapsulate some common method of stock lots registry with the aims
+ * of reducing lines in registry.js
+ */
+function LotsRegistryService(uiGridConstants, Session) {
+  const service = this;
+
+  service.groupingBox = [
+    { label : 'STOCK.INVENTORY', value : 'text' },
+    { label : 'STOCK.INVENTORY_GROUP', value : 'group_name' },
+  ];
+
+  service.columnDefs = [
+    {
+      field : 'depot_text',
+      displayName : 'STOCK.DEPOT',
+      headerCellFilter : 'translate',
+    }, {
+      field : 'code',
+      displayName : 'STOCK.CODE',
+      headerCellFilter : 'translate',
+      sort : {
+        direction : uiGridConstants.ASC,
+        priority : 0,
+      },
+    }, {
+      field : 'text',
+      displayName : 'STOCK.INVENTORY',
+      headerCellFilter : 'translate',
+      sort : {
+        direction : uiGridConstants.ASC,
+        priority : 1,
+      },
+    }, {
+      field : 'group_name',
+      displayName : 'TABLE.COLUMNS.INVENTORY_GROUP',
+      headerCellFilter : 'translate',
+    }, {
+      field : 'label',
+      displayName : 'STOCK.LOT',
+      headerCellFilter : 'translate',
+    }, {
+      field : 'quantity',
+      displayName : 'STOCK.QUANTITY',
+      headerCellFilter : 'translate',
+    }, {
+      field : 'unit_cost',
+      displayName : 'STOCK.UNIT_COST',
+      headerCellFilter : 'translate',
+      type : 'number',
+      cellFilter : 'currency: '.concat(Session.enterprise.currency_id),
+    }, {
+      field : 'unit_type',
+      width : 75,
+      displayName : 'TABLE.COLUMNS.UNIT',
+      headerCellFilter : 'translate',
+      cellTemplate : 'modules/stock/inventories/templates/unit.tmpl.html',
+    }, {
+      field : 'entry_date',
+      displayName : 'STOCK.ENTRY_DATE',
+      headerCellFilter : 'translate',
+      cellFilter : 'date',
+    }, {
+      field : 'expiration_date',
+      displayName : 'STOCK.EXPIRATION_DATE',
+      headerCellFilter : 'translate',
+      cellFilter : 'date',
+    }, {
+      field : 'delay_expiration',
+      displayName : 'STOCK.EXPIRATION',
+      headerCellFilter : 'translate',
+    }, {
+      field : 'avg_consumption',
+      displayName : 'STOCK.CMM',
+      headerCellFilter : 'translate',
+      type : 'number',
+    }, {
+      field : 'S_MONTH',
+      displayName : 'STOCK.MSD',
+      headerCellFilter : 'translate',
+      type : 'number',
+    }, {
+      field : 'lifetime',
+      displayName : 'STOCK.LIFETIME',
+      headerCellFilter : 'translate',
+      cellTemplate     : 'modules/stock/lots/templates/lifetime.cell.html',
+      type : 'number',
+      sort : {
+        direction : uiGridConstants.ASC,
+        priority : 2,
+      },
+    }, {
+      field : 'S_LOT_LIFETIME',
+      displayName : 'STOCK.LOT_LIFETIME',
+      headerCellFilter : 'translate',
+      cellTemplate     : 'modules/stock/lots/templates/lot_lifetime.cell.html',
+      type : 'number',
+    }, {
+      field : 'S_RISK',
+      displayName : 'STOCK.RISK',
+      headerCellFilter : 'translate',
+      cellTemplate     : 'modules/stock/lots/templates/risk.cell.html',
+      type : 'number',
+      sort : {
+        direction : uiGridConstants.DESC,
+        priority : 3,
+      },
+    }, {
+      field : 'S_RISK_QUANTITY',
+      displayName : 'STOCK.RISK_QUANTITY',
+      headerCellFilter : 'translate',
+      cellTemplate     : 'modules/stock/lots/templates/risk_quantity.cell.html',
+      type : 'number',
+    }, {
+      field : 'action',
+      displayName : '',
+      enableFiltering : false,
+      enableSorting : false,
+      cellTemplate : 'modules/stock/lots/templates/action.cell.html',
+    }];
+
+  service.gridFooterTemplate = `
+    <div>
+      <b>{{ grid.appScope.countGridRows() }}</b>
+      <span translate>TABLE.AGGREGATES.ROWS</span>
+    </div>
+  `;
+
+  service.orderByDepot = (rowA, rowB) => {
+    return rowA.depot_text > rowB.depot_text ? 1 : -1;
+  };
+}
