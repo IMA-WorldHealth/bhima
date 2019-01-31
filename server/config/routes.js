ad0c0f1d75afa3bfcf72586bcb21d02fc67f6f4a
Jonathan Niles <jonathanwniles@gmail.com>
2019-01-31 15:52:13 +0100

    fix(vouchers): tools populate entity/records
    Vouchers tools can now set the entity and records for each of the items
    in the voucher grid.

--- server/config/routes.js
+++ server/config/routes.js
@@ -1069,7 +1069,9 @@ exports.configure = function configure(app) {
   app.get('/lots/generate_barcodes/:number', lots.generateBarcodes);
 
   app.get('/finance/entities', financeShared.lookupFinancialEntity);
+  app.get('/finance/entities/:uuid', financeShared.lookupFinancialEntityByUuid);
   app.get('/finance/records', financeShared.lookupFinancialRecord);
+  app.get('/finance/records/:uuid', financeShared.lookupFinancialRecordByUuid);
 
   // lots API
   app.get('/lots/:uuid', lots.details);
