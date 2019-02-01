4c03f0b09e0744df8bf6a02f84732149fc40fdd0
mbayopanda <mbayopanda@gmail.com>
2019-02-01 10:30:05 +0100

    update travis script

--- sh/travis.sh
+++ sh/travis.sh
@@ -31,9 +31,9 @@ mysql -h $DB_HOST -u root -e "FLUSH PRIVILEGES;"
 /sbin/start-stop-daemon --start --quiet --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1280x1024x16
 
 # download and install wkhtmltopdf globally
-wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-tar -xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
-sudo mv ./wkhtmltox/bin/wkhtmltopdf  /usr/bin/wkhtmltopdf
+# wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+# tar -xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+# sudo mv ./wkhtmltox/bin/wkhtmltopdf  /usr/bin/wkhtmltopdf
 
 # install greenkeeper lockfile
 yarn global add greenkeeper-lockfile@1
