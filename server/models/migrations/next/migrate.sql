a1946d906c7c235f36db25b946a9bc72bb5ad604
mbayopanda <mbayopanda@gmail.com>
2019-03-26 12:16:29 +0100

    update migration script

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -108,91 +108,6 @@ UPDATE unit SET parent = 160, `key` = 'DEPOT.TITLE' WHERE id = 20;
 DELETE FROM role_unit WHERE unit_id=196;
 DELETE FROM unit WHERE id=196;
 
--- indicators
-
-DROP TABLE IF EXISTS `indictor_status`;
-CREATE TABLE `indictor_status` (
-  `id` SMALLINT(5) UNSIGNED NOT NULL,
-  `text` VARCHAR(40) NOT NULL,
-  `translate_key` VARCHAR(100) NOT NULL,
-  PRIMARY KEY(`id`)
-)ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS `indicator`;
-CREATE TABLE `indicator` (
-  `uuid` BINARY(16) NOT NULL,
-  `status_id` SMALLINT(5) UNSIGNED NOT NULL,
-  `period_id` MEDIUMINT(8) UNSIGNED NOT NULL,
-  `user_id` SMALLINT(5) UNSIGNED NOT NULL,
-  `type` VARCHAR(40) NOT NULL,
-  PRIMARY KEY(`uuid`),
-  FOREIGN KEY (`period_id`) REFERENCES `period` (`id`) ON UPDATE CASCADE,
-  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
-  FOREIGN KEY (`status_id`) REFERENCES `indicator_status` (`id`) ON UPDATE CASCADE
-)ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
-
-
-DROP TABLE IF EXISTS `hospitalization_indicator`;
-CREATE TABLE `hospitalization_indicator` (
-  `uuid` BINARY(16),
-  `service_id`SMALLINT(5) UNSIGNED NOT NULL,
-  `day_realized` INT DEFAULT 0,
-  `bed_number` INT DEFAULT 0,
-  `daysOfHospitalization` INT DEFAULT 0,
-  `hospitalizedPatients` INT DEFAULT 0,
-  `hospitalizedPatientPerDay` INT DEFAULT 0,
-  `PatientsDied` INT DEFAULT 0,  
-  `indicator_uuid` BINARY(16) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  FOREIGN KEY (`service_id`) REFERENCES `service` (`id`) ON UPDATE CASCADE,
-  FOREIGN KEY (`indicator_uuid`) REFERENCES `indicator` (`uuid`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS `personel_indicator`;
-CREATE TABLE `personel_indicator` (
-  `uuid` BINARY(16),
-  `bed_number` INT DEFAULT 0,
-  `doctorsNumber` INT DEFAULT 0,
-  `nurseNumber` INT DEFAULT 0,
-  `caregiversNumber` INT DEFAULT 0,
-  `totalStaff` INT DEFAULT 0,
-  `externalConsultationNumber` INT DEFAULT 0,
-  `consultationNumber` INT DEFAULT 0,
-  `surgeryByDoctor` INT DEFAULT 0,
-  `day_realized` INT DEFAULT 0,
-  `hospitalizedPatients` INT DEFAULT 0,
-  `indicator_uuid` BINARY(16) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  FOREIGN KEY (`indicator_uuid`) REFERENCES `indicator` (`uuid`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
-
-DROP TABLE IF EXISTS `finances_indicator`;
-CREATE TABLE `finances_indicator` (
-  `uuid` BINARY(16),
-  `bed_number` INT DEFAULT 0,
-  `totalReceiptAmount` INT DEFAULT 0,
-  `subsidyAmount` INT DEFAULT 0,
-  `medicationSaleAmount` INT DEFAULT 0,
-  `totalExpenseAmount` INT DEFAULT 0,
-  `variousChargesAmount` INT DEFAULT 0,
-  `purchaseMedicationAmount` INT DEFAULT 0,
-  `personalChargeAmount` INT DEFAULT 0,
-  `totalOperatingExpenditureAmount` INT DEFAULT 0,
-  `totalDepreciationAmount` INT DEFAULT 0,
-  `totalDebtAmount` INT DEFAULT 0,
-  `totalCashAmount` INT DEFAULT 0,
-  `totalStockValueAmount` INT DEFAULT 0,
-  `personelNumber` INT DEFAULT 0,
-  `indicator_uuid` BINARY(16) NOT NULL,
-  PRIMARY KEY (`uuid`),
-  FOREIGN KEY (`indicator_uuid`) REFERENCES `indicator` (`uuid`) ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
-
-INSERT INTO `indicator_status`(`id`, `text`,`translate_key`)VALUES
-  (1, 'incomplete', 'FORM.LABELS.INCOMPLETE'),
-  (2, 'complete', 'FORM.LABELS.COMPLETE'),
-  (3, 'validated', 'FORM.LABELS.VALIDATED');
-
 -- @lomamech 2019-01-22 Break Even
 INSERT INTO `report` (`report_key`, `title_key`) VALUES
   ('breakEven', 'TREE.BREAK_EVEN_REPORT');
@@ -244,6 +159,107 @@ INSERT INTO `account_reference_type` (`id`, `label`, `fixed`) VALUES
 
 -- @lomamech 2019-03-11
 ALTER TABLE `voucher` ADD COLUMN `posted` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0;
+
+
+/**
+* dashboard indicators tables
+* @author: mbayopanda
+* @date: 3/26/2019
+**/
+DROP TABLE IF EXISTS `indicator_status`;
+CREATE TABLE `indicator_status` (
+  `id` SMALLINT(5) UNSIGNED NOT NULL,
+  `text` VARCHAR(40) NOT NULL,
+  `translate_key` VARCHAR(100) NOT NULL,
+  PRIMARY KEY(`id`)
+)ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `indicator_type`;
+CREATE TABLE `indicator_type` (
+  `id` SMALLINT(5) UNSIGNED NOT NULL,
+  `text` VARCHAR(40) NOT NULL,
+  `translate_key` VARCHAR(100) NOT NULL,
+  PRIMARY KEY(`id`)
+)ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `indicator`;
+CREATE TABLE `indicator` (
+  `uuid` BINARY(16) NOT NULL,
+  `service_id`SMALLINT(5) UNSIGNED NULL,
+  `status_id` SMALLINT(5) UNSIGNED NOT NULL,
+  `period_id` MEDIUMINT(8) UNSIGNED NOT NULL,
+  `user_id` SMALLINT(5) UNSIGNED NOT NULL,
+  `type_id` SMALLINT(5) UNSIGNED NOT NULL,
+  `created_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`uuid`),
+  UNIQUE KEY `unique_indicator_1` (`service_id`, `period_id`),
+  FOREIGN KEY (`period_id`) REFERENCES `period` (`id`) ON UPDATE CASCADE,
+  FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON UPDATE CASCADE,
+  FOREIGN KEY (`status_id`) REFERENCES `indicator_status` (`id`) ON UPDATE CASCADE,
+  FOREIGN KEY (`type_id`) REFERENCES `indicator_type` (`id`) ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `hospitalization_indicator`;
+CREATE TABLE `hospitalization_indicator` (
+  `uuid` BINARY(16),
+  `total_day_realized` INT DEFAULT 0,
+  `total_beds` INT DEFAULT 0,
+  `total_hospitalized_patient` INT DEFAULT 0,
+  `total_death` INT DEFAULT 0,  
+  `indicator_uuid` BINARY(16) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  FOREIGN KEY (`indicator_uuid`) REFERENCES `indicator` (`uuid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `staff_indicator`;
+CREATE TABLE `staff_indicator` (
+  `uuid` BINARY(16),
+  `total_doctors` INT DEFAULT 0,
+  `total_nurses` INT DEFAULT 0,
+  `total_caregivers` INT DEFAULT 0,
+  `total_staff` INT DEFAULT 0,
+  `total_external_visit` INT DEFAULT 0,
+  `total_visit` INT DEFAULT 0,
+  `total_surgery_by_doctor` INT DEFAULT 0,
+  `total_day_realized` INT DEFAULT 0,
+  `total_hospitalized_patient` INT DEFAULT 0,
+  `indicator_uuid` BINARY(16) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  FOREIGN KEY (`indicator_uuid`) REFERENCES `indicator` (`uuid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `finance_indicator`;
+CREATE TABLE `finance_indicator` (
+  `uuid` BINARY(16),
+  `total_revenue` INT DEFAULT 0,
+  `total_subsidies` INT DEFAULT 0,
+  `total_drugs_sale` INT DEFAULT 0,
+  `total_expenses` INT DEFAULT 0,
+  `total_other_charge` INT DEFAULT 0,
+  `total_drugs_purchased` INT DEFAULT 0,
+  `total_staff_charge` INT DEFAULT 0,
+  `total_operating_charge` INT DEFAULT 0,
+  `total_depreciation` INT DEFAULT 0,
+  `total_debts` INT DEFAULT 0,
+  `total_cash` INT DEFAULT 0,
+  `total_stock_value` INT DEFAULT 0,
+  `total_staff` INT DEFAULT 0,
+  `indicator_uuid` BINARY(16) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  FOREIGN KEY (`indicator_uuid`) REFERENCES `indicator` (`uuid`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci; 
+
+-- indicators status values
+INSERT INTO `indicator_status`(`id`, `text`,`translate_key`)VALUES
+  (1, 'incomplete', 'FORM.LABELS.INCOMPLETE'),
+  (2, 'complete', 'FORM.LABELS.COMPLETE'),
+  (3, 'validated', 'FORM.LABELS.VALIDATED');
+
+-- indicators types
+INSERT INTO `indicator_type`(`id`, `text`,`translate_key`)VALUES
+  (1, 'hospitalization', 'DASHBOARD.HOSPITALIZATION'),
+  (2, 'staff', 'DASHBOARD.STAFF'),
+  (3, 'fianance', 'DASHBOARD.FINANCE');
 
 -- dashbord folder
 INSERT INTO `unit` VALUES 
