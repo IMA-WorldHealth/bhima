04598bfd55ec1f0f4e9152988546db69d08384a6
Chris Lomame <lomamech@gmail.com>
2019-09-10 16:13:21 +0100

    Complete migrate.sql

---
+++ server/models/migrations/next/migrate.sql
@@ -0,0 +1,8 @@
+-- author: lomamech
+-- date: 2019-08-30
+-- Analysis of auxiliary cashboxes.
+INSERT INTO unit VALUES
+  (248, 'Analysis of cashboxes', 'REPORT.ANALYSIS_AUX_CASHBOXES.TITLE', 'Analysis of auxiliary cashboxes', 144, '/modules/reports/analysisAuxiliaryCash', '/reports/analysisAuxiliaryCash');
+
+INSERT INTO report (id, report_key, title_key) VALUES
+  (38, 'analysisAuxiliaryCash', 'REPORT.ANALYSIS_AUX_CASHBOXES.TITLE');
