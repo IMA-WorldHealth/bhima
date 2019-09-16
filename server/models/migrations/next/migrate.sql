8faf8d1618e20ce7c6c45204d0875747fabef7d1
mbayopanda <mbayopanda@gmail.com>
2019-09-16 14:00:46 +0100

    release 1.4.0

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,8 +1,3 @@
--- author: lomamech
--- date: 2019-08-30
--- Analysis of auxiliary cashboxes.
-INSERT INTO unit VALUES
-  (248, 'Analysis of cashboxes', 'REPORT.ANALYSIS_AUX_CASHBOXES.TITLE', 'Analysis of auxiliary cashboxes', 144, '/modules/reports/analysisAuxiliaryCash', '/reports/analysisAuxiliaryCash');
-
-INSERT INTO report (id, report_key, title_key) VALUES
-  (38, 'analysisAuxiliaryCash', 'REPORT.ANALYSIS_AUX_CASHBOXES.TITLE');
+/*
+ * DATABASE CHANGES FOR VERSION 1.4.0 TO 1.5.0 
+ */
