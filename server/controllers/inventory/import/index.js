ddd7a1c49f96a9e002c266391a3a09d03111c4ce
Jonathan Niles <jonathanwniles@gmail.com>
2019-10-08 14:38:15 +0100

    chore(inventory): use async/await for import logic
    Migrates the inventory import controller to async/await.

--- server/controllers/inventory/import/index.js
+++ server/controllers/inventory/import/index.js
@@ -1,8 +1,8 @@
 /**
- * Inventory Import Module
+ * @overview Inventory Import Module
  *
- * This module is responsible of handling the import of inventories
- * and related stock quantities
+ * This module implements the logic for importing the inventory into the application.  It provides
+ * a template file to show how the inventory schema must be modeled for the user to fill out.
  */
 const path = require('path');
 
@@ -32,52 +32,52 @@ function downloadTemplate(req, res, next) {
  *
  * @description this method allow to do an import of inventory and stock
  */
-function importInventories(req, res, next) {
-  if (!req.files || req.files.length === 0) {
-    const errorDescription = 'Expected at least one file upload but did not receive any files.';
-    const errorDetails = new BadRequest(errorDescription, 'ERRORS.MISSING_UPLOAD_FILES');
-    next(errorDetails);
-    return;
+async function importInventories(req, res, next) {
+  try {
+    if (!req.files || req.files.length === 0) {
+      const errorDescription = 'Expected at least one file upload but did not receive any files.';
+      throw new BadRequest(errorDescription, 'ERRORS.MISSING_UPLOAD_FILES');
+    }
+
+    const filePath = req.files[0].path;
+
+    const data = await util.formatCsvToJson(filePath);
+    if (!hasValidDataFormat(data)) {
+      throw new BadRequest('The given file has a bad data format for inventories', 'ERRORS.BAD_DATA_FORMAT');
+    }
+
+    const transaction = db.transaction();
+    const query = 'CALL ImportInventory(?, ?, ?, ?, ?, ?, ?);';
+
+    data.forEach(item => {
+      const queryParams = [
+        req.session.enterprise.id,
+        item.inventory_group_name,
+        item.inventory_code,
+        item.inventory_text,
+        item.inventory_type,
+        item.inventory_unit,
+        item.inventory_unit_price,
+      ];
+
+      transaction.addQuery(query, queryParams);
+    });
+
+    await transaction.execute();
+    res.sendStatus(200);
+  } catch (e) {
+    next(e);
   }
-
-  let query;
-  let queryParams;
-  const filePath = req.files[0].path;
-
-  util.formatCsvToJson(filePath)
-    .then(data => {
-      if (!hasValidDataFormat(data)) {
-        throw new BadRequest('The given file has a bad data format for inventories', 'ERRORS.BAD_DATA_FORMAT');
-      }
-
-      const transaction = db.transaction();
-
-      data.forEach(item => {
-        query = 'CALL ImportInventory(?, ?, ?, ?, ?, ?, ?);';
-        queryParams = [
-          req.session.enterprise.id,
-          item.inventory_group_name,
-          item.inventory_code,
-          item.inventory_text,
-          item.inventory_type,
-          item.inventory_unit,
-          item.inventory_unit_price,
-        ];
-        transaction.addQuery(query, queryParams);
-      });
-
-      return transaction.execute();
-    })
-    .then(() => res.sendStatus(200))
-    .catch(next);
 }
 
 /**
- * hasValidDataFormat
+ * @function hasValidDataFormat
  *
  * @description check if data has a valid format for inventories
  *
- * @param {object} data
+ * @param {Array} data - array of objects to check for valid properties
+ *
+ * @returns {boolean} - true if data is valid
  */
 function hasValidDataFormat(data = []) {
   return data.every(item => {
