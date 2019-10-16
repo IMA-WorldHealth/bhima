6945882584ca5f59b4c8d1fc01863fd7cca35fb7
mbayopanda <mbayopanda@gmail.com>
2019-10-16 16:04:42 +0100

    remove not necessary components

--- client/src/js/components/bhDropdownMenuItem/bhDropdownMenuItem.js
+++
@@ -1,9 +0,0 @@
-// angular.module('bhima.directives')
-//   .component('bhDropdownMenuItem', {
-//     controller : bhDropdownMenuItem,
-//     transclude : true,
-//     template : '<li role="menuitem" ng-transclude></li>',
-//   });
-
-// bhDropdownMenuItem.$inject = [];
-// function bhDropdownMenuItem() {}
