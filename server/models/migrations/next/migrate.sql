0581e9a51e8ae10e61b7e3a9e4f435d280763326
Jonathan Niles <jonathanwniles@gmail.com>
2019-10-16 10:42:43 +0100

    fix: add invoice_uuid to stock_movement
    Another patch to get the system into a stable place!

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -4,3 +4,6 @@
 
 ALTER TABLE inventory_log ADD COLUMN user_id SMALLINT(5) UNSIGNED NOT NULL;
 ALTER TABLE inventory_log ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES user(id);
+
+
+ALTER TABLE `stock_movement` ADD COLUMN `invoice_uuid` BINARY(16) NULL;
