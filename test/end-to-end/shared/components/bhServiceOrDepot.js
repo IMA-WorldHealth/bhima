4c4d0645c6f6201c3dae5f3c2d74521e51ba1d14
mbayopanda <mbayopanda@gmail.com>
2019-11-13 09:54:35 +0100

    enhance bhDepotSelect test component

--- test/end-to-end/shared/components/bhServiceOrDepot.js
+++ test/end-to-end/shared/components/bhServiceOrDepot.js
@@ -5,17 +5,12 @@ const bhDepot = require('./bhDepotSelect');
 module.exports = {
   selector : '[bh-service-or-depot-select]',
   set      : async function set(requestor, type = 'service', id) {
-    const SERVICE_REQUESTOR = 'service';
-    const DEPOT_REQUESTOR = 'depot';
+    const map = {
+      service : bhService,
+      depot : bhDepot,
+    };
 
-    if (type === 'service') {
-      await element(by.css(`[data-requestor-option="${SERVICE_REQUESTOR}"]`)).click();
-      await bhService.set(requestor, id);
-    }
-
-    if (type === 'depot') {
-      await element(by.css(`[data-requestor-option="${DEPOT_REQUESTOR}"]`)).click();
-      await bhDepot.set(requestor, id);
-    }
+    await element(by.css(`[data-requestor-option="${type}"]`)).click();
+    await map[type].set(requestor, id);
   },
 };
