6f27384b9795bb71d22fa980969206d35e0048c7
mbayopanda <mbayopanda@gmail.com>
2019-11-30 02:21:45 +0100

    remove data collector reference in migration script

--- server/models/migrations/v1.7.0-v1.8.0/migrate.sql
+++ server/models/migrations/v1.7.0-v1.8.0/migrate.sql
@@ -75,35 +75,6 @@ INSERT INTO `stock_requestor_type` (`type_key`, `title_key`) VALUES
   ('service', 'FORM.LABELS.SERVICE'),
   ('depot', 'FORM.LABELS.DEPOT');
 
-INSERT INTO `unit` VALUES
-  (254, 'Data Collection', 'TREE.DATA_COLLECTION', '', 0, '/modules/data_collection', '/data_collection'),
-  (255, 'Fill Form', 'TREE.FILL_FORM', '', 254, '/modules/fill_form', '/fill_form'),
-  (256, 'Display Metadata', 'TREE.DISPLAY_METADATA', '', 254, '/modules/display_metadata', '/display_metadata'),
-  (257, 'Data Kit', 'TREE.DATA_KIT', 'Data Kit', 254, '/modules/data_kit', '/data_kit'),
-  (258, 'Data Collector Management', 'TREE.FORMS_MANAGEMENT', '', 257, '/modules/data_collector_management', '/data_collector_management'),
-  (259, 'Choices list management', 'TREE.CHOICES_LIST_MANAGEMENT', '', 257, '/modules/choices_list_management', '/choices_list_management'),
-  (260, 'Survey Form', 'TREE.FORMS_CONFIGURATION', '', 257, '/modules/survey_form', '/survey_form'),  
-  (261, 'Data Kit Report', 'TREE.DATA_KIT_REPORT', 'Data Kit Report', 144, '/modules/reports/dataKit', '/reports/dataKit');
-  
-INSERT INTO `report` (`report_key`, `title_key`) VALUES
-  ('dataKit', 'TREE.DATA_KIT_REPORT');
-
--- Survey Form Type
-INSERT INTO `survey_form_type` (`id`, `label`, `type`, `is_list`) VALUES
-  (1, 'FORM.LABELS.NUMBER', 'number', 0),
-  (2, 'FORM.LABELS.TEXT', 'text', 0),
-  (3, 'FORM.LABELS.SELECT_ONE', 'select_one', 1),
-  (4, 'FORM.LABELS.SELECT_MULTIPLE', 'select_multiple', 1),
-  (5, 'FORM.LABELS.NOTE', 'note', 0),
-  (6, 'FORM.LABELS.DATE', 'date', 0),
-  (7, 'FORM.LABELS.TIME', 'time', 0),
-  (8, 'FORM.LABELS.IMAGE', 'image', 0),
-  (9, 'FORM.LABELS.CALCULATION', 'calculation', 0),
-  (10, 'FORM.LABELS.TEXT_AREA', 'text_area', 0);
-
--- BHIMA DATA COLLECTOR
--- 2019-11-05
-
 /*
  * @author: lomamech
  * @date: 2019-09-30
