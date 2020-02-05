a4810a203626d9c0cc131f1f3709a0406aca69be
Jonathan Niles <jonathanwniles@gmail.com>
2020-02-05 10:44:47 +0100

    fix(patients): remove all patient groups submits
    Removing all patient groups from a patient didn't work previously.  This
    was because we had an invalid check in the submit method that would
    never send the request to the server.  Now, we check that either there
    are groups to submit or the user is removing all groups.
    
    We also ensure that if the user is removing all groups, the checkboxes
    are cleared and vice versa.

--- client/src/modules/patients/registry/editPatientGroup.js
+++ client/src/modules/patients/registry/editPatientGroup.js
@@ -8,6 +8,12 @@ EditPatientGroupModalController.$inject = [
 
 /**
  * @class EditPatientGroupModalController
+ *
+ * @description
+ * This controller provides the functionality to change patient groups
+ * in bulk.  Multiple patients are provided in the 'data' parameter, and
+ * they are able to be put into the desired patient groups by selecting
+ * checkboxes.
  */
 function EditPatientGroupModalController(data, Instance, Notify, PatientGroups, Patients) {
   const vm = this;
@@ -15,12 +21,17 @@ function EditPatientGroupModalController(data, Instance, Notify, PatientGroups, 
   vm.submit = submit;
   vm.subscribedGroups = {};
 
-  PatientGroups.read().then(sessionGroups => {
-    vm.patientGroups = sessionGroups;
-    sessionGroups.forEach((patientGroup) => {
-      vm.subscribedGroups[patientGroup.uuid] = false;
-    });
-  }).catch(Notify.handleError);
+  vm.onToggleCheckboxes = onToggleCheckboxes;
+  vm.onToggleRemoveAll = onToggleRemoveAll;
+
+  PatientGroups.read()
+    .then(sessionGroups => {
+      vm.patientGroups = sessionGroups;
+      sessionGroups.forEach((patientGroup) => {
+        vm.subscribedGroups[patientGroup.uuid] = false;
+      });
+    })
+    .catch(Notify.handleError);
 
   function getGroups() {
     const subscribedGroups = [];
@@ -35,22 +46,37 @@ function EditPatientGroupModalController(data, Instance, Notify, PatientGroups, 
     return subscribedGroups;
   }
 
+  // fires when any of the patient group checkboxes are toggled
+  function onToggleCheckboxes() {
+    if (vm.removeAssignedGroups) {
+      vm.removeAssignedGroups = 0;
+    }
+  }
+
+  // fires when the "remove all" checkbox is toggled
+  function onToggleRemoveAll() {
+    vm.subscribedGroups = {};
+  }
+
   function submit() {
     if (vm.patient_group_uuid) {
       Notify.danger('FORM.ERRORS.HAS_ERRORS');
-    } else {
-      const subscribedGroups = getGroups();
-      if (subscribedGroups.length > 0) {
-        const options = {
-          subscribedGroups,
-          patientUuids : data,
-          removeAssignedGroups : vm.removeAssignedGroups,
-        };
-
-        Patients.bulkUpdateGroups(options)
-          .then(() => Instance.close(true))
-          .catch(Notify.handleError);
-      }
+      return null;
     }
+
+    const subscribedGroups = getGroups();
+    if (subscribedGroups.length === 0 && !vm.removeAssignedGroups) {
+      return null;
+    }
+
+    const options = {
+      subscribedGroups,
+      patientUuids : data,
+      removeAssignedGroups : vm.removeAssignedGroups,
+    };
+
+    return Patients.bulkUpdateGroups(options)
+      .then(() => Instance.close(true))
+      .catch(Notify.handleError);
   }
-}//
+}
