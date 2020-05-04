b70178af45a816b90ba4ccc463fcfe2313e87679
mbayopanda <mbayopanda@gmail.com>
2020-05-04 12:42:53 +0100

    fix sql linting issues

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -5,24 +5,24 @@
 INSERT INTO unit VALUES
 (268, '[Stock] Consumption graph','TREE.STOCK_CONSUMPTION_GRAPH_REPORT','Stock Consumption graph report', 144,'/modules/reports/generated/stock_consumption_graph_report','/reports/stock_consumption_graph_report');
   
-INSERT INTO `report` (`report_key`, `title_key`) VALUES
+INSERT INTO report (`report_key`, `title_key`) VALUES
 ('stock_consumption_graph_report', 'REPORT.STOCK_CONSUMPTION_GRAPH_REPORT.TITLE');
 
 /*
  * @author: mbayopanda
  * @date: 2020-04-20
  */
-INSERT INTO `unit` VALUES
+INSERT INTO unit VALUES
   (269, 'Inventory Adjustment', 'TREE.INVENTORY_ADJUSTMENT', 'Inventory Adjustment', 160, '/modules/stock/inventory-adjustment/inventory-adjustment.html', '/stock/inventory-adjustment');
 
-INSERT INTO `flux` VALUES 
+INSERT INTO flux VALUES 
   (14, 'STOCK_FLUX.INVENTORY_RESET'),
   (15, 'STOCK_FLUX.INVENTORY_ADJUSTMENT');
 
 
 --@lomamech 2020-04-25
-ALTER TABLE `stock_movement` ADD COLUMN `period_id` MEDIUMINT(8) UNSIGNED NOT NULL;
-ALTER TABLE `stock_movement` ADD FOREIGN KEY (`period_id`) REFERENCES `period` (`id`);
+ALTER TABLE stock_movement ADD COLUMN `period_id` MEDIUMINT(8) UNSIGNED NOT NULL;
+ALTER TABLE stock_movement ADD FOREIGN KEY (`period_id`) REFERENCES `period` (`id`);
 
 UPDATE stock_movement AS sm
 INNER JOIN (
