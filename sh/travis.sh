ea8586362a61568f0834f3040a90939dda0483c3
Artur Neumann <artur@jankaritech.com>
2020-05-05 17:02:05 +0545

    run CI with MariaDB
    fixes #3501

--- sh/travis.sh
+++ sh/travis.sh
@@ -7,6 +7,8 @@ set -a
 source .env.development
 set +a
 
+mysql --version
+
 # setup usernames and permissions
 mysql -h $DB_HOST -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'$DB_HOST' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
 mysql -h $DB_HOST -u root -e "FLUSH PRIVILEGES;"
