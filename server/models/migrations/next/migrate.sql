fcb7684e8d7b777bc34703d264463c3d2079e5f5
Jonathan Niles <jonathanwniles@gmail.com>
2020-05-11 12:24:45 +0100

    fix(purchase): make DECIMALs compatible with PJ/GL
    Fixes the purchase table to make unit price and total DECIMAL columns
    compatible with the DECIMAL values stored in the general_ledger and
    posting_journal. The previous iteration made the values far too small.
    
    Closes #4478.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -5,10 +5,10 @@
 INSERT INTO unit VALUES
   (269, 'Inventory Adjustment', 'TREE.INVENTORY_ADJUSTMENT', 'Inventory Adjustment', 160, '/modules/stock/inventory-adjustment/inventory-adjustment.html', '/stock/inventory-adjustment');
 
-INSERT INTO flux VALUES 
+INSERT INTO flux VALUES
   (14, 'STOCK_FLUX.INVENTORY_RESET'),
   (15, 'STOCK_FLUX.INVENTORY_ADJUSTMENT');
-  
+
 
 /*
  * @author: @lomamech
@@ -19,3 +19,11 @@ INSERT INTO unit VALUES
 
 INSERT INTO `report` (`report_key`, `title_key`) VALUES
   ('invoicedReceivedStock', 'REPORT.COMPARE_INVOICED_RECEIVED.TITLE');
+
+
+/**
+@author: jniles
+@date: 2020-05-11
+*/
+ALTER TABLE `purchase_item` MODIFY COLUMN `unit_price` DECIMAL(19,8) UNSIGNED NOT NULL DEFAULT 0.00;
+ALTER TABLE `purchase_item` MODIFY COLUMN `total` DECIMAL(19,8) UNSIGNED NULL DEFAULT 0.00;
