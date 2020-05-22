1bf93472d7c1dc1bc326f4ce83b61c603e4d1f76
Chris Lomame <lomamech@gmail.com>
2020-05-22 16:16:39 +0100

    Saving changes

---
+++ migration-hev_mai_2020.sql
@@ -0,0 +1,9 @@
+SET names 'utf8mb4';
+SET character_set_database = 'utf8mb4';
+SET collation_database = 'utf8mb4_unicode_ci';
+SET CHARACTER SET utf8mb4, CHARACTER_SET_CONNECTION = utf8mb4;
+SET collation_connection = 'utf8mb4_unicode_ci';
+
+
+
+
--- server/controllers/stock/reports/stock/value.js
+++ server/controllers/stock/reports/stock/value.js
@@ -20,7 +20,7 @@ function stockValue(req, res, next) {
 }
 
 async function reporting(_options, session) {
-
+  const transaction = db.transaction();
   const data = {};
   const enterpriseId = session.enterprise.id;
 
@@ -30,15 +30,63 @@ async function reporting(_options, session) {
 
   const report = new ReportManager(STOCK_VALUE_REPORT_TEMPLATE, session, optionReport);
 
+
   const options = (typeof (_options.params) === 'string') ? JSON.parse(_options.params) : _options.params;
   data.dateTo = options.dateTo;
   data.depot = await db.one('SELECT * FROM depot WHERE uuid=?', [db.bid(options.depot_uuid)]);
-  const stockValues = await db.exec('CALL stockValue(?,?,?);', [
-    db.bid(options.depot_uuid), options.dateTo, options.currency_id,
-  ]);
+
+  // Get inventories movemented
+  const sqlGetInventories = `
+    SELECT DISTINCT(BUID(mov.inventory_uuid)) AS inventory_uuid, mov.text AS inventory_name
+    FROM(
+      SELECT inv.uuid AS inventory_uuid, inv.text, sm.date
+      FROM stock_movement AS sm
+      JOIN lot AS l ON l.uuid = sm.lot_uuid
+      JOIN inventory AS inv ON inv.uuid = l.inventory_uuid
+      WHERE sm.depot_uuid = ? AND DATE(sm.date) <= DATE(?)
+    ) AS mov
+    ORDER BY mov.text ASC;
+  `;
+
+  const stockValues = await db.exec(sqlGetInventories, [db.bid(options.depot_uuid), options.dateTo]);
+
+  stockValues.forEach(inventory => {
+    const sqlGetValueByMovement = `
+      SELECT BUID(gl1.inventory_uuid) AS inventory_uuid, gl1.inventory_text, gl1.quantity, gl1.unit_cost, gl1.is_exit,
+      gl1.valueStock, gl1.quantityCheck, (@cumquant := gl1.quantity + @cumquant) AS quantityStock,
+      (@cumsum := gl1.quantityCheck + @cumsum) AS quantityCum, (@cumtotal := gl1.valueStock + @cumtotal) AS cumtotal
+      FROM (
+        SELECT inv.uuid AS inventory_uuid, inv.text AS inventory_text, sm.date, IF(sm.is_exit, (sm.quantity * (-1)),
+        sm.quantity) AS quantity, sm.unit_cost, sm.is_exit, sm.flux_id,
+        IF (sm.is_exit, 0, (sm.quantity * sm.unit_cost)) AS valueStock, IF (sm.is_exit, 0, sm.quantity) AS quantityCheck
+        FROM stock_movement AS sm
+        JOIN lot AS l ON l.uuid = sm.lot_uuid
+        JOIN inventory AS inv ON inv.uuid = l.inventory_uuid
+        WHERE inv.uuid = ? AND sm.depot_uuid = ? AND DATE(sm.date) <= DATE(?)
+        ORDER BY sm.date, sm.flux_id ASC
+      ) AS gl1, (SELECT @cumquant := 0)z, (SELECT @cumsum := 0) AS b, (SELECT @cumtotal := 0)a
+    `;
+
+    transaction
+      .addQuery(sqlGetValueByMovement, [db.bid(inventory.inventory_uuid), db.bid(options.depot_uuid), options.dateTo]);
+  });
+
+  const inventoriesValues = await transaction.execute();
+
+  stockValues.forEach(inventory => {
+    inventoriesValues.forEach(item => {
+      const lastMovement = item[item.length - 1];
+
+      if (inventory.inventory_uuid === lastMovement.inventory_uuid) {
+        inventory.stockQtt = lastMovement.quantityStock;
+        inventory.stockUnitCost = lastMovement.cumtotal / lastMovement.quantityCum;
+        inventory.stockValue = lastMovement.quantityStock * inventory.stockUnitCost;
+      }
+    });
+  });
 
   const stockValueElements = options.exclude_zero_value
-    ? stockValues[0].filter(item => item.stockValue > 0) : stockValues[0];
+    ? stockValues.filter(item => item.stockValue > 0) : stockValues;
 
   data.stockValues = stockValueElements || [];
   const stokTolal = stockValues[1][0] || {};
