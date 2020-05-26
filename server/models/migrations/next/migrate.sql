093c244aba02819e94940c7120f8a9ba3eb6bd63
jeremielodi <jeremielodi@gmail.com>
2020-05-26 07:55:47 +0100

    feat(inventory group) set account not required if enterprise setting enable_auto_stock_accounting = 0;
    closes #4525

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -6,3 +6,9 @@
 */
 UPDATE `report` SET `report_key` = 'stock_sheet' WHERE `title_key` = 'REPORT.STOCK.INVENTORY_REPORT';
 UPDATE `unit` SET `path` = '/reports/stock_sheet', `url` = '/modules/reports/stock_sheet' WHERE id = 182;
+
+/**
+@author: jeremielodi:
+@date: 2020-05-26
+*/
+ALTER TABLE enterprise_setting MODIFY `enable_auto_stock_accounting` TINYINT(1) NOT NULL DEFAULT 1;
