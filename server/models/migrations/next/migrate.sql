9294816db8a8b9ade0ae1fec590f0963b795c443
Jonathan Niles <jonathanwniles@gmail.com>
2020-06-11 12:13:34 +0100

    fix(db): ensure HR modules are under HR
    Closes #4597.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,3 +1,7 @@
 /* migration script from the version 1.13.1 to the next one */
 
 ALTER TABLE `unit` DROP COLUMN `url`;
+
+-- update units to make sure they are in the right category
+UPDATE `unit` SET parent = 57 WHERE id = 183;
+UPDATE `unit` SET parent = 57 WHERE id = 184;
