a8b7d1f7cbfac971f335ab09b66a5a52d3196dd5
Jonathan Niles <jonathanwniles@gmail.com>
2020-08-03 10:13:45 +0100

    test: add browserstack to protractor
    Adds BrowserStack for testing our end to end tests.

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,16 +1,12 @@
 /* global browser, element, by */
-const q = require('q');
 
 // we want to make sure we run tests locally, but TravisCI
 // should run tests on it's own driver.  To find out if it
 // is Travis loading the configuration, we parse the
-// process.env.TRAVIS_BUILD_NUMBER and reconfigure for travis
-// as appropriate.
+// process.env.CI and reconfigure for travis as appropriate.
 
 const config = {
   specs : ['test/end-to-end/**/*.spec.js'],
-
-  // SELENIUM_PROMISE_MANAGER : false,
 
   framework : 'mocha',
   baseUrl   : 'http://localhost:8080/',
@@ -34,48 +30,50 @@ const config = {
   },
 
   // this will log the user in to begin with
-  onPrepare : () => {
-    return q.fcall(async () => {
+  onPrepare : async () => {
+    await browser.get('http://localhost:8080/#!/login');
 
-      await browser.driver.manage().window().maximize();
+    // Turns off ng-animate animations for all elements in the
+    await element(by.css('body')).allowAnimations(false);
 
-      await browser.get('http://localhost:8080/#!/login');
+    await element(by.model('LoginCtrl.credentials.username')).sendKeys('superuser');
+    await element(by.model('LoginCtrl.credentials.password')).sendKeys('superuser');
+    await element(by.css('[data-method="submit"]')).click();
 
-      // Turns off ng-animate animations for all elements in the
-      await element(by.css('body')).allowAnimations(false);
-
-      await element(by.model('LoginCtrl.credentials.username')).sendKeys('superuser');
-      await element(by.model('LoginCtrl.credentials.password')).sendKeys('superuser');
-      await element(by.css('[data-method="submit"]')).click();
-
-      // NOTE - you may need to play with the delay time to get this to work properly
-      // Give this plenty of time to run
-    }).delay(3000);
+    // NOTE - you may need to play with the delay time to get this to work properly
+    // Give this plenty of time to run
+    await new Promise(resolve => setTimeout(resolve, 3000));
   },
 };
 
-// configuration for running on SauceLabs via Travis
+// configuration for running on BrowserStack via Travis
 if (process.env.CI) {
-  // SauceLabs credentials
+  delete config.plugins;
+  delete config.capabilities;
+  delete config.localSeleniumStandaloneOpts;
+  delete config.seleniumServerJar;
+
+  config.browserstackUser = process.env.BROWSERSTACK_USERNAME;
+  config.browserstackKey = process.env.BROWSERSTACK_ACCESS_KEY;
+
+  config.mochaOpts.timeout = (90 * 1000); // 1.5 minute timeout
+
   // report directory on the server(ubuntu)
   process.env.REPORT_DIR = '/opt/reports/';
 
   // modify the browsers to use Travis identifiers
   config.multiCapabilities = [{
-    // 'browserName': 'firefox',
-    //  'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-    //  'build': process.env.TRAVIS_BUILD_NUMBER,
-  // }, {
-    browserName         : 'chrome',
-    'tunnel-identifier' : process.env.TRAVIS_JOB_NUMBER,
-    build               : process.env.TRAVIS_BUILD_NUMBER,
-    chromeOptions : {
-      args : ['--headless', '--disable-gpu', '--window-size=1920,1080'],
-    },
+    browserName : 'Chrome',
+    name : 'BHIMA',
+    'browserstack.localIdentifier' : process.env.BROWSERSTACK_LOCAL_IDENTIFIER,
+    'browserstack.local' : true,
+    'browserstack.timezone' : '\'Africa/Kinshasa\'',
+    'browserstack.selenium_version' : '3.141.59',
+    build : process.env.TRAVIS_BUILD_NUMBER,
+    os_version : '10',
+    os : 'Windows',
+    resolution : '1920x1080',
   }];
-
-  delete config.capabilities;
-  delete config.plugins;
 }
 
 // expose to the outside world
