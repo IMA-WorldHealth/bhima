744f4009e0b861c4a4a5f7815f5d111e2c6a0f8b
jeremielodi <jeremielodi@gmail.com>
2020-08-24 09:56:52 +0100

    e2e test for donor management

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -784,3 +784,8 @@ ALTER TABLE `transaction_history`  ADD COLUMN `action` VARCHAR(15) DEFAULT 'edit
 */
 INSERT INTO unit VALUES
 (290,'Donors','TREE.DONOR','Donors management',1,'/donors');
+
+ALTER TABLE `donor` 
+  ADD COLUMN `email`  VARCHAR(50) DEFAULT NULL,
+  ADD COLUMN `phone`  VARCHAR(50) DEFAULT NULL,
+  ADD COLUMN `address`  VARCHAR(150) DEFAULT NULL;
