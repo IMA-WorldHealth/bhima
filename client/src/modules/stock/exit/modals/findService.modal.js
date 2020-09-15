dccba8dc03cb219ee4e87356debebb4ea853a1ef
Chris Lomame <lomamech@gmail.com>
2020-09-15 11:09:51 +0100

    feature(Stock Requisition) - Add requisition status excessive received - Add stock_requisition uuid for document map - Test: set data for E2E test and integration - Test: Reset value for many E2E requisition test
    closes #4849

--- client/src/modules/stock/exit/modals/findService.modal.js
+++ client/src/modules/stock/exit/modals/findService.modal.js
@@ -56,7 +56,8 @@ function StockFindServiceModalController(Instance, Service, Notify, Data, Stock)
       throw new Error('Requisition Not Found');
     }
 
-    if (requisition.status_key === 'done' || requisition.status_key === 'completed') {
+    if (requisition.status_key === 'done' || requisition.status_key === 'completed'
+      || requisition.status_key === 'excessive') {
       vm.requisitionMessage = 'REQUISITION.ALREADY_USED';
       vm.requisitionLabel = 'label label-success';
       throw new Error('Requisition Already Used');
