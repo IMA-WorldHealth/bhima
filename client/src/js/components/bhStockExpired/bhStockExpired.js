f65b7f1919fe64772dcf25d0214f05a59b9ebcd1
Jonathan Niles <jonathanwniles@gmail.com>
2020-09-24 15:06:25 +0100

    fix: improve UI of stock exits
    Improves the user interface of stock exits by addressing three
    concerns:
      1. The components for bhStockOut and bhStockExpired both now fit into
         the available space.
      2. The columns now add up to 12 on both the large and medium display
         sizes.
      3. The dates now show the raw date on hover with the mouse in case
         a user wants further details.
    
    Finally, it fixes a bug with the bhStockSoldOut where the template
    wasn't loading correctly.

--- client/src/js/components/bhStockExpired/bhStockExpired.js
+++ client/src/js/components/bhStockExpired/bhStockExpired.js
@@ -2,7 +2,6 @@ angular.module('bhima.components')
   .component('bhStockExpired', {
     templateUrl : 'js/components/bhStockExpired/bhStockExpired.html',
     controller  : bhStockExpiredController,
-    transclude  : true,
     bindings    : {
       depotUuid : '<',
       date : '<?',
@@ -10,53 +9,64 @@ angular.module('bhima.components')
   });
 
 bhStockExpiredController.$inject = [
-  'StockService', 'moment', 'NotifyService', 'DepotService',
+  'StockService', 'moment', 'NotifyService', 'DepotService', '$filter',
 ];
 
 /**
  * Stock Expired component
  */
-function bhStockExpiredController(Stock, moment, Notify, Depot) {
+function bhStockExpiredController(Stock, moment, Notify, Depot, $filter) {
   const $ctrl = this;
+
   $ctrl.loading = false;
   $ctrl.expiredInventories = [];
 
+  const $date = $filter('date');
+
   $ctrl.$onInit = () => {
-    expired();
+    fetchExpiredStock();
     getDepot();
   };
 
   $ctrl.$onChanges = () => {
-    expired();
+    fetchExpiredStock();
     getDepot();
   };
 
   function getDepot() {
     if (!$ctrl.depotUuid) return;
-    Depot.read($ctrl.depotUuid).then(depot => {
-      $ctrl.depot = depot;
-    });
+    Depot.read($ctrl.depotUuid)
+      .then(depot => {
+        $ctrl.depot = depot;
+      });
   }
 
   /**
-   * @function expred
-   * get expired inventories for a depot
+   * @function fetchExpiredStock()
+   *
+   * @description
+   * Gets expired inventories for a depot
    */
-  function expired() {
-    const dateTo = $ctrl.date || new Date();
+  function fetchExpiredStock() {
     if (!$ctrl.depotUuid) return;
+    const dateTo = $ctrl.date || new Date();
     $ctrl.loading = true;
+
     Stock.inventories.read(null, {
       is_expired : 1,
       depot_uuid : $ctrl.depotUuid,
       includeEmptyLot : 0,
       dateTo,
-    }).then(inventories => {
-      inventories.forEach(inventory => {
-        inventory.expiration_date = moment(inventory.expiration_date).fromNow();
-      });
-      $ctrl.expiredInventories = inventories;
-    }).catch(Notify.handleError)
+    })
+      .then(inventories => {
+        inventories.forEach(inventory => {
+          inventory.expiration_date_raw = $date(inventory.expiration_date);
+          inventory.expiration_date = moment(inventory.expiration_date).fromNow();
+        });
+
+        $ctrl.expiredInventories = inventories;
+      })
+      .catch(Notify.handleError)
       .finally(() => {
         $ctrl.loading = false;
       });
