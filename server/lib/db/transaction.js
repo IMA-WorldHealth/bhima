7c811716c1905cd51cdebf0fccc6b17d2caf7fe7
Jonathan Niles <jonathanwniles@gmail.com>
2020-09-29 15:44:28 +0100

    wip: make this work

--- server/lib/db/transaction.js
+++ server/lib/db/transaction.js
@@ -96,7 +96,7 @@ class Transaction {
    *
    * @returns {Promise} - the results of the transaction execution
    */
-  execute() {
+  execute(DEBUG_TEST) {
     debug(`#execute(): Executing ${this.queries.length} queries.`);
     const deferred = q.defer();
 
@@ -142,11 +142,29 @@ class Transaction {
             debug('#execute(): An error occured in the transaction. Rolling back.');
             debug('#execute(): %o', err);
 
-            // individual query did not work - rollback transaction
-            connection.rollback(() => {
-              connection.destroy();
-            });
+            if (DEBUG_TEST === 'yes') {
+              const pj = `
+              SELECT project_id, uuid, fiscal_year_id, period_id, trans_id,
+                trans_id_reference_number, trans_date, posting_journal.record_uuid,
+                description, account_id, debit, credit, debit_equiv, credit_equiv, currency_id,
+                entity_uuid, reference_uuid, comment, transaction_type_id, user_id
+              FROM posting_journal JOIN stage_trial_balance_transaction AS staged
+              ON posting_journal.record_uuid = staged.record_uuid;`;
 
+              queryConnection(connection, pj)
+                .then(result => {
+                  debug('result:', JSON.stringify(result));
+                  // individual query did not work - rollback transaction
+                  connection.rollback(() => {
+                    connection.destroy();
+                  });
+                });
+
+            } else {
+              connection.rollback(() => {
+                connection.destroy();
+              });
+            }
 
             // increment the number of restarts
             this.restarts += 1;
