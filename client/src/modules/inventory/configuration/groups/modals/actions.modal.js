fc0ade3a64017225673f32e65dcb5afe17f2855e
Jonathan Cameron <jmcameron@gmail.com>
2020-09-29 13:01:26 -0700

    WIP

--- client/src/modules/inventory/configuration/groups/modals/actions.modal.js
+++ client/src/modules/inventory/configuration/groups/modals/actions.modal.js
@@ -14,6 +14,8 @@ function InventoryGroupsActionsModalController(InventoryGroups, Notify, Instance
 
   // session
   vm.session = {};
+  console.log("EASA: ");
+  console.log(JSON.stringify(Session.stock_settings,null,2));
   vm.enableAutoStockAccounting = Session.stock_settings.enable_auto_stock_accounting;
 
   vm.isCreateState = (Data.action === 'add');
