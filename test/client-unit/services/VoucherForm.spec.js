025f215ead65296d6985ee08edc47f63caaa7f93
Jonathan Cameron <jmcameron@gmail.com>
2020-09-30 11:57:45 -0700

    Another test fix

--- test/client-unit/services/VoucherForm.spec.js
+++ test/client-unit/services/VoucherForm.spec.js
@@ -27,7 +27,7 @@ describe('VoucherForm', () => {
     $timeout = _$timeout_;
 
     // set up the required properties for the session
-    Session.create(Mocks.user(), Mocks.enterprise(), Mocks.project());
+    Session.create(Mocks.user(), Mocks.enterprise(), Mocks.stock_settings(), Mocks.project());
 
     httpBackend = $httpBackend;
 
