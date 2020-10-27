c1cfe6d9699a7e20a3962f3d4a89005bfb5455b2
Jonathan Niles <jonathanwniles@gmail.com>
2020-10-27 13:29:16 +0100

    fix(stock): update E2E tests

--- test/integration-stock/depots.js
+++ test/integration-stock/depots.js
@@ -51,70 +51,69 @@ describe('(/depots) The depots API ', () => {
       .catch(helpers.handler);
   });
 
-  it('GET /depots/:uuid/inventories/:uuid/cmm returns the CMM for a depot', () => {
+  it('GET /depots/:uuid/inventories/:uuid/cmm returns the CMM for a depot', async () => {
     const { principal } = helpers.data.depots;
     const { quinine, oxytocine, ampicilline } = helpers.data.inventories;
-    return agent.get(`/depots/${principal}/inventories/${quinine}/cmm`)
-      .then(res => {
-        expect(res).to.be.json; // eslint-disable-line
+    try {
+      let res = await agent.get(`/depots/${principal}/inventories/${quinine}/cmm`);
 
-        // eslint-disable-next-line
-        console.log('res.body:', res.body);
+      expect(res).to.be.json; // eslint-disable-line
 
-        const values = {
-          algo1 : 53.19,
-          algo2 : 2168.89,
-          algo3 : 53.19,
-          algo_msh : 53.33,
-          sum_days : 367,
-          sum_stock_day : 367,
-          sum_consumption_day : 9,
-          sum_consumed_quantity : 640,
-          number_of_month : 12,
-          sum_stock_out_days : 0,
-          days_before_consumption : 0,
-        };
+      // eslint-disable-next-line
+      console.log('res.body:', res.body);
 
-        expect(res.body, 'quinine CMM not calculated correctly').to.deep.include(values);
+      let values = {
+        algo1 : 53.19,
+        algo2 : 2168.89,
+        algo3 : 53.19,
+        algo_msh : 53.33,
+        sum_days : 367,
+        sum_stock_day : 367,
+        sum_consumption_day : 9,
+        sum_consumed_quantity : 640,
+        number_of_month : 12,
+        sum_stock_out_days : 0,
+        days_before_consumption : 0,
+      };
 
-        return agent.get(`/depots/${principal}/inventories/${oxytocine}/cmm`);
-      })
-      .then(res => {
-        const values = {
-          algo1 : 73.96,
-          algo2 : 3393.13,
-          algo3 : 73.96,
-          algo_msh : 74.17,
-          sum_days : 367,
-          sum_stock_day : 367,
-          sum_consumption_day : 8,
-          sum_consumed_quantity : 890,
-          number_of_month : 12,
-          sum_stock_out_days : 0,
-          days_before_consumption : 0,
-        };
+      expect(res.body, 'quinine CMM not calculated correctly').to.deep.include(values);
 
-        expect(res.body, 'oxytocine CMM not calculated correctly').to.deep.include(values);
-        return agent.get(`/depots/${principal}/inventories/${ampicilline}/cmm`);
-      })
-      .then(res => {
-        const values = {
-          algo1 : 33.39,
-          algo2 : 1143.75,
-          algo3 : 24.93,
-          algo_msh : 33.52,
-          sum_days : 367,
-          sum_stock_day : 274,
-          sum_consumption_day : 8,
-          sum_consumed_quantity : 300,
-          number_of_month : 12,
-          sum_stock_out_days : 93,
-          days_before_consumption : 0,
-        };
+      res = await agent.get(`/depots/${principal}/inventories/${oxytocine}/cmm`);
+      values = {
+        algo1 : 73.96,
+        algo2 : 3393.13,
+        algo3 : 73.96,
+        algo_msh : 74.17,
+        sum_days : 367,
+        sum_stock_day : 367,
+        sum_consumption_day : 8,
+        sum_consumed_quantity : 890,
+        number_of_month : 12,
+        sum_stock_out_days : 0,
+        days_before_consumption : 0,
+      };
 
-        expect(res.body, 'ampicilline CMM not calculated correctly').to.deep.include(values);
-      })
-      .catch(helpers.handler);
+      expect(res.body, 'oxytocine CMM not calculated correctly').to.deep.include(values);
+
+      res = await agent.get(`/depots/${principal}/inventories/${ampicilline}/cmm`);
+      values = {
+        algo1 : 33.39,
+        algo2 : 1143.75,
+        algo3 : 24.93,
+        algo_msh : 33.52,
+        sum_days : 367,
+        sum_stock_day : 274,
+        sum_consumption_day : 8,
+        sum_consumed_quantity : 300,
+        number_of_month : 12,
+        sum_stock_out_days : 93,
+        days_before_consumption : 0,
+      };
+
+      expect(res.body, 'ampicilline CMM not calculated correctly').to.deep.include(values);
+    } catch (err) {
+      helpers.handler(err);
+    }
   });
 
   it('GET /depots/:uuid/inventories/:uuid/lots returns the lots for a depot', () => {
