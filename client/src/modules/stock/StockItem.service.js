28d7d85367760758cca7e75d45e7eb5071f6099b
Jonathan Niles <jonathanwniles@gmail.com>
2020-11-10 20:28:06 +0100

    fix(stock): make adjustments one HTTP request
    Makes stock adjustments load all data in a single HTTP query.
    Also removes and simplifies the templates for stock adjustments.
    
    Closes #4982.

--- client/src/modules/stock/StockItem.service.js
+++ client/src/modules/stock/StockItem.service.js
@@ -27,5 +27,22 @@ function StockItemService() {
     this.isValid = this.inventory_uuid && this.lots.length > 0 && this.code;
   };
 
+  // a quick way to merge properties onto the sink if it exists in the source
+  function mergeIfPropertyExists(property, source, sink) {
+    if (angular.isDefined(source[property])) {
+      sink[property] = source[property];
+    }
+  }
+
+  StockItem.prototype.configure = function configure(item) {
+    const mergeableProperties = [
+      'uuid', 'inventory_uuid', 'code', 'label', 'quantity', 'unit_cost', 'expiration_date', 'lots', 'text',
+    ];
+
+    mergeableProperties.forEach(prop => mergeIfPropertyExists(prop, item, this));
+
+    this._initialised = true;
+  };
+
   return StockItem;
 }
