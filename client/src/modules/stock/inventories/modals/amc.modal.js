87d0204c44dc5297d2986d8743d2052797fa1b5e
Jonathan Niles <jonathanwniles@gmail.com>
2021-02-01 13:14:45 +0100

    chore(stock): touch up AMC/CMM modal
    Allows you to double click from anywhere in the cell, plus removes the
    double click on the row header.  Starts process towards getting proper
    AMC quantity in stock projections working.

--- client/src/modules/stock/inventories/modals/amc.modal.js
+++ client/src/modules/stock/inventories/modals/amc.modal.js
@@ -8,6 +8,10 @@ StockAMCModalController.$inject = [
 function StockAMCModalController(Stock, Notify, Instance, data) {
   const vm = this;
 
+  vm.loading = true;
+
+  console.log('quantity:', data.quantity);
+
   vm.close = () => Instance.dismiss();
 
   Stock.inventories.loadAMCForInventory(data.inventory_uuid, data.depot_uuid)
@@ -20,12 +24,18 @@ function StockAMCModalController(Stock, Notify, Instance, data) {
 
       vm.data.avg_consumption = vm.data[vm.settings.average_consumption_algo];
 
+      console.log('vm.data.quantity_in_stock:', vm.data.quantity_in_stock);
+
+      // calculate date when the stock will run out at current consumption rate
+      //
+
       // nicer aliases to use in the HTML
       vm.isAlgo1 = vm.settings.average_consumption_algo === 'algo1';
       vm.isAlgo2 = vm.settings.average_consumption_algo === 'algo2';
       vm.isAlgo3 = vm.settings.average_consumption_algo === 'algo3';
       vm.isAlgo4 = vm.settings.average_consumption_algo === 'algo_msh';
     })
-    .catch(Notify.handleError);
+    .catch(Notify.handleError)
+    .finally(() => { vm.loading = false; });
 
 }
