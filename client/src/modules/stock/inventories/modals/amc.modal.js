b625e544a0206ce3f79be46f2ad618bd26774c9d
Jonathan Niles <jonathanwniles@gmail.com>
2021-02-15 14:20:22 +0100

    chore: fix AMC modal to reflect API changes
    This commit fixes the AMC modal to better reflect the API changes made
    with the GetAMC function.

--- client/src/modules/stock/inventories/modals/amc.modal.js
+++ client/src/modules/stock/inventories/modals/amc.modal.js
@@ -20,10 +20,6 @@ function StockAMCModalController(Stock, Notify, Instance, data, moment, Constant
       vm.inventory = items.inventory;
       vm.depot = items.depot;
 
-      // FIXME(@jniles) - make this use the quantity from the GetAMC() algorithm which
-      // is currently returning incorrect data.
-      vm.data.quantity_in_stock = data.quantity_in_stock;
-
       vm.data.avg_consumption = vm.data[vm.settings.average_consumption_algo];
 
       // calculate date when the stock will run out at current consumption rate
