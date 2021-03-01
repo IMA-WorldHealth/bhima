25e36fceb4c96ed2f19f1cdd9694736f8b7b2bfd
Jonathan Niles <jonathanwniles@gmail.com>
2021-03-01 22:11:15 +0100

    chore: fix CMM calculation tests
    This commit fixes the CMM calculation tests now that we have gone
    through a month of 28 days.

--- test/integration-stock/depots.js
+++ test/integration-stock/depots.js
@@ -60,12 +60,12 @@ describe('(/depots) The depots API ', () => {
       expect(res).to.be.json; // eslint-disable-line
 
       let values = {
-        algo1 : 53.33,
+        algo1 : 53.48,
         algo2 : 2168.89,
-        algo3 : 53.33,
+        algo3 : 53.48,
         algo_msh : 53.33,
-        sum_days : 366,
-        sum_stock_day : 366,
+        sum_days : 365,
+        sum_stock_day : 365,
         sum_consumption_day : 9,
         sum_consumed_quantity : 640,
         number_of_month : 12,
@@ -77,16 +77,17 @@ describe('(/depots) The depots API ', () => {
       expect(res.body.algo2, 'quinine CMM algorithm 2 is not calculated correctly').to.equal(values.algo2);
       expect(res.body.algo3, 'quinine CMM algorithm 3 is not calculated correctly').to.equal(values.algo3);
       expect(res.body.algo_msh, 'quinine CMM algorithm MSH is not calculated correctly').to.equal(values.algo_msh);
+
       expect(res.body, 'quinine CMM not calculated correctly').to.deep.include(values);
 
       res = await agent.get(`/depots/${principal}/inventories/${oxytocine}/cmm`);
       values = {
-        algo1 : 74.17,
+        algo1 : 74.37,
         algo2 : 3393.13,
-        algo3 : 74.17,
+        algo3 : 74.37,
         algo_msh : 74.17,
-        sum_days : 366,
-        sum_stock_day : 366,
+        sum_days : 365,
+        sum_stock_day : 365,
         sum_consumption_day : 8,
         sum_consumed_quantity : 890,
         number_of_month : 12,
@@ -102,16 +103,16 @@ describe('(/depots) The depots API ', () => {
 
       res = await agent.get(`/depots/${principal}/inventories/${ampicilline}/cmm`);
       values = {
-        algo1 : 33.39,
+        algo1 : 33.27,
         algo2 : 1143.75,
-        algo3 : 25,
-        algo_msh : 33.39,
-        sum_days : 366,
-        sum_stock_day : 274,
+        algo3 : 25.07,
+        algo_msh : 33.15,
+        sum_days : 365,
+        sum_stock_day : 275,
         sum_consumption_day : 8,
         sum_consumed_quantity : 300,
         number_of_month : 12,
-        sum_stock_out_days : 92,
+        sum_stock_out_days : 90,
         head_days : 0,
       };
 
