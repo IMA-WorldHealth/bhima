b598c150babcbd83f69a77fd709d3f4a8506d4c1
Jonathan Niles <jonathanwniles@gmail.com>
2021-03-05 14:05:20 +0100

    feat(stock): add minimum delay to stock settings
    Adds a "minimum delay" setting to stock management to ensure that we are
    able to detect stock outs on time, even if the purchase data is messy.
    Previously, we relied on purchasing data solely for the delay, and users
    who put in a purchase order, then immediately delivered it, shoved the
    delay closer to 0.  This caused us to not warn users in time of stock
    outs.  The user can now configure the minimum number of months in
    decimal form on the stock settings page.
    
    Closes #5407.

--- test/integration/stock/stockSettings.js
+++ test/integration/stock/stockSettings.js
@@ -21,7 +21,7 @@ describe('(/stock/setting) Stock Settings API', () => {
     'month_average_consumption', 'default_min_months_security_stock',
     'enable_auto_purchase_order_confirmation', 'enable_auto_stock_accounting',
     'enable_strict_depot_permission', 'enable_supplier_credit', 'enable_strict_depot_distribution',
-    'average_consumption_algo',
+    'average_consumption_algo', 'min_delay',
   ];
 
   it('GET /stock/setting/:id returns the stock settings for the default Enterprise and checks a value', () => {
