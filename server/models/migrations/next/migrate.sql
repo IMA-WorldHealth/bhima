febdbe67cb8349e64856284a3e66b43928aa8566
Jonathan Niles <jonathanwniles@gmail.com>
2021-03-18 14:24:53 +0100

    feat(purchase): update purchase orders
    This commit implements the ability to update non-confirmed purchase
    orders.  If a purchase order has been confirmed, the only thing that
    should be updated is the status (canceled).  This is yet to be
    discussed.
    
    To modify a purchase order, one must go to the purchase registry and
    select "edit" on the purchase orders that is non yet confirmed.  This
    will take you to the regular purchase order creation page with all
    values filled out.
    
    Closes #5489.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -11,3 +11,11 @@ CREATE TABLE `stock_adjustment_log` (
   `new_quantity`  INT(11) NOT NULL DEFAULT 0,
   PRIMARY KEY `movement_uuid` (`movement_uuid`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+
+/**
+@author: jniles
+@date: 2021-03-18
+@description: add edited flag to the purchase order.
+*/
+CALL add_column_if_missing('purchase', 'edited', 'BOOLEAN NOT NULL DEFAULT FALSE');
