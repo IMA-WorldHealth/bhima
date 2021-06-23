c9b471079f8205bd4686a6b692233579d3a56a2b
mbayopanda <mbayopanda@gmail.com>
2021-06-23 21:23:41 +0100

    wac procedures
    fix performance issue due to not exiting the loop
    
    fix calculations
    
    wac on stock entry
    
    wac notify from purchase entry
    
    fix wac message
    
    fix procedure syntax
    
    fix and enhancement
    
    fix no inventory displayed by calling RecomputeStockValue
    
    fix date issue in test database

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -66,3 +66,26 @@ ALTER TABLE `purchase` MODIFY `cost` DECIMAL(19,8);
  * @description: If a medicine has only expired stock, we should consider it as "stock out" is enable_expired_stock_out
 */
 CALL add_column_if_missing('stock_setting', 'enable_expired_stock_out', 'TINYINT(1) NOT NULL DEFAULT 0');
+
+/**
+ * @author: mbayopanda
+ * @date: 2021-06-23
+ */
+DROP TABLE IF EXISTS `stock_value`;
+CREATE TABLE  `stock_value` (
+    `depot_uuid` BINARY(16) NOT NULL,
+    `inventory_uuid` BINARY(16) NOT NULL,
+    `date` DATE NOT NULL,
+    `quantity` INT(11) NOT NULL,
+    `wac` DECIMAL(19,4) NOT NULL,
+    KEY `depot_uuid` (`depot_uuid`),
+    KEY `inventory_uuid` (`inventory_uuid`),
+    INDEX `depot_inventory` (`depot_uuid`, `inventory_uuid`),
+    INDEX `date` (`date`),
+    CONSTRAINT `stock_value__depot` FOREIGN KEY (`depot_uuid`) REFERENCES `depot` (`uuid`),
+    CONSTRAINT `stock_value__inventory` FOREIGN KEY (`inventory_uuid`) REFERENCES `inventory` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+/** Recompute stock values for all depots */
+/** All databases in production must run this script */
+CALL RecomputeStockValue(NULL);
