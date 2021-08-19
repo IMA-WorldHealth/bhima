9ac40117461e960741379b506e9191bfc4426ea3
Jonathan Cameron <jmcameron@gmail.com>
2021-08-19 09:25:30 +0100

    Support selection of currency in Fee Center Report

--- client/src/modules/reports/generate/fee_center/fee_center.config.js
+++ client/src/modules/reports/generate/fee_center/fee_center.config.js
@@ -2,18 +2,20 @@ angular.module('bhima.controllers')
   .controller('fee_centerController', FeeCenterConfigController);
 
 FeeCenterConfigController.$inject = [
-  '$sce', 'NotifyService', 'BaseReportService', 'AppCache', 'reportData', '$state',
+  '$sce', 'NotifyService', 'BaseReportService', 'AppCache', 'reportData', '$state', 'SessionService',
 ];
 
 // TODO(@jniles) - this name is remarkably close to the fee center controller.  Let's name
 // it something else that is more distant.
-function FeeCenterConfigController($sce, Notify, SavedReports, AppCache, reportData, $state) {
+function FeeCenterConfigController($sce, Notify, SavedReports, AppCache, reportData, $state, Session) {
   const vm = this;
   const cache = new AppCache('configure_fee_center');
   const reportUrl = 'reports/finance/fee_center';
   vm.reportDetails = {};
   vm.previewGenerated = false;
   checkCachedConfiguration();
+
+  vm.currency_id = Session.enterprise.currency_id;
 
   vm.onSelectFiscalYear = (fiscalYear) => {
     vm.reportDetails.fiscal_id = fiscalYear.id;
@@ -26,9 +28,14 @@ function FeeCenterConfigController($sce, Notify, SavedReports, AppCache, reportD
     vm.reportDetails.start_date = period.start_date;
   };
 
+  vm.onSelectCurrency = currency => {
+    vm.currency_id = currency.id;
+  };
+
   vm.preview = function preview(form) {
     if (form.$invalid) { return null; }
     // update cached configuration
+    vm.reportDetails.currency_id = vm.currency_id;
     cache.reportDetails = angular.copy(vm.reportDetails);
 
     return SavedReports.requestPreview(reportUrl, reportData.id, angular.copy(vm.reportDetails))
