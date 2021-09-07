d345263b643081f59ca6a5fa12cb3b6a4becf605
mbayopanda <mbayopanda@gmail.com>
2021-09-07 16:58:30 +0100

    Enhancing the cost table

--- server/controllers/finance/reports/fee_center_step_down/index.js
+++ server/controllers/finance/reports/fee_center_step_down/index.js
@@ -13,7 +13,7 @@ exports.report = document;
  * @function reporting
  *
  * @description
- * Renders the Compte d'Exploitation
+ * Renders the fee center report
  *
  * @param {*} options the report options
  * @param {*} session the session
@@ -34,32 +34,19 @@ async function buildReport(params, session) {
   };
 
   const range = await fiscal.getDateRangeFromPeriods(periods);
-
   const query = 'CALL FeeCenterCostWithIndexes(?, ?);';
   const [feeCenters] = await db.exec(query, [range.dateFrom, range.dateTo]);
-
   const formattedFeeCenters = feeCenters.map(item => {
     item.principal = !!item.is_principal;
+    item.auxiliary = !item.principal;
     item.directCost = item.direct_cost;
     if (item.default_fee_center_index_id && !item.principal) {
       item.allocation = { method : 'proportion', field : item.default_fee_center_index_id };
     }
     return item;
   });
-
   const data = Stepdown.compute(formattedFeeCenters);
-
-  const cumulatedAllocatedCosts = Array(data.length).fill(0);
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].toDist.length; j++) {
-      cumulatedAllocatedCosts[j] += data[i].toDist[j];
-    }
-  }
-
-  const afterAllocations = cumulatedAllocatedCosts.map((value, index) => {
-    return data[index].principal ? value : 0;
-  });
-
+  const cumulatedAllocatedCosts = data.map((item, index, array) => (item.principal ? 0 : array[index].toDist[index]));
   const queryFeeCenterIndexesList = `
     SELECT 
       fci.id, 
@@ -72,9 +59,7 @@ async function buildReport(params, session) {
     ORDER BY fc.step_order ASC;
   `;
   const feeCenterIndexesList = await db.exec(queryFeeCenterIndexesList);
-
   const indexes = _.groupBy(feeCenterIndexesList, 'fee_center_index_label');
-
   const feeCenterList = [];
   const feeCenterIndexes = _.keys(indexes).map((index, i) => {
     const fcIndex = _.sortBy(indexes[index], 'step_order');
@@ -85,14 +70,53 @@ async function buildReport(params, session) {
     });
     return line;
   });
-
+  const auxiliaryIndexes = data.map((item, i) => (item.auxiliary ? i : null)).filter(item => !!item);
   const services = data.map(item => {
     item.distribution = item.toDist.map((value, i) => {
       const ratio = item.ratio ? item.ratio[i] : undefined;
       return { value, ratio };
     });
+    item.auxiliaryDistribution = item.distribution.map((value, i) => {
+      return auxiliaryIndexes.includes(i) ? value : null;
+    }).filter(value => !!value);
     return item;
   });
+  const directCostTotal = services.reduce((prev, curr) => { return curr.directCost + prev; }, 0);
+
+  // horizontal view
+  const hView = [];
+  let totalAfterAllocation = 0;
+
+  for (let i = 0; i < data.length; i++) {
+    const ei = data[i];
+    const row = {
+      name : ei.fee_center_label,
+      principal : ei.principal,
+      direct : ei.directCost,
+      values : [],
+      total : 0,
+    };
+    for (let z = 0; z < data.length; z++) {
+      const value = data[z].toDist[i];
+      const ratio = data[z].ratio ? data[z].ratio[i] : 0;
+      const selfCenter = !!(i === z);
+      const selfCenterValue = selfCenter ? value * -1 : null;
+      row.total += i !== z ? value : 0;
+      row.values.push({
+        value,
+        ratio,
+        selfCenter,
+        selfCenterValue,
+      });
+    }
+    // the total is the direct cost plus all allocations
+    row.total += ei.directCost;
+    // auxiliary cost center distribute all of their value
+    row.total = ei.auxiliary ? 0 : row.total;
+    // the total after all allocation (this value must be equal to the sum of all direct cost)
+    totalAfterAllocation += row.total;
+    hView.push(row);
+  }
 
   const context = {
     dateFrom : range.dateFrom,
@@ -100,10 +124,11 @@ async function buildReport(params, session) {
     currencyId : enterpriseCurrencyId,
     data,
     cumulatedAllocatedCosts,
-    afterAllocations,
     feeCenterIndexes,
     feeCenterList,
-    services,
+    directCostTotal,
+    totalAfterAllocation,
+    hView,
   };
 
   return report.render(context);
