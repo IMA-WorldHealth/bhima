883fc7dcc094e613b9c4d9fb4fd8f390f8d0c60b
mbayopanda <mbayopanda@gmail.com>
2021-09-14 09:43:30 +0100

    Refactoring migrate file

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -26,21 +26,21 @@ CREATE TABLE `cost_center_aggregate` (
   KEY `principal_center_id` (`principal_center_id`),
   KEY `period_id` (`period_id`),
   CONSTRAINT `cost_center_aggregate__period` FOREIGN KEY (`period_id`) REFERENCES `period` (`id`),
-  CONSTRAINT `cost_center_aggregate__cost_center_id` FOREIGN KEY (`cost_center_id`) REFERENCES `cost_center` (`id`),
-  CONSTRAINT `cost_center_aggregate__principal_center_id` FOREIGN KEY (`principal_center_id`) REFERENCES `cost_center` (`id`)
+  CONSTRAINT `cost_center_aggregate__cost_center_id` FOREIGN KEY (`cost_center_id`) REFERENCES `fee_center` (`id`),
+  CONSTRAINT `cost_center_aggregate__principal_center_id` FOREIGN KEY (`principal_center_id`) REFERENCES `fee_center` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
 
 CALL add_column_if_missing('posting_journal', 'cost_center_id', 'MEDIUMINT(8) UNSIGNED NULL');
 CALL add_column_if_missing('posting_journal', 'principal_center_id', 'MEDIUMINT(8) UNSIGNED NULL');
 
-CALL add_constraint_if_missing('posting_journal', 'pg__cost_center_1', 'FOREIGN KEY (`cost_center_id`) REFERENCES `cost_center` (`id`) ON UPDATE CASCADE');
-CALL add_constraint_if_missing('posting_journal', 'pg__cost_center_2', 'FOREIGN KEY (`principal_center_id`) REFERENCES `cost_center` (`id`) ON UPDATE CASCADE');
+CALL add_constraint_if_missing('posting_journal', 'pg__cost_center_1', 'FOREIGN KEY (`cost_center_id`) REFERENCES `fee_center` (`id`) ON UPDATE CASCADE');
+CALL add_constraint_if_missing('posting_journal', 'pg__cost_center_2', 'FOREIGN KEY (`principal_center_id`) REFERENCES `fee_center` (`id`) ON UPDATE CASCADE');
 
 CALL add_column_if_missing('general_ledger', 'cost_center_id', 'MEDIUMINT(8) UNSIGNED NULL');
 CALL add_column_if_missing('general_ledger', 'principal_center_id', 'MEDIUMINT(8) UNSIGNED NULL');
 
-CALL add_constraint_if_missing('general_ledger', 'general_ledger__cost_center_1', 'FOREIGN KEY (`cost_center_id`) REFERENCES `cost_center` (`id`) ON UPDATE CASCADE');
-CALL add_constraint_if_missing('general_ledger', 'general_ledger__cost_center_2', 'FOREIGN KEY (`principal_center_id`) REFERENCES `cost_center` (`id`) ON UPDATE CASCADE');
+CALL add_constraint_if_missing('general_ledger', 'general_ledger__cost_center_1', 'FOREIGN KEY (`cost_center_id`) REFERENCES `fee_center` (`id`) ON UPDATE CASCADE');
+CALL add_constraint_if_missing('general_ledger', 'general_ledger__cost_center_2', 'FOREIGN KEY (`principal_center_id`) REFERENCES `fee_center` (`id`) ON UPDATE CASCADE');
 
 /**
 author: @jmcameron
@@ -81,7 +81,7 @@ CALL add_column_if_missing('fee_center', 'allocation_basis_id', 'MEDIUMINT(8) UN
 CALL add_column_if_missing('fee_center', 'allocation_method', "VARCHAR(14) NOT NULL DEFAULT 'proportional'");
 
 INSERT IGNORE INTO `unit` VALUES 
-  (298, 'Fee Center Step-down','TREE.COST_CENTER_STEPDOWN','The fee center report with step-down algorithm', 286,'/reports/fee_center_step_down');
+  (298, 'Cost Center Step-down','TREE.COST_CENTER_STEPDOWN','The fee center report with step-down algorithm', 286,'/reports/cost_center_step_down');
 
 ALTER TABLE `cost_center_basis` MODIFY COLUMN `name` VARCHAR(200) NOT NULL;
 
@@ -97,22 +97,19 @@ author: @jniles
 date: 2021-09-02
 description: rename all cost centers to cost centers
 */
+
+/**
+ * THE USE OF RENAME IMPLY THAT TABLES EXISTS BEFORE TO RENAME
+ * AFTER A FRESH BUILD (yarn build:db) THIS CODES ARE NOT NECESSARY
+ */
 RENAME TABLE fee_center TO cost_center,
              reference_fee_center TO reference_cost_center,
              fee_center_distribution TO cost_center_allocation,
-             allocation_key TO allocation_key,
              service_fee_center TO service_cost_center;
-
-ALTER TABLE `reference_cost_center` RENAME COLUMN `fee_center_id` TO `cost_center_id`;
-ALTER TABLE `reference_cost_center` RENAME COLUMN `fee_center_id` TO `cost_center_id`;
 
 ALTER TABLE `cost_center_allocation` RENAME COLUMN `auxiliary_fee_center_id` TO `auxiliary_cost_center_id`;
 ALTER TABLE `cost_center_allocation` RENAME COLUMN `principal_fee_center_id` TO `principal_cost_center_id`;
-
 ALTER TABLE `service_cost_center` RENAME COLUMN `fee_center_id` TO `cost_center_id`;
-
-ALTER TABLE `allocation_key` RENAME COLUMN `auxiliary_fee_center_id` TO `auxiliary_cost_center_id`;
-ALTER TABLE `allocation_key` RENAME COLUMN `principal_fee_center_id` TO `principal_cost_center_id`;
 
 /**
 author: mbayopanda
@@ -121,4 +118,3 @@ description: rename allocation tables
 */
 RENAME TABLE cost_center_basis TO cost_center_allocation_basis,
              cost_center_basis_value TO cost_center_allocation_basis_value;
-
