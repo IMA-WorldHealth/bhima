e51c14b8c41e2af6767396b271746e78c917aead
mbayopanda <mbayopanda@gmail.com>
2021-10-21 00:50:07 +0100

    Cost center profit and expense report

---
+++ client/src/modules/reports/generate/cost_center_profit_and_expense/cost_center_profit_and_expense.config.js
@@ -0,0 +1,81 @@
+angular.module('bhima.controllers')
+  .controller('cost_center_profit_and_expenseController', CostCenterProfitAndExpenseReportConfigController);
+
+CostCenterProfitAndExpenseReportConfigController.$inject = [
+  '$sce', 'NotifyService', 'BaseReportService', 'AppCache', 'reportData', '$state',
+];
+
+/**
+ * @function CostCenterProfitAndExpenseReportConfigController
+ *
+ * @description
+ * This function renders the cost_center_profit_and_expense report.
+ */
+function CostCenterProfitAndExpenseReportConfigController($sce, Notify, SavedReports, AppCache, reportData, $state) {
+  const vm = this;
+  const cache = new AppCache('CostCenterProfitAndExpenseReport');
+  const reportUrl = 'reports/finance/cost_center_profit_and_expense';
+
+  vm.reportDetails = {};
+  vm.previewGenerated = false;
+
+  vm.onSelectFiscalYear = (fiscalYear) => {
+    vm.reportDetails.fiscal_id = fiscalYear.id;
+  };
+
+  vm.onSelectPeriodFrom = function onSelectPeriodFrom(period) {
+    vm.reportDetails.periodFrom = period.id;
+  };
+
+  vm.onSelectPeriodTo = function onSelectPeriodTo(period) {
+    vm.reportDetails.periodTo = period.id;
+  };
+
+  vm.onSelectCurrency = (currency) => {
+    vm.reportDetails.currency_id = currency.id;
+  };
+
+  vm.clearPreview = function clearPreview() {
+    vm.previewGenerated = false;
+    vm.previewResult = null;
+  };
+
+  vm.preview = function preview(form) {
+    if (form.$invalid) {
+      Notify.danger('FORM.ERRORS.RECORD_ERROR');
+      return 0;
+    }
+
+    // update cached configuration
+    cache.reportDetails = angular.copy(vm.reportDetails);
+
+    return SavedReports.requestPreview(reportUrl, reportData.id, angular.copy(vm.reportDetails))
+      .then(result => {
+        vm.previewGenerated = true;
+        vm.previewResult = $sce.trustAsHtml(result);
+      })
+      .catch(Notify.handleError);
+  };
+
+  vm.requestSaveAs = function requestSaveAs() {
+    const options = {
+      url : reportUrl,
+      report : reportData,
+      reportOptions : angular.copy(vm.reportDetails),
+    };
+
+    return SavedReports.saveAsModal(options)
+      .then(() => {
+        $state.go('reportsBase.reportsArchive', { key : options.report.report_key });
+      })
+      .catch(Notify.handleError);
+  };
+
+  checkCachedConfiguration();
+
+  function checkCachedConfiguration() {
+    if (cache.reportDetails) {
+      vm.reportDetails = angular.copy(cache.reportDetails);
+    }
+  }
+}
