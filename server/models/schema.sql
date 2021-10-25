750199d0f135c5f86f0c4c539ae5c7d268e3101f
lomamech <lomamech@gmail.com>
2021-10-25 09:33:17 +0100

    Sanitaze code

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -590,6 +590,7 @@ CREATE TABLE `enterprise_setting` (
   `enable_auto_email_report` TINYINT(1) NOT NULL DEFAULT 0,
   `enable_index_payment_system` TINYINT(1) NOT NULL DEFAULT 0,
   `base_index_growth_rate` TINYINT(3) UNSIGNED NOT NULL DEFAULT 0,
+  `posting_payroll_cost_center_mode` VARCHAR(100) NOT NULL DEFAULT 'default', -- With this function, transactions related to employee payment are done in bulk and require that each expense account be linked to a cost center
   PRIMARY KEY (`enterprise_id`),
   CONSTRAINT `enterprise_setting__enterprise` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprise` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
