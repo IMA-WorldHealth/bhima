905e8a5afbd39dee65ccd5e6a9e7f75490285d87
Jonathan Cameron <jmcameron@gmail.com>
2021-11-12 16:50:43 -0800

    New report for avg cost of meds per patient

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,3 +1,13 @@
 /*
  * Migration file from v1.21.1
  */
+
+/*
+ * @author: jmcameron
+ * @date: 2021-11-12
+ */
+INSERT IGNORE INTO `unit` VALUES
+  (305, 'Average Medical Costs Per Patient', 'TREE.AVERAGE_MED_COST_REPORT', 'Report of avg med costs', 282, '/reports/avg_med_costs_per_patient');
+
+INSERT IGNORE INTO `report` (`report_key`, `title_key`) VALUES
+  ('avg_med_costs_per_patient', 'TREE.AVERAGE_MED_COST_REPORT');
