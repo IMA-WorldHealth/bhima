1df27e73b871e295714dc960529dbb5b818a8f5e
Jonathan Niles <jonathanwniles@gmail.com>
2021-12-05 16:26:31 +0100

    chore: create release migration file

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,9 +1,3 @@
 /*
- * Migration file from v1.21.4
+ * Migrate Next
  */
-
- /*
-  * @author: mbayopanda
-  * @date: 2021-12-04
-  */
-CALL add_column_if_missing('enterprise_setting', 'enable_require_cost_center_for_posting', 'TINYINT(1) NOT NULL DEFAULT 0 AFTER `posting_payroll_cost_center_mode`');
