e4ed220da10c38b0529f7a4e84898fc223d970b0
Jonathan Niles <jonathanwniles@gmail.com>
2022-01-02 22:43:23 +0100

    wip(docker): ensure mysql builds database
    Ensures that the database is built by MySQL.

--- sh/docker-init.sh
+++ sh/docker-init.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-cat server/models/schema.sql \
- server/models/functions.sql \
+set -e
+
+cat server/models/schema.sql > temp/docker-build-schema.sql
+
+cat server/models/functions.sql \
  server/models/procedures/cash.sql \
  server/models/procedures/invoicing.sql \
  server/models/procedures/time_period.sql \
@@ -16,12 +19,25 @@ cat server/models/schema.sql \
  server/models/procedures/analysis.sql \
  server/models/procedures/migration-process.sql \
  server/models/procedures/cost_centers.sql \
- server/models/procedures.sql \
  server/models/admin.sql \
- server/models/triggers.sql \
- server/models/icd10.sql \
- server/models/bhima.sql \
-> temp/docker-build.sql
+ server/models/triggers.sql > temp/docker-build-procedures.sql
 
+cat server/models/icd10.sql \
+ server/models/bhima.sql > temp/docker-build-data.sql
+
+
+sed -i '/DELIMITER/d' temp/docker-build-schema.sql
+sed -i '/DELIMITER/d' temp/docker-build-procedures.sql
+sed -i '/DELIMITER/d' temp/docker-build-data.sql
+
+cat temp/docker-build-schema.sql > temp/docker-build.sql
+echo "DELIMITER \$\$" >> temp/docker-build.sql
+cat temp/docker-build-procedures.sql >> temp/docker-build.sql
+echo "DELIMITER ;" >> temp/docker-build.sql
+cat temp/docker-build-data.sql >> temp/docker-build.sql
 echo "Call zRecomputeEntityMap();" >> temp/docker-build.sql
 echo "Call zRecomputeDocumentMap();" >> temp/docker-build.sql
+
+rm temp/docker-build-schema.sql
+rm temp/docker-build-procedures.sql
+rm temp/docker-build-data.sql
