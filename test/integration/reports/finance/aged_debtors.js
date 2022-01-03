0702abb4a13876f1097e9e64827e15b14b10067f
Jonathan Cameron <jmcameron@gmail.com>
2022-01-03 12:56:45 -0800

    Updates for Aged Creditor Report

--- test/integration/reports/finance/aged_debtors.js
+++ test/integration/reports/finance/aged_debtors.js
@@ -2,7 +2,10 @@ const RenderingTests = require('../rendering');
 
 const target = '/reports/finance/debtors/aged';
 const options = {
+  fiscal_id : 4,
   period_id : 201802,
+  currency_id : 2,
+  enterprise_id : 1,
 };
 
 describe(`(${target}) Aged Debtors`, RenderingTests(target, null, options));
