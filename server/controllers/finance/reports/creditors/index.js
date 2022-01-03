bca7879739d5e01f429d9f7f5c0d7d808bd72f40
Jonathan Cameron <jmcameron@gmail.com>
2022-01-03 12:56:45 -0800

    Updates for Aged Creditor Report

--- server/controllers/finance/reports/creditors/index.js
+++ server/controllers/finance/reports/creditors/index.js
@@ -47,6 +47,7 @@ function agedCreditorReport(req, res, next) {
   db.one(sql, [qs.period_id])
     .then(period => {
       qs.date = period.end_date;
+      qs.enterprise_id = metadata.enterprise.id;
       // fire the SQL for the report
       return queryContext(qs);
     })
@@ -74,28 +75,49 @@ async function queryContext(params = {}) {
   // format the dates for MySQL escape
   const dates = _.fill(Array(5), params.date);
   const data = {};
+  const currencyId = db.escape(params.currency_id);
+  const enterpriseId = db.escape(params.enterprise_id);
+
+  const fySql = `
+    SELECT locked FROM fiscal_year
+    WHERE ID = (SELECT previous_FISCAL_year_id from fiscal_year where id = ?)
+  `;
+
+  const previousFy = await db.one(fySql, [params.fiscal_id]);
 
   const groupByMonthColumns = `
-    SUM(IF(MONTH(?) - MONTH(gl.trans_date) = 0, gl.debit_equiv - gl.credit_equiv, 0)) AS thirty,
-    SUM(IF(MONTH(?) - MONTH(gl.trans_date) = 1, gl.debit_equiv - gl.credit_equiv, 0)) AS sixty,
-    SUM(IF(MONTH(?) - MONTH(gl.trans_date) = 2, gl.debit_equiv - gl.credit_equiv, 0)) AS ninety,
-    SUM(IF(MONTH(?) - MONTH(gl.trans_date) > 2, gl.debit_equiv - gl.credit_equiv, 0)) AS excess,
+    SUM(IF(MONTH(?) - MONTH(gl.trans_date) = 0, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS thirty,
+    SUM(IF(MONTH(?) - MONTH(gl.trans_date) = 1, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS sixty,
+    SUM(IF(MONTH(?) - MONTH(gl.trans_date) = 2, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS ninety,
+    SUM(IF(MONTH(?) - MONTH(gl.trans_date) > 2, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS excess,
   `;
 
   const groupByRangeColumns = `
-    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) BETWEEN 0 AND 29, gl.debit_equiv - gl.credit_equiv, 0)) AS thirty,
-    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) BETWEEN 30 AND 59, gl.debit_equiv - gl.credit_equiv, 0)) AS sixty,
-    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) BETWEEN 60 AND 89, gl.debit_equiv - gl.credit_equiv, 0)) AS ninety,
-    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) > 90, gl.debit_equiv - gl.credit_equiv, 0)) AS excess,
+    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) BETWEEN 0 AND 29, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS thirty,
+    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) BETWEEN 30 AND 59, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS sixty,
+    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) BETWEEN 60 AND 89, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS ninety,
+    SUM(IF(DATEDIFF(DATE(?), DATE(gl.trans_date)) > 90, (gl.debit_equiv - gl.credit_equiv) *
+      IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1), 0)) AS excess,
   `;
 
-  const columns = useMonthGrouping ? groupByMonthColumns : groupByRangeColumns;
+  // switch between grouping by month and grouping by period
+  const columns = useMonthGrouping
+    ? groupByMonthColumns
+    : groupByRangeColumns;
 
   // selects into columns of 30, 60, 90, and >90
   const creditorSql = `
     SELECT BUID(cg.uuid) AS id, cg.name, a.number,
       ${columns}
-      SUM(gl.debit_equiv - gl.credit_equiv) AS total
+      SUM((gl.debit_equiv - gl.credit_equiv) *
+        IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1)) AS total
     FROM creditor_group AS cg
       JOIN general_ledger AS gl ON gl.account_id = cg.account_id
       JOIN account AS a ON a.id = cg.account_id
@@ -110,7 +132,8 @@ async function queryContext(params = {}) {
   const aggregateSql = `
     SELECT
       ${columns}
-      SUM(gl.debit_equiv - gl.credit_equiv) AS total
+      SUM((gl.debit_equiv - gl.credit_equiv) *
+        IFNULL(GetExchangeRate(${enterpriseId}, ${currencyId}, gl.trans_date), 1)) AS total
     FROM creditor_group AS cg
       JOIN general_ledger AS gl ON gl.account_id = cg.account_id
     WHERE DATE(gl.trans_date) <= DATE(?)
@@ -121,6 +144,7 @@ async function queryContext(params = {}) {
   const creditors = await db.exec(creditorSql, [...dates, params.fiscal_id]);
   data.creditors = creditors;
   data.dateUntil = params.date;
+  data.previousFyClosed = previousFy.locked;
 
   // this is specific to grouping by months
   data.firstMonth = params.date;
@@ -131,6 +155,7 @@ async function queryContext(params = {}) {
 
   const [aggregates] = await db.exec(aggregateSql, [...dates, params.fiscal_id]);
   data.aggregates = aggregates;
+  data.currency_id = params.currency_id;
 
   return data;
 }
