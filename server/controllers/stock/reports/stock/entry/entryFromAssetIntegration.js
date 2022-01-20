c09c660293b4bca2828a39574129dad06a7ae4fe
lomamech <lomamech@gmail.com>
2022-01-20 15:21:34 +0100

    Update report entry stock: add Asset Integration

--- server/controllers/stock/reports/stock/entry/entryFromAssetIntegration.js
+++ server/controllers/stock/reports/stock/entry/entryFromAssetIntegration.js
@@ -14,7 +14,8 @@ function fetch(depotUuid, dateFrom, dateTo, showDetails) {
     SUM(m.quantity) as quantity, m.date, m.description,
     u.display_name AS user_display_name,
     SUM(m.quantity * m.unit_cost) AS cost, m.unit_cost,
-    dm.text AS document_reference, d.text AS depot_name
+    dm.text AS document_reference, d.text AS depot_name,
+    ast.abt_inventory_no, ast.origin, ast.purchase_order, ast.vendor, ast.condition
   FROM stock_movement m
     JOIN lot l ON l.uuid = m.lot_uuid
     JOIN inventory i ON i.uuid = l.inventory_uuid
@@ -22,6 +23,7 @@ function fetch(depotUuid, dateFrom, dateTo, showDetails) {
     JOIN depot d ON d.uuid = m.depot_uuid
     JOIN user u ON u.id = m.user_id
     LEFT JOIN document_map dm ON dm.uuid = m.document_uuid
+    LEFT JOIN lot_asset AS ast ON ast.lot_uuid = l.uuid
   WHERE m.is_exit = ${IS_EXIT} AND m.flux_id = ${ENTRY_FROM_ASSET_INTEGRATION_ID} AND d.uuid = ?
     AND (DATE(m.date) BETWEEN DATE(?) AND DATE(?))
   GROUP BY i.uuid`;
