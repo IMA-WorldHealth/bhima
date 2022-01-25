6aca5b0e7831b810e9f70eed2ed8cbb221a6c242
Jonathan Niles <jonathanwniles@gmail.com>
2022-01-25 09:02:18 +0100

    feat(odk): implement ODK Central Link
    This commit implements the ODK Central link with BHIMA, developped
    during the `prosani-sprint`.  The goal is to have a functional, optional
    link to ODK Central that can be merged into the BHIMA repository.
    
    Closes #6235.

--- server/models/schema.sql
+++ server/models/schema.sql
@@ -2633,4 +2633,35 @@ CREATE TABLE `cost_center_aggregate` (
   CONSTRAINT `cost_center_aggregate__cost_center_id` FOREIGN KEY (`cost_center_id`) REFERENCES `cost_center` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
 
+DROP TABLE IF EXISTS `odk_central_integration`;
+CREATE TABLE `odk_central_integration` (
+  `enterprise_id` SMALLINT(5) UNSIGNED NOT NULL,
+  `odk_central_url` TEXT NOT NULL,
+  `odk_admin_user` TEXT NOT NULL,
+  `odk_admin_password` TEXT NOT NULL,
+  `odk_project_id` INTEGER UNSIGNED NULL,
+  KEY `enterprise_id` (`enterprise_id`),
+  CONSTRAINT `odk_central__enterprise` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprise` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+
+-- @jniles
+DROP TABLE IF EXISTS `odk_user`;
+CREATE TABLE `odk_user` (
+  `odk_user_id` INT UNSIGNED NOT NULL,
+  `odk_user_password` TEXT NOT NULL,
+  `bhima_user_id` SMALLINT(5) UNSIGNED NOT NULL,
+  CONSTRAINT `odk_user__user` FOREIGN KEY (`bhima_user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+-- @mbayopanda
+DROP TABLE IF EXISTS `odk_app_user`;
+CREATE TABLE `odk_app_user` (
+  `odk_app_user_id` INT UNSIGNED NOT NULL,
+  `odk_app_user_token` TEXT NOT NULL,
+  `display_name` TEXT NOT NULL,
+  `bhima_user_id` SMALLINT(5) UNSIGNED NOT NULL,
+  CONSTRAINT `odk_app_user__user` FOREIGN KEY (`bhima_user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
 SET foreign_key_checks = 1;
