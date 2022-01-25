beee5098e40781e69c93aa534c4b0a7281922331
Jonathan Niles <jonathanwniles@gmail.com>
2022-01-25 09:02:18 +0100

    feat(odk): implement ODK Central Link
    This commit implements the ODK Central link with BHIMA, developped
    during the `prosani-sprint`.  The goal is to have a functional, optional
    link to ODK Central that can be merged into the BHIMA repository.
    
    Closes #6235.

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,11 +1,37 @@
-/**
- * @author: lomamech
- * @description: update migrate.sql file for
- * @date: 2022-01-24
-*/
-UPDATE report SET `report_key` = 'analysis_auxiliary_cashbox', `title_key` = 'REPORT.ANALYSIS_AUX_CASHBOX.TITLE'
-  WHERE report_key = 'analysis_auxiliary_cashboxes';
+/* migration file for next release */
 
-UPDATE unit SET
-  `name` = 'Analysis of Cashbox', `key` = 'REPORT.ANALYSIS_AUX_CASHBOX.TITLE', `description` = 'Analysis of auxiliary cashbox', `path` = '/reports/analysis_auxiliary_cashbox'
-  WHERE path = '/reports/analysis_auxiliary_cashboxes';
+CALL add_column_if_missing('enterprise_setting', 'enable_odk_central_integration', 'BOOLEAN NOT NULL DEFAULT FALSE');
+
+DROP TABLE IF EXISTS `odk_central_integration`;
+CREATE TABLE `odk_central_integration` (
+  `enterprise_id` SMALLINT(5) UNSIGNED NOT NULL,
+  `odk_central_url` TEXT NOT NULL,
+  `odk_admin_user` TEXT NOT NULL,
+  `odk_admin_password` TEXT NOT NULL,
+  `odk_project_id` INTEGER UNSIGNED NULL,
+  KEY `enterprise_id` (`enterprise_id`),
+  CONSTRAINT `odk_central__enterprise` FOREIGN KEY (`enterprise_id`) REFERENCES `enterprise` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+
+-- @jniles
+DROP TABLE IF EXISTS `odk_user`;
+CREATE TABLE `odk_user` (
+  `odk_user_id` INT UNSIGNED NOT NULL,
+  `odk_user_password` TEXT NOT NULL,
+  `bhima_user_id` SMALLINT(5) UNSIGNED NOT NULL,
+  CONSTRAINT `odk_user__user` FOREIGN KEY (`bhima_user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+-- @mbayopanda
+DROP TABLE IF EXISTS `odk_app_user`;
+CREATE TABLE `odk_app_user` (
+  `odk_app_user_id` INT UNSIGNED NOT NULL,
+  `odk_app_user_token` TEXT NOT NULL,
+  `display_name` TEXT NOT NULL,
+  `bhima_user_id` SMALLINT(5) UNSIGNED NOT NULL,
+  CONSTRAINT `odk_app_user__user` FOREIGN KEY (`bhima_user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+INSERT INTO unit VALUES
+  (306, '[SETTINGS] ODK Settings', 'TREE.ODK_SETTINGS', 'ODK Settings', 1, '/admin/odk-settings');
