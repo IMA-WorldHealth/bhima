9825455a83cabb44a184c5c253a56095c47dcb7a
mbayopanda <mbayopanda@gmail.com>
2022-02-07 13:53:38 +0100

    shipment database structure

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -94,3 +94,85 @@ INSERT IGNORE INTO `entity_type` (`label`, `translation_key`) VALUES
   ('service', 'ENTITY.TYPE.SERVICE'),
   ('office', 'ENTITY.TYPE.OFFICE'),
   ('enterprise', 'ENTITY.TYPE.ENTERPRISE');
+
+/*
+ * @author: mbayopanda
+ * @description: Shipment tables
+ * @date: 2022-02-07
+ */
+DROP TABLE IF EXISTS `shipper`;
+CREATE TABLE `shipper` {
+  `id` SMALLINT(5) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+} ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `shipment_status`;
+CREATE TABLE `shipment_status` {
+  `id` SMALLINT(5) NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(100) NOT NULL,
+  `translation_key` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`)
+} ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `shipment`;
+CREATE TABLE `shipment` (
+  `uuid` BINARY(16) NOT NULL,
+  `reference` INT(11) UNSIGNED NOT NULL,
+  `name` VARCHAR(100) NOT NULL,
+  `description` TEXT NULL,
+  `note` TEXT NULL,
+  `shipper_id` SMALLINT(5) NULL,
+  `requisition_uuid` BINARY(16),
+  `origin_depot_uuid` BINARY(16),
+  `current_depot_uuid` BINARY(16),
+  `destination_depot_uuid` BINARY(16),
+  `anticipated_delivery_date` DATE,
+  `date_sent` DATE,
+  `date_delivered` DATE,
+  `receiver` VARCHAR(100),
+  `status_id` SMALLINT(5),
+  `created_by` SMALLINT(5) UNSIGNED NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_by` SMALLINT(5) UNSIGNED NULL,
+  `updated_at` TIMESTAMP NULL,
+  PRIMARY KEY (`uuid`),
+  CONSTRAINT `shipment__shipper` FOREIGN KEY (`shipper_id`) REFERENCES `shipper` (`id`),
+  CONSTRAINT `shipment__requisition` FOREIGN KEY (`requisition_uuid`) REFERENCES `requisition` (`uuid`),
+  CONSTRAINT `shipment__origin_depot` FOREIGN KEY (`origin_depot_uuid`) REFERENCES `depot` (`uuid`),
+  CONSTRAINT `shipment__current_depot` FOREIGN KEY (`current_depot_uuid`) REFERENCES `depot` (`uuid`),
+  CONSTRAINT `shipment__destination_depot` FOREIGN KEY (`destination_depot_uuid`) REFERENCES `depot` (`uuid`),
+  CONSTRAINT `shipment__status` FOREIGN KEY (`status_id`) REFERENCES `shipment_status` (`id`),
+  CONSTRAINT `shipment__created_by` FOREIGN KEY (`created_by`) REFERENCES `user` (`id`),
+  CONSTRAINT `shipment__updated_by` FOREIGN KEY (`updated_by`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+DROP TABLE IF EXISTS `shipment_item`;
+CREATE TABLE `shipment_item` (
+  `uuid` BINARY(16) NOT NULL,
+  `shipment_uuid` BINARY(16) NOT NULL,
+  `lot_uuid` BINARY(16) NOT NULL,
+  `date_packed` DATE,
+  `date_sent` DATE,
+  `date_delivered` DATE,
+  `quantity_sent` INT(11),
+  `quantity_delivered` INT(11),
+  `note` TEXT NULL,
+  PRIMARY KEY (`uuid`),
+  CONSTRAINT `shipment_item__shipment` FOREIGN KEY (`shipment_uuid`) REFERENCES `shipment` (`uuid`),
+  CONSTRAINT `shipment_item__lot` FOREIGN KEY (`lot_uuid`) REFERENCES `lot` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARACTER SET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
+/** ADD DEFAULT SHIPMENT STATUS */
+INSERT INTO `shipment_status` (`id`, `name`, `translation_key`) VALUES 
+  (1, 'empty', 'ASSET.STATUS.EMPTY'),
+  (2, 'partial', 'ASSET.STATUS.PARTIAL'),
+  (3, 'complete', 'ASSET.STATUS.COMPLETE'),
+  (4, 'in_transit', 'ASSET.STATUS.IN_TRANSIT'),
+  (5, 'at_depot', 'ASSET.STATUS.AT_DEPOT'),
+  (6, 'delivered', 'ASSET.STATUS.DELIVERED'),
+  (7, 'lost', 'ASSET.STATUS.LOST');
+
+/** ADD DEFAULT SHIPPER */
+INSERT INTO `shipper` (`id`, `name`) VALUES 
+  (1, 'Transit');
