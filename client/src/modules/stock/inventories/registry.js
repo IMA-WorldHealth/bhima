d12a7ea924f529ff8240945fcc4d9f239c1bc365
mbayopanda <mbayopanda@gmail.com>
2022-02-09 23:03:10 +0100

    shipment entry and status handling

--- client/src/modules/stock/inventories/registry.js
+++ client/src/modules/stock/inventories/registry.js
@@ -191,6 +191,11 @@ function StockInventoriesController(
   vm.openColumnConfigModal = openColumnConfigModal;
   vm.clearGridState = clearGridState;
 
+  // shipment in transit
+  vm.isInTransitOrPartial = status => {
+    return bhConstants.shipmentStatus.IN_TRANSIT === status || bhConstants.shipmentStatus.PARTIAL === status;
+  };
+
   // select group
   vm.selectGroup = (group) => {
     if (!group) { return; }
