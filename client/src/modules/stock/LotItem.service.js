46a91d32ad9d7a62b7f2eec502a2ed6e68ecc1bf
Jonathan Niles <jonathanwniles@gmail.com>
2022-02-09 14:10:28 +0100

    chore(StockExit): improve form validation
    This commit encorporates common error cases into the LotItemService to
    be used by the StockExitForm.

--- client/src/modules/stock/LotItem.service.js
+++ client/src/modules/stock/LotItem.service.js
@@ -1,12 +1,25 @@
 angular.module('bhima.services')
   .service('LotItemService', LotItemService);
 
-LotItemService.$inject = ['uuid'];
+LotItemService.$inject = ['uuid', '$translate'];
 
 /**
  * @class Lot
  */
-function LotItemService(uuid) {
+function LotItemService(uuid, $translate) {
+
+  // error codes that can apply to a lot
+  const ER_EXPIRED = 'STOCK.ERRORS.LOT_EXPIRED';
+  const ER_MISSING_INVENTORY = 'STOCK.ERRORS.LOT_MISSING_INVENTORY_INFO';
+  const ER_MISSING_LOT = 'STOCK.ERRORS.LOT_MISSING_LOT_INFO';
+  const ER_NEGATIVE_QUANTITY = 'STOCK.ERRORS.LOT_NEGATIVE_QUANTITY';
+  const ER_OVERCONSUMPTION = 'STOCK.ERRORS.LOT_OVERCONSUMPTION';
+  const ER_UNUSED = 'STOCK.ERRORS.LOT_UNUSED';
+
+  // toggles the error message based on the condition
+  function toggleErrorMessage(condition, set, msg) {
+    if (condition) { set.add(msg); } else { set.delete(msg); }
+  }
 
   /**
    * @constructor Lot
@@ -19,6 +32,7 @@ function LotItemService(uuid) {
     this.code = null;
     this.text = null;
     this.group_uuid = null;
+    this.unit = null;
 
     // lot properties
     this.lot_uuid = null;
@@ -32,28 +46,49 @@ function LotItemService(uuid) {
     this.__tracking_expiration = true;
     this.__tracking_consumption = true;
 
+    // stock-exit specific properties.  These will not be used
+    // in the general case of lots loaded from the database, but
+    // is useful for the stock store.
+    this._quantity_available = 0;
     this._initialised = true;
+    this._errors = new Set(); // ensures only a single instance of an error exists at any time.
 
-    // if options were passed in, use them to configure the
-    //
+    // if options were passed in, use them to configure the lot
     if (options) {
       this.configure(options);
     }
   }
 
   /**
+   * @method errors
+   *
+   * @description
+   * Formats the errors with a nice i18n formating
+   */
+  Lot.prototype.errors = function errors() {
+    return Array.from(this._errors)
+      .map(i18nKey => $translate.instant(i18nKey, this));
+  };
+
+  /**
    * @function validate
    *
    * @description
    * This function ensures that is lot is valid.  A valid lot is:
-   *   - one that has a positive quantity in stock
-   *   - one that is not expired
    *   - has all required information
+   *   - is not "unused" (has stock available, but quantity = 0)
+   *   - is not expired.
+   *
+   * Note that this takes a comparison date to pass to isExpired().
+   * The comparison date will only trigger expiry if the expiration
+   * date is before the comparion date.  Thus, if you pass it a
+   * date for yesterday and the expiration is today, it will not
+   * register as expired.
    */
   Lot.prototype.validate = function validate(date) {
     return this.hasInventoryInformation()
       && this.hasLotInformation()
-      && !this.isEmpty()
+      && !this.isUnused()
       && !this.isExpired(date);
   };
 
@@ -71,9 +106,14 @@ function LotItemService(uuid) {
    * Checks if the inventory information is available.
    */
   Lot.prototype.hasInventoryInformation = function hasInventoryInformation() {
-    return isUuid(this.inventory_uuid)
+    const hasInfo = isUuid(this.inventory_uuid)
       && typeof this.code === 'string'
+      && typeof this.unit === 'string'
       && typeof this.text === 'string';
+
+    toggleErrorMessage(!hasInfo, this._errors, ER_MISSING_INVENTORY);
+
+    return hasInfo;
   };
 
   /**
@@ -83,26 +123,59 @@ function LotItemService(uuid) {
    * Checks if the lot information is availability.
    */
   Lot.prototype.hasLotInformation = function hasLotInformation() {
-    return isUuid(this.lot_uuid)
+    const hasInfo = isUuid(this.lot_uuid)
       && typeof this.label === 'string'
       && typeof this.quantity === 'number';
+
+    toggleErrorMessage(!hasInfo, this._errors, ER_MISSING_LOT);
+
+    return hasInfo;
   };
 
-  // we need to differentiate between server-sent expiration date
-  // and this calculated one.
-  Lot.prototype.isExpired = function isExpired(date = new Date()) {
+  /**
+   * @method isExpired
+   *
+   * @description
+   * This function labels the lot as expired or not.  The logic is thus:
+   *
+   * 1) If the expiration date of the inventory should not be tracked (e.g. an asset)
+   *    we always return FALSE: the lot is not expired.  Note this condition applies
+   *    even if the lot has an expiration date and even if that expiration date is past.
+   * 2) If is_expired is set, we return that value rather than performing a
+   *    calculation.  This allows the server to have the same logic as the client and
+   *    override the logic here without overriding the error handling.
+   * 3) If a date comparison is passed in, the lot's expiration state is computed by
+   *    comparing to the comparison date.  If the lot's expiration_date  is before the
+   *    comparison date, it is considered expired.  If the lot's expiration_date is after,
+   *    it is considered valid.
+   * 4) If no comparison date is provided, the same comparison is performed with today's
+   *    date (the date of the client computer).  Thus, if the expiration date is before
+   *    the current date, the lot is considered expired.  If the expiration date is after
+   *    the current date, it is considered not expired.
+   */
+  Lot.prototype.isExpired = function isExpired(comparisonDate = new Date()) {
+
+    let isLotExpired;
 
     // if we are not tracking expiration, always return false.
-    if (this.__tracking_expiration === false) { return false; }
+    if (this.__tracking_expiration === false) {
+      isLotExpired = false;
 
     // the `is_expired` flag is set on the server.  If this
     // flag is set, return the results of this flag.
-    if (this.is_expired !== undefined) { return !!this.is_expired; }
+    } else if (this.is_expired !== undefined) {
+      isLotExpired = !!this.is_expired;
 
     // if a date has been passed in, it will validate against that date
     // (for example, if this is a past date).  If not, it will validate
     // against the current date.
-    return this.expiration_date < date;
+    } else {
+      isLotExpired = this.expiration_date < comparisonDate;
+    }
+
+    toggleErrorMessage(isLotExpired, this._errors, ER_EXPIRED);
+
+    return isLotExpired;
   };
 
   /**
@@ -112,7 +185,22 @@ function LotItemService(uuid) {
    * Returns true if the quantity is 0.
    */
   Lot.prototype.isEmpty = function isEmpty() {
-    return this.quantity === 0;
+    const hasEmptyQuantity = this.quantity === 0;
+    return hasEmptyQuantity;
+  };
+
+  /**
+   * @function isUnused
+   *
+   * @description
+   * Returns true if the lot has quantity available and is unused. This is to catch
+   * error cases where a user did not complete the form correctly.  However, if a lot
+   * is empty by default (no quantity in stock), no error is triggered.
+   */
+  Lot.prototype.isUnused = function isUnused() {
+    const hasEmptyQuantityAndUnused = this.isEmpty() && this._quantity_available !== 0;
+    toggleErrorMessage(hasEmptyQuantityAndUnused, this._errors, ER_UNUSED);
+    return hasEmptyQuantityAndUnused;
   };
 
   /**
@@ -164,11 +252,38 @@ function LotItemService(uuid) {
     // assign properties to current lot
     Object.assign(this, clone);
 
+    // set the total quantity available
+    this._quantity_available = clone.quantity;
+
     // parse the date if it is not already a date
     if (!(this.expiration_date instanceof Date) && this.expiration_date !== undefined) {
       this.expiration_date = new Date(this.expiration_date);
     }
+  };
 
+  /**
+   * @function hasEnoughQuantityAvailable
+   *
+   * @description
+   * Returns true if the available quantity in the lot is enough to satisfy the
+   * quantity required.
+   */
+  Lot.prototype.hasEnoughQuantityAvailable = function hasEnoughQuantityAvailable() {
+    const hasEnoughQuantity = this.quantity <= this._quantity_available;
+    toggleErrorMessage(!hasEnoughQuantity, this._errors, ER_OVERCONSUMPTION);
+    return hasEnoughQuantity;
+  };
+
+  /**
+   * @function hasPositiveQuantity
+   *
+   * @description returns true if the both quantities used and available are
+   * true.
+   */
+  Lot.prototype.hasPositiveQuantity = function hasPositiveQuantity() {
+    const isPositive = this.quantity > 0;
+    toggleErrorMessage(!isPositive, this._errors, ER_NEGATIVE_QUANTITY);
+    return isPositive;
   };
 
   /**
