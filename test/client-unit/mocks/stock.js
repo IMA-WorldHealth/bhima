9c9baf60ccd1b3758a112188d7d1df9f294d1f16
Jonathan Cameron <jmcameron@gmail.com>
2022-02-16 08:19:07 -0800

    Fix client unit test

--- test/client-unit/mocks/stock.js
+++ test/client-unit/mocks/stock.js
@@ -32,7 +32,7 @@
         expiration_date : '2019-01-01',
         inventory_uuid : 'd03e7870-0c8e-47d4-a7a8-a17a9924b3f4',
         description : '',
-        serial_number: undefined,
+        serial_number : undefined,
       },
       {
         uuid : 'd03e7870-0c8e-47d4-a7a8-a17a9924b124',
@@ -42,7 +42,7 @@
         expiration_date : '2018-12-01',
         inventory_uuid : 'd03e7870-0c8e-47d4-a7a8-a17a9924b3f4',
         description : '',
-        serial_number: undefined,
+        serial_number : undefined,
       },
     ]);
   }
