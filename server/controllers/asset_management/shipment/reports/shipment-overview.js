d7ddde5af551ae58ebb19433bba76cb012f52db1
mbayopanda <mbayopanda@gmail.com>
2022-02-18 12:16:58 +0100

    Handle shipment overview

--- server/controllers/asset_management/shipment/reports/shipment-overview.js
+++ server/controllers/asset_management/shipment/reports/shipment-overview.js
@@ -32,4 +32,42 @@ async function getShipmentOverview(req, res, next) {
   }
 }
 
+/**
+ * @function getStep
+ * @param {string} statusName
+ * @desc returns the step according the shipment status
+ * AT_DEPOT => Step 1
+ * READY_TO_SHIP => Step 2
+ * IN_TRANSIT => Step 3
+ * PARTIAL => Step 4
+ * DELIVERED => Step 5
+ */
+function getStep(statusName) {
+  const definedSteps = {
+    AT_DEPOT : 1,
+    READY_TO_SHIP : 2,
+    IN_TRANSIT : 3,
+    PARTIAL : 4,
+    DELIVERED : 5,
+  };
+  const map = {
+    empty : 1,
+    at_depot : 1,
+    ready : 2,
+    in_transit : 3,
+    partial : 4,
+    complete : 5,
+    delivered : 5,
+  };
+  const current = map[statusName];
+  const steps = {
+    at_depot : current >= definedSteps.AT_DEPOT,
+    ready : current >= definedSteps.READY_TO_SHIP,
+    in_transit : current >= definedSteps.IN_TRANSIT,
+    partial : current >= definedSteps.PARTIAL,
+    delivered : current >= definedSteps.DELIVERED,
+  };
+  return steps;
+}
+
 exports.getShipmentOverview = getShipmentOverview;
