49296033698a77e7c6d2c24bdd9c8c86d2a411cc
mbayopanda <mbayopanda@gmail.com>
2022-02-18 12:16:58 +0100

    Handle shipment overview

--- server/controllers/asset_management/shipment/index.js
+++ server/controllers/asset_management/shipment/index.js
@@ -1,5 +1,6 @@
 const shipment = require('./shipment');
 const shipmentReports = require('./reports/shipments');
+const shipmentOverviewReceipt = require('./reports/shipment-overview');
 
 module.exports = {
   list : shipment.list,
@@ -7,10 +8,12 @@ module.exports = {
   single : shipment.single,
   update : shipment.update,
   create : shipment.create,
+  deleteShipment : shipment.deleteShipment,
   updateLocation : shipment.updateLocation,
   setReadyForShipment : shipment.setReadyForShipment,
   listInTransitInventories : shipment.listInTransitInventories,
   getReport : shipmentReports.getReport,
+  getOverview : shipmentOverviewReceipt.getShipmentOverview,
   writeStockExitShipment : shipment.writeStockExitShipment,
   writeStockEntryShipment : shipment.writeStockEntryShipment,
   updateShipmentStatusAfterEntry : shipment.updateShipmentStatusAfterEntry,
