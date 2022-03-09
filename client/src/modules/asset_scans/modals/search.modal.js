14207187c2cb7bb8f9ae82f03c72644f3de55869
Jonathan Cameron <jmcameron@gmail.com>
2022-03-09 08:53:52 +0100

    Create assets inventory management

--- client/src/modules/asset_scans/modals/search.modal.js
+++ client/src/modules/asset_scans/modals/search.modal.js
@@ -1,13 +1,13 @@
 angular.module('bhima.controllers')
-  .controller('AssetsSearchModalController', AssetsSearchModalController);
+  .controller('AssetScansSearchModalController', AssetScansSearchModalController);
 
 // dependencies injections
-AssetsSearchModalController.$inject = [
+AssetScansSearchModalController.$inject = [
   'data', 'util', 'Store', '$uibModalInstance', 'StockService',
   'SearchModalUtilService',
 ];
 
-function AssetsSearchModalController(data, util, Store, Instance, Stock, SearchModal) {
+function AssetScansSearchModalController(data, util, Store, Instance, Stock, SearchModal) {
   const vm = this;
   const changes = new Store({ identifier : 'key' });
 
@@ -16,8 +16,7 @@ function AssetsSearchModalController(data, util, Store, Instance, Stock, SearchM
   vm.defaultQueries = {};
 
   const searchQueryOptions = [
-    'depot_uuid', 'inventory_uuid', 'group_uuid', 'label',
-    'is_assigned', 'entry_date_from', 'entry_date_to', 'tags',
+    'depot_uuid', 'inventory_uuid', 'group_uuid', 'asset_label',
   ];
 
   // displayValues will be an id:displayValue pair
@@ -26,12 +25,6 @@ function AssetsSearchModalController(data, util, Store, Instance, Stock, SearchM
 
   // assign already defined custom filters to searchQueries object
   vm.searchQueries = util.maskObjectFromKeys(data, searchQueryOptions);
-
-  // Adjust the assignment filters, if present, so caching works
-  if ('is_assigned' in vm.searchQueries) {
-    // Switch back to numerical values for the radio html tags
-    vm.searchQueries.is_assigned = vm.searchQueries.is_assigned === 'ASSET.SHOW_ONLY_ASSIGNED' ? 1 : 2;
-  }
 
   // custom filter depot_uuid - assign the value to the params object
   vm.onSelectDepot = function onSelectDepot(depot) {
@@ -49,25 +42,6 @@ function AssetsSearchModalController(data, util, Store, Instance, Stock, SearchM
   vm.onSelectGroup = (group) => {
     vm.searchQueries.group_uuid = group.uuid;
     displayValues.group_uuid = group.name;
-  };
-
-  // Save the entry dates
-  vm.onEntryDate = (dateFrom, dateTo) => {
-    vm.searchQueries.entry_date_from = dateFrom;
-    displayValues.entry_date_from = dateFrom;
-    vm.searchQueries.entry_date_to = dateTo;
-    displayValues.entry_date_to = dateTo;
-  };
-
-  // toggle is_assigned search flag
-  vm.onToggleAssigned = function onToggleAssigned(val) {
-    vm.searchQueries.is_assigned = val;
-  };
-
-  // Save the Tags
-  vm.onSelectTags = tags => {
-    vm.searchQueries.tags = tags;
-    displayValues.tags = tags.map(t => t.name).join(',');
   };
 
   // default filter limit - directly write to changes list
@@ -96,26 +70,7 @@ function AssetsSearchModalController(data, util, Store, Instance, Stock, SearchM
 
   vm.submit = () => {
 
-    // Adjust the is_assigned search flag for a nice active filter display
-    if (vm.searchQueries.is_assigned) {
-      vm.searchQueries.is_assigned = vm.searchQueries.is_assigned === 1
-        ? 'ASSET.SHOW_ONLY_ASSIGNED' : 'ASSET.SHOW_NOT_ASSIGNED';
-    }
-
     const loggedChanges = SearchModal.getChanges(vm.searchQueries, changes, displayValues, lastDisplayValues);
-
-    // The following work-around is necessary to deal with the case where you
-    // select a tag but then delete it before clicking [Submit]; the element
-    // has an empty array so it needs to be deleted. There probably is a
-    // cleaner way to do this.
-    loggedChanges.some((elt, i) => {
-      if (elt.key === 'tags') {
-        if (elt.value.length === 0) {
-          delete loggedChanges[i];
-        }
-      }
-      return elt.key === 'tags';
-    });
 
     return Instance.close(loggedChanges);
   };
