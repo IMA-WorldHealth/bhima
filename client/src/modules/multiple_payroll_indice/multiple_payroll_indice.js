d695d021d813971a5133cf8ef3de235e40d7ae37
Jonathan Niles <jonathanwniles@gmail.com>
2022-05-31 19:56:18 +0100

    fix(payroll); improve multipayroll grid columns
    This commit adds the ability to filter on any of the rubrics in the
    multiple payroll by index grid.  It also removes the row selection
    checkbox.
    
    Closes #6387.

--- client/src/modules/multiple_payroll_indice/multiple_payroll_indice.js
+++ client/src/modules/multiple_payroll_indice/multiple_payroll_indice.js
@@ -99,27 +99,25 @@ function MultiplePayrollIndiceController(
 
     MultiplePayroll.read(null, filters)
       .then((result) => {
-        renameGidHeaders(result.rubrics);
+        renameGridHeaders(result.rubrics);
         vm.gridOptions.data = setGridData(result.employees);
       })
       .catch(errorHandler)
       .finally(toggleLoadingIndicator);
   }
 
-  function renameGidHeaders(rubrics) {
+  function renameGridHeaders(rubrics) {
     const actions = angular.copy(columnDefs[columnDefs.length - 1]);
     const newColumns = columnDefs.slice(0, 1);
 
     const header = {
       type : 'number',
-      enableFiltering  : false,
       headerCellFilter : 'translate',
       cellClass  : 'text-right',
       footerCellClass  : 'text-right',
       footerCellFilter : 'number:2',
       cellFilter : 'number:2',
       aggregationType : uiGridConstants.aggregationTypes.sum,
-      // width : 100,
       aggregationHideLabel : true,
     };
 
@@ -131,6 +129,9 @@ function MultiplePayrollIndiceController(
     });
 
     vm.gridOptions.columnDefs = [...newColumns, actions];
+
+    // notify the grid of a data change
+    vm.gridApi.core.notifyDataChange(uiGridConstants.dataChange.COLUMN);
   }
 
   function setGridData(employees) {
