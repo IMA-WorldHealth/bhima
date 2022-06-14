eaab4b327d1f67d34250a5c8d80e2596bfce6207
= <jeremielodi@gmail.com>
2022-06-14 05:47:04 +0100

    Feat(remote access) using bearer token to handle the remote access

---
+++ server/config/access.js
@@ -0,0 +1,56 @@
+const _ = require('lodash');
+const debug = require('debug')('app');
+const JWTConfig = require('./jwt');
+const { Unauthorized } = require('../lib/errors');
+
+const publicRoutes = [
+  '/auth/login',
+  '/helpdesk_info',
+  '/languages',
+  '/projects',
+  '/projects/',
+  '/auth/logout',
+  '/install',
+  '/currencies',
+];
+
+module.exports = (app) => {
+  // eslint-disable-next-line consistent-return
+  app.use((req, res, next) => {
+    const token = req.headers['x-access-token'];
+    let sessionExists = true;
+
+    if (token) {
+      JWTConfig.verify(req, (decoded) => {
+        if (!decoded) {
+          sessionExists = false;
+          return;
+        }
+        req.session.enterprise = decoded.enterprise;
+        req.session.project = decoded.project;
+        req.session.user = decoded.user;
+      }, (error) => {
+        sessionExists = false;
+        debug(error);
+      });
+    }
+
+    if (sessionExists) {
+      if (_.isUndefined(req.session.user) && !within(req.path, publicRoutes)) {
+        debug(`Rejecting unauthorized access to ${req.path} from ${req.ip}`);
+        next(new Unauthorized('You are not logged into the system.'));
+      } else {
+        next();
+      }
+    } else {
+      debug('token not valide');
+      res.status(401).json({
+        msg : 'token not valid'
+      });
+    }
+  });
+
+};
+
+// quick way to find out if a value is in an array
+function within(value, array) { return array.includes(value.trim()); }
