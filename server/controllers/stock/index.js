e2283b94d4dd23614c4b0a060cf473e37b2c2595
mbayopanda <mbayopanda@gmail.com>
2022-07-29 14:28:39 +0100

    handle mobile post

--- server/controllers/stock/index.js
+++ server/controllers/stock/index.js
@@ -413,6 +413,59 @@ async function createInventoryAdjustment(req, res, next) {
   }
 }
 
+async function movementsFromMobile(mobileLots) {
+  const [mobile] = mobileLots;
+  // find the initial stock exit movement
+  const STOCK_EXIT_TO_DEPOT = 8;
+  const findMovements = `
+    SELECT 
+      BUID(m.document_uuid) document_uuid,
+      BUID(m.depot_uuid) depot_uuid,
+      BUID(l.inventory_uuid) inventory_uuid,
+      BUID(m.lot_uuid) lot_uuid,
+      m.quantity
+    FROM stock_movement m
+    JOIN lot l ON l.uuid = m.lot_uuid
+    WHERE m.entity_uuid = ? AND m.reference = ? AND m.is_exit = 1 AND m.flux_id = ?;
+  `;
+  const movements = await db.exec(findMovements, [
+    db.bid(mobile.depotUuid), mobile.reference, STOCK_EXIT_TO_DEPOT,
+  ]);
+
+  const pickLot = lotUuid => movements.filter(item => item.lot_uuid === lotUuid)[0];
+
+  // filter known lots of the movement initial movement
+  // check that the quantity sent is less or equal to
+  // the inital quantity
+  console.log('Mobile lots: ', mobileLots);
+  console.log('Bhima lots: ', movements);
+  const validLots = mobileLots.filter(lot => {
+    const bhimaLot = pickLot(lot.lotUuid);
+    return bhimaLot && bhimaLot.quantity >= lot.quantity;
+  }).map(item => {
+    const bhimaLot = pickLot(item.lotUuid);
+    return {
+      uuid : item.lotUuid,
+      inventory_uuid : bhimaLot.inventory_uuid,
+      description : item.description,
+      quantity : item.quantity,
+      unit_cost : item.unitCost,
+    };
+  });
+
+  const [bhima] = movements;
+  return validLots.length ? {
+    document_uuid : bhima.document_uuid,
+    flux_id : mobile.fluxId,
+    is_exit : mobile.isExit,
+    depot_uuid : mobile.depotUuid,
+    date : mobile.date,
+    from_depot : bhima.depot_uuid,
+    to_depot : mobile.depotUuid,
+    lots : validLots,
+  } : {};
+}
+
 /**
  * @function createMovement
  *
@@ -422,7 +475,17 @@ async function createInventoryAdjustment(req, res, next) {
  * POST /stock/lots/movement
  */
 async function createMovement(req, res, next) {
-  const params = req.body;
+  let params = req.body;
+  const isMobileSync = params.sync_mobile;
+
+  if (isMobileSync === 1) {
+    params = await movementsFromMobile(params.lots);
+    console.log('Params: ', params);
+  }
+
+  if (!params.lots || !params.lots.length) {
+    return next(new BadRequest('The stock movement is not valid'));
+  }
 
   const document = {
     uuid : params.document_uuid || uuid(),
@@ -482,9 +545,14 @@ async function createMovement(req, res, next) {
     const stockMovementFn = isDepotMovement ? depotMovement : normalMovement;
     await stockMovementFn(document, params, metadata);
 
-    res.status(201).json({ uuid : document.uuid });
+    if (isMobileSync === 1) {
+      const uuids = params.lots.map(l => l.uuid).join(',');
+      return res.status(201).json({ uuid : document.uuid, uuids });
+    }
+
+    return res.status(201).json({ uuid : document.uuid });
   } catch (err) {
-    next(err);
+    return next(err);
   }
 }
 
