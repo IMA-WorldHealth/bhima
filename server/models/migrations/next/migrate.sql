2ccc65837bf14de024c180faf88c69522b809b8e
lomamech <lomamech@gmail.com>
2022-09-20 13:11:04 +0100

    feature(Manage Drug Packing) Pharmaceutical packaging configuration management - Setup - Purchase order - Shipping management - Stock entries and exits
    closes #6811

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -18,3 +18,13 @@ CALL add_column_if_missing('supplier', 'contact_uuid', 'BINARY(16) NULL');
 
 CALL add_column_if_missing('enterprise_setting', 'enable_prf_details', 'TINYINT(1) NOT NULL DEFAULT 0');
 CALL add_column_if_missing('enterprise_setting', 'purchase_general_condition', 'TEXT NULL');
+
+/*
+ * @author: lomamech
+ * @date: 2022-09-07
+ * @description: Problem with drug packaging
+*/
+CALL add_column_if_missing('stock_setting', 'enable_packaging_pharmaceutical_products', 'TINYINT(1) NOT NULL DEFAULT 0');
+CALL add_column_if_missing('inventory', 'is_count_per_container', 'TINYINT(1) NOT NULL DEFAULT 0');
+CALL add_column_if_missing('depot', 'is_count_per_container', 'TINYINT(1) NOT NULL DEFAULT 0');
+CALL add_column_if_missing('lot', 'package_size', 'INT(11) DEFAULT 1');
