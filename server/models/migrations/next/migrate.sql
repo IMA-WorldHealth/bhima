6b1dd5b355bd483bcd99ab9d79ccfa88015fb668
lomamech <lomamech@gmail.com>
2022-09-20 13:11:04 +0100

    feature(Manage Drug Packing) Pharmaceutical packaging configuration management - Setup - Purchase order - Shipping management - Stock entries and exits
    closes #6811

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,3 +1,13 @@
 /*
  * next version
  */
+
+/*
+ * @author: lomamech
+ * @date: 2022-09-07
+ * @description: Problem with drug packaging
+*/
+CALL add_column_if_missing('stock_setting', 'enable_packaging_pharmaceutical_products', 'TINYINT(1) NOT NULL DEFAULT 0');
+CALL add_column_if_missing('inventory', 'is_count_per_container', 'TINYINT(1) NOT NULL DEFAULT 0');
+CALL add_column_if_missing('depot', 'is_count_per_container', 'TINYINT(1) NOT NULL DEFAULT 0');
+CALL add_column_if_missing('lot', 'package_size', 'INT(11) DEFAULT 1');
