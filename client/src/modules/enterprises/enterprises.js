e0e6085a595e97298e0eca1eec770a15f3aa215b
mbayopanda <mbayopanda@gmail.com>
2022-10-11 07:00:49 +0100

    fix hound issue

--- client/src/modules/enterprises/enterprises.js
+++ client/src/modules/enterprises/enterprises.js
@@ -81,7 +81,6 @@ function EnterpriseController(Enterprises, util, Notify, Projects, Modal, Scroll
          * this choice need the team point of view for to setting the default enterprise
          */
         vm.enterprise = vm.hasEnterprise ? vm.enterprises[0] : {};
-        console.log(vm.enterprise);
         return refreshProjects();
       })
       .catch(Notify.handleError);
