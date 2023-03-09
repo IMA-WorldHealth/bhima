0c12115a5b8991ef8d6e3e55e39512b2a105d51f
Jonathan Cameron <jmcameron@gmail.com>
2023-03-09 13:46:20 -0800

    Updated

--- test/end-to-end-playwright/login/login.spec.js
+++ test/end-to-end-playwright/login/login.spec.js
@@ -1,48 +1,45 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { test, expect } = require('@playwright/test');
 
-test.beforeAll(async ({ browser }) => {
+// console.debug('B: ', Object.getOwnPropertyNames(page));
+// console.table(Object.getOwnPropertyNames(Object.getPrototypeOf(page)).sort());
 
-  // Login once for all following tests
-  const page = await browser.newPage();
+test.beforeEach(async ({ page }) => {
+  console.log(`Logging into the BHIMA server`);
+
   await page.goto('http://localhost:8080/#!/login');
   expect(page).toHaveTitle(/BHIMA/);
 
-  await page.getByPlaceholder('Enter your username').fill('superuser');
-  await page.getByPlaceholder('Enter your password').fill('superuser');
+  // First, switch to English
+  expect((await page.innerText('li[role=menuitem]:last-child > a')).trim()).toBe('English');
+  await page.click('div.panel-heading > div.dropdown > a'); // Expose the language drop-down menu
+  await page.click('li[role=menuitem]:last-child > a'); // Click on the English option
+  expect(await page.innerText('.panel-heading')).toBe('Login');
 
-  // await page.locator('[ng-model="LoginCtrl.credentials.username"]').sendKeys('superuser');
-  // await page.locator('[ng-model="LoginCtrl.credentials.password"]').sendKeys('superuser');
-
-  // await browser.get('http://localhost:8080/#!/login');
-
-  // // Turns off ng-animate animations for all elements in the
-  // await element(by.css('body')).allowAnimations(false);
-
-  // element(by.model('...')) ==>	page.locator('[ng-model="..."]')
-
-  // await element(by.model('LoginCtrl.credentials.username')).sendKeys('superuser');
-  // await element(by.model('LoginCtrl.credentials.password')).sendKeys('superuser');
-  // await element(by.css('[data-method="submit"]')).click();
-
-  // const uname = await page.getByLabel('username');
-  // console.log("Uname: ", uname);
-  // await page.getByLabel('username').fill('superuser');
-  // await page.getByRole('input', { name : 'username' }).fill('superuser');
-  // await page.getByPlaceholder('Enter your username').fill('superuser');
-
-  // await page.getByLabel('password').fill('superuser');
-  // await page.getByText('Log in').click();
-
-  // await page.getByRole('button', { name: 'Sign in' }).click();
+  // Log in
+  await page.fill('input[name=username]', 'superuser');
+  await page.fill('input[name=password]', 'superuser');
+  await page.click('button[type=submit]');
+  await page.waitForURL('http://localhost:8080/#!/');
 });
 
-test('verify we are logged in', async ({ page }) => {
-  // console.log("-> page: ", Object.getOwnPropertyNames(page));
-  // console.debug('FNS: ', Object.getOwnPropertyNames(Object.getPrototypeOf(<OBJ>)));
-  await page.goto('http://localhost:8080');
-  await expect(page).toHaveTitle(/BHIMA/);
+test.afterEach(async ({ page }) => {
+  // Log out
+  console.log(`Logging out of the BHIMA server`);
 
-  // Check the project name to make sure we are logged in
-  // await expect(page.getByTestId('header-title')).toHaveText('TEST PROJECT A');
+  await page.goto('http://localhost:8080/#!/settings');
+  await page.waitForURL('http://localhost:8080/#!/settings');
+
+  await page.click('button[ng-click="SettingsCtrl.logout()"]');
+  await page.waitForURL('http://localhost:8080/#!/login');
+
+  // Verify that we have logged out
+  expect(await page.innerText('div.panel-heading')).toBe('Login');
+});
+
+test('verify we can log in', async ({ page }) => {
+
+  // Check the project title to verify that we are logged in
+  expect(await page.innerText('.title-content')).toBe('Test Project A');
+
 });
