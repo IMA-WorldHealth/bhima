187ab0a0096c81919c428858bdd3900810cf41eb
Jonathan Cameron <jmcameron@gmail.com>
2023-04-06 12:49:10 -0700

    Updated

--- test/end-to-end-playwright/login/login.spec.js
+++ test/end-to-end-playwright/login/login.spec.js
@@ -12,34 +12,36 @@ test.describe('Login', () => {
     await TU.navigate(login);
   });
 
-  test('verify we can log in', async ({ page }) => {
+  test('verify we can log in', async () => {
     await TU.login();
 
     // Check the project title to verify that we are logged in
-    const title = await page.locator('.title-content');
+    const title = await TU.locator('.title-content');
     expect(await title.innerText()).toBe('Test Project A');
 
     await TU.logout();
 
     // Verify that we have logged out
-    expect(await page.innerText('div.panel-heading')).toBe('Login');
+    const heading = await TU.locator('div.panel-heading');
+    // ??? expect(await page.innerText('div.panel-heading')).toBe('Login');
+    expect(await heading.innerText()).toBe('Login');
   });
 
-  test('rejects an invalid username/password combo with (only) a growl notification', async ({ page }) => {
+  test('rejects an invalid username/password combo with (only) a growl notification', async () => {
     // NOTE: This test should pass even though it generates an 'Unauthorized' console error
     await TU.input('LoginCtrl.credentials.username', 'undefineds');
     await TU.input('LoginCtrl.credentials.password', 'undefined1');
     await TU.buttons.submit();
 
     // Verify that we get a warning message
-    await page.waitForSelector('[data-notification-type="notification-danger"]');
+    await TU.waitForSelector('[data-notification-type="notification-danger"]');
 
     // Verify that we have not logged in
-    expect(page.url().endsWith('/#!/login'));
+    expect(TU.getCurrentPath().endsWith('/login'));
   });
 
-  test('has a default project value', async ({ page }) => {
-    const projectOptions = await page.waitForSelector('select[name="project"]');
+  test('has a default project value', async () => {
+    const projectOptions = await TU.waitForSelector('select[name="project"]');
     const defaultProject = (await projectOptions.$('option:checked')).innerText();
     expect(defaultProject.length > 0);
   });
