2832c71abd439283e3388beb0e8e3e007978d710
Jonathan Cameron <jmcameron@gmail.com>
2023-06-01 10:44:23 -0700

    Cosmetic

--- test/end-to-end-playwright/fillForm/fillForm.spec.js
+++ test/end-to-end-playwright/fillForm/fillForm.spec.js
@@ -1,7 +1,6 @@
 const { chromium } = require('@playwright/test');
-const { test, expect } = require('@playwright/test');
+const { test } = require('@playwright/test');
 const TU = require('../shared/TestUtils');
-const { by } = require('../shared/TestUtils');
 
 test.beforeAll(async () => {
   const browser = await chromium.launch();
