13cec340296ca2080c5b7a2311ae35e9baea8f1e
Jonathan Cameron <jmcameron@gmail.com>
2023-06-05 11:03:35 -0700

    Improvements/fixes to 'yarn test:show-results' script

--- sh/show-test-results.sh
+++ sh/show-test-results.sh
@@ -2,22 +2,32 @@
 
 echo "TEST RESULTS SUMMARY"
 echo
-echo "Client Unit Tests"
-echo "   " `grep "TOTAL" ./test/client-unit/report`
-echo
-echo "Client Unit Tests"
-echo "   " `grep 'passing' ./test/server-unit/report`
-echo "   " `grep 'failing' ./test/server-unit/report`
-echo
-echo "Integration Tests"
-echo "   " `grep passing ./test/integration/report`
-echo "   " `egrep -e '(failing|pending)' ./test/integration/report`
-echo
-echo "Stock Integration Tests"
-echo "   " `grep passing ./test/integration-stock/report`
-echo "   " `egrep -e '(failing|pending)' ./test/integration-stock/report`
-echo
-echo "End-to-end Tests (Playwright)"
-echo "   " `grep passed ./test/end-to-end-playwright/report`
-echo "   " `egrep -e '(failing|pending)' ./test/end-to-end-playwright/report`
-echo
+if test -f "./test/client-unit/report"; then
+  echo "Client Unit Tests"
+  echo "   " `grep "TOTAL" ./test/client-unit/report`
+  echo
+fi
+if test -f "./test/server-unit/report"; then
+  echo "Server Unit Tests"
+  echo "   " `grep 'passing' ./test/server-unit/report`
+  echo "   " `grep 'failing' ./test/server-unit/report`
+  echo
+fi
+if test -f "./test/integration/report"; then
+  echo "Integration Tests"
+  echo "   " `grep passing ./test/integration/report`
+  echo "   " `egrep -e '(failing|pending)' ./test/integration/report`
+  echo
+fi
+if test -f "./test/integration-stock/report"; then
+  echo "Stock Integration Tests"
+  echo "   " `grep passing ./test/integration-stock/report`
+  echo "   " `egrep -e '(failing|pending)' ./test/integration-stock/report`
+  echo
+fi
+if test -f "./test/end-to-end-playwright/report"; then
+  echo "End-to-end Tests (Playwright)"
+  echo "   " `grep passed ./test/end-to-end-playwright/report`
+  echo "   " `egrep -e '(failing|failed|pending)' ./test/end-to-end-playwright/report`
+  echo
+fi
