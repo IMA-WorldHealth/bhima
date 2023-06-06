ab81dd0292ce3413e1a80510422e1028f6e8f0b4
Jonathan Cameron <jmcameron@gmail.com>
2023-06-06 10:48:55 -0700

    Cosmetic

--- client/src/js/services/NotifyService.js
+++ client/src/js/services/NotifyService.js
@@ -12,7 +12,7 @@ NotifyService.$inject = ['$translate'];
 function NotifyService($translate) {
   const service = this;
 
-  // default time to live of 3 seconds
+  // default time to live of 5 seconds
   const TTL = 5000;
 
   // this will be used to specify how long a major error will be displayed,
