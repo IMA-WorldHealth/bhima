1db6911d85fd1d25f2fe21d97569c2b499ef7b49
Jonathan Cameron <jmcameron@gmail.com>
2023-06-30 11:37:49 +0100

    Hound

--- test/end-to-end-playwright/holidays/holidays.page.js
+++ test/end-to-end-playwright/holidays/holidays.page.js
@@ -1,7 +1,4 @@
-const { chromium } = require('@playwright/test');
-const { test, expect } = require('@playwright/test');
 const TU = require('../shared/TestUtils');
-const { by } = require('../shared/TestUtils');
 
 const GridRow = require('../shared/GridRow');
 const components = require('../shared/components');
