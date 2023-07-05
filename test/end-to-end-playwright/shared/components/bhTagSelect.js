46d0a11e7cb9e13d46f48a4c65885e1b746fd5cf
Jonathan Cameron <jmcameron@gmail.com>
2023-07-05 10:41:12 +0100

    Fixed for multiple tags

--- test/end-to-end-playwright/shared/components/bhTagSelect.js
+++ test/end-to-end-playwright/shared/components/bhTagSelect.js
@@ -6,13 +6,15 @@ module.exports = {
 
   set : async function set(tags) {
     const root = await TU.locator(selector);
-
     const select = await TU.getModel('$ctrl.tagUuids', root);
+
     await select.click();
+
     if (Array.isArray(tags)) {
       tags.forEach(async tag => {
         await select.locator('.dropdown-menu [role="option"]').locator(`//*[contains(text(), '${tag}')]`).click();
       });
+      await select.blur(); // Necessary to deselect the last tag so submit works
       return true;
     }
 
