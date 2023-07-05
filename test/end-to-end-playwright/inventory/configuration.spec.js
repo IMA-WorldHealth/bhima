989ea9430f59c1d07e556099e23da30e59a6e6ba
Jonathan Cameron <jmcameron@gmail.com>
2023-07-05 10:41:43 +0100

    Working

--- test/end-to-end-playwright/inventory/configuration.spec.js
+++ test/end-to-end-playwright/inventory/configuration.spec.js
@@ -12,20 +12,17 @@ test.beforeAll(async () => {
 });
 
 test.describe('Inventory Configuration', () => {
-  const url = '/#/inventory/configuration';
 
-  // // navigate to the page
-  // test.beforeEach(async () => {
-  //   await TU.navigate(url);
-  // });
+  const url = '/#/inventory/configuration';
 
   const group = {
     name : 'Médicaments en Sirop Comprimes',
     code : '1700',
     sales_account : 'Vente Médicaments en comprimes',
     stock_account : 'Médicaments en comprimes',
-    cogs_account  : 'Achat Médicaments',
+    cogs_account  : 'Achat Médicaments en comprimés',
     expires : 1,
+    deprecation_rate : 0,
     unique_item : 0,
   };
 
@@ -33,6 +30,7 @@ test.describe('Inventory Configuration', () => {
     name : 'Group With Only Sales Account',
     code : '1900',
     sales_account : '70611012', // Hospitalisation
+    deprecation_rate : 0,
   };
 
   const updateGroup = {
@@ -56,100 +54,114 @@ test.describe('Inventory Configuration', () => {
       await TU.navigate(url);
     });
 
-    //   test('creates a new inventory group', async () => {
-    //     await TU.locator('[data-create-group]').click();
-    //     await TU.input('$ctrl.session.name', group.name);
-    //     await TU.input('$ctrl.session.code', group.code);
+    test('creates a new inventory group', async () => {
+      await TU.locator('[data-create-group]').click();
 
-    //     await components.accountSelect.set(group.sales_account, null, $('[data-sales-account]'), 'accountName');
-    //     await components.accountSelect.set(group.stock_account, null, TU.locator('[data-stock-account]'), 'accountName');
-    //     await components.accountSelect.set(group.cogs_account, null, TU.locator('[data-cogs-account]'), 'accountName');
+      await TU.input('$ctrl.session.name', group.name);
+      await TU.input('$ctrl.session.code', group.code);
 
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+      await components.accountSelect.set(group.sales_account, null, TU.locator('[data-sales-account]'));
+      await components.accountSelect.set(group.stock_account, null, TU.locator('[data-stock-account]'));
+      await components.accountSelect.set(group.cogs_account, null, TU.locator('[data-cogs-account]'));
 
-    //   test('updates an existing inventory group', async () => {
-    //     await TU.locator(`[data-edit-group="${group.code}"]`).click();
-    //     await TU.input('$ctrl.session.name', updateGroup.name);
-    //     await TU.input('$ctrl.session.code', updateGroup.code);
+      await TU.input('$ctrl.session.depreciation_rate', group.deprecation_rate);
 
-    //     await components.accountSelect.set(updateGroup.sales_account, null, TU.locator('[data-sales-account]'));
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
 
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+    test('updates an existing inventory group', async () => {
+      await TU.locator(`[data-edit-group="${group.code}"]`).click();
+      await TU.input('$ctrl.session.name', updateGroup.name);
+      await TU.input('$ctrl.session.code', updateGroup.code);
 
-    //   test('deletes an existing inventory group', async () => {
-    //     await TU.locator(`[data-delete-group="${updateGroup.code}"]`).click();
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+      await components.accountSelect.set(updateGroup.sales_account, null, TU.locator('[data-sales-account]'));
 
-    //   test('creates an inventory group with only a sales account', async () => {
-    //     await TU.locator('[data-create-group]').click();
-    //     await TU.input('$ctrl.session.name', groupWithOnlySalesAccount.name);
-    //     await TU.input('$ctrl.session.code', groupWithOnlySalesAccount.code);
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
 
-    //     await components.accountSelect.set(groupWithOnlySalesAccount.sales_account, null, TU.locator('[data-sales-account]'));
+    test('deletes an existing inventory group', async () => {
+      await TU.locator(`[data-delete-group="${updateGroup.code}"]`).click();
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
 
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
-    // });
+    test('creates an inventory group with only a sales account', async () => {
+      await TU.locator('[data-create-group]').click();
+      await TU.input('$ctrl.session.name', groupWithOnlySalesAccount.name);
+      await TU.input('$ctrl.session.code', groupWithOnlySalesAccount.code);
 
-    // // test inventory type
-    // test.describe('Types', () => {
-    //   // navigate to the page
-    //   test.beforeEach(async () => TU.navigate(url));
+      await components.accountSelect.set(groupWithOnlySalesAccount.sales_account, null,
+        TU.locator('[data-sales-account]'));
 
-    //   test('creates a new inventory type', async () => {
-    //     await TU.locator('[data-create-type]').click();
-    //     await TU.input('$ctrl.session.text', type.text);
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+      await TU.input('$ctrl.session.depreciation_rate', groupWithOnlySalesAccount.deprecation_rate);
 
-    //   test('updates an existing inventory type', async () => {
-    //     await TU.locator(`[data-edit-type="${type.text}"]`).click();
-    //     await TU.input('$ctrl.session.text', updateType.text);
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
 
-    //   test('deletes an existing inventory type', async () => {
-    //     await TU.locator(`[data-delete-type="${updateType.text}"]`).click();
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
-    // });
+  });
 
-    // // test inventory unit
-    // test.describe('Units', () => {
-    //   // navigate to the page
-    //   test.beforeEach(async () => TU.navigate(url));
+  // test inventory type
+  test.describe('Types', () => {
 
-    //   test('creates a new inventory unit', async () => {
-    //     await TU.locator('[data-create-unit]').click();
-    //     await TU.input('$ctrl.session.text', unit.text);
-    //     await TU.input('$ctrl.session.abbr', unit.abbr);
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+    // navigate to the page
+    test.beforeEach(async () => {
+      await TU.navigate(url);
+    });
 
-    //   test('updates an existing inventory unit', async () => {
-    //     await TU.locator(`[data-edit-unit="${unit.abbr}"]`).click();
-    //     await TU.input('$ctrl.session.text', updateUnit.text);
-    //     await TU.input('$ctrl.session.abbr', updateUnit.abbr);
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+    test('creates a new inventory type', async () => {
+      await TU.locator('[data-create-type]').click();
+      await TU.input('$ctrl.session.text', type.text);
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
 
-    //   test('deletes an existing inventory unit', async () => {
-    //     await TU.locator(`[data-delete-unit="${updateUnit.abbr}"]`).click();
-    //     await TU.buttons.submit();
-    //     await components.notification.hasSuccess();
-    //   });
+    test('updates an existing inventory type', async () => {
+      await TU.locator(`[data-edit-type="${type.text}"]`).click();
+      await TU.input('$ctrl.session.text', updateType.text);
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
+
+    test('deletes an existing inventory type', async () => {
+      await TU.locator(`[data-delete-type="${updateType.text}"]`).click();
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
+
+  });
+
+  // test inventory unit
+  test.describe('Units', () => {
+
+    // navigate to the page
+    test.beforeEach(async () => {
+      await TU.navigate(url);
+    });
+
+    test('creates a new inventory unit', async () => {
+      await TU.locator('[data-create-unit]').click();
+      await TU.input('$ctrl.session.text', unit.text);
+      await TU.input('$ctrl.session.abbr', unit.abbr);
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
+
+    test('updates an existing inventory unit', async () => {
+      await TU.locator(`[data-edit-unit="${unit.abbr}"]`).click();
+      await TU.input('$ctrl.session.text', updateUnit.text);
+      await TU.input('$ctrl.session.abbr', updateUnit.abbr);
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
+
+    test('deletes an existing inventory unit', async () => {
+      await TU.locator(`[data-delete-unit="${updateUnit.abbr}"]`).click();
+      await TU.buttons.submit();
+      await components.notification.hasSuccess();
+    });
 
   });
 
