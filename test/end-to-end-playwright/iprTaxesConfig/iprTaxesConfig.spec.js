7900399945fac2d01d5aa3473879d807a3e95a23
Jonathan Cameron <jmcameron@gmail.com>
2023-07-10 12:44:01 +0100

    Adjusted

--- test/end-to-end-playwright/iprTaxesConfig/iprTaxesConfig.spec.js
+++ test/end-to-end-playwright/iprTaxesConfig/iprTaxesConfig.spec.js
@@ -40,20 +40,20 @@ test.describe('Ipr Tax Configuration Scale Management', () => {
   };
 
   const iprTax2 = {
-    label         : 'IPR 2000',
-    description   : 'Impot Professionnel sur le revenu 2000',
+    label         : 'IPR 2002',
+    description   : 'Impot Professionnel sur le revenu 2002',
     currency_id   : 2,
   };
 
-  const iprTaxScale2000 = {
-    scale : 'IPR 2000',
+  const iprTaxScale2002 = {
+    scale : 'IPR 2002',
     rate : 20,
     tranche_annuelle_debut : 12000,
     tranche_annuelle_fin : 24000,
   };
 
-  const iprTaxScale2000Update = {
-    scale : 'IPR 2000',
+  const iprTaxScale2002Update = {
+    scale : 'IPR 2002',
     rate : 0,
     tranche_annuelle_debut : 0,
     tranche_annuelle_fin : 72000,
@@ -67,23 +67,23 @@ test.describe('Ipr Tax Configuration Scale Management', () => {
     await page.createIprTaxConfig(iprTaxScale2);
   });
 
-  test('successfully creates a Scale 1 in IPR Scale 2000', async () => {
-    const exists = await components.iprScale.exists(iprTaxScale2000.scale);
+  test('successfully creates a Scale 1 in IPR Scale 2002', async () => {
+    const exists = await components.iprScale.exists(iprTaxScale2002.scale);
     if (!exists) {
       await TU.navigate('/#!/ipr_tax');
       await taxPage.create(iprTax2);
       await TU.navigate(path);
     }
 
-    await page.createIprTaxConfig(iprTaxScale2000);
+    await page.createIprTaxConfig(iprTaxScale2002);
   });
 
-  test('successfully edits Scale for IPR 2000', async () => {
-    await page.editIprTaxConfig(iprTaxScale2000.rate, iprTaxScale2000Update);
+  test('successfully edits Scale for IPR 2002', async () => {
+    await page.editIprTaxConfig(iprTaxScale2002.rate, iprTaxScale2002Update);
   });
 
   test('successfully delete a Ipr tax Scale', async () => {
-    await page.deleteIprTaxConfig(iprTaxScale2000Update.rate);
+    await page.deleteIprTaxConfig(iprTaxScale2002Update.rate);
   });
 
   test('cannot create when incorrect Ipr Tax', async () => {
