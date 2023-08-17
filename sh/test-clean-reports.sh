5490c7902d3a6e568475b23b88149164ee9c9b25
Jonathan Cameron <jmcameron@gmail.com>
2023-08-17 15:23:00 +0100

    Added

---
+++ sh/test-clean-reports.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -f ./test/client-unit/report
+rm -f ./test/integration/report
+rm -f ./test/server-unit/report
+rm -f ./test/integration-stock/report
+rm -f ./test/end-to-end-playwright/report
+rm -f ./test/end-to-end-playwright/report-stock
