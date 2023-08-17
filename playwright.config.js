e268839ccf2b50ce45b70e977c32436768d164dd
Jonathan Cameron <jmcameron@gmail.com>
2023-08-17 15:30:13 +0100

    Hound

--- playwright.config.js
+++ playwright.config.js
@@ -12,7 +12,7 @@ const { defineConfig, devices } = require('@playwright/test');
 module.exports = defineConfig({
   testDir : process.env.TEST_STOCK ? './test/end-to-end-playwright/stock' : './test/end-to-end-playwright',
 
-  testIgnore: process.env.TEST_STOCK ? '' : '*/stock/*',
+  testIgnore : process.env.TEST_STOCK ? '' : '*/stock/*',
 
   /* Maximum time one test can run for. */
   timeout : 10 * 1000, // JMC WAS 30*1000, temporarily shortened for test development
