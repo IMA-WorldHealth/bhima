601d59bd5872c2792de418ff022d518fdab8c46f
Jonathan Cameron <jmcameron@gmail.com>
2023-11-09 11:30:06 +0100

    Added budget module

--- server/models/migrations/next/migrate.sql
+++ server/models/migrations/next/migrate.sql
@@ -1,1 +1,17 @@
 /* v1.29.0 */
+
+/**
+ * @author: jmcameron
+ * @description: Updates for budget module
+ * @date: 2023-11-02
+ */
+
+-- Update the budget table
+ALTER TABLE `budget` MODIFY COLUMN `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `budget` MODIFY COLUMN `budget` DECIMAL(19,4) UNSIGNED NOT NULL DEFAULT 0;
+CALL add_column_if_missing('budget', 'locked', 'BOOLEAN NOT NULL DEFAULT 0');
+CALL add_column_if_missing('budget', 'updated_at', 'TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP');
+
+-- Add the Finance > Budget page
+INSERT INTO unit VALUES
+  (319, 'Budget Management', 'TREE.BUDGET', '', 5, '/budget');
