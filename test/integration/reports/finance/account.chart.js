04b2d7313f70dcf957916b42a7e948120df65781
Jonathan Cameron <jmcameron@gmail.com>
2023-12-08 16:04:18 +0100

    Fix test hierarchy in xml test reports

--- test/integration/reports/finance/account.chart.js
+++ test/integration/reports/finance/account.chart.js
@@ -1,4 +1,5 @@
 const RenderingTests = require('../rendering');
+
 const target = '/reports/finance/accounts/chart';
 
-describe(`(${target}) Chart of Accounts`, RenderingTests(target));
+describe(`test/integration${target} Chart of Accounts Report`, RenderingTests(target));
