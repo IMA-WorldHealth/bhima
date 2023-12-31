4160810622f6e3a8452d1d7cd90408d2398f9984
Jonathan Cameron <jmcameron@gmail.com>
2023-12-31 13:14:20 -0800

    Updated

--- test/end-to-end/fiscalYears/fiscalYears.spec.js
+++ test/end-to-end/fiscalYears/fiscalYears.spec.js
@@ -1,7 +1,9 @@
 const moment = require('moment');
 const { chromium } = require('@playwright/test');
 const { test, expect } = require('@playwright/test');
+
 const TU = require('../shared/TestUtils');
+const { by } = require('../shared/TestUtils');
 
 test.beforeAll(async () => {
   const browser = await chromium.launch();
@@ -62,8 +64,7 @@ test.describe('Fiscal Year', () => {
     // select the proper date
     await components.dateInterval.range(`01/01/${newFY}`, `31/12/${newFY}`);
     console.debug("4");
-    const pfyi = await TU.locator(by.model('FiscalManageCtrl.fiscal.previous_fiscal_year_id'));
-    console.groupCollapseddebug(pfyi);
+    await TU.locator(by.model('FiscalManageCtrl.fiscal.previous_fiscal_year_id')).click();
     await TU.select('FiscalManageCtrl.fiscal.previous_fiscal_year_id', fiscalYear.previous);
     console.debug("5");
     // model, label, anchor
