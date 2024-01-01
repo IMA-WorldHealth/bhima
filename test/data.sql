78ea03c5c41f380234c569ec882289c4694cfb8c
Jonathan Cameron <jmcameron@gmail.com>
2023-12-31 17:06:05 -0800

    Added fiscal 2024 to test data set

--- test/data.sql
+++ test/data.sql
@@ -383,6 +383,9 @@ CALL CreateFiscalYear(1, @fiscalYear2021, @superUser, 'Fiscal Year 2022', 12, DA
 SET @fiscalYear2023 = 0;
 CALL CreateFiscalYear(1, @fiscalYear2022, @superUser, 'Fiscal Year 2023', 12, DATE('2023-01-01'), DATE('2023-12-31'), 'Notes for 2023', @fiscalYear2023);
 
+SET @fiscalYear2024 = 0;
+CALL CreateFiscalYear(1, @fiscalYear2024, @superUser, 'Fiscal Year 2024', 12, DATE('2024-01-01'), DATE('2024-12-31'), 'Notes for 2024', @fiscalYear2024);
+
 -- give test permission to all projects
 INSERT INTO `project_permission` VALUES (1, 1, 1),(2, 1, 2),(3, 2, 1),(4, 4, 1);
 
