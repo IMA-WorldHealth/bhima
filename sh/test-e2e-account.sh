ecad3aec78ee83ce4afad98f1f4b92d8b30cb322
Jonathan Cameron <jmcameron@gmail.com>
2024-01-09 08:57:25 +0100

    Improvements for more test robustness

--- sh/test-e2e-account.sh
+++ sh/test-e2e-account.sh
@@ -54,13 +54,13 @@ sed -i 's/.spec.js//g' "./results/end-to-end-account-results.xml"
 set -o nounset
 if [[ -z "${CI:-}" ]]
 then
-    procs=`netstat -tulpn |& grep 8080`
+    procs=`netstat -tulpn |& grep 8080` || true
     proc=`echo $procs | sed -r 's/.* ([0-9]+)\/node$/\1/g'`
 
     if [[ ! -z "$proc" ]]
     then
         echo "Deleting zombie node Bhima process $proc"
-        kill -9 $proc
+        kill -9 $proc || true
     fi
 fi
 
